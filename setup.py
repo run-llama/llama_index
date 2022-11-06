799df3254eb98b6de57c522f721d88b382b8eae3
Jerry Liu <jerryjliu98@gmail.com>
2022-11-05 17:18:49 -0700

    rename to gpt_index (#2)
    rename
    
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-with open(Path(__file__).absolute().parents[0] / "gpt_db_retrieve" / "VERSION") as _f:
+with open(Path(__file__).absolute().parents[0] / "gpt_index" / "VERSION") as _f:
     __version__ = _f.read().strip()
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="gpt_db_retrieve",
+    name="gpt_index",
     version=__version__,
     packages=find_packages(),
     description="Building an index of GPT summaries.",
     install_requires=["langchain", "openai", "dataclasses_json", "transformers"],
     long_description=long_description,
     license="MIT",
-    url="https://github.com/jerryjliu/gpt_db_retrieve",
+    url="https://github.com/jerryjliu/gpt_index",
     include_package_data=True,
     long_description_content_type="text/markdown",
 )
