045d3689f8ebec264bdb99c5a84fc9dc1d6ecdb6
Jerry Liu <jerryjliu98@gmail.com>
2022-11-08 19:11:26 -0800

    change prompts, update notebook outputs (#13)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- gpt_index/prompts.py
+++ gpt_index/prompts.py
@@ -11,23 +11,38 @@ DEFAULT_SUMMARY_PROMPT = (
     "SUMMARY:\"\"\"\n"
 )
 
+# # single choice
 DEFAULT_QUERY_PROMPT = (
     "Some choices are given below. It is provided in a numbered list (1 to {num_chunks}),"
     "where each item in the list corresponds to a summary.\n"
     "---------------------\n"
     "{context_list}"
-    "---------------------\n"
-    "Return the choice that is most relevant to the question: {query_str}\n"
-    # "Given the context information, answer the question: {query_str}\n"
-    # "The answer should be the number corresponding to the "
-    # "summary that is most relevant to the question. "
-    "Provide answer in the following format: 'ANSWER: <number>' and explain why.\n"
+    "\n---------------------\n"
+    "Using only the choices above and not prior knowledge, return the choice that "
+    "is most relevant to the question: '{query_str}'\n"
+    "Provide choice in the following format: 'ANSWER: <number>' and explain why "
+    "this summary was selected in relation to the question.\n"
 )
+
+# multiple choice
+# DEFAULT_QUERY_PROMPT = (
+#     "Some choices are given below. It is provided in a numbered list (1 to {num_chunks}),"
+#     "where each item in the list corresponds to a summary.\n"
+#     "---------------------\n"
+#     "{context_list}"
+#     "\n---------------------\n"
+#     "Using only the choices above and not prior knowledge, return the top choices "
+#     "(no more than 3, ranked by most relevant to least) that "
+#     "are most relevant to the question: '{query_str}'\n"
+#     "Provide choices in the following format: 'ANSWER: <numbers>' and explain why "
+#     "these summaries were selected in relation to the question.\n"
+# )
+
 
 DEFAULT_TEXT_QA_PROMPT = (
     "Context information is below. "
     "---------------------\n"
     "{context_str}"
     "---------------------\n"
-    "Given the context information, answer the question: {query_str}\n"
+    "Given the context information and not prior knowledge, answer the question: {query_str}\n"
 )
