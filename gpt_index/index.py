c044ac898aa97f6678e2ff4b6b793479bcde97be
Jerry Liu <jerryjliu98@gmail.com>
2022-11-09 20:47:26 -0800

    add linting + some refactors (#16)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- gpt_index/index.py
+++ gpt_index/index.py
@@ -1,42 +1,26 @@
 """Core abstractions for building an index of GPT data."""
 
-from dataclasses import dataclass
-from dataclasses_json import DataClassJsonMixin, Undefined, dataclass_json
-from pathlib import Path
-from gpt_index.file_reader import SimpleDirectoryReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain import OpenAI, Prompt, LLMChain
-from gpt_index.prompts import DEFAULT_SUMMARY_PROMPT, DEFAULT_QUERY_PROMPT, DEFAULT_TEXT_QA_PROMPT
-from gpt_index.utils import get_chunk_size_given_prompt, extract_number_given_response
-from gpt_index.text_splitter import TokenTextSplitter
-
-from typing import List, Dict, Set
 import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, List
 
+from dataclasses_json import DataClassJsonMixin
+from langchain import LLMChain, OpenAI, Prompt
+
+from gpt_index.file_reader import SimpleDirectoryReader
+from gpt_index.prompts import (
+    DEFAULT_QUERY_PROMPT,
+    DEFAULT_SUMMARY_PROMPT,
+    DEFAULT_TEXT_QA_PROMPT,
+)
+from gpt_index.schema import IndexGraph, Node
+from gpt_index.text_splitter import TokenTextSplitter
+from gpt_index.utils import extract_number_given_response, get_chunk_size_given_prompt
 
 MAX_CHUNK_SIZE = 3900
 MAX_CHUNK_OVERLAP = 200
 NUM_OUTPUTS = 256
-
-
-
-@dataclass
-class Node(DataClassJsonMixin):
-    """A node in the GPT index."""
-
-    text: str
-    index: int
-    child_indices: Set[int]
-
-
-@dataclass
-class IndexGraph(DataClassJsonMixin):
-    all_nodes: Dict[int, Node]
-    root_nodes: Dict[int, Node]
-
-    @property
-    def size(self):
-        return len(self.all_nodes)
 
 
 def _get_sorted_node_list(node_dict: Dict[int, Node]) -> List[Node]:
@@ -68,9 +52,7 @@ class GPTIndexBuilder:
     """GPT Index builder."""
 
     def __init__(
-        self, 
-        num_children: int = 10,
-        summary_prompt: str = DEFAULT_SUMMARY_PROMPT
+        self, num_children: int = 10, summary_prompt: str = DEFAULT_SUMMARY_PROMPT
     ) -> None:
         """Initialize with params."""
         self.num_children = num_children
@@ -83,8 +65,8 @@ class GPTIndexBuilder:
         )
         self.text_splitter = TokenTextSplitter(
             separator=" ",
-            chunk_size=chunk_size, 
-            chunk_overlap=MAX_CHUNK_OVERLAP // num_children
+            chunk_size=chunk_size,
+            chunk_overlap=MAX_CHUNK_OVERLAP // num_children,
         )
 
     def build_from_text(self, text: str) -> IndexGraph:
@@ -92,7 +74,7 @@ class GPTIndexBuilder:
 
         Returns:
             IndexGraph: graph object consisting of all_nodes, root_nodes
-        
+
         """
         text_chunks = self.text_splitter.split_text(text)
 
@@ -108,18 +90,18 @@ class GPTIndexBuilder:
         cur_node_list = _get_sorted_node_list(cur_nodes)
         cur_index = len(all_nodes)
         new_node_dict = {}
-        print(f'> Building index from nodes: {len(cur_nodes) // self.num_children} chunks')
+        print(
+            f"> Building index from nodes: {len(cur_nodes) // self.num_children} chunks"
+        )
         for i in range(0, len(cur_node_list), self.num_children):
-            print(f'{i}/{len(cur_nodes)}')
-            cur_nodes_chunk = cur_node_list[i:i+self.num_children]
+            print(f"{i}/{len(cur_nodes)}")
+            cur_nodes_chunk = cur_node_list[i : i + self.num_children]
 
             text_chunk = _get_text_from_nodes(cur_nodes_chunk)
 
             new_summary = self.llm_chain.predict(text=text_chunk)
-            print(f'> {i}/{len(cur_nodes)}, summary: {new_summary}')
-            new_node = Node(
-                new_summary, cur_index, {n.index for n in cur_nodes_chunk}
-            )
+            print(f"> {i}/{len(cur_nodes)}, summary: {new_summary}")
+            new_node = Node(new_summary, cur_index, {n.index for n in cur_nodes_chunk})
             new_node_dict[cur_index] = new_node
             cur_index += 1
 
@@ -139,89 +121,87 @@ class GPTIndex(DataClassJsonMixin):
     query_template: str = DEFAULT_QUERY_PROMPT
     text_qa_template: str = DEFAULT_TEXT_QA_PROMPT
 
-    def _query(self, cur_nodes: Dict[int, Node], query_str: str, verbose: bool = False) -> str:
+    def _query(
+        self, cur_nodes: Dict[int, Node], query_str: str, verbose: bool = False
+    ) -> str:
         """Answer a query recursively."""
         cur_node_list = _get_sorted_node_list(cur_nodes)
         query_prompt = Prompt(
-            template=self.query_template, 
-            input_variables=["num_chunks", "context_list", "query_str"]
+            template=self.query_template,
+            input_variables=["num_chunks", "context_list", "query_str"],
         )
         llm = OpenAI(temperature=0)
-        llm_chain = LLMChain(prompt=query_prompt, llm=llm) 
+        llm_chain = LLMChain(prompt=query_prompt, llm=llm)
         response = llm_chain.predict(
             query_str=query_str,
-            num_chunks=len(cur_node_list), 
-            context_list=_get_numbered_text_from_nodes(cur_node_list)
+            num_chunks=len(cur_node_list),
+            context_list=_get_numbered_text_from_nodes(cur_node_list),
         )
-        
+
         if verbose:
             formatted_query = self.query_template.format(
                 num_chunks=len(cur_node_list),
                 query_str=query_str,
-                context_list=_get_numbered_text_from_nodes(cur_node_list)
+                context_list=_get_numbered_text_from_nodes(cur_node_list),
             )
-            print(f'==============')
-            print(f'> current prompt template: {formatted_query}')
+            print("==============")
+            print(f"> current prompt template: {formatted_query}")
         number = extract_number_given_response(response)
         if number is None:
             if verbose:
-                print(f"> Could not retrieve response - no numbers present")
+                print("> Could not retrieve response - no numbers present")
             # just join text from current nodes as response
             return response
         elif number > len(cur_node_list):
             if verbose:
-                print(f'> Invalid response: {response} - number {number} out of range')
+                print(f"> Invalid response: {response} - number {number} out of range")
             return response
 
         # number is 1-indexed, so subtract 1
-        selected_node = cur_node_list[number-1]
+        selected_node = cur_node_list[number - 1]
         print(f"> Selected node: {response}")
         print(f"> Node Summary text: {' '.join(selected_node.text.splitlines())}")
 
         if len(selected_node.child_indices) == 0:
             answer_prompt = Prompt(
-                template=self.text_qa_template, 
-                input_variables=["context_str", "query_str"]
+                template=self.text_qa_template,
+                input_variables=["context_str", "query_str"],
             )
-            llm_chain = LLMChain(prompt=answer_prompt, llm=llm) 
+            llm_chain = LLMChain(prompt=answer_prompt, llm=llm)
             response = llm_chain.predict(
-                context_str=selected_node.text,
-                query_str=query_str
+                context_str=selected_node.text, query_str=query_str
             )
             if verbose:
                 formatted_answer_prompt = self.text_qa_template.format(
-                    context_str=selected_node.text,
-                    query_str=query_str
+                    context_str=selected_node.text, query_str=query_str
                 )
-                print('==============')
-                print(f'> final answer prompt: {formatted_answer_prompt}')
+                print("==============")
+                print(f"> final answer prompt: {formatted_answer_prompt}")
             return response
         else:
             return self._query(
-                {i: self.graph.all_nodes[i] for i in selected_node.child_indices}, 
+                {i: self.graph.all_nodes[i] for i in selected_node.child_indices},
                 query_str,
-                verbose=verbose
+                verbose=verbose,
             )
 
     def query(self, query_str: str, verbose: bool = False) -> str:
         """Answer a query."""
-        print(f'> Starting query: {query_str}')
+        print(f"> Starting query: {query_str}")
         return self._query(self.graph.root_nodes, query_str, verbose=verbose).strip()
-            
+
     @classmethod
     def from_input_dir(
-        cls, 
-        input_dir: str,
-        index_builder: GPTIndexBuilder = GPTIndexBuilder()
+        cls, input_dir: str, index_builder: GPTIndexBuilder = GPTIndexBuilder()
     ) -> "GPTIndex":
-        """Builds an index from an input directory.
+        """Build an index from an input directory.
 
         Uses the default index builder.
-        
+
         """
-        input_dir = Path(input_dir)
+        input_d = Path(input_dir)
         # instantiate file reader
-        reader = SimpleDirectoryReader(input_dir)
+        reader = SimpleDirectoryReader(input_d)
         text_data = reader.load_data()
 
         # Use index builder
@@ -229,7 +209,7 @@ class GPTIndex(DataClassJsonMixin):
         return cls(index_graph)
 
     @classmethod
-    def load_from_disk(cls, save_path: str) -> None:
+    def load_from_disk(cls, save_path: str) -> "GPTIndex":
         """Load from disk."""
         with open(save_path, "r") as f:
             return cls(graph=IndexGraph.from_dict(json.load(f)))
@@ -238,6 +218,3 @@ class GPTIndex(DataClassJsonMixin):
         """Safe to file."""
         with open(save_path, "w") as f:
             json.dump(self.graph.to_dict(), f)
-
-        
-        
--- gpt_index/prompts.py
+++ gpt_index/prompts.py
@@ -1,32 +1,35 @@
 """Set of default prompts."""
 
 DEFAULT_SUMMARY_PROMPT = (
-    "Write a summary of the following. Try to use only the information provided. "
+    "Write a summary of the following. Try to use only the "
+    "information provided. "
     "Try to include as many key details as possible.\n"
     "\n"
     "\n"
     "{text}\n"
     "\n"
     "\n"
-    "SUMMARY:\"\"\"\n"
+    'SUMMARY:"""\n'
 )
 
 # # single choice
 DEFAULT_QUERY_PROMPT = (
-    "Some choices are given below. It is provided in a numbered list (1 to {num_chunks}),"
+    "Some choices are given below. It is provided in a numbered list "
+    "(1 to {num_chunks}),"
     "where each item in the list corresponds to a summary.\n"
     "---------------------\n"
     "{context_list}"
     "\n---------------------\n"
-    "Using only the choices above and not prior knowledge, return the choice that "
-    "is most relevant to the question: '{query_str}'\n"
+    "Using only the choices above and not prior knowledge, return "
+    "the choice that is most relevant to the question: '{query_str}'\n"
     "Provide choice in the following format: 'ANSWER: <number>' and explain why "
     "this summary was selected in relation to the question.\n"
 )
 
 # multiple choice
 # DEFAULT_QUERY_PROMPT = (
-#     "Some choices are given below. It is provided in a numbered list (1 to {num_chunks}),"
+#     "Some choices are given below. It is provided in a numbered "
+#     "list (1 to {num_chunks}), "
 #     "where each item in the list corresponds to a summary.\n"
 #     "---------------------\n"
 #     "{context_list}"
@@ -44,5 +47,6 @@ DEFAULT_TEXT_QA_PROMPT = (
     "---------------------\n"
     "{context_str}"
     "\n---------------------\n"
-    "Given the context information and not prior knowledge, answer the question: {query_str}\n"
+    "Given the context information and not prior knowledge, "
+    "answer the question: {query_str}\n"
 )
