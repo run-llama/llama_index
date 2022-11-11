39b8c9e71ecad1508129db4b20ea2fe263084704
Jerry Liu <jerryjliu98@gmail.com>
2022-11-10 18:48:06 -0800

    Add functionality for a branching factor  (#17)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- gpt_index/prompts.py
+++ gpt_index/prompts.py
@@ -27,19 +27,33 @@ DEFAULT_QUERY_PROMPT = (
 )
 
 # multiple choice
-# DEFAULT_QUERY_PROMPT = (
-#     "Some choices are given below. It is provided in a numbered "
-#     "list (1 to {num_chunks}), "
-#     "where each item in the list corresponds to a summary.\n"
-#     "---------------------\n"
-#     "{context_list}"
-#     "\n---------------------\n"
-#     "Using only the choices above and not prior knowledge, return the top choices "
-#     "(no more than 3, ranked by most relevant to least) that "
-#     "are most relevant to the question: '{query_str}'\n"
-#     "Provide choices in the following format: 'ANSWER: <numbers>' and explain why "
-#     "these summaries were selected in relation to the question.\n"
-# )
+DEFAULT_QUERY_PROMPT_MULTIPLE = (
+    "Some choices are given below. It is provided in a numbered "
+    "list (1 to {num_chunks}), "
+    "where each item in the list corresponds to a summary.\n"
+    "---------------------\n"
+    "{context_list}"
+    "\n---------------------\n"
+    "Using only the choices above and not prior knowledge, return the top choices "
+    "(no more than {branching_factor}, ranked by most relevant to least) that "
+    "are most relevant to the question: '{query_str}'\n"
+    "Provide choices in the following format: 'ANSWER: <numbers>' and explain why "
+    "these summaries were selected in relation to the question.\n"
+)
+
+
+DEFAULT_REFINE_PROMPT = (
+    "The original question is as follows: {query_str}\n"
+    "We have provided an existing answer: {existing_answer}\n"
+    "We have the opportunity to refine the existing answer"
+    "(only if needed) with some more context below.\n"
+    "------------\n"
+    "{context_msg}\n"
+    "------------\n"
+    "Given the new context, refine the original answer to better "
+    "answer the question. "
+    "If the context isn't useful, return the original answer."
+)
 
 
 DEFAULT_TEXT_QA_PROMPT = (
