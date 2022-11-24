cee67d3131263c3c6452b2f8daa48fad65e8cab3
Jerry Liu <jerry@robustintelligence.com>
2022-11-24 00:28:24 -0800

    cr

---
+++ tests/mock_utils/mock_prompts.py
@@ -0,0 +1,28 @@
+"""Mock prompt utils."""
+
+from gpt_index.prompts.base import Prompt
+
+MOCK_SUMMARY_PROMPT_TMPL = "{text}\n"
+MOCK_SUMMARY_PROMPT = Prompt(
+    input_variables=["text"], template=MOCK_SUMMARY_PROMPT_TMPL
+)
+
+# # single choice
+MOCK_QUERY_PROMPT_TMPL = "{num_chunks}\n" "{context_list}\n" "{query_str}'\n"
+MOCK_QUERY_PROMPT = Prompt(
+    input_variables=["num_chunks", "context_list", "query_str"],
+    template=MOCK_QUERY_PROMPT_TMPL,
+)
+
+
+MOCK_REFINE_PROMPT_TMPL = "{query_str}\n" "{existing_answer}\n" "{context_msg}\n"
+MOCK_REFINE_PROMPT = Prompt(
+    input_variables=["query_str", "existing_answer", "context_msg"],
+    template=MOCK_REFINE_PROMPT_TMPL,
+)
+
+
+MOCK_TEXT_QA_PROMPT_TMPL = "{context_str}\n" "{query_str}\n"
+MOCK_TEXT_QA_PROMPT = Prompt(
+    input_variables=["context_str", "query_str"], template=MOCK_TEXT_QA_PROMPT_TMPL
+)
