66e0df7db6469a4dd6f3c1df19df326d33b097fa
Jerry Liu <jerryjliu98@gmail.com>
2022-11-26 11:27:45 -0800

    add insert for keyword table (#48)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- gpt_index/indices/keyword_table/utils.py
+++ gpt_index/indices/keyword_table/utils.py
@@ -11,11 +11,12 @@ from gpt_index.indices.utils import expand_tokens_with_subtokens
 
 
 def simple_extract_keywords(
-    text_chunk: str, max_keywords: Optional[int] = None
+    text_chunk: str, max_keywords: Optional[int] = None, filter_stopwords: bool = True
 ) -> Set[str]:
     """Extract keywords with simple algorithm."""
     tokens = [t.strip().lower() for t in re.findall(r"\w+", text_chunk)]
-    tokens = [t for t in tokens if t not in stopwords.words("english")]
+    if filter_stopwords:
+        tokens = [t for t in tokens if t not in stopwords.words("english")]
     value_counts = pd.Series(tokens).value_counts()
     keywords = value_counts.index.tolist()[:max_keywords]
     return set(keywords)
