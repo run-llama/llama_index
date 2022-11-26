165e7db12ab759dba5c56fca6c729e67feb2cfe6
Jerry Liu <jerryjliu98@gmail.com>
2022-11-26 10:46:46 -0800

    add insert to list (#47)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

---
+++ tests/indices/list/test_base.py
@@ -0,0 +1,49 @@
+"""Test list index."""
+
+from typing import Any, List
+from unittest.mock import patch
+
+import pytest
+
+from gpt_index.indices.list.base import GPTListIndex
+from gpt_index.langchain_helpers.text_splitter import TokenTextSplitter
+from gpt_index.schema import Document
+from tests.mock_utils.mock_text_splitter import mock_token_splitter_newline
+
+
+@pytest.fixture
+def documents() -> List[Document]:
+    """Get documents."""
+    # NOTE: one document for now
+    doc_text = (
+        "Hello world.\n"
+        "This is a test.\n"
+        "This is another test.\n"
+        "This is a test v2."
+    )
+    return [Document(doc_text)]
+
+
+@patch.object(TokenTextSplitter, "split_text", side_effect=mock_token_splitter_newline)
+def test_build_list(_mock_splitter: Any, documents: List[Document]) -> None:
+    """Test build list."""
+    list_index = GPTListIndex(documents)
+    assert len(list_index.index_struct.nodes) == 4
+    # check contents of nodes
+    assert list_index.index_struct.nodes[0].text == "Hello world."
+    assert list_index.index_struct.nodes[1].text == "This is a test."
+    assert list_index.index_struct.nodes[2].text == "This is another test."
+    assert list_index.index_struct.nodes[3].text == "This is a test v2."
+
+
+@patch.object(TokenTextSplitter, "split_text", side_effect=mock_token_splitter_newline)
+def test_list_insert(_mock_splitter: Any, documents: List[Document]) -> None:
+    """Test build list."""
+    list_index = GPTListIndex([])
+    assert len(list_index.index_struct.nodes) == 0
+    list_index.insert(documents[0])
+    # check contents of nodes
+    assert list_index.index_struct.nodes[0].text == "Hello world."
+    assert list_index.index_struct.nodes[1].text == "This is a test."
+    assert list_index.index_struct.nodes[2].text == "This is another test."
+    assert list_index.index_struct.nodes[3].text == "This is a test v2."
--- tests/mock_utils/mock_text_splitter.py
+++ tests/mock_utils/mock_text_splitter.py
@@ -5,4 +5,6 @@ from typing import List
 
 def mock_token_splitter_newline(text: str) -> List[str]:
     """Mock token splitter by newline."""
+    if text == "":
+        return []
     return text.split("\n")
