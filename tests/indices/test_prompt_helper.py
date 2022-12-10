fd36a6a7450f3eaf30a0c340f8465920b653a261
Jerry Liu <jerry@robustintelligence.com>
2022-12-10 15:56:21 -0800

    cr

--- tests/indices/test_prompt_helper.py
+++ tests/indices/test_prompt_helper.py
@@ -51,7 +51,6 @@ def test_get_text_splitter() -> None:
     text_splitter = prompt_helper.get_text_splitter_given_prompt(
         test_prompt, 2, padding=1
     )
-    print(text_splitter._chunk_size)
     test_text = "Hello world foo Hello world bar"
     text_chunks = text_splitter.split_text(test_text)
     assert text_chunks == ["Hello world", "foo Hello", "world bar"]
