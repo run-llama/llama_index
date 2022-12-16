d7fe92873e60447e4dadb30cbe5cf0fbb1b13856
Junyi Du <junyidu.cn@gmail.com>
2022-12-16 11:48:46 +0800

    improve file read robustness (#104)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- gpt_index/readers/file.py
+++ gpt_index/readers/file.py
@@ -12,14 +12,20 @@ class SimpleDirectoryReader(BaseReader):
     Can read files into separate documents, or concatenates
     files into one document text.
 
-    input_dir (str): Path to the directory.
-    exclude_hidden (bool): Whether to exclude hidden files (dotfiles).
+    Args:
+        input_dir (str): Path to the directory.
+        exclude_hidden (bool): Whether to exclude hidden files (dotfiles).
+        errors (bool): how encoding and decoding errors are to be handled,
+              see https://docs.python.org/3/library/functions.html#open
 
     """
 
-    def __init__(self, input_dir: str, exclude_hidden: bool = True) -> None:
+    def __init__(
+        self, input_dir: str, exclude_hidden: bool = True, errors: str = "ignore"
+    ) -> None:
         """Initialize with parameters."""
         self.input_dir = Path(input_dir)
+        self.errors = errors
         input_files = list(self.input_dir.iterdir())
         if exclude_hidden:
             input_files = [f for f in input_files if not f.name.startswith(".")]
@@ -42,7 +48,7 @@ class SimpleDirectoryReader(BaseReader):
         data = ""
         data_list = []
         for input_file in self.input_files:
-            with open(input_file, "r") as f:
+            with open(input_file, "r", errors=self.errors) as f:
                 data = f.read()
                 data_list.append(data)
 
