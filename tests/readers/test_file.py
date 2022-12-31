05f2dbd64f203fcd733055f3c260a4d8800ba8b0
Jerry Liu <jerryjliu98@gmail.com>
2022-12-30 23:16:38 -0800

    Fix recursive directory reader with file filter (#159)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- tests/readers/test_file.py
+++ tests/readers/test_file.py
@@ -19,6 +19,7 @@ def test_recursive() -> None:
                     f.write("test3")
                 with open(f"{tmp_sub_sub_dir}/test4.txt", "w") as f:
                     f.write("test4")
+
                     reader = SimpleDirectoryReader(tmp_dir, recursive=True)
                     input_file_names = [f.name for f in reader.input_files]
                     assert len(reader.input_files) == 4
@@ -27,6 +28,29 @@ def test_recursive() -> None:
                         "test2.txt",
                         "test3.txt",
                         "test4.txt",
+                    }
+
+    # test recursive with .md files
+    with TemporaryDirectory() as tmp_dir:
+        with open(f"{tmp_dir}/test1.md", "w") as f:
+            f.write("test1")
+        with TemporaryDirectory(dir=tmp_dir) as tmp_sub_dir:
+            with open(f"{tmp_sub_dir}/test2.txt", "w") as f:
+                f.write("test2")
+            with TemporaryDirectory(dir=tmp_sub_dir) as tmp_sub_sub_dir:
+                with open(f"{tmp_sub_sub_dir}/test3.md", "w") as f:
+                    f.write("test3")
+                with open(f"{tmp_sub_sub_dir}/test4.txt", "w") as f:
+                    f.write("test4")
+
+                    reader = SimpleDirectoryReader(
+                        tmp_dir, recursive=True, required_exts=[".md"]
+                    )
+                    input_file_names = [f.name for f in reader.input_files]
+                    assert len(reader.input_files) == 2
+                    assert set(input_file_names) == {
+                        "test1.md",
+                        "test3.md",
                     }
 
 
