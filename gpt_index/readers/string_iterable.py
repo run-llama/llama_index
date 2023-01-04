01793103f32891d6db2598cbd6562db1de555951
Mat <mteohh@gmail.com>
2023-01-04 11:27:02 -0800

    small refactor

--- gpt_index/readers/string_iterable.py
+++ gpt_index/readers/string_iterable.py
@@ -10,30 +10,24 @@ class StringIterableReader(BaseReader):
 
     Gets a list of documents, given an iterable (e.g. list) of strings
 
-    Args:
-        texts (Iterable[str]): Iterable of strings to be added to the Document list result.
-
     Example:
         .. code-block:: python
             from gpt_index import StringIterableReader, GPTTreeIndex
 
-            documents = StringIterableReader(["I went to the store", "I bought an apple"]).load_data()
+            documents = StringIterableReader().load_data(texts=["I went to the store", "I bought an apple"]).load_data()
             index = GPTTreeIndex(documents)
             index.query("what did I buy?")
 
             # response should be something like "You bought an apple."
     """
 
-    def __init__(self, texts: Iterable[str]):
-        """Initialize with parameters."""
-        self.texts = texts
-
     def load_data(self, **load_kwargs: Any) -> List[Document]:
         """
         Load the data
         """
+        texts = load_kwargs.get("texts", list())
         results = []
-        for text in self.texts:
+        for text in texts:
             results.append(Document(text))
 
         return results
--- tests/readers/test_string_iterable.py
+++ tests/readers/test_string_iterable.py
@@ -5,6 +5,6 @@ from gpt_index.readers.string_iterable import StringIterableReader
 
 def test_load() -> None:
     """Test loading data into StringIterableReader."""
-    reader = StringIterableReader(["I went to the store", "I bought an apple"])
-    documents = reader.load_data()
+    reader = StringIterableReader()
+    documents = reader.load_data(texts=["I went to the store", "I bought an apple"])
     assert len(documents) == 2
