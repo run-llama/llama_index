16e543b16281121750bfd04d1d9c5d7634845002
Harrison Chase <hw.chase.17@gmail.com>
2023-01-04 06:57:44 -0800

    to/from langchain doc format (#174)
    * to langchain doc format
    
    * cr

--- gpt_index/readers/base.py
+++ gpt_index/readers/base.py
@@ -2,6 +2,8 @@
 from abc import abstractmethod
 from typing import Any, List
 
+from langchain.docstore.document import Document as LCDocument
+
 from gpt_index.readers.schema.base import Document
 
 
@@ -11,3 +13,8 @@ class BaseReader:
     @abstractmethod
     def load_data(self, **load_kwargs: Any) -> List[Document]:
         """Load data from the input directory."""
+
+    def load_langchain_documents(self, **load_kwargs: Any) -> List[LCDocument]:
+        """Load data in LangChain document format."""
+        docs = self.load_data(**load_kwargs)
+        return [d.to_langchain_format() for d in docs]
--- gpt_index/readers/schema/base.py
+++ gpt_index/readers/schema/base.py
@@ -2,6 +2,8 @@
 from dataclasses import dataclass
 from typing import Dict, Optional
 
+from langchain.docstore.document import Document as LCDocument
+
 from gpt_index.schema import BaseDocument
 
 
@@ -19,3 +21,13 @@ class Document(BaseDocument):
         """Post init."""
         if self.text is None:
             raise ValueError("text field not set.")
+
+    def to_langchain_format(self) -> LCDocument:
+        """Convert struct to LangChain document format."""
+        metadata = self.extra_info or {}
+        return LCDocument(page_content=self.text, metadata=metadata)
+
+    @classmethod
+    def from_langchain_format(cls, doc: LCDocument) -> "Document":
+        """Convert struct from LangChain document format."""
+        return cls(text=doc.page_content, extra_info=doc.metadata)
