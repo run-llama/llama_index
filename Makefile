e2c7441d530d9f3f436eb535738585ddc056bf93
Doc Emmett Brown <teoh@users.noreply.github.com>
2023-01-04 19:25:33 -0800

    Add Reader for String Iterable (#173)

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ lint:
 	black . --check
 	isort . --check
 	flake8 .
+
+test:
+	pytest tests
---
+++ gpt_index/readers/string_iterable.py
@@ -0,0 +1,32 @@
+"""Simple reader that turns an iterable of strings into a list of Documents."""
+from typing import Any, List
+
+from gpt_index.readers.base import BaseReader
+from gpt_index.readers.schema.base import Document
+
+
+class StringIterableReader(BaseReader):
+    """String Iterable Reader.
+
+    Gets a list of documents, given an iterable (e.g. list) of strings.
+
+    Example:
+        .. code-block:: python
+            from gpt_index import StringIterableReader, GPTTreeIndex
+
+            documents = StringIterableReader().load_data(
+                texts=["I went to the store", "I bought an apple"])
+            index = GPTTreeIndex(documents)
+            index.query("what did I buy?")
+
+            # response should be something like "You bought an apple."
+    """
+
+    def load_data(self, **load_kwargs: Any) -> List[Document]:
+        """Load the data."""
+        texts = load_kwargs.get("texts", list())
+        results = []
+        for text in texts:
+            results.append(Document(text))
+
+        return results
