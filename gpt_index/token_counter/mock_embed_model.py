a27a9a303ae89cc114423ad28bf8ee1eb967b78d
Jerry Liu <jerry@robustintelligence.com>
2023-01-23 13:54:44 -0800

    cr

---
+++ gpt_index/token_counter/mock_embed_model.py
@@ -0,0 +1,27 @@
+"""Mock embedding model."""
+
+from gpt_index.embeddings.base import BaseEmbedding
+from typing import List, Any
+
+
+class MockEmbedding(BaseEmbedding):
+    """Mock embedding.
+    
+    Used for token prediction.
+
+    Args:
+        embed_dim (int): embedding dimension
+
+    """
+    def __init__(self, embed_dim: int, *args: Any, **kwargs: Any) -> None:
+        """Init params."""
+        super().__init__(*args, **kwargs)
+        self.embed_dim = embed_dim
+
+    def _get_query_embedding(self, query: str) -> List[float]:
+        """Get query embedding."""
+        return [0.5] * self.embed_dim
+
+    def get_text_embedding(self, text: str) -> List[float]:
+        """Get text embedding."""
+        return [0.5] * self.embed_dim
