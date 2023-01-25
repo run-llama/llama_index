622e5e43eca2c5bdc5c803633d11c6b665ea0bfe
Jerry Liu <jerry@robustintelligence.com>
2023-01-24 17:30:40 -0800

    cr

--- gpt_index/readers/twitter.py
+++ gpt_index/readers/twitter.py
@@ -15,29 +15,20 @@ class TwitterTweetReader(BaseReader):
         on how to get access to twitter API.
 
     Args:
-        consumer_key (str): consumer_key that you get from twitter API.
-        consumer_secret (str): consumer_secret that you get from twitter API.
-        access_token (str): access_token that you get from twitter API.
-        access_token_secret (str): access_token_secret that you get from twitter API.
+        bearer_token (str): bearer_token that you get from twitter API.
         num_tweets (Optional[int]): Number of tweets for each user twitter handle.\
             Default is 100 tweets.
     """
 
     def __init__(
         self,
-        consumer_key: str,
-        consumer_secret: str,
-        access_token: str,
-        access_token_secret: str,
+        bearer_token: str,
         num_tweets: Optional[int] = 100,
         verbose: bool = False,
     ) -> None:
         """Initialize with parameters."""
         super().__init__(verbose=verbose)
-        self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret
-        self.access_token = access_token
-        self.access_token_secret = access_token_secret
+        self.bearer_token = bearer_token
         self.num_tweets = num_tweets
 
     def load_data(
@@ -56,16 +47,14 @@ class TwitterTweetReader(BaseReader):
                 "`tweepy` package not found, please run `pip install tweepy`"
             )
 
-        # Authenticate with Twitter API
-        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        auth.set_access_token(self.access_token, self.access_token_secret)
-        api = tweepy.API(auth)
-
+        client = tweepy.Client(bearer_token=self.bearer_token)
         results = []
-        for user in twitterhandles:
-            tweets = api.user_timeline(screen_name=user, count=self.num_tweets)
+        for username in twitterhandles:
+            # tweets = api.user_timeline(screen_name=user, count=self.num_tweets)
+            user = client.get_user(username=username)
+            tweets = client.get_users_tweets(user.data.id, max_results=self.num_tweets)
             response = " "
-            for tweet in tweets:
+            for tweet in tweets.data:
                 response = response + tweet.text + "\n"
             results.append(Document(response))
         return results
