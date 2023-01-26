b44d777de7fa1ae3870469450c6edfa60c043cf0
Simon Suo <simonsdsuo@gmail.com>
2023-01-25 23:28:54 -0500

    wip

--- tests/mock_utils/mock_text_splitter.py
+++ tests/mock_utils/mock_text_splitter.py
@@ -1,18 +1,22 @@
 """Mock text splitter."""
 
-from typing import List
+from typing import List, Optional
 
 from gpt_index.langchain_helpers.text_splitter import TextSplit
 
 
-def mock_token_splitter_newline(text: str) -> List[str]:
+def mock_token_splitter_newline(
+    text: str, extra_info_str: Optional[str] = None
+) -> List[str]:
     """Mock token splitter by newline."""
     if text == "":
         return []
     return text.split("\n")
 
 
-def mock_token_splitter_newline_with_overlaps(text: str) -> List[TextSplit]:
+def mock_token_splitter_newline_with_overlaps(
+    text: str, extra_info_str: Optional[str]
+) -> List[TextSplit]:
     """Mock token splitter by newline."""
     if text == "":
         return []
