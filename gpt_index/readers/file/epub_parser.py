73359e871ed160840817f6c5f778d4d94ec4c3ae
Jimmy <46383546+Haowjy@users.noreply.github.com>
2023-02-01 11:23:02 -0500

    use html2text instead of bs4

--- gpt_index/readers/file/epub_parser.py
+++ gpt_index/readers/file/epub_parser.py
@@ -5,6 +5,7 @@ Contains parsers for epub files.
 
 from pathlib import Path
 from typing import Dict
+
 from gpt_index.readers.file.base_parser import BaseParser
 
 
@@ -26,9 +27,9 @@ class EpubParser(BaseParser):
         except ImportError:
             raise ValueError("`EbookLib` is required to read Epub files.")
         try:
-            from bs4 import BeautifulSoup
+            import html2text
         except ImportError:
-            raise ValueError("`beautifulsoup4` is required to parse Epub files.")
+            raise ValueError("`html2text` is required to parse Epub files.")
 
         text_list = []
         book = epub.read_epub(file, options={'ignore_ncx': True})
@@ -37,10 +38,7 @@ class EpubParser(BaseParser):
         for item in book.get_items():
             # Chapters are typically located in epub documents items.
             if item.get_type() == ebooklib.ITEM_DOCUMENT:
-                soup = BeautifulSoup(item.get_content(), 'html.parser')
-                all_chapter_texts = soup.findAll(text=True)
-                chapter_text = u"\n".join(filter(self.check_tag, all_chapter_texts))
-                text_list.append(chapter_text)
+                text_list.append(html2text.html2text(item.get_content().decode('utf-8')))
 
         text = "\n".join(text_list)
         return text
