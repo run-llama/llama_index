1ba2f12f57240e79764432d7378cd1529cfa163f
Jimmy <46383546+Haowjy@users.noreply.github.com>
2023-02-01 13:09:55 -0500

    format with formatter

--- gpt_index/readers/file/epub_parser.py
+++ gpt_index/readers/file/epub_parser.py
@@ -29,13 +29,15 @@ class EpubParser(BaseParser):
             raise ValueError("`html2text` is required to parse Epub files.")
 
         text_list = []
-        book = epub.read_epub(file, options={'ignore_ncx': True})
+        book = epub.read_epub(file, options={"ignore_ncx": True})
 
         # Iterate through all chapters.
         for item in book.get_items():
             # Chapters are typically located in epub documents items.
             if item.get_type() == ebooklib.ITEM_DOCUMENT:
-                text_list.append(html2text.html2text(item.get_content().decode('utf-8')))
+                text_list.append(
+                    html2text.html2text(item.get_content().decode("utf-8"))
+                )
 
         text = "\n".join(text_list)
         return text
