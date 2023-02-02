500e40d9dc199198c5a3216ab994c7fa47f60e1d
Jimmy <46383546+Haowjy@users.noreply.github.com>
2023-02-02 02:58:36 -0500

    Add epub file parser (#355)
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- gpt_index/readers/file/base.py
+++ gpt_index/readers/file/base.py
@@ -5,6 +5,7 @@ from typing import Callable, Dict, List, Optional, Union
 from gpt_index.readers.base import BaseReader
 from gpt_index.readers.file.base_parser import BaseParser
 from gpt_index.readers.file.docs_parser import DocxParser, PDFParser
+from gpt_index.readers.file.epub_parser import EpubParser
 from gpt_index.readers.file.image_parser import ImageParser
 from gpt_index.readers.file.markdown_parser import MarkdownParser
 from gpt_index.readers.file.slides_parser import PptxParser
@@ -22,6 +23,7 @@ DEFAULT_FILE_EXTRACTOR: Dict[str, BaseParser] = {
     ".mp3": VideoAudioParser(),
     ".mp4": VideoAudioParser(),
     ".csv": CSVParser(),
+    ".epub": EpubParser(),
     ".md": MarkdownParser(),
 }
 
