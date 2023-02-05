9d21f85797ef8c5a007f3565c7e9588a2bc1c309
ahmetkca <ahmet.karapinar@ontariotechu.net>
2023-02-05 04:24:58 -0500

    Add(GitBlobResponseModel): Dataclass for Github API's getBlob endpoint
    - Create GitBlobResponseModel dataclass
    - Add content, encoding, url, sha, size, and node_id attributes
    - Provide explanation of attributes in docstring

--- gpt_index/readers/github_readers/github_api_client.py
+++ gpt_index/readers/github_readers/github_api_client.py
@@ -45,3 +45,25 @@ class GitTreeResponseModel(DataClassJsonMixin):
     url: str
     tree: List[GitTreeObject]
     truncated: bool
+
+
+@dataclass
+class GitBlobResponseModel(DataClassJsonMixin):
+    """
+    Dataclass for the response from the Github API's getBlob endpoint.
+
+    Attributes:
+        - content (str): Content of the blob.
+        - encoding (str): Encoding of the blob.
+        - url (str): URL for the blob.
+        - sha (str): SHA1 checksum ID of the blob.
+        - size (int): Size of the blob.
+        - node_id (str): Node ID of the blob.
+    """
+
+    content: str
+    encoding: str
+    url: str
+    sha: str
+    size: int
+    node_id: str
