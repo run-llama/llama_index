a00b55b23689612517970c2c9b30c1e0d7d375e2
ahmetkca <ahmet.karapinar@ontariotechu.net>
2023-02-05 05:24:42 -0500

    Add(readers/utils.py) BufferedGitBlobDataIterator
    this is the concrete implementation of BufferedIterator
    for Git Blob API. This class prevents gpt_index to hold
    twice the memory for documents at any given time. Iterating over
    list of blob contents (without BufferedIterator) and also creating
    documents is 2x the memory until list of blob contents are
    deleted by garbage collector. Thus this class provides a way to
    iterate over list of blob contents and create documents at the same time

--- gpt_index/readers/github_readers/utils.py
+++ gpt_index/readers/github_readers/utils.py
@@ -1,7 +1,12 @@
+import asyncio
 from abc import ABC, abstractmethod
 from typing import List, Tuple
 
-from gpt_index.readers.github_readers.github_api_client import GitBlobResponseModel
+from gpt_index.readers.github_readers.github_api_client import (
+    GitBlobResponseModel,
+    GithubClient,
+    GitTreeResponseModel,
+)
 
 
 def print_if_verbose(verbose: bool, message: str):
@@ -33,3 +38,45 @@ class BufferedAsyncIterator(ABC):
         item = self.buffer.pop(0)
         self.index += 1
         return item
+
+
+class BufferedGitBlobDataIterator(BufferedAsyncIterator):
+    def __init__(
+        self,
+        blobs_and_paths: List[Tuple[GitTreeResponseModel.GitTreeObject, str]],
+        github_client: GithubClient,
+        owner: str,
+        repo: str,
+        loop: asyncio.AbstractEventLoop,
+        buffer_size: int,
+    ):
+        super().__init__(buffer_size)
+        self.blobs_and_paths = blobs_and_paths
+        self.github_client = github_client
+        self.owner = owner
+        self.repo = repo
+        if loop is None:
+            loop = asyncio.get_event_loop()
+            if loop is None:
+                raise ValueError("No event loop found")
+
+    async def _fill_buffer(self):
+        del self.buffer[:]
+        self.buffer = []
+        start = self.index
+        end = min(start + self.buffer_size, len(self.blobs_and_paths))
+
+        if start >= end:
+            return
+
+        results: List[GitBlobResponseModel] = await asyncio.gather(
+            *[
+                self.github_client.get_blob(self.owner, self.repo, blob.sha)
+                for blob, _ in self.blobs_and_paths[start:end]
+            ]
+        )
+
+        self.buffer = [
+            (result, path)
+            for result, (_, path) in zip(results, self.blobs_and_paths[start:end])
+        ]
