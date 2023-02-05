671b3d5480f746684af0cdd8bf6184eae4e4385f
ahmetkca <ahmet.karapinar@ontariotechu.net>
2023-02-05 04:23:45 -0500

    Add(Github API Client): Create GitTreeResponseModel
    - Create dataclass for the response from the Github API's getTree endpoint
    - Define GitTreeObject dataclass for objects in the tree
    - Add attributes to GitTreeObject dataclass

---
+++ gpt_index/readers/github_readers/github_api_client.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+from dataclasses_json import DataClassJsonMixin
+
+
+@dataclass
+class GitTreeResponseModel(DataClassJsonMixin):
+    """
+    Dataclass for the response from the Github API's getTree endpoint.
+
+    Attributes:
+        - sha (str): SHA1 checksum ID of the tree.
+        - url (str): URL for the tree.
+        - tree (List[GitTreeObject]): List of objects in the tree.
+        - truncated (bool): Whether the tree is truncated.
+
+    Examples:
+        >>> tree = client.get_tree("owner", "repo", "branch")
+        >>> tree.sha
+    """
+
+    @dataclass
+    class GitTreeObject(DataClassJsonMixin):
+        """
+        Dataclass for the objects in the tree.
+
+        Attributes:
+            - path (str): Path to the object.
+            - mode (str): Mode of the object.
+            - type (str): Type of the object.
+            - sha (str): SHA1 checksum ID of the object.
+            - url (str): URL for the object.
+            - size (Optional[int]): Size of the object (only for blobs).
+        """
+
+        path: str
+        mode: str
+        type: str
+        sha: str
+        url: str
+        size: Optional[int] = None
+
+    sha: str
+    url: str
+    tree: List[GitTreeObject]
+    truncated: bool
