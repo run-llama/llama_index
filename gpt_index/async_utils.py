000640348e09c13e22bf7a206756601311871b25
Simon Suo <simonsdsuo@gmail.com>
2023-02-18 22:31:49 -0500

    type ignore

--- gpt_index/async_utils.py
+++ gpt_index/async_utils.py
@@ -6,5 +6,5 @@ from typing import Any, Coroutine, List
 def run_async_tasks(tasks: List[Coroutine]) -> List[Any]:
     """Run async tasks."""
     all_tasks = asyncio.gather(*tasks)
-    outputs: List[Any] = asyncio.run(all_tasks)
+    outputs: List[Any] = asyncio.run(all_tasks)  # type: ignore
     return outputs
