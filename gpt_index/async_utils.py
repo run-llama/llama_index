2b4ab8357ac6e79e0a041648f3ef2987aa599016
EmptyCrown <jessetanzhang@gmail.com>
2023-02-22 20:58:30 -0800

    Fixed test!

--- gpt_index/async_utils.py
+++ gpt_index/async_utils.py
@@ -5,6 +5,9 @@ from typing import Any, Coroutine, List
 
 def run_async_tasks(tasks: List[Coroutine]) -> List[Any]:
     """Run a list of async tasks."""
-    all_tasks = asyncio.gather(*tasks)
-    outputs: List[Any] = asyncio.run(all_tasks)  # type: ignore
+
+    async def _gather() -> List[Any]:
+        return await asyncio.gather(*tasks)
+
+    outputs: List[Any] = asyncio.run(_gather())
     return outputs
