bdac3f14282569341a974a1c382f824a14f6ad9c
Douglas Reid <douglas-reid@users.noreply.github.com>
2023-03-07 20:29:52 -0800

    feat: add support for loading Docs from Steamship Files (#654)
    Co-authored-by: Douglas Reid <doug@steamship.com>

--- gpt_index/readers/__init__.py
+++ gpt_index/readers/__init__.py
@@ -31,6 +31,7 @@ from gpt_index.readers.pinecone import PineconeReader
 from gpt_index.readers.qdrant import QdrantReader
 from gpt_index.readers.schema.base import Document
 from gpt_index.readers.slack import SlackReader
+from gpt_index.readers.steamship.file_reader import SteamshipFileReader
 from gpt_index.readers.string_iterable import StringIterableReader
 from gpt_index.readers.twitter import TwitterTweetReader
 from gpt_index.readers.weaviate.reader import WeaviateReader
@@ -70,4 +71,5 @@ __all__ = [
     "GithubRepositoryReader",
     "MboxReader",
     "ElasticsearchReader",
+    "SteamshipFileReader",
 ]
