9c7dba4c8e248e0fb4894a794633640c9ddfce56
Logan <logan.markewich@live.com>
2023-03-10 00:07:42 -0600

    Add "refresh" functionality to existing indexes (#658)

--- tests/test_docstore.py
+++ tests/test_docstore.py
@@ -23,10 +23,13 @@ def test_docstore() -> None:
 
     # test serialize/deserialize
     doc_dict = docstore.serialize_to_dict()
+    d1_hash = "950fec7ace0281237e5983582c6bb3d76e03cc3931faa695a0fbf039edc69d8c"
+    d2_hash = "40d67b32bf21864f2578065c951bb862f99ab0bed0bf527a8680a42b7a027f18"
     assert doc_dict["docs"] == {
         "d1": {
             "text": "hello world",
             "doc_id": "d1",
+            "doc_hash": d1_hash,
             "embedding": None,
             "extra_info": {"foo": "bar"},
             "__type__": "Document",
@@ -34,6 +37,7 @@ def test_docstore() -> None:
         "d2": {
             "text": "my node",
             "doc_id": "d2",
+            "doc_hash": d2_hash,
             "embedding": None,
             "extra_info": None,
             "node_info": {"node": "info"},
