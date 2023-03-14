3b9aa0d1d011bfcc9595d38f3fb4aee5586511c5
Simon Suo <simonsdsuo@gmail.com>
2023-03-13 23:47:52 -0400

    docs

--- gpt_index/indices/query/query_transform/base.py
+++ gpt_index/indices/query/query_transform/base.py
@@ -172,15 +172,31 @@ class DecomposeQueryTransform(BaseQueryTransform):
 
 
 class ImageOutputQueryTransform(BaseQueryTransform):
+    """Image output query transform.
+
+    Adds instructions for formatting image output.
+    By default, this prompts the LLM to format image output as an HTML <img> tag,
+    which can be displayed nicely in jupyter notebook.
+    """
+
     def __init__(
         self,
         width: int = 400,
         query_prompt: Optional[ImageOutputQueryTransformPrompt] = None
     ) -> None:
+        """Init ImageOutputQueryTransform.
+        
+        Args:
+            width (int): desired image display width in pixels
+            query_prompt (ImageOutputQueryTransformPrompt): custom prompt for
+                augmenting query with image output instructions. 
+        """
         self._width = width
         self._query_prompt = query_prompt or DEFAULT_IMAGE_OUTPUT_PROMPT
 
     def _run(self, query_bundle: QueryBundle, extra_info: Dict) -> QueryBundle:
+        """Run query transform."""
+        del extra_info  # Unused
         new_query_str = self._query_prompt.format(
             query_str=query_bundle.query_str, image_width=self._width
         )
