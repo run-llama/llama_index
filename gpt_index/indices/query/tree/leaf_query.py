4bddad700a35663b52292e0494f9dbd8e41d9823
Jerry Liu <jerryjliu98@gmail.com>
2023-03-15 23:21:34 -0700

    cr

--- gpt_index/indices/query/tree/leaf_query.py
+++ gpt_index/indices/query/tree/leaf_query.py
@@ -9,10 +9,7 @@ from gpt_index.data_structs.data_structs import IndexGraph, Node
 from gpt_index.indices.query.base import BaseGPTIndexQuery
 from gpt_index.indices.query.schema import QueryBundle
 from gpt_index.indices.response.builder import ResponseBuilder
-from gpt_index.indices.utils import (
-    extract_numbers_given_response,
-    get_sorted_node_list,
-)
+from gpt_index.indices.utils import extract_numbers_given_response, get_sorted_node_list
 from gpt_index.prompts.default_prompts import (
     DEFAULT_QUERY_PROMPT,
     DEFAULT_QUERY_PROMPT_MULTIPLE,
