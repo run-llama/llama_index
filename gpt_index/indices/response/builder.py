3cdac208a4155819dd68d5276273b01231f98289
Simon Suo <simonsdsuo@gmail.com>
2023-03-31 18:11:29 -0400

    wuip

--- gpt_index/indices/response/builder.py
+++ gpt_index/indices/response/builder.py
@@ -10,7 +10,7 @@ Will support different modes, from 1) stuffing chunks into prompt,
 import logging
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Dict, Generator, List, Optional, Tuple, Union, cast
+from typing import Any, Dict, Generator, List, Optional, Tuple, cast
 
 from gpt_index.data_structs.data_structs_v2 import IndexGraph
 from gpt_index.data_structs.node_v2 import Node, NodeWithScore
@@ -21,9 +21,8 @@ from gpt_index.indices.utils import get_sorted_node_list, truncate_text
 from gpt_index.logger.base import LlamaLogger
 from gpt_index.prompts.prompts import QuestionAnswerPrompt, RefinePrompt, SummaryPrompt
 from gpt_index.response.utils import get_response_text
+from gpt_index.types import RESPONSE_TEXT_TYPE
 from gpt_index.utils import temp_set_attrs
-
-RESPONSE_TEXT_TYPE = Union[str, Generator]
 
 logger = logging.getLogger(__name__)
 
