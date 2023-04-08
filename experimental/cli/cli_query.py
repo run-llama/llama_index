3f04971bc33d988cc26faf5fc94c864c5d6c91e6
iaalm <iaalmsimon@gmail.com>
2023-04-08 14:40:09 +0800

    [experimental] Add command line tool for llama_index (#1068)

---
+++ experimental/cli/cli_query.py
@@ -0,0 +1,19 @@
+from .configuration import load_index
+from argparse import Namespace, _SubParsersAction
+
+
+def query_cli(args: Namespace) -> None:
+    """Handle subcommand "query" """
+    index = load_index()
+    print(index.query(args.query))
+
+
+def register_query_cli(subparsers: _SubParsersAction) -> None:
+    """Register subcommand "query" to ArgumentParser"""
+    parser = subparsers.add_parser("query")
+    parser.add_argument(
+        "query",
+        help="Query",
+    )
+
+    parser.set_defaults(func=query_cli)
