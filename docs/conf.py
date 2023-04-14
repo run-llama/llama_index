de6f8bbe6d7f8961a4c9296b17698b5fcf7491b7
Logan Markewich <logan.markewich@live.com>
2023-04-13 20:58:11 -0600

    update template

--- docs/conf.py
+++ docs/conf.py
@@ -51,5 +51,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
