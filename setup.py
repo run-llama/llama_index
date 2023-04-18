8f9bf4b24eab3c5f52d1fecde3d4233c3e44242c
Simon Suo <simonsdsuo@gmail.com>
2023-04-18 10:49:14 -0700

    pin langchain verion

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 install_requires = [
     "dataclasses_json",
-    "langchain>=0.0.123",
+    "langchai==0.0.139",
     "numpy",
     "tenacity>=8.2.0,<9.0.0",
     "openai>=0.26.4",
