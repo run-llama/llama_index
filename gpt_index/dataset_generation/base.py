04dfcaec5fe4c3688fc6f71b453cd6d056e99701
ravi03071991 <ravi03071991@gmail.com>
2023-04-18 19:21:33 +0530

    resolve lint errors

--- gpt_index/dataset_generation/base.py
+++ gpt_index/dataset_generation/base.py
@@ -72,7 +72,7 @@ class DatasetGenerator:
         Generates questions for each document.
         """
 
-        def document_question_generator(chunks) -> List[str]:
+        def document_question_generator(chunks: List[str]) -> List[str]:
             questions = []
 
             for chunk in chunks:
