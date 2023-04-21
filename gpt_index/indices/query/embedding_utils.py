9245a8907770702907a21810670ac98e80175b03
Jerry Liu <jerryjliu98@gmail.com>
2023-04-20 22:54:42 -0700

    add SVM retrieval (#1273)
    covfefe

--- gpt_index/indices/query/embedding_utils.py
+++ gpt_index/indices/query/embedding_utils.py
@@ -5,6 +5,8 @@ from typing import Callable, Dict, List, Optional, Tuple
 from dataclasses import field, dataclass
 from gpt_index.data_structs.node_v2 import Node, NodeWithScore
 from gpt_index.embeddings.base import similarity as default_similarity_fn
+import numpy as np
+from gpt_index.vector_stores.types import VectorStoreQueryMode
 
 
 def get_top_k_embeddings(
@@ -38,6 +40,62 @@ def get_top_k_embeddings(
 
     result_similarities = [s for s, _ in result_tups]
     result_ids = [n for _, n in result_tups]
+
+    return result_similarities, result_ids
+
+
+def get_top_k_embeddings_learner(
+    query_embedding: List[float],
+    embeddings: List[List[float]],
+    similarity_top_k: Optional[int] = None,
+    embedding_ids: Optional[List] = None,
+    query_mode: VectorStoreQueryMode = VectorStoreQueryMode.SVM,
+) -> Tuple[List[float], List]:
+    """Get top embeddings by fitting a learner against query.
+
+    Inspired by Karpathy's SVM demo:
+    https://github.com/karpathy/randomfun/blob/master/knn_vs_svm.ipynb
+
+    Can fit SVM, linear regression, and more.
+
+    """
+    try:
+        from sklearn import svm, linear_model
+    except ImportError:
+        raise ImportError("Please install scikit-learn to use this feature.")
+
+    if embedding_ids is None:
+        embedding_ids = [i for i in range(len(embeddings))]
+    query_embedding_np = np.array(query_embedding)
+    embeddings_np = np.array(embeddings)
+    # create dataset
+    dataset_len = len(embeddings) + 1
+    dataset = np.concatenate([query_embedding_np[None, ...], embeddings_np])
+    y = np.zeros(dataset_len)
+    y[0] = 1
+
+    if query_mode == VectorStoreQueryMode.SVM:
+        # train our SVM
+        # TODO: make params configurable
+        clf = svm.LinearSVC(
+            class_weight="balanced", verbose=False, max_iter=10000, tol=1e-6, C=0.1
+        )
+    elif query_mode == VectorStoreQueryMode.LINEAR_REGRESSION:
+        clf = linear_model.LinearRegression()
+    elif query_mode == VectorStoreQueryMode.LOGISTIC_REGRESSION:
+        clf = linear_model.LogisticRegression(class_weight="balanced")
+    else:
+        raise ValueError(f"Unknown query mode: {query_mode}")
+
+    clf.fit(dataset, y)  # train
+
+    # infer on whatever data you wish, e.g. the original data
+    similarities = clf.decision_function(dataset[1:])
+    sorted_ix = np.argsort(-similarities)
+    top_sorted_ix = sorted_ix[:similarity_top_k]
+
+    result_similarities = similarities[top_sorted_ix]
+    result_ids = [embedding_ids[ix] for ix in top_sorted_ix]
 
     return result_similarities, result_ids
 
