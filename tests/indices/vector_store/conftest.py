eec408f2344557b2fb8bfeb3f9dd638c5b2eadce
Simon Suo <simonsdsuo@gmail.com>
2023-04-26 15:33:22 -0400

    fix faiss

--- tests/indices/vector_store/conftest.py
+++ tests/indices/vector_store/conftest.py
@@ -13,16 +13,13 @@ from tests.indices.vector_store.mock_faiss import MockFaissIndex
 
 @pytest.fixture()
 def faiss_vector_store(tmp_path: pathlib) -> FaissVectorStore:
-    # NOTE: use temp file
-    file_path = tmp_path / "test_file.txt"
-
     # NOTE: mock faiss import
     sys.modules["faiss"] = MagicMock()
 
     # NOTE: mock faiss index
     faiss_index = MockFaissIndex()
 
-    return FaissVectorStore(faiss_index=faiss_index, persist_path=file_path)
+    return FaissVectorStore(faiss_index=faiss_index, persist_dir=tmp_path)
 
 
 @pytest.fixture()
