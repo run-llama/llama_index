b4296a1ce0dcbf0b45c6a57a0cadf1cbb1db6b53
Logan Markewich <logan.markewich@live.com>
2023-04-25 19:39:55 -0600

    wip add ID to events

--- gpt_index/callback_manager/schema.py
+++ gpt_index/callback_manager/schema.py
@@ -1,4 +1,5 @@
 """Base schema for callback managers."""
+import uuid
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
@@ -20,6 +21,9 @@ class CBEvent:
     event_type: CBEventType
     payload: Dict[str, str]
     time: str = ""
+    id: str = ""
 
     def __post_init__(self):
         self.time = datetime.now().strftime("%H:%M:%S")
+        if not self.id:
+            self.id = str(uuid.uuid4())
