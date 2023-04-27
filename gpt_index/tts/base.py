1e35c79276a698e03ebcf31d0b56295ed0eb2ca3
ravi03071991 <ravi03071991@gmail.com>
2023-04-28 00:05:29 +0530

    Updated with reviews

--- gpt_index/tts/base.py
+++ gpt_index/tts/base.py
@@ -1,17 +1,27 @@
 """Text to speech module."""
 
 from typing import Optional
+import tempfile
+import os
+import numpy as np
 
 
-class barkTTS:
-    def __init__(self, text_temp: float = 0.7, waveform_temp: float = 0.7) -> None:
+class BaseTTS:
+    def __init__(self) -> None:
+        pass
+
+class BarkTTS(BaseTTS):
+    def __init__(self, text_temp: float = 0.7, waveform_temp: float = 0.7, lang_speaker_voice: Optional[str] = None) -> None:
         """
         Args:
             text_temp: generation temperature (1.0 more diverse, \
                 0.0 more conservative)
             waveform_temp: generation temperature (1.0 more diverse, \
                 0.0 more conservative)
+            lang_speaker_voice: language speaker voice for audio cloning.
         """
+
+        super().__init__()
 
         import_err_msg = "`bark` package not found, \
             please run `pip install git+https://github.com/suno-ai/bark.git`"
@@ -20,23 +30,17 @@ class barkTTS:
         except ImportError:
             raise ImportError(import_err_msg)
 
-        self.generate_audio = bark.generate_audio
+        self.generate_audio_fn = bark.generate_audio
         self.save_as_prompt = bark.save_as_prompt
         self.text_temp = text_temp
         self.waveform_temp = waveform_temp
+        self.lang_speaker_voice = lang_speaker_voice
 
-    def generate_bark_audio(
-        self, text: Optional[str], lang_speaker_voice: Optional[str]
-    ) -> None:
+    def generate_bark_audio(self, text: str) -> None:
         """
         Args:
             text: text to be turned into audio.
-            lang_speaker_voice: language speaker voice for audio cloning.
         """
-
-        import tempfile
-        import os
-        import numpy as np
 
         # text to be chunked into 10 words each to avoid hallicunation
         words = text.split()
@@ -44,7 +48,7 @@ class barkTTS:
         chunks = [" ".join(chunk) for chunk in chunks]
 
         full_generation = None
-        history_prompt = lang_speaker_voice
+        history_prompt = self.lang_speaker_voice
         audio_chunks = []
 
         for chunk in chunks:
@@ -53,7 +57,7 @@ class barkTTS:
                     f = os.path.join(d, "history_prompt.npz")
                     self.save_as_prompt(f, full_generation)
                     history_prompt = f
-                full_generation, audio_array = self.generate_audio(
+                full_generation, audio_array = self.generate_audio_fn(
                     chunk,
                     history_prompt=history_prompt,
                     text_temp=self.text_temp,
