b7259bb3eb9e1967bb13d9ba22ae99437b1026f6
Jerry Liu <jerryjliu98@gmail.com>
2023-04-29 00:33:20 -0700

    add imports + custom retriever tutorial (#1376)

--- gpt_index/data_structs/__init__.py
+++ gpt_index/data_structs/__init__.py
@@ -6,11 +6,12 @@ from gpt_index.data_structs.data_structs_v2 import (
     IndexList,
     KeywordTable,
 )
-from gpt_index.data_structs.node_v2 import Node
+from gpt_index.data_structs.node_v2 import Node, NodeWithScore
 from gpt_index.data_structs.table_v2 import StructDatapoint
 
 __all__ = [
     "Node",
+    "NodeWithScore",
     "IndexGraph",
     "KeywordTable",
     "IndexList",
