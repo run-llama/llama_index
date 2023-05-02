5d6ff5868e9b94d80142f288108dbcfb160c42f9
Logan Markewich <Logan.Markewich@yardi.com>
2023-05-01 20:33:48 -0600

    proper init

--- gpt_index/callbacks/__init__.py
+++ gpt_index/callbacks/__init__.py
@@ -2,4 +2,4 @@ from .base import CallbackManager
 from .llama_debug import LlamaDebugHandler
 from .schema import CBEvent, CBEventType
 
-__all__ = [CallbackManager, CBEvent, CBEventType, LlamaDebugHandler]
+__all__ = ["CallbackManager", "CBEvent", "CBEventType", "LlamaDebugHandler"]
