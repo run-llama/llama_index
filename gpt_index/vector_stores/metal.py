794bc629889c10ad1967b6546f291e633139636a
James O'Dwyer <james@getmetal.io>
2023-05-02 11:46:06 -0400

    add deletion

--- gpt_index/vector_stores/metal.py
+++ gpt_index/vector_stores/metal.py
@@ -13,12 +13,17 @@ class MetalVectorStore(VectorStore):
             import metal_sdk  # noqa: F401
         except ImportError:
             raise ImportError(import_err_msg)
-        from metal_sdk.metal import Metal
+        from metal_sdk.metal import Metal   # noqa: F401
+
+
+        self.api_key = api_key
+        self.client_id = client_id
+        self.index_id = index_id
+        self.filters = filters
 
         self.metal_client = Metal(api_key, client_id, index_id)
         self.stores_text = True
         self.is_embedding_query = True
-        self.filters = filters
 
     @classmethod
     def from_dict(cls, config_dict: Dict[str, Any]) -> "MetalVectorStore":
@@ -47,3 +52,62 @@ class MetalVectorStore(VectorStore):
             ids.append(item["id"])
 
         return VectorStoreQueryResult(nodes=nodes, similarities=distances, ids=ids)
+
+    @property
+    def client(self) -> Any:
+        """Return Metal client."""
+        return self.metal_client
+
+    @property
+    def config_dict(self) -> dict:
+        """Return config dict."""
+        return {
+            "api_key": self.api_key,
+            "client_id": self.client_id,
+            "index_id": self.index_id,
+            "filters": self.filters
+        }
+
+
+    def add(self, embedding_results: List[NodeEmbeddingResult]) -> List[str]:
+        """Add embedding results to index.
+
+        Args
+            embedding_results: List[NodeEmbeddingResult]: list of embedding results
+
+        """
+        if not self.metal_client:
+            raise ValueError("metal_client not initialized")
+
+
+        ids = []
+        for result in embedding_results:
+            ids.append(result.id)
+
+            payload = {
+                "text": result.node.get_text(),
+                "metadata": result.node.extra_info or {},
+            }
+
+            if result.id:
+                payload["id"] = result.id
+
+            if result.doc_id:
+                payload["metadata"]["document_id"] = result.doc_id
+
+            self.metal_client.index(payload)
+
+        return ids
+
+    def delete(self, ids: List[str]) -> None:
+        """Delete nodes from index.
+
+        Args:
+            ids (List[str]): list of node ids to delete
+
+        """
+        if not self.metal_client:
+            raise ValueError("metal_client not initialized")
+
+        for id in ids:
+            self.metal_client.deleteOne(id)
