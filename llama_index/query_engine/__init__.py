6ad761fa1d2c26e6ebbfafd91dca8cb5c796711f
Jerry Liu <jerryjliu98@gmail.com>
2023-05-05 16:38:05 -0700

    cr

--- llama_index/query_engine/__init__.py
+++ llama_index/query_engine/__init__.py
@@ -2,10 +2,7 @@ from llama_index.query_engine.graph_query_engine import ComposableGraphQueryEngi
 from llama_index.query_engine.retriever_query_engine import RetrieverQueryEngine
 from llama_index.query_engine.transform_query_engine import TransformQueryEngine
 from llama_index.query_engine.multistep_query_engine import MultiStepQueryEngine
-from llama_index.query_engine.router_query_engine import (
-    RouterQueryEngine,
-    RetrieverRouterQueryEngine,
-)
+from llama_index.query_engine.router_query_engine import RouterQueryEngine
 
 
 __all__ = [
@@ -14,5 +11,4 @@ __all__ = [
     "TransformQueryEngine",
     "MultiStepQueryEngine",
     "RouterQueryEngine",
-    "RetrieverRouterQueryEngine",
 ]
