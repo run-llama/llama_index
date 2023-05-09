2efff471c9054fc6db3e72a62871efd49ce9c0d6
宋闻萱 <songwenxuan@songwenxuandeMacBook-Air.local>
2023-05-09 19:45:23 +0800

    add a new test python file

---
+++ examples/paul_graham_essay/test.py
@@ -0,0 +1,11 @@
+import os
+os.environ["OPENAI_API_KEY"] = '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'
+
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
+documents = SimpleDirectoryReader('data').load_data()
+index = GPTVectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+# query_engine.query("what is AI?")
+# index.storage_context.persist()
+response = query_engine.query("What did the author do growing up?")
+print(response)
