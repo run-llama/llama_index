76fb949aadc98f283049d28803adb0d2966c7799
Logan Markewich <logan.markewich@live.com>
2023-05-11 08:43:37 -0600

    linting

--- llama_index/callbacks/aim.py
+++ llama_index/callbacks/aim.py
@@ -1,11 +1,15 @@
+import logging
+from abc import abstractmethod
 from typing import Any, Dict, List, Optional
-from llama_index.callbacks.base import BaseCallbackHandler
-from llama_index.callbacks.schema import CBEventType
+
 from aim import Run, Text
 from aim.ext.system_info import DEFAULT_SYSTEM_TRACKING_INT
-import logging
 
-logger = logging.getLogger(__name__).setLevel(logging.WARNING)
+from llama_index.callbacks.base import BaseCallbackHandler
+from llama_index.callbacks.schema import CBEventType
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 
 class AimCallback(BaseCallbackHandler):
@@ -13,17 +17,25 @@ class AimCallback(BaseCallbackHandler):
     AimCallback callback class.
 
     Args:
-        repo (:obj:`str`, optional): Aim repository path or Repo object to which Run object is bound.
+        repo (:obj:`str`, optional):
+            Aim repository path or Repo object to which Run object is bound.
             If skipped, default Repo is used.
-        experiment_name (:obj:`str`, optional): Sets Run's `experiment` property. 'default' if not specified.
+        experiment_name (:obj:`str`, optional):
+            Sets Run's `experiment` property. 'default' if not specified.
             Can be used later to query runs/sequences.
-        system_tracking_interval (:obj:`int`, optional): Sets the tracking interval in seconds for system usage
-            metrics (CPU, Memory, etc.). Set to `None` to disable system metrics tracking.
-        log_system_params (:obj:`bool`, optional): Enable/Disable logging of system params such as installed packages,
+        system_tracking_interval (:obj:`int`, optional):
+            Sets the tracking interval in seconds for system usage
+            metrics (CPU, Memory, etc.). Set to `None` to disable
+            system metrics tracking.
+        log_system_params (:obj:`bool`, optional):
+            Enable/Disable logging of system params such as installed packages,
             git info, environment variables, etc.
-        capture_terminal_logs (:obj:`bool`, optional): Enable/Disable terminal stdout logging.
-        event_starts_to_ignore (Optional[List[CBEventType]]): list of event types to ignore when tracking event starts.
-        event_ends_to_ignore (Optional[List[CBEventType]]): list of event types to ignore when tracking event ends.
+        capture_terminal_logs (:obj:`bool`, optional):
+            Enable/Disable terminal stdout logging.
+        event_starts_to_ignore (Optional[List[CBEventType]]):
+            list of event types to ignore when tracking event starts.
+        event_ends_to_ignore (Optional[List[CBEventType]]):
+            list of event types to ignore when tracking event ends.
     """
 
     def __init__(
@@ -36,7 +48,7 @@ class AimCallback(BaseCallbackHandler):
         event_starts_to_ignore: Optional[List[CBEventType]] = None,
         event_ends_to_ignore: Optional[List[CBEventType]] = None,
         run_params: Optional[Dict[str, Any]] = None,
-    ):
+    ) -> None:
         event_starts_to_ignore = (
             event_starts_to_ignore if event_starts_to_ignore else []
         )
@@ -51,13 +63,14 @@ class AimCallback(BaseCallbackHandler):
         self.system_tracking_interval = system_tracking_interval
         self.log_system_params = log_system_params
         self.capture_terminal_logs = capture_terminal_logs
-        self._run = None
+        self._run: Optional[Any] = None
         self._run_hash = None
 
         self._llm_response_step = 0
 
         self.setup(run_params)
 
+    @abstractmethod
     def on_event_start(
         self,
         event_type: CBEventType,
@@ -85,6 +98,9 @@ class AimCallback(BaseCallbackHandler):
             payload (Optional[Dict[str, Any]]): payload to store.
             event_id (str): event id to store.
         """
+        if not self._run:
+            raise ValueError("AimCallback failed to init properly.")
+
         if event_type is CBEventType.LLM and payload:
             self._run.track(
                 Text(payload["formatted_prompt"]),
@@ -116,7 +132,7 @@ class AimCallback(BaseCallbackHandler):
             self.setup()
         return self._run
 
-    def setup(self, args=None):
+    def setup(self, args: Optional[Dict[str, Any]] = None) -> None:
         if not self._run:
             if self._run_hash:
                 self._run = Run(
@@ -144,6 +160,6 @@ class AimCallback(BaseCallbackHandler):
             except Exception as e:
                 logger.warning(f"Aim could not log config parameters -> {e}")
 
-    def __del__(self):
+    def __del__(self) -> None:
         if self._run and self._run.active:
             self._run.close()
