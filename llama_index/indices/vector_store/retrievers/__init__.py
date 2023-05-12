b824f855f1e1be87e8b837e3a1975ac96639d05c
Simon Suo <simonsdsuo@gmail.com>
2023-05-12 17:27:51 -0400

    wip

--- llama_index/indices/vector_store/retrievers/__init__.py
+++ llama_index/indices/vector_store/retrievers/__init__.py
@@ -1,7 +1,7 @@
-from llama_index.indices.vector_store.retrievers.auto_retriever import (
-    VectorIndexAutoRetriever,
-)
-from llama_index.indices.vector_store.retrievers.retriever import VectorIndexRetriever
+from llama_index.indices.vector_store.retrievers.retriever import \
+    VectorIndexRetriever
+from llama_index.indices.vector_store.retrievers.auto_retriever import \
+    VectorIndexAutoRetriever
 
 __all__ = [
     "VectorIndexRetriever",
