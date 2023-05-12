a902c41af073bc680cae88159d0da2fcc42e833e
Sam Partee <sam.partee@redis.com>
2023-05-11 17:22:57 -0700

    Change index deletion logic

--- llama_index/vector_stores/redis.py
+++ llama_index/vector_stores/redis.py
@@ -120,19 +120,20 @@ class RedisVectorStore(VectorStore):
         Raises:
             ValueError: If the index already exists and overwrite is False.
         """
+        # check to see if empty document list was passed
+        if len(embedding_results) == 0:
+            return []
 
-        # check index exists, call once to avoid calling multiple times
-        index_exists = self._index_exists()
-        if index_exists and self._overwrite:
-            self.delete_index()
+        # set vector dim for creation if index doesn't exist
+        self._index_args["dims"] = len(embedding_results[0].embedding)
 
-        elif index_exists and not self._overwrite:
-            raise ValueError("Index already exists and overwrite is False.")
-
-        else:  # index does not exist, create it
-            # get vector dim from embedding results if index does not exist
-            # as it will be created from the embedding result attributes.
-            self._index_args["dims"] = len(embedding_results[0].embedding)
+        if self._index_exists():
+            if self._overwrite:
+                self.delete_index()
+                self._create_index()
+            else:
+                logging.info(f"Adding document to existing index {self._index_name}")
+        else:
             self._create_index()
 
         ids = []
