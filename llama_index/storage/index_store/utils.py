352be85f5776b1956638f42e2b22ba9c716e4cd6
aidenchen01 <132824290+aidenchen01@users.noreply.github.com>
2023-05-13 15:21:24 +0800

    Update utils.py

--- llama_index/storage/index_store/utils.py
+++ llama_index/storage/index_store/utils.py
@@ -15,4 +15,4 @@ def json_to_index_struct(struct_dict: dict) -> IndexStruct:
     type = struct_dict[TYPE_KEY]
     data_dict = struct_dict[DATA_KEY]
     cls = INDEX_STRUCT_TYPE_TO_INDEX_STRUCT_CLASS[type]
-    return cls.to_dict(data_dict)
+    return cls.from_dict(data_dict)
