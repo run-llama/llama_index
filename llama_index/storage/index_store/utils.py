2af5db37bd9666e74d450a3c93cb5f29e014462a
aidenchen01 <132824290+aidenchen01@users.noreply.github.com>
2023-05-13 14:54:43 +0800

    Update utils.py
    catch errors for those already use dict form

--- llama_index/storage/index_store/utils.py
+++ llama_index/storage/index_store/utils.py
@@ -15,4 +15,7 @@ def json_to_index_struct(struct_dict: dict) -> IndexStruct:
     type = struct_dict[TYPE_KEY]
     data_dict = struct_dict[DATA_KEY]
     cls = INDEX_STRUCT_TYPE_TO_INDEX_STRUCT_CLASS[type]
-    return cls.from_json(data_dict)
+    try:
+        return cls.from_json(data_dict)
+    except TypeError:
+        return cls.from_dict(data_dict)
