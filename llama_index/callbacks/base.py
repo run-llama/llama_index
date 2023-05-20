c36b647a78c680e22fba86fc845406abc3ea8454
Logan Markewich <logan.markewich@live.com>
2023-05-19 18:14:17 -0600

    tracing poc

--- llama_index/callbacks/base.py
+++ llama_index/callbacks/base.py
@@ -1,3 +1,5 @@
+import sys
+from functools import wraps
 import uuid
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Optional
@@ -38,6 +40,57 @@ class BaseCallbackHandler(ABC):
         """Run when an event ends."""
 
 
+leaf_events = [
+    CBEventType.CHUNKING,
+    CBEventType.LLM,
+    CBEventType.EMBEDDING
+]
+
+
+class TraceCalls(object):
+    """ Use as a decorator on functions that should be traced. Several
+        functions can be decorated - they will all be indented according
+        to their call depth.
+    """
+    def __init__(self, stream=sys.stdout, indent_step=2, show_ret=False, is_start=False, is_end=False):
+        self.stream = stream
+        self.indent_step = indent_step
+        self.show_ret = show_ret
+        self.is_start = is_start
+        self.is_end = is_end
+        self.parent_id = 'root'
+
+        # This is a class attribute since we want to share the indentation
+        # level between different traced functions, in case they call
+        # each other.
+        TraceCalls.cur_indent = 0
+        TraceCalls.run_map = {}
+
+    def __call__(self, fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            indent = ' ' * TraceCalls.cur_indent
+            argstr = ', '.join(
+                [repr(a) for a in args] +
+                ["%s=%s" % (a, repr(b)) for a, b in kwargs.items()])
+            
+            if self.is_start:
+                self.stream.write('%s%s(%s)\n' % (indent, fn.__name__, argstr.split(', ')[1]))
+
+            if self.is_start and args[1] not in leaf_events:
+                TraceCalls.cur_indent += self.indent_step
+            ret = fn(*args, **kwargs)
+
+            if self.is_end and args[1] not in leaf_events:
+                TraceCalls.cur_indent -= self.indent_step
+
+            if self.show_ret:
+                self.stream.write('%s--> %s\n' % (indent, ret))
+            return ret
+        return wrapper
+
+
+
 class CallbackManager(BaseCallbackHandler, ABC):
     """Callback manager that handles callbacks for events within LlamaIndex.
 
@@ -50,6 +103,7 @@ class CallbackManager(BaseCallbackHandler, ABC):
         """Initialize the manager with a list of handlers."""
         self.handlers = handlers
 
+    @TraceCalls(is_start=True)
     def on_event_start(
         self,
         event_type: CBEventType,
@@ -64,6 +118,7 @@ class CallbackManager(BaseCallbackHandler, ABC):
                 handler.on_event_start(event_type, payload, event_id=event_id, **kwargs)
         return event_id
 
+    @TraceCalls(is_end=True)
     def on_event_end(
         self,
         event_type: CBEventType,
