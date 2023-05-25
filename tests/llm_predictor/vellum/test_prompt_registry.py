adaf5442f551a5370c6d0615fe4b230dd286dead
Noa Flaherty <noaflaherty@gmail.com>
2023-05-25 12:15:27 -0400

    Fix failing test

--- tests/llm_predictor/vellum/test_prompt_registry.py
+++ tests/llm_predictor/vellum/test_prompt_registry.py
@@ -14,16 +14,20 @@ def test_from_prompt__new(
     mock_vellum_client_factory: Callable[..., mock.MagicMock],
     vellum_prompt_registry_factory: Callable[..., VellumPromptRegistry],
 ) -> None:
-    """We should register a new prompt if no deployment id or name are specified"""
+    """We should register a new prompt if no deployment exists"""
+
+    from vellum.core import ApiError
 
     dummy_prompt = dummy_prompt_class(template="What's your favorite {thing}?")
 
     vellum_client = mock_vellum_client_factory()
 
+    vellum_client.deployments.retrieve.side_effect = ApiError(status_code=404)
+
     prompt_registry = vellum_prompt_registry_factory(vellum_client=vellum_client)
     prompt_registry.from_prompt(dummy_prompt)
 
-    vellum_client.registered_prompts.register_prompt.assrt_called_once()
+    vellum_client.registered_prompts.register_prompt.assert_called_once()
 
 
 def test_from_prompt__existing(
