5a6e13795379ef69c1bec0c6062bcd0217a54676
Simon Suo <simonsdsuo@gmail.com>
2023-05-29 23:09:20 -0700

    wio

--- llama_index/indices/prompt_helper.py
+++ llama_index/indices/prompt_helper.py
@@ -7,13 +7,14 @@ structs but keeping token limitations in mind.
 
 from typing import Callable, List, Optional, Sequence
 
-from llama_index.constants import MAX_CHUNK_OVERLAP
-from llama_index.data_structs.node import Node
 from llama_index.langchain_helpers.text_splitter import TokenTextSplitter
 from llama_index.llm_predictor.base import BaseLLMPredictor
 from llama_index.prompts.base import Prompt
 from llama_index.prompts.utils import get_empty_prompt_txt
 from llama_index.utils import globals_helper
+
+DEFAULT_PADDING = 5
+DEFAULT_CHUNK_OVERLAP_RATIO = 0.1
 
 
 class PromptHelper:
@@ -23,133 +24,138 @@ class PromptHelper:
     and fill in context information according to necessary token limitations.
 
     Args:
-        max_input_size (int): Maximum input size for the LLM.
-        num_output (int): Number of outputs for the LLM.
-        max_chunk_overlap (int): Maximum chunk overlap for the LLM.
-        embedding_limit (Optional[int]): Maximum number of embeddings to use.
-        chunk_size_limit (Optional[int]): Maximum chunk size to use.
+        context_window (int):                   Context window for the LLM.
+        num_output (int):                       Number of outputs for the LLM.
+        chunk_overlap_ratio (float):            Chunk overlap as a ratio of chunk size
+        max_chunk_size (Optional[int]):         Maximum chunk size to use.
         tokenizer (Optional[Callable[[str], List]]): Tokenizer to use.
-
+        separator (str):                        Separator for text splitter
     """
 
     def __init__(
         self,
-        max_input_size: int,
+        context_window: int,
         num_output: int,
-        max_chunk_overlap: int,
-        embedding_limit: Optional[int] = None,
-        chunk_size_limit: Optional[int] = None,
+        chunk_overlap_ratio: float = DEFAULT_CHUNK_OVERLAP_RATIO,
+        max_chunk_size: Optional[int] = None,
         tokenizer: Optional[Callable[[str], List]] = None,
         separator: str = " ",
     ) -> None:
         """Init params."""
-        self.max_input_size = max_input_size
+        self.context_window = context_window
         self.num_output = num_output
-        self.max_chunk_overlap = max_chunk_overlap
-        self.embedding_limit = embedding_limit
-        self.chunk_size_limit = chunk_size_limit
+
+        self.chunk_overlap_ratio = chunk_overlap_ratio
+        if self.chunk_overlap_ratio >= 1. or self.chunk_overlap_ratio <= 0.:
+            raise ValueError('chunk_overlap_ratio must be a float between 0. and 1.')
+        self.max_chunk_size = max_chunk_size
+
         # TODO: make configurable
         self._tokenizer = tokenizer or globals_helper.tokenizer
         self._separator = separator
-        self.use_chunk_size_limit = chunk_size_limit is not None
 
     @classmethod
     def from_llm_predictor(
-        self,
+        cls,
         llm_predictor: BaseLLMPredictor,
-        max_chunk_overlap: Optional[int] = None,
-        embedding_limit: Optional[int] = None,
-        chunk_size_limit: Optional[int] = None,
+        chunk_overlap_ratio: float = DEFAULT_CHUNK_OVERLAP_RATIO,
+        max_chunk_size: Optional[int] = None,
         tokenizer: Optional[Callable[[str], List]] = None,
+        separator: str = " ",
     ) -> "PromptHelper":
         """Create from llm predictor.
 
-        This will autofill values like max_input_size and num_output.
+        This will autofill values like context_window and num_output.
 
         """
         llm_metadata = llm_predictor.get_llm_metadata()
-        max_chunk_overlap = max_chunk_overlap or min(
-            MAX_CHUNK_OVERLAP,
-            llm_metadata.max_input_size // 10,
-        )
-        if chunk_size_limit is not None:
-            max_chunk_overlap = min(max_chunk_overlap, chunk_size_limit // 10)
 
-        return self(
-            llm_metadata.max_input_size,
-            llm_metadata.num_output,
-            max_chunk_overlap,
-            embedding_limit=embedding_limit,
-            chunk_size_limit=chunk_size_limit,
+        return cls(
+            context_window=llm_metadata.max_input_size,
+            num_output=llm_metadata.num_output,
+            chunk_overlap_ratio=chunk_overlap_ratio,
+            max_chunk_size=max_chunk_size,
             tokenizer=tokenizer,
+            separator=separator,
         )
 
-    def _get_chunk_size_given_prompt(
-        self, prompt_text: str, num_chunks: int, padding: Optional[int] = 5
-    ) -> int:
-        """Get chunk size making sure we can also fit the prompt in.
+    def _get_available_context_size(self, prompt: Prompt) -> int:
+        """Get available context size.
 
-        Chunk size is computed based on a function of the total input size,
-        the prompt length, the number of outputs, and the number of chunks.
-
-        If padding is specified, then we subtract that from the chunk size.
-        By default we assume there is a padding of 5 (for the newline between chunks
-        and other formatting needs).
-
-        Limit by embedding_limit and chunk_size_limit if specified.
-
+        This is calculated as:
+            available context window = total context window
+                - input (partially filled prompt)
+                - output (room reserved for response)
         """
-        prompt_tokens = self._tokenizer(prompt_text)
+        empty_prompt_txt = get_empty_prompt_txt(prompt)
+        prompt_tokens = self._tokenizer(empty_prompt_txt)
         num_prompt_tokens = len(prompt_tokens)
 
-        # NOTE: if embedding limit is specified, then chunk_size must not be larger than
-        # embedding_limit
-        result = (
-            self.max_input_size - num_prompt_tokens - self.num_output
-        ) // num_chunks
-        if padding is not None:
-            result -= padding
+        return self.context_window - num_prompt_tokens - self.num_output
 
-        if self.embedding_limit is not None:
-            result = min(result, self.embedding_limit)
-        if self.chunk_size_limit is not None and self.use_chunk_size_limit:
-            result = min(result, self.chunk_size_limit)
+    def _get_available_chunk_size(
+        self, prompt: Prompt, num_chunks: int = 1, padding: int = 5
+    ) -> int:
+        """Get available chunk size.
+
+        This is calculated as:
+            available context window = total context window
+                - input (partially filled prompt)
+                - output (room reserved for response)
+
+            available chunk size  = available context window  // number_chunks
+                - padding 
+        
+        Note:
+        - By default, we use padding of 5 (to save space for formatting needs).
+        - The available chunk size is further clamped to max_chunk_size if specified
+        """
+        available_context_size = self._get_available_context_size(prompt)
+
+        result = available_context_size // num_chunks
+        result -= padding
+
+        if self.max_chunk_size is not None:
+            result = min(result, self.max_chunk_size)
 
         return result
 
     def get_text_splitter_given_prompt(
-        self, prompt: Prompt, num_chunks: int = 1, padding: Optional[int] = 5
+        self, prompt: Prompt, num_chunks: int = 1, padding: int = DEFAULT_PADDING
     ) -> TokenTextSplitter:
         """Get text splitter configured to maximally pack available context window,
         taking into account of given prompt, and desired number of chunks.
         """
-        empty_prompt_txt = get_empty_prompt_txt(prompt)
-        chunk_size = self._get_chunk_size_given_prompt(
-            empty_prompt_txt, num_chunks, padding=padding
-        )
+        chunk_size = self._get_available_chunk_size(prompt, num_chunks, padding=padding)
+        chunk_overlap = int(self.chunk_overlap_ratio * chunk_size)
         text_splitter = TokenTextSplitter(
             separator=self._separator,
             chunk_size=chunk_size,
-            chunk_overlap=self.max_chunk_overlap // num_chunks,
+            chunk_overlap=chunk_overlap,
             tokenizer=self._tokenizer,
         )
         return text_splitter
 
-    def truncate(self, prompt: Prompt, text_chunks: Sequence[str]) -> List[str]:
+    def truncate(
+        self, prompt: Prompt, text_chunks: Sequence[str], padding: int = DEFAULT_PADDING
+    ) -> List[str]:
         """Truncate text chunks to fit available context window."""
         text_splitter = self.get_text_splitter_given_prompt(
             prompt,
             num_chunks=len(text_chunks),
+            padding=padding,
         )
         return [text_splitter.truncate_text(chunk) for chunk in text_chunks]
 
-    def repack(self, prompt: Prompt, text_chunks: Sequence[str]) -> List[str]:
+    def repack(
+        self, prompt: Prompt, text_chunks: Sequence[str], padding: int = DEFAULT_PADDING
+    ) -> List[str]:
         """Repack text chunks to fit available context window.
 
         This will combine text chunks into consolidated chunks
         that more fully "pack" the prompt template given the max_input_size.
 
         """
-        text_splitter = self.get_text_splitter_given_prompt(prompt)
+        text_splitter = self.get_text_splitter_given_prompt(prompt, padding=padding)
         combined_str = "\n\n".join([c.strip() for c in text_chunks if c.strip()])
         return text_splitter.split_text(combined_str)
