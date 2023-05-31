46be0f1449424378236295c2e3a371f697b40b04
Simon Suo <simonsdsuo@gmail.com>
2023-05-30 23:54:01 -0700

    fix lint

--- llama_index/readers/psychic.py
+++ llama_index/readers/psychic.py
@@ -1,7 +1,7 @@
 """Psychic reader."""
 import logging
 import os
-from typing import List
+from typing import List, Optional
 
 from llama_index.readers.base import BaseReader
 from llama_index.readers.schema.base import Document
@@ -12,21 +12,23 @@ logger = logging.getLogger(__name__)
 class PsychicReader(BaseReader):
     """Psychic reader.
 
-    Psychic is a platform that allows syncing data from many SaaS apps through one universal API. 
-    This reader connects to an instance of Psychic and reads data from it, given a connector ID, 
-    account ID, and API key.
+    Psychic is a platform that allows syncing data from many SaaS apps through one
+        universal API.
+    This reader connects to an instance of Psychic and reads data from it, given a
+        connector ID, account ID, and API key.
 
     Learn more at docs.psychic.dev.
 
     Args:
-        psychic_key (str): Secret key for Psychic. Get one at https://dashboard.psychic.dev/api-keys.
+        psychic_key (str): Secret key for Psychic.
+            Get one at https://dashboard.psychic.dev/api-keys.
 
     """
 
-    def __init__(self, psychic_key: str = None) -> None:
+    def __init__(self, psychic_key: Optional[str] = None) -> None:
         """Initialize with parameters."""
         try:
-            from psychicapi import Psychic, ConnectorId  # noqa: F401
+            from psychicapi import ConnectorId, Psychic  # noqa: F401
         except ImportError:
             raise ImportError(
                 "`psychicapi` package not found, please run `pip install psychicapi`"
@@ -42,7 +44,9 @@ class PsychicReader(BaseReader):
         self.psychic = Psychic(secret_key=psychic_key)
         self.ConnectorId = ConnectorId
 
-    def load_data(self, connector_id: str = None, account_id: str = None) -> List[Document]:
+    def load_data(
+        self, connector_id: Optional[str] = None, account_id: Optional[str] = None
+    ) -> List[Document]:
         """Load data from a Psychic connection
 
         Args:
@@ -57,16 +61,23 @@ class PsychicReader(BaseReader):
             raise ValueError("Must specify both `connector_id` and `account_id`.")
         if connector_id not in self.ConnectorId.__members__.keys():
             raise ValueError("Invalid connector ID.")
-        
+
         # get all the documents in the database
         docs = []
         data = self.psychic.get_documents(self.ConnectorId[connector_id], account_id)
         for resource in data:
             text = resource.get("content")
             doc_id = resource.get("uri")
-            docs.append(Document(text=text, doc_id=doc_id, extra_info={"connector_id": connector_id, "account_id": account_id}))
+            docs.append(
+                Document(
+                    text=text,
+                    doc_id=doc_id,
+                    extra_info={"connector_id": connector_id, "account_id": account_id},
+                )
+            )
 
         return docs
+
 
 if __name__ == "__main__":
     reader = PsychicReader(psychic_key="public_key")
