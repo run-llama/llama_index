e7d032f82a4d41716ed392b61992af9d6c7074f8
hongyishi <shihongyi88@gmail.com>
2023-05-31 10:19:02 -0700

    Hongyi error correction (#3823)
    * wip
    
    * Working test
    
    * formatting
    
    * fix
    
    * fix
    
    * Rewrite with abstractions.

--- llama_index/query_engine/__init__.py
+++ llama_index/query_engine/__init__.py
@@ -1,13 +1,14 @@
 from llama_index.query_engine.graph_query_engine import ComposableGraphQueryEngine
 from llama_index.query_engine.multistep_query_engine import MultiStepQueryEngine
 from llama_index.query_engine.retriever_query_engine import RetrieverQueryEngine
+from llama_index.query_engine.retry_query_engine import RetryQueryEngine
 from llama_index.query_engine.router_query_engine import (
     RetrieverRouterQueryEngine,
     RouterQueryEngine,
 )
+from llama_index.query_engine.sql_vector_query_engine import SQLAutoVectorQueryEngine
 from llama_index.query_engine.sub_question_query_engine import SubQuestionQueryEngine
 from llama_index.query_engine.transform_query_engine import TransformQueryEngine
-from llama_index.query_engine.sql_vector_query_engine import SQLAutoVectorQueryEngine
 
 __all__ = [
     "ComposableGraphQueryEngine",
@@ -18,4 +19,5 @@ __all__ = [
     "RetrieverRouterQueryEngine",
     "SubQuestionQueryEngine",
     "SQLAutoVectorQueryEngine",
+    "RetryQueryEngine",
 ]
