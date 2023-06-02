8dcd846b5f5555e58aff5259b9d5bd2a13f9d571
Jerry Liu <jerryjliu98@gmail.com>
2023-06-02 00:23:23 -0700

    cr

--- llama_index/tools/tool_spec/base.py
+++ llama_index/tools/tool_spec/base.py
@@ -13,22 +13,21 @@ class BaseToolSpec:
     # list of functions that you'd want to convert to spec
     spec_functions: List[str]
 
-    @classmethod
     def to_tool_list(
-        cls,
+        self,
         func_to_metadata_mapping: Optional[ToolMetadata] = None,
     ) -> List[FunctionTool]:
         """Convert tool spec to list of tools."""
         func_to_metadata_mapping = func_to_metadata_mapping or {}
         tool_list = []
-        for func_name in cls.spec_functions:
-            func = getattr(cls, func_name)
+        for func_name in self.spec_functions:
+            func = getattr(self, func_name)
             metadata = func_to_metadata_mapping.get(func_name, None)
             if metadata is None:
                 name = func_name
                 docstring = func.__doc__
                 description = f"{name}{signature(func)}\n{docstring}"
                 metadata = ToolMetadata(name=name, description=description)
-            tool = FunctionTool.from_defaults(fn=func, metadata=metadata)
+            tool = FunctionTool(fn=func, metadata=metadata)
             tool_list.append(tool)
         return tool_list
