d11c88c0c9e5628dfe5a9375711bd5966fe31ee2
Nicholas Hoffs <nic.hoffs@gmail.com>
2023-06-01 21:51:48 -0700

    Reformatted properly with black

--- llama_index/indices/knowledge_graph/base.py
+++ llama_index/indices/knowledge_graph/base.py
@@ -16,7 +16,9 @@ from llama_index.data_structs.node import Node
 from llama_index.indices.base import BaseGPTIndex
 from llama_index.indices.base_retriever import BaseRetriever
 from llama_index.prompts.default_prompts import (
-    DEFAULT_KG_TRIPLET_EXTRACT_PROMPT, DEFAULT_QUERY_KEYWORD_EXTRACT_TEMPLATE)
+    DEFAULT_KG_TRIPLET_EXTRACT_PROMPT,
+    DEFAULT_QUERY_KEYWORD_EXTRACT_TEMPLATE,
+)
 from llama_index.prompts.prompts import KnowledgeGraphPrompt
 from llama_index.storage.docstore.types import RefDocInfo
 
@@ -76,7 +78,9 @@ class GPTKnowledgeGraphIndex(BaseGPTIndex[KG]):
 
     def as_retriever(self, **kwargs: Any) -> BaseRetriever:
         from llama_index.indices.knowledge_graph.retrievers import (
-            KGRetrieverMode, KGTableRetriever)
+            KGRetrieverMode,
+            KGTableRetriever,
+        )
 
         if len(self.index_struct.embedding_dict) > 0 and "retriever_mode" not in kwargs:
             kwargs["retriever_mode"] = KGRetrieverMode.HYBRID
@@ -153,7 +157,7 @@ class GPTKnowledgeGraphIndex(BaseGPTIndex[KG]):
                     index_struct.add_to_embedding_dict(rel_text, rel_embed)
 
         return index_struct
-    
+
     def _insert(self, nodes: Sequence[Node], **insert_kwargs: Any) -> None:
         """Insert a document."""
         for n in nodes:
