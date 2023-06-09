2bef5d5bd405366b2cba00997badddbc9d9c2703
Simon Suo <simonsdsuo@gmail.com>
2023-06-08 19:37:19 -0700

    wip

---
+++ llama_index/program/guidance_program.py
@@ -0,0 +1,47 @@
+from typing import TYPE_CHECKING, Any, Generic, Optional, Type, TypeVar
+
+from pydantic import BaseModel
+
+from llama_index.prompts.guidance_utils import (
+    parse_pydantic_from_guidance_program,
+    pydantic_to_guidance_output_template_markdown,
+)
+
+if TYPE_CHECKING:
+    from guidance.llms import LLM as GuidanceLLM
+
+Model = TypeVar("Model", bound=BaseModel)
+
+
+class PydanticGuidanceProgram(Generic[Model]):
+    def __init__(
+        self,
+        output_cls: Type[Model],
+        prompt_template_str: str,
+        llm: Optional["GuidanceLLM"] = None,
+        verbose: bool = False,
+    ):
+        try:
+            from guidance import Program
+            from guidance.llms import OpenAI
+        except ImportError as e:
+            raise ImportError(
+                "guidance package not found." "please run `pip install guidance`"
+            ) from e
+
+        llm = llm or OpenAI("text-davinci-003")
+        output_str = pydantic_to_guidance_output_template_markdown(output_cls)
+        full_str = prompt_template_str + "\n" + output_str
+        self._guidance_program = Program(full_str, llm=llm, silent=not verbose)
+        self._output_cls = output_cls
+
+    def __call__(
+        self,
+        **kwargs: Any,
+    ) -> Model:
+        executed_program = self._guidance_program(**kwargs)
+
+        pydantic_obj = parse_pydantic_from_guidance_program(
+            program=executed_program, cls=self._output_cls
+        )
+        return pydantic_obj
--- llama_index/question_gen/guidance_generator.py
+++ llama_index/question_gen/guidance_generator.py
@@ -2,17 +2,22 @@ from typing import TYPE_CHECKING, List, Optional, Sequence
 
 from pydantic import BaseModel
 
+from llama_index.program.guidance_program import PydanticGuidanceProgram
 from llama_index.prompts.guidance_utils import (
-    convert_to_handlebars, parse_pydantic_from_guidance_program,
-    pydantic_to_guidance_output_template_markdown)
+    convert_to_handlebars,
+    parse_pydantic_from_guidance_program,
+    pydantic_to_guidance_output_template_markdown,
+)
 
 if TYPE_CHECKING:
     from guidance import Program
     from guidance.llms import LLM
 
 from llama_index.indices.query.schema import QueryBundle
-from llama_index.question_gen.prompts import (DEFAULT_SUB_QUESTION_PROMPT_TMPL,
-                                              build_tools_text)
+from llama_index.question_gen.prompts import (
+    DEFAULT_SUB_QUESTION_PROMPT_TMPL,
+    build_tools_text,
+)
 from llama_index.question_gen.types import BaseQuestionGenerator, SubQuestion
 from llama_index.tools.types import ToolMetadata
 
@@ -28,52 +33,37 @@ class SubQuestionList(BaseModel):
 class GuidanceQuestionGenerator(BaseQuestionGenerator):
     def __init__(
         self,
-        guidance_program: "Program",
+        program: PydanticGuidanceProgram,
         verbose: bool = False,
     ) -> None:
-        self._guidance_program = guidance_program
+        self._program = program
         self._verbose = verbose
 
     @classmethod
     def from_defaults(
         cls,
         prompt_template_str: str = DEFAULT_GUIDANCE_SUB_QUESTION_PROMPT_TMPL,
-        output_str: Optional[str] = None,
         llm: Optional["LLM"] = None,
         verbose: bool = False,
     ) -> "GuidanceQuestionGenerator":
-        try:
-            from guidance import Program
-            from guidance.llms import OpenAI
-        except ImportError as e:
-            raise ImportError(
-                "guidance package not found." "please run `pip install guidance`"
-            ) from e
-
-        # construct guidance program
-        llm = llm or OpenAI("text-davinci-003")
-        output_str = output_str or pydantic_to_guidance_output_template_markdown(
-            SubQuestionList
+        program = PydanticGuidanceProgram(
+            output_cls=SubQuestionList,
+            llm=llm,
+            prompt_template_str=prompt_template_str,
+            verbose=verbose,
         )
-        full_str = prompt_template_str + "\n" + output_str
-        guidance_program = Program(full_str, llm=llm, silent=not verbose)
 
-        return cls(guidance_program, verbose)
+        return cls(program, verbose)
 
     def generate(
         self, tools: Sequence[ToolMetadata], query: QueryBundle
     ) -> List[SubQuestion]:
         tools_str = build_tools_text(tools)
         query_str = query.query_str
-        program = self._guidance_program(
+        return self._program(
             tools_str=tools_str,
             query_str=query_str,
         )
-
-        subq_list = parse_pydantic_from_guidance_program(
-            program=program, cls=SubQuestionList
-        )
-        return subq_list.sub_questions
 
     async def agenerate(
         self, tools: Sequence[ToolMetadata], query: QueryBundle
