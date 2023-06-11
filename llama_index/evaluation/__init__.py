951f48cc8f120626b78861a5312a7d27f950fe7a
Jerry Liu <jerryjliu98@gmail.com>
2023-06-11 02:05:56 -0700

    cr

--- llama_index/evaluation/__init__.py
+++ llama_index/evaluation/__init__.py
@@ -2,5 +2,11 @@
 
 from llama_index.evaluation.base import ResponseEvaluator, QueryResponseEvaluator
 from llama_index.evaluation.dataset_generation import DatasetGenerator
+from llama_index.evaluation.guideline_eval import GuidelineEvaluator
 
-__all__ = ["ResponseEvaluator", "QueryResponseEvaluator", "DatasetGenerator"]
+__all__ = [
+    "ResponseEvaluator",
+    "QueryResponseEvaluator",
+    "DatasetGenerator",
+    "GuidelineEvaluator",
+]
