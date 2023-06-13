e4156f0e530433c197de6af77feb5ba1bf525418
Logan Markewich <logan.markewich@usask.ca>
2023-06-13 15:44:50 -0600

    fix citation engine import

--- llama_index/query_engine/__init__.py
+++ llama_index/query_engine/__init__.py
@@ -1,3 +1,5 @@
+from llama_index.query_engine.citation_query_engine import CitationQueryEngine
+from llama_index.query_engine.flare.base import FLAREInstructQueryEngine
 from llama_index.query_engine.graph_query_engine import ComposableGraphQueryEngine
 from llama_index.query_engine.multistep_query_engine import MultiStepQueryEngine
 from llama_index.query_engine.retriever_query_engine import RetrieverQueryEngine
@@ -10,11 +12,10 @@ from llama_index.query_engine.router_query_engine import (
     RetrieverRouterQueryEngine,
     RouterQueryEngine,
 )
-from llama_index.query_engine.sql_vector_query_engine import SQLAutoVectorQueryEngine
 from llama_index.query_engine.sql_join_query_engine import SQLJoinQueryEngine
+from llama_index.query_engine.sql_vector_query_engine import SQLAutoVectorQueryEngine
 from llama_index.query_engine.sub_question_query_engine import SubQuestionQueryEngine
 from llama_index.query_engine.transform_query_engine import TransformQueryEngine
-from llama_index.query_engine.flare.base import FLAREInstructQueryEngine
 
 __all__ = [
     "CitationQueryEngine",
