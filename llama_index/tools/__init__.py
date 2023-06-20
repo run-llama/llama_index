b1cc3d89d7404a69467fb82c0d085d460df49b04
Jerry Liu <jerryjliu98@gmail.com>
2023-06-20 00:07:17 -0700

    cr

--- llama_index/tools/__init__.py
+++ llama_index/tools/__init__.py
@@ -3,10 +3,12 @@
 from llama_index.tools.query_engine import QueryEngineTool
 from llama_index.tools.types import BaseTool, ToolMetadata
 from llama_index.tools.function_tool import FunctionTool
+from llama_index.tools.query_plan import QueryPlanTool
 
 __all__ = [
     "BaseTool",
     "QueryEngineTool",
     "ToolMetadata",
     "FunctionTool",
+    "QueryPlanTool",
 ]
