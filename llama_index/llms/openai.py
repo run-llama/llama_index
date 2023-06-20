1e45e0831233fecb2b868e7296453c270de2379f
Simon Suo <simonsdsuo@gmail.com>
2023-06-20 16:20:17 -0700

    wip

--- llama_index/llms/openai.py
+++ llama_index/llms/openai.py
@@ -1,4 +1,4 @@
-from typing import Any, Awaitable, Dict, Sequence
+from typing import Any, Awaitable, Dict, Optional, Sequence
 
 from pydantic import BaseModel, Field
 from llama_index.llm_predictor.base import LLMMetadata
@@ -32,7 +32,7 @@ class OpenAI(LLM, BaseModel):
     model: str = Field("gpt-3.5-turbo")
     stream: bool = False
     temperature: float = 0.0
-    max_tokens: int = -1
+    max_tokens: Optional[int] = None
     additional_kwargs: Dict[str, Any] = Field(default_factory=dict)
     max_retries: int = 10
 
@@ -40,7 +40,7 @@ class OpenAI(LLM, BaseModel):
     def metadata(self) -> LLMMetadata:
         return LLMMetadata(
             context_window=openai_modelname_to_contextsize(self.model),
-            num_output=self.max_tokens,
+            num_output=self.max_tokens or -1,
         )
 
     def chat(self, messages: Sequence[Message], **kwargs: Any) -> ChatResponseType:
@@ -133,10 +133,14 @@ class OpenAI(LLM, BaseModel):
             return gen()
 
     def _complete(self, prompt: str, **kwargs: Any) -> CompletionResponseType:
-        if self.is_chat_model:
+        if self._is_chat_model:
             raise ValueError("This model is a chat model.")
 
         all_kwargs = self._get_all_kwargs(**kwargs)
+        if self.max_tokens is None:
+            # NOTE: non-chat completion endpoint requires max_tokens to be set
+            max_tokens = self._get_max_token_for_prompt(prompt)
+            all_kwargs["max_tokens"] = max_tokens
 
         if not self.stream:
             response = completion_with_retry(
@@ -169,6 +173,24 @@ class OpenAI(LLM, BaseModel):
                     )
 
             return gen()
+
+    def _get_max_token_for_prompt(self, prompt: str) -> int:
+        try:
+            import tiktoken
+        except ImportError:
+            raise ImportError(
+                "Please install tiktoken to use the max_tokens=None feature."
+            )
+        context_window = self.metadata.context_window
+        encoding = tiktoken.encoding_for_model(self.model)
+        tokens = encoding.encode(prompt)
+        max_token = context_window - len(tokens)
+        if max_token <= 0:
+            raise ValueError(
+                f"The prompt is too long for the model. "
+                f"Please use a prompt that is less than {context_window} tokens."
+            )
+        return max_token
 
     async def achat(
         self,
