5b80f3b7c355f3957b2f6a0b5ff9b3929b2a38f8
Simon Suo <simonsdsuo@gmail.com>
2023-06-20 10:56:30 -0700

    wip

--- llama_index/indices/struct_store/pandas.py
+++ llama_index/indices/struct_store/pandas.py
@@ -62,8 +62,7 @@ class PandasIndex(BaseStructStoreIndex[PandasStructTable]):
 
     def as_query_engine(self, **kwargs: Any) -> BaseQueryEngine:
         # NOTE: lazy import
-        from llama_index.query_engine.pandas_query_engine import \
-            PandasQueryEngine
+        from llama_index.query_engine.pandas_query_engine import PandasQueryEngine
 
         return PandasQueryEngine.from_index(self, **kwargs)
 
