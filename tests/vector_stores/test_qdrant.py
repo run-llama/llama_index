a16f8dcf19f83248e2e14ebaf0713eb83df4b338
Logan Markewich <logan.markewich@live.com>
2023-06-22 15:47:34 -0600

    linting

--- tests/vector_stores/test_qdrant.py
+++ tests/vector_stores/test_qdrant.py
@@ -27,6 +27,7 @@ def node_embeddings() -> List[NodeWithEmbedding]:
                 id_="c330d77f-90bd-4c51-9ed2-57d8d693b3b0",
                 relationships={
                     NodeRelationship.SOURCE: RelatedNodeInfo(node_id="test-0")
+                },
                 metadata={
                     "author": "Stephen King",
                     "theme": "Friendship",
@@ -40,6 +41,7 @@ def node_embeddings() -> List[NodeWithEmbedding]:
                 id_="c3d1e1dd-8fb4-4b8f-b7ea-7fa96038d39d",
                 relationships={
                     NodeRelationship.SOURCE: RelatedNodeInfo(node_id="test-1")
+                },
                 metadata={
                     "director": "Francis Ford Coppola",
                     "theme": "Mafia",
