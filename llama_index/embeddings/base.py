643265136f22257844475a50a738b8a8596568f2
Yi Ding <yi.s.ding@gmail.com>
2023-06-22 21:35:17 -0700

    use negation instead of inverse to avoid div 0

--- llama_index/embeddings/base.py
+++ llama_index/embeddings/base.py
@@ -37,8 +37,8 @@ def similarity(
 ) -> float:
     """Get embedding similarity."""
     if mode == SimilarityMode.EUCLIDEAN:
-        # Using 1 / euclidean distance as similarity to achieve same ordering as others
-        return 1 / float(np.linalg.norm(np.array(embedding1) - np.array(embedding2)))
+        # Using -euclidean distance as similarity to achieve same ranking order
+        return -float(np.linalg.norm(np.array(embedding1) - np.array(embedding2)))
     elif mode == SimilarityMode.DOT_PRODUCT:
         product = np.dot(embedding1, embedding2)
         return product
