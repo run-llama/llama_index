da0bfb294751b5f1089f6ebf34557260ee6ef009
NickNYU <cz739@nyu.edu>
2023-06-24 23:18:21 +0800

    [feature]add redis document store and test passed by connecting a local redis

--- llama_index/storage/docstore/redis_docstore.py
+++ llama_index/storage/docstore/redis_docstore.py
@@ -1,15 +1,17 @@
 from typing import Optional
-from llama_index.storage.index_store.keyval_index_store import KVIndexStore
+from llama_index.storage.docstore.keyval_docstore import KVDocumentStore
 from llama_index.storage.kvstore.redis_kvstore import RedisKVStore
 from redis import Redis
 
 
-class RedisIndexStore(KVIndexStore):
-    """Redis Index store.
+class RedisDocumentStore(KVDocumentStore):
+    """Redis Document (Node) store.
+
+    A Redis store for Document and Node objects.
 
     Args:
         redis_kvstore (RedisKVStore): Redis key-value store
-        namespace (str): namespace for the index store
+        namespace (str): namespace for the docstore
 
     """
 
@@ -18,16 +20,16 @@ class RedisIndexStore(KVIndexStore):
         redis_kvstore: RedisKVStore,
         namespace: Optional[str] = None,
     ) -> None:
-        """Init a RedisIndexStore."""
-        super().__init__(redis_kvstore, namespace=namespace)
+        """Init a RedisDocumentStore."""
+        super().__init__(redis_kvstore, namespace)
 
     @classmethod
     def from_redis_client(
         cls,
         redis_client: Redis,
         namespace: Optional[str] = None,
-    ) -> "RedisIndexStore":
-        """Load a RedisIndexStore from a Redis Client."""
+    ) -> "RedisDocumentStore":
+        """Load a RedisDocumentStore from a Redis Client."""
         redis_kvstore = RedisKVStore.from_redis_client(redis_client=redis_client)
         return cls(redis_kvstore, namespace)
 
@@ -37,7 +39,7 @@ class RedisIndexStore(KVIndexStore):
         host: str,
         port: int,
         namespace: Optional[str] = None,
-    ) -> "RedisIndexStore":
-        """Load a RedisIndexStore from a Redis host and port."""
+    ) -> "RedisDocumentStore":
+        """Load a RedisDocumentStore from a Redis host and port."""
         redis_kvstore = RedisKVStore.from_host_and_port(host, port)
         return cls(redis_kvstore, namespace)
---
+++ tests/storage/docstore/test_redis_docstore.py
@@ -0,0 +1,74 @@
+from typing import List
+
+import pytest
+
+from llama_index.storage.docstore.redis_docstore import RedisDocumentStore
+from llama_index.readers.schema.base import Document
+from llama_index.schema import BaseDocument
+from llama_index.storage.kvstore.redis_kvstore import RedisKVStore
+
+try:
+    from redis import Redis
+except ImportError:
+    Redis = None  # type: ignore
+
+
+@pytest.fixture
+def documents() -> List[Document]:
+    return [
+        Document("doc_1"),
+        Document("doc_2"),
+    ]
+
+
+@pytest.fixture()
+def redis_docstore(redis_kvstore: RedisKVStore) -> RedisDocumentStore:
+    return RedisDocumentStore(redis_kvstore=redis_kvstore)
+
+
+@pytest.mark.skipif(Redis is None, reason="redis not installed")
+def test_redis_docstore(
+    redis_docstore: RedisDocumentStore, documents: List[Document]
+) -> None:
+    ds = redis_docstore
+    assert len(ds.docs) == 0
+
+    # test adding documents
+    ds.add_documents(documents)
+    assert len(ds.docs) == 2
+    assert all(isinstance(doc, BaseDocument) for doc in ds.docs.values())
+
+    # test updating documents
+    ds.add_documents(documents)
+    print(ds.docs)
+    assert len(ds.docs) == 2
+
+    # test getting documents
+    doc0 = ds.get_document(documents[0].get_doc_id())
+    assert doc0 is not None
+    assert documents[0].text == doc0.text
+
+    # test deleting documents
+    ds.delete_document(documents[0].get_doc_id())
+    assert len(ds.docs) == 1
+
+
+@pytest.mark.skipif(Redis is None, reason="redis not installed")
+def test_redis_docstore_hash(
+    redis_docstore: RedisDocumentStore, documents: List[Document]
+) -> None:
+    ds = redis_docstore
+
+    # Test setting hash
+    ds.set_document_hash("test_doc_id", "test_doc_hash")
+    doc_hash = ds.get_document_hash("test_doc_id")
+    assert doc_hash == "test_doc_hash"
+
+    # Test updating hash
+    ds.set_document_hash("test_doc_id", "test_doc_hash_new")
+    doc_hash = ds.get_document_hash("test_doc_id")
+    assert doc_hash == "test_doc_hash_new"
+
+    # Test getting non-existent
+    doc_hash = ds.get_document_hash("test_not_exist")
+    assert doc_hash is None
