4340466f8e61cec273b7413253571d7c80e01538
Jerry Liu <jerryjliu98@gmail.com>
2023-06-24 20:17:54 -0700

    cr

---
+++ llama_index/output_parsers/pydantic_program.py
@@ -0,0 +1,34 @@
+"""Pydantic program output parser."""
+
+
+from pydantic import BaseModel
+from llama_index.output_parsers.base import BaseOutputParser
+from llama_index.program.base_program import Model, BasePydanticProgram
+from typing import Optional, Any, Generic
+
+
+DEFAULT_INPUT_PROMPT_TMPL = "{input_str}"
+
+
+class PydanticProgramOutputParser(BaseOutputParser, Generic[Model]):
+    """Pydantic Program output parser.
+
+    Extracts text into a schema.
+
+    """
+
+    def __init__(
+        self, pydantic_program: BasePydanticProgram[Model], input_key: str = "input_str"
+    ) -> None:
+        """Init params."""
+        self._pydantic_program = pydantic_program
+        self._input_key = input_key
+
+    def parse(self, output: str) -> Model:
+        """Parse, validate, and correct errors programmatically."""
+        input_dict = {self._input_key: output}
+        return self._pydantic_program(**input_dict)
+
+    def format(self, query: str) -> str:
+        """Format a query with structured output formatting instructions."""
+        raise NotImplementedError("`format` method not supported for Pydantic parser.")
