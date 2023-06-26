6cd378d8b5a912783d80c76db09939d0c42658f1
Simon Suo <simonsdsuo@gmail.com>
2023-06-26 14:16:33 -0700

    wip

--- llama_index/llms/langchain_utils.py
+++ llama_index/llms/langchain_utils.py
@@ -1,27 +1,86 @@
 from typing import List, Sequence
-from langchain.base_language import BaseLanguageModel
-from langchain.chat_models import ChatOpenAI
-from langchain.chat_models.base import BaseChatModel
-from langchain.llms import Cohere, AI21, OpenAI
+
+from llama_index.bridge.langchain import (
+    BaseLanguageModel,
+    ChatOpenAI,
+    BaseChatModel,
+    Cohere,
+    AI21,
+    OpenAI,
+    BaseMessage as LCMessage,
+    HumanMessage,
+    AIMessage,
+    FunctionMessage as LCFunctionMessage,
+)
 
 from llama_index.constants import AI21_J2_CONTEXT_WINDOW, COHERE_CONTEXT_WINDOW
-from llama_index.llms.base import LLMMetadata, Message
+from llama_index.llms.base import ChatMessage, LLMMetadata, Message, FunctionMessage
 from llama_index.llms.openai_utils import openai_modelname_to_contextsize
-from langchain.schema import BaseMessage as LCMessage
 
 
 def is_chat_model(llm: BaseLanguageModel):
     return isinstance(llm, BaseChatModel)
 
 
-def to_lc_messages(messages: Sequence[Message]) -> List[LCMessage]:
-    # TODO
-    return []
+def to_lc_messages(messages: Sequence[ChatMessage]) -> List[LCMessage]:
+    lc_messages = []
+    for message in messages:
+        if message.role == "user":
+            lc_messages.append(
+                HumanMessage(
+                    content=message.content, additional_kwargs=message.additional_kwargs
+                )
+            )
+        elif message.role == "assistant":
+            lc_messages.append(
+                AIMessage(
+                    content=message.content, additional_kwargs=message.additional_kwargs
+                )
+            )
+        elif message.role == "function":
+            assert isinstance(message, FunctionMessage)
+            lc_messages.append(
+                LCFunctionMessage(
+                    content=message.content,
+                    additional_kwargs=message.additional_kwargs,
+                    name=message.name,
+                )
+            )
+        else:
+            raise ValueError(f"Invalid role: {message.role}")
+    return lc_messages
 
 
 def from_lc_messages(messages: Sequence[LCMessage]) -> List[Message]:
-    # TODO
-    return []
+    messages = []
+    for lc_message in messages:
+        if isinstance(lc_message, HumanMessage):
+            messages.append(
+                ChatMessage(
+                    content=lc_message.content,
+                    additional_kwargs=lc_message.additional_kwargs,
+                    role="user",
+                )
+            )
+        elif isinstance(lc_message, AIMessage):
+            messages.append(
+                ChatMessage(
+                    content=lc_message.content,
+                    additional_kwargs=lc_message.additional_kwargs,
+                    role="assistant",
+                )
+            )
+        elif isinstance(lc_message, LCFunctionMessage):
+            messages.append(
+                FunctionMessage(
+                    content=lc_message.content,
+                    additional_kwargs=lc_message.additional_kwargs,
+                    name=lc_message.name,
+                    role="function",
+                )
+            )
+        else:
+            raise ValueError(f"Invalid message type: {type(lc_message)}")
 
 
 def get_llm_metadata(llm: BaseLanguageModel) -> LLMMetadata:
