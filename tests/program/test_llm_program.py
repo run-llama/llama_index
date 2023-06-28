b9ef76eb5dc5184cf44500ff3b8ebe17d8019636
Jerry Liu <jerryjliu98@gmail.com>
2023-06-27 23:01:21 -0700

    cr

--- tests/program/test_llm_program.py
+++ tests/program/test_llm_program.py
@@ -11,27 +11,25 @@ from llama_index.bridge.langchain import BaseLanguageModel
 import json
 
 
-def mock_llmpredictor_predict(prompt: str, **prompt_args: Any) -> str:
-    """Mock LLMPredictor predict."""
-    test_object = {"hello": "world"}
-    return json.dumps(test_object)
+class MockLLM:
+    def predict(self, prompt: str) -> str:
+        test_object = {"hello": "world"}
+        return json.dumps(test_object)
 
 
 class TestModel(BaseModel):
     hello: str
 
 
-@patch.object(BaseLanguageModel, "predict", side_effect=mock_llmpredictor_predict)
-def test_llm_program(mock_predict: Any) -> None:
+def test_llm_program() -> None:
     """Test LLM program."""
     output_parser = PydanticOutputParser(output_cls=TestModel)
     llm_program = LLMTextCompletionProgram.from_defaults(
         output_parser=output_parser,
         prompt_template_str="This is a test prompt with a {test_input}.",
+        llm=MockLLM(),
     )
     # mock llm
-    llm_program._llm = MagicMock()
-    llm_program._llm.predict = mock_llmpredictor_predict
     obj_output = llm_program(test_input="hello")
     assert isinstance(obj_output, TestModel)
     assert obj_output.hello == "world"
