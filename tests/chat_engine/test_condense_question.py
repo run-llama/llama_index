7985c11be10537db5cda14e5774d07da854dbe3b
Simon Suo <simonsdsuo@gmail.com>
2023-07-02 17:08:30 -0700

    wip

--- tests/chat_engine/test_condense_question.py
+++ tests/chat_engine/test_condense_question.py
@@ -1,7 +1,6 @@
 from unittest.mock import Mock
 
-from llama_index.chat_engine.condense_question import \
-    CondenseQuestionChatEngine
+from llama_index.chat_engine.condense_question import CondenseQuestionChatEngine
 from llama_index.indices.query.base import BaseQueryEngine
 from llama_index.indices.service_context import ServiceContext
 from llama_index.llms.base import ChatMessage, MessageRole
