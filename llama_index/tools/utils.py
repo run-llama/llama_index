d00fad566e444be982673dc0fe0481597785d409
Yi Ding <yi.s.ding@gmail.com>
2023-07-06 15:16:20 -0700

    fix required function fields

--- llama_index/tools/utils.py
+++ llama_index/tools/utils.py
@@ -1,7 +1,9 @@
 """Tool utilies."""
-from typing import Callable, Any, Optional, List, Tuple, Type
-from pydantic import BaseModel, create_model
 from inspect import signature
+from typing import Any, Callable, List, Optional, Tuple, Type
+
+from pydantic import BaseModel, create_model
+from pydantic.fields import FieldInfo
 
 
 def create_schema_from_function(
@@ -16,15 +18,27 @@ def create_schema_from_function(
     for param_name in params.keys():
         param_type = params[param_name].annotation
         param_default = params[param_name].default
-        if param_default is params[param_name].empty:
-            param_default = None
 
         if param_type is params[param_name].empty:
             param_type = Any
-        fields[param_name] = (param_type, param_default)
+
+        if param_default is params[param_name].empty:
+            fields[param_name] = (param_type, FieldInfo())
+        else:
+            fields[param_name] = (param_type, FieldInfo(default=param_default))
 
     additional_fields = additional_fields or []
-    for field_name, field_type, field_default in additional_fields:
-        fields[field_name] = (field_type, field_default)
+    for field_info in additional_fields:
+        if len(field_info) == 3:
+            field_name, field_type, field_default = field_info
+            fields[field_name] = (field_type, FieldInfo(default=field_default))
+        elif len(field_info) == 2:
+            field_name, field_type = field_info
+            fields[field_name] = (field_type, FieldInfo())
+        else:
+            raise ValueError(
+                f"Invalid additional field info: {field_info}. "
+                "Must be a tuple of length 2 or 3."
+            )
 
     return create_model(name, **fields)  # type: ignore
