bb2edb1ca1228aec54c7655112e74c8365161404
Simon Suo <simonsdsuo@gmail.com>
2023-07-07 15:38:28 -0700

    move into module

--- llama_index/agent/react/base.py
+++ llama_index/agent/react/base.py
@@ -1,103 +1,25 @@
 # ReAct agent
 
+from llama_index.agent.react.formatter import ReActChatFormatter
 from llama_index.llms.base import LLM
 from typing import Sequence, cast, Tuple
 from llama_index.tools import BaseTool
 from llama_index.agent.types import BaseAgent
-from abc import abstractmethod
 from typing import List, Optional
 from llama_index.llms.base import ChatMessage, ChatResponse, MessageRole
 from llama_index.response.schema import RESPONSE_TYPE
 from llama_index.response.schema import Response
-from llama_index.agent.react.prompts import (
-    REACT_CHAT_SYSTEM_HEADER,
-    REACT_CHAT_LAST_USER_MESSAGE,
-)
 from llama_index.agent.react.types import (
     BaseReasoningStep,
     ActionReasoningStep,
     ObservationReasoningStep,
     ResponseReasoningStep,
 )
-from pydantic import BaseModel
 from llama_index.callbacks.base import CallbackManager
 from llama_index.llms.openai import OpenAI
 
 from llama_index.agent.react.output_parser import ReActOutputParser
 from llama_index.bridge.langchain import print_text
-
-
-def get_react_tool_descriptions(tools: Sequence[BaseTool]) -> List[str]:
-    """Tool"""
-    tool_descs = []
-    for tool in tools:
-        tool_desc = (
-            f"> Tool Name: {tool.metadata.name}\n"
-            f"Tool Description: {tool.metadata.description}\n"
-            f"Tool Args: {tool.metadata.fn_schema_str}\n"
-        )
-        tool_descs.append(tool_desc)
-    return tool_descs
-
-
-# TODO: come up with better name
-# TODO: move into module
-class BaseAgentChatFormatter(BaseModel):
-    """Base chat formatter."""
-
-    tools: Sequence[BaseTool]
-
-    class Config:
-        arbitrary_types_allowed = True
-
-    @abstractmethod
-    def format(
-        self,
-        chat_history: List[ChatMessage],
-        current_reasoning: Optional[List[BaseReasoningStep]] = None,
-    ) -> List[ChatMessage]:
-        """Format chat history into list of ChatMessage."""
-
-
-class ReActChatFormatter(BaseAgentChatFormatter):
-    """ReAct chat formatter."""
-
-    system_header: str = REACT_CHAT_SYSTEM_HEADER
-    last_user_message: str = REACT_CHAT_LAST_USER_MESSAGE
-
-    def format(
-        self,
-        chat_history: List[ChatMessage],
-        current_reasoning: Optional[List[BaseReasoningStep]] = None,
-    ) -> List[ChatMessage]:
-        """Format chat history into list of ChatMessage."""
-        current_reasoning = current_reasoning or []
-        current_reasoning_str = (
-            "\n".join(r.get_content() for r in current_reasoning)
-            if current_reasoning
-            else "None"
-        )
-
-        tool_descs_str = "\n".join(get_react_tool_descriptions(self.tools))
-
-        fmt_sys_header = self.system_header.format(
-            tool_desc=tool_descs_str,
-            tool_names=", ".join([tool.metadata.get_name() for tool in self.tools]),
-        )
-        prev_chat_history = chat_history[:-1]
-        last_chat = chat_history[-1]
-
-        fmt_last_user_message = self.last_user_message.format(
-            new_message=last_chat.content,
-            current_reasoning=current_reasoning_str,
-        )
-
-        formatted_chat = [
-            ChatMessage(role=MessageRole.SYSTEM, content=fmt_sys_header),
-            *prev_chat_history,
-            ChatMessage(role=MessageRole.USER, content=fmt_last_user_message),
-        ]
-        return formatted_chat
 
 
 class ReActAgent(BaseAgent):
---
+++ llama_index/agent/react/formatter.py
@@ -0,0 +1,87 @@
+# ReAct agent formatter
+
+from typing import Sequence
+from llama_index.tools import BaseTool
+from abc import abstractmethod
+from typing import List, Optional
+from llama_index.llms.base import ChatMessage, MessageRole
+from llama_index.agent.react.prompts import (
+    REACT_CHAT_SYSTEM_HEADER,
+    REACT_CHAT_LAST_USER_MESSAGE,
+)
+from llama_index.agent.react.types import (
+    BaseReasoningStep,
+)
+from pydantic import BaseModel
+
+
+def get_react_tool_descriptions(tools: Sequence[BaseTool]) -> List[str]:
+    """Tool"""
+    tool_descs = []
+    for tool in tools:
+        tool_desc = (
+            f"> Tool Name: {tool.metadata.name}\n"
+            f"Tool Description: {tool.metadata.description}\n"
+            f"Tool Args: {tool.metadata.fn_schema_str}\n"
+        )
+        tool_descs.append(tool_desc)
+    return tool_descs
+
+
+# TODO: come up with better name
+class BaseAgentChatFormatter(BaseModel):
+    """Base chat formatter."""
+
+    tools: Sequence[BaseTool]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @abstractmethod
+    def format(
+        self,
+        chat_history: List[ChatMessage],
+        current_reasoning: Optional[List[BaseReasoningStep]] = None,
+    ) -> List[ChatMessage]:
+        """Format chat history into list of ChatMessage."""
+
+
+class ReActChatFormatter(BaseAgentChatFormatter):
+    """ReAct chat formatter."""
+
+    system_header: str = REACT_CHAT_SYSTEM_HEADER
+    last_user_message: str = REACT_CHAT_LAST_USER_MESSAGE
+
+    def format(
+        self,
+        chat_history: List[ChatMessage],
+        current_reasoning: Optional[List[BaseReasoningStep]] = None,
+    ) -> List[ChatMessage]:
+        """Format chat history into list of ChatMessage."""
+        current_reasoning = current_reasoning or []
+        current_reasoning_str = (
+            "\n".join(r.get_content() for r in current_reasoning)
+            if current_reasoning
+            else "None"
+        )
+
+        tool_descs_str = "\n".join(get_react_tool_descriptions(self.tools))
+
+        fmt_sys_header = self.system_header.format(
+            tool_desc=tool_descs_str,
+            tool_names=", ".join([tool.metadata.get_name() for tool in self.tools]),
+        )
+        prev_chat_history = chat_history[:-1]
+        last_chat = chat_history[-1]
+
+        fmt_last_user_message = self.last_user_message.format(
+            new_message=last_chat.content,
+            current_reasoning=current_reasoning_str,
+        )
+
+        formatted_chat = [
+            ChatMessage(role=MessageRole.SYSTEM, content=fmt_sys_header),
+            *prev_chat_history,
+            ChatMessage(role=MessageRole.USER, content=fmt_last_user_message),
+        ]
+        return formatted_chat
