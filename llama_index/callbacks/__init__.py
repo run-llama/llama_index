79c885f1cc4f1339e6c6ab8e3d315cb389a4625e
Alexander Song <axiomofjoy@gmail.com>
2023-07-11 14:03:43 -0700

    arize callback handler

--- llama_index/callbacks/__init__.py
+++ llama_index/callbacks/__init__.py
@@ -1,11 +1,13 @@
+from .aim import AimCallback
+from .arize_callback import ArizeCallbackHandler
 from .base import CallbackManager
 from .llama_debug import LlamaDebugHandler
-from .aim import AimCallback
 from .schema import CBEvent, CBEventType
-from .wandb_callback import WandbCallbackHandler
 from .token_counting import TokenCountingHandler
+from .wandb_callback import WandbCallbackHandler
 
 __all__ = [
+    "ArizeCallbackHandler",
     "CallbackManager",
     "CBEvent",
     "CBEventType",
