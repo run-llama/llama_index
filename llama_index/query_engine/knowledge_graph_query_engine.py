493dc84d16bfec7799537f41cd2022e4110f2a73
Wey Gu <weyl.gu@gmail.com>
2023-07-11 18:20:29 +0800

    lint: fix E501 Line too long

--- llama_index/query_engine/knowledge_graph_query_engine.py
+++ llama_index/query_engine/knowledge_graph_query_engine.py
@@ -29,14 +29,16 @@ Schema:
 Note: NebulaGraph speaks a dialect of Cypher, comparing to standard Cypher:
 
 1. it uses double equals sign for comparison: `==` rather than `=`
-2. it needs explicit label specification when referring to node properties, i.e. v is a variable of a node, and we know its label is Foo, v.`foo`.name is correct while v.name is not.
+2. it needs explicit label specification when referring to node properties, i.e.
+v is a variable of a node, and we know its label is Foo, v.`foo`.name is correct
+while v.name is not.
 
 For example, see this diff between standard and NebulaGraph Cypher dialect:
 ```diff
-< MATCH (p:person)-[:directed]->(m:movie) WHERE m.name = 'The Godfather II'
+< MATCH (p:person)-[:directed]->(m:movie) WHERE m.name = 'The Godfather'
 < RETURN p.name;
 ---
-> MATCH (p:`person`)-[:directed]->(m:`movie`) WHERE m.`movie`.`name` == 'The Godfather II'
+> MATCH (p:`person`)-[:directed]->(m:`movie`) WHERE m.`movie`.`name` == 'The Godfather'
 > RETURN p.`person`.`name`;
 ```
 
@@ -55,7 +57,8 @@ DEFAULT_NL2GRAPH_PROMPT_MAP = {
 
 DEFAULT_KG_RESPONSE_ANSWER_PROMPT_TMPL = """
 The original question is given below.
-This question has been translated into a Graph Database query. Both the Graph query and the response are given below.
+This question has been translated into a Graph Database query.
+Both the Graph query and the response are given below.
 Given the Graph Query response, synthesise a response to the original question.
 
 Original question: {query_str}
