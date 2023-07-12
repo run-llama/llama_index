942a35b3b852fb224095754540663895bf9ec7e8
Logan Markewich <logan.markewich@live.com>
2023-07-11 20:08:15 -0600

    add from_llm function

--- llama_index/memory/chat_memory_buffer.py
+++ llama_index/memory/chat_memory_buffer.py
@@ -1,17 +1,35 @@
 from pydantic import Field
-from typing import List, Callable
+from typing import Callable, List, Optional
 
-from llama_index.llms.base import ChatMessage
+from llama_index.llms.base import ChatMessage, LLM
 from llama_index.memory.types import BaseMemory
 from llama_index.utils import GlobalsHelper
+
+DEFUALT_TOKEN_LIMIT_RATIO = 0.75
 
 
 class ChatMemoryBuffer(BaseMemory):
     """Simple buffer for storing chat history."""
 
+    token_limit: int
     chat_history: List[ChatMessage] = Field(default_factory=list)
-    token_limit: int = 3000
     tokenizer_fn: Callable[[str], List] = Field(default=GlobalsHelper.tokenizer)
+
+    @classmethod
+    def from_llm(
+        cls,
+        llm: LLM,
+        token_limit: Optional[int] = None,
+        tokenizer_fn: Optional[Callable[[str], List]] = None,
+    ) -> "ChatMemoryBuffer":
+        """Create a chat memory buffer from an LLM."""
+        context_window = llm.metadata.context_window
+        token_limit = token_limit or int(context_window * DEFUALT_TOKEN_LIMIT_RATIO)
+
+        return cls(
+            token_limit=token_limit,
+            tokenizer_fn=tokenizer_fn or GlobalsHelper.tokenizer,
+        )
 
     def get(self) -> List[ChatMessage]:
         """Get chat history."""
