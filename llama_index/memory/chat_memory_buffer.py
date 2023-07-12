5e55a306432e2075290929066c90caf4651a2847
Logan Markewich <logan.markewich@live.com>
2023-07-11 20:51:12 -0600

    improve interface

--- llama_index/memory/chat_memory_buffer.py
+++ llama_index/memory/chat_memory_buffer.py
@@ -6,29 +6,35 @@ from llama_index.memory.types import BaseMemory
 from llama_index.utils import GlobalsHelper
 
 DEFUALT_TOKEN_LIMIT_RATIO = 0.75
+DEFAULT_TOKEN_LIMIT = 3000
 
 
 class ChatMemoryBuffer(BaseMemory):
     """Simple buffer for storing chat history."""
 
     token_limit: int
+    tokenizer_fn: Callable[[str], List]
     chat_history: List[ChatMessage] = Field(default_factory=list)
-    tokenizer_fn: Callable[[str], List] = Field(default=GlobalsHelper.tokenizer)
 
     @classmethod
-    def from_llm(
+    def from_defaults(
         cls,
-        llm: LLM,
+        chat_history: Optional[List[ChatMessage]] = None,
+        llm: Optional[LLM] = None,
         token_limit: Optional[int] = None,
         tokenizer_fn: Optional[Callable[[str], List]] = None,
     ) -> "ChatMemoryBuffer":
         """Create a chat memory buffer from an LLM."""
-        context_window = llm.metadata.context_window
-        token_limit = token_limit or int(context_window * DEFUALT_TOKEN_LIMIT_RATIO)
+        if llm is not None:
+            context_window = llm.metadata.context_window
+            token_limit = token_limit or int(context_window * DEFUALT_TOKEN_LIMIT_RATIO)
+        elif token_limit is None:
+            token_limit = DEFAULT_TOKEN_LIMIT
 
         return cls(
             token_limit=token_limit,
-            tokenizer_fn=tokenizer_fn or GlobalsHelper.tokenizer,
+            tokenizer_fn=tokenizer_fn or GlobalsHelper().tokenizer,
+            chat_history=chat_history or [],
         )
 
     def get(self) -> List[ChatMessage]:
@@ -52,6 +58,14 @@ class ChatMemoryBuffer(BaseMemory):
 
         return self.chat_history[:message_count]
 
+    def get_all(self) -> List[ChatMessage]:
+        """Get all chat history."""
+        return self.chat_history
+
     def put(self, message: ChatMessage) -> None:
         """Put chat history."""
         self.chat_history.append(message)
+
+    def reset(self) -> None:
+        """Reset chat history."""
+        return self.chat_history.clear()
--- llama_index/memory/types.py
+++ llama_index/memory/types.py
@@ -1,12 +1,22 @@
 from abc import abstractmethod
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
-from llama_index.llms.base import ChatMessage
+from llama_index.llms.base import ChatMessage, LLM
 
 
 class BaseMemory(BaseModel):
     """Base class for all memory types."""
+
+    @classmethod
+    @abstractmethod
+    def from_defaults(
+        cls,
+        chat_history: Optional[List[ChatMessage]] = None,
+        llm: Optional[LLM] = None,
+    ) -> "BaseMemory":
+        """Create a chat memory from defualts."""
+        pass
 
     @abstractmethod
     def get(self) -> List[ChatMessage]:
@@ -14,6 +24,16 @@ class BaseMemory(BaseModel):
         pass
 
     @abstractmethod
+    def get_all(self) -> List[ChatMessage]:
+        """Get all chat history."""
+        pass
+
+    @abstractmethod
     def put(self, message: ChatMessage) -> None:
         """Put chat history."""
+        pass
+
+    @abstractmethod
+    def reset(self) -> None:
+        """Reset chat history."""
         pass
