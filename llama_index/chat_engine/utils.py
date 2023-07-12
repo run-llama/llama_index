f00dbbc7283e60243866cd944c5cabfd7f5dbf39
Jerry Liu <jerryjliu98@gmail.com>
2023-07-12 00:15:43 -0700

    cr

--- llama_index/chat_engine/utils.py
+++ llama_index/chat_engine/utils.py
@@ -1,13 +1,18 @@
 from typing import Generator
 
-from llama_index.llms.base import ChatMessage, MessageRole, ChatResponse
+from llama_index.llms.base import (
+    ChatMessage,
+    MessageRole,
+    ChatResponse,
+    ChatResponseGen,
+)
 from llama_index.memory import BaseMemory
 from llama_index.types import TokenGen
 
 
 def response_gen_with_chat_history(
     message: str, memory: BaseMemory, response_gen: TokenGen
-) -> Generator[ChatResponse, None, None]:
+) -> ChatResponseGen:
     response_str = ""
     for token in response_gen:
         response_str += token
