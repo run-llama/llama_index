a317988431b77fc56cecb9b8959e8dae21a5b838
Raynor Chavez <raynorkirksonchavez@gmail.com>
2023-07-14 14:25:09 +0800

    modified tests

--- tests/vector_stores/test_marqo.py
+++ tests/vector_stores/test_marqo.py
@@ -4,7 +4,10 @@ from llama_index.schema import TextNode
 from llama_index.vector_stores.types import VectorStoreQuery
 from llama_index.vector_stores.marqo import MarqoVectorStore
 from llama_index.indices.vector_store.marqo_index import MarqoVectorStoreIndex
+from llama_index.vector_stores.types import NodeWithEmbedding
+from llama_index.data_structs.data_structs import IndexDict
 from llama_index.readers.marqo import MarqoReader
+from marqo import Client
 
 try:
     import marqo
@@ -16,85 +19,87 @@ except (ImportError, Exception):
 
 
 @pytest.fixture
-def marqo_client():
-    return marqo.Client(url="http://localhost:8882")
+def marqo_client() -> Client:
+    return Client(url="http://localhost:8882", api_key="foobar")
 
 
 @pytest.fixture
-def marqo_vector_store(marqo_client):
+def marqo_vector_store(marqo_client: Client) -> MarqoVectorStore:
     return MarqoVectorStore(index_name="test", marqo_client=marqo_client)
 
 
 @pytest.mark.skipif(marqo_not_available, reason="marqo is not available")
-def test_add_documents(marqo_vector_store):
+def test_add_documents(
+    marqo_vector_store: MarqoVectorStore, marqo_client: Client
+) -> None:
     # Add some documents to the Marqo vector store
     documents = [
-        ("doc1", "This is a test document about cats."),
-        ("doc2", "This is a test document about dogs."),
+        NodeWithEmbedding(
+            node=TextNode(id_="doc1", text="This is a test document about cats."),
+            embedding=[],
+        ),
+        NodeWithEmbedding(
+            node=TextNode(id_="doc2", text="This is a test document about dogs."),
+            embedding=[],
+        ),
     ]
-    ids = marqo_vector_store.add(documents)
+    ids = marqo_vector_store.add(documents=documents)
 
     # Check that the returned IDs match the ones we provided
-    print(ids)
-    assert ids == [doc_id for doc_id, _ in documents]
+    assert ids == [doc.node.node_id for doc in documents]
 
 
 @pytest.mark.skipif(marqo_not_available, reason="marqo is not available")
-def test_query(marqo_vector_store):
+def test_query(marqo_vector_store: MarqoVectorStore) -> None:
     # Add some documents to the Marqo vector store
     documents = [
-        ("doc1", "This is a test document about cats."),
-        ("doc2", "This is a test document about dogs."),
+        NodeWithEmbedding(
+            node=TextNode(id_="doc1", text="This is a test document about cats."),
+            embedding=[],
+        ),
+        NodeWithEmbedding(
+            node=TextNode(id_="doc2", text="This is a test document about dogs."),
+            embedding=[],
+        ),
     ]
-    marqo_vector_store.add(documents)
+    marqo_vector_store.add(documents=documents)
 
     # Query the Marqo vector store
     query = VectorStoreQuery(query_str="cats")
     result = marqo_vector_store.query(query)
-    print(result)
 
     # Check that the result contains the ID of the document about cats
-    assert "doc1" in result.ids
+    assert "doc1" in (result.ids or [])
 
 
 @pytest.mark.skipif(marqo_not_available, reason="marqo is not available")
-def test_add_nodes_to_index(marqo_vector_store):
-    # print("test1:", type(marqo_vector_store))  # This line should print "<class 'MarqoVectorStore'>"
-
+def test_add_nodes_to_index(marqo_vector_store: MarqoVectorStore) -> None:
     # Create a custom storage context with MarqoVectorStore
     storage_context = StorageContext.from_defaults(vector_store=marqo_vector_store)
 
     # Create some nodes to add to the index
     nodes = [
-        TextNode(text="This is a test document about cats.", id_="doc1"),
-        TextNode(text="This is a test document about dogs.", id_="doc2"),
+        TextNode(text="This is a test document about cats.", id_="doc1", embedding=[]),
+        TextNode(text="This is a test document about dogs.", id_="doc2", embedding=[]),
     ]
 
     marqo_vector_store_index = MarqoVectorStoreIndex(
         storage_context=storage_context, nodes=nodes
     )
 
-    print(
-        "test2:", type(marqo_vector_store_index)
-    )  # This line should print "<class 'MarqoVectorStoreIndex'>"
-    print(
-        "test3:", type(marqo_vector_store_index._vector_store)
-    )  # This line should print "<class 'MarqoVectorStore'>"
-
     # Add the nodes to the index
-    marqo_vector_store_index._add_nodes_to_index({}, nodes)
+    marqo_vector_store_index._add_nodes_to_index(IndexDict(), nodes)
 
     # Query the Marqo vector store to check that the documents were added
     query = VectorStoreQuery(query_str="cats")
     result = marqo_vector_store.query(query)
-    print(result)
-    assert "doc1" in result.ids
+    assert "doc1" in (result.ids or [])
 
 
 @pytest.mark.skipif(marqo_not_available, reason="marqo is not available")
-def test_load_data():
+def test_load_data() -> None:
     # Initialize MarqoReader
-    marqo_reader = MarqoReader(url="http://localhost:8882")
+    marqo_reader = MarqoReader(api_key="foobar", url="http://localhost:8882")
 
     # Define the index name and id-to-text map
     index_name = "test"
