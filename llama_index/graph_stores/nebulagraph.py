8fe2739706349908ad6be5b55d1b616e940bc192
Wey Gu <weyl.gu@gmail.com>
2023-07-19 04:10:27 +0800

    chore: fixed escape caused spacing in head and tail (#6950)

--- llama_index/graph_stores/nebulagraph.py
+++ llama_index/graph_stores/nebulagraph.py
@@ -70,6 +70,8 @@ def escape_str(value: str) -> str:
     for pattern in patterns:
         if pattern in value:
             value = value.replace(pattern, patterns[pattern])
+    if value[0] == " " or value[-1] == " ":
+        value = value.strip()
 
     return value
 
