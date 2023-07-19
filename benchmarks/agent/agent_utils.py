2e5c4c5aba72f59fd9000130f1374266d79b5e5a
Simon Suo <simonsdsuo@gmail.com>
2023-07-19 01:34:10 -0700

    wip

--- benchmarks/agent/agent_utils.py
+++ benchmarks/agent/agent_utils.py
@@ -4,6 +4,8 @@ from llama_index.agent import OpenAIAgent, ReActAgent
 from llama_index.agent.types import BaseAgent
 from llama_index.llms import Anthropic, OpenAI
 from llama_index.llms.base import LLM
+from llama_index.llms.llama_utils import messages_to_prompt
+from llama_index.llms.replicate import Replicate
 
 OPENAI_MODELS = [
     "text-davinci-003",
@@ -11,6 +13,11 @@ OPENAI_MODELS = [
     "gpt-4-0613",
 ]
 ANTHROPIC_MODELS = ["claude-instant-1", "claude-2"]
+LLAMA_MODELS = [
+    "llama13b-v2-chat",
+    "llama70b-v2-chat",
+]
+REPLICATE_MODELS = []
 ALL_MODELS = OPENAI_MODELS + ANTHROPIC_MODELS
 
 AGENTS: Dict[str, Type[BaseAgent]] = {
@@ -25,6 +32,18 @@ def get_model(model: str) -> LLM:
         llm = OpenAI(model=model)
     elif model in ANTHROPIC_MODELS:
         llm = Anthropic(model=model)
+    elif model in LLAMA_MODELS:
+        model_dict = {
+            "llama13b-v2-chat": "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+            "llama70b-v2-chat": "replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48",
+        }
+        replicate_model = model_dict[model]
+        llm = Replicate(
+            model=replicate_model,
+            temperature=0.01,
+            max_tokens=4096,  # override max tokens since it's interpreted as context window instead of max tokens
+            messages_to_prompt=messages_to_prompt,  # override message representation for llama 2
+        )
     else:
         raise ValueError(f"Unknown model {model}")
     return llm
