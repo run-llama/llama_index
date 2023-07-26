f4248afba9bccd0eca3e3b89e82032a749765989
Simon Suo <simonsdsuo@gmail.com>
2023-07-25 23:06:46 -0700

    Fix Azure function calling bug (#7041)

--- llama_index/llms/openai_utils.py
+++ llama_index/llms/openai_utils.py
@@ -231,11 +231,12 @@ def to_openai_message_dicts(messages: Sequence[ChatMessage]) -> List[dict]:
 def from_openai_message_dict(message_dict: dict) -> ChatMessage:
     """Convert openai message dict to generic message."""
     role = message_dict["role"]
-    content = message_dict["content"]
+    # NOTE: Azure OpenAI returns function calling messages without a content key
+    content = message_dict.get("content", None)
 
     additional_kwargs = message_dict.copy()
     additional_kwargs.pop("role")
-    additional_kwargs.pop("content")
+    additional_kwargs.pop("content", None)
 
     return ChatMessage(role=role, content=content, additional_kwargs=additional_kwargs)
 
