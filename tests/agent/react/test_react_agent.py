d63a28ca63123ef27085eb8fb792654dad29c674
Simon Suo <simonsdsuo@gmail.com>
2023-08-01 21:12:30 -0700

    wip

--- tests/agent/react/test_react_agent.py
+++ tests/agent/react/test_react_agent.py
@@ -23,6 +23,8 @@ class MockChatLLM(MockLLM):
         self._i = 0  # call counter, determines which response to return
         self._responses = responses  # list of responses to return
 
+        super().__init__()
+
     def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
         del messages  # unused
         response = ChatResponse(
