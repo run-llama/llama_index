c72b52ea35719c5d792513a1e39f15094e3b240b
Jerry Liu <jerryjliu98@gmail.com>
2023-08-02 00:17:38 -0400

    cr

---
+++ llama_index/callbacks/global_eval_handlers.py
@@ -0,0 +1,27 @@
+"""Global eval handlers."""
+
+from llama_index.callbacks.base_handler import BaseCallbackHandler
+from llama_index.callbacks.wandb_callback import WandbCallbackHandler
+from typing import Any
+
+
+def set_global_eval_handler(eval_mode: str, **eval_params: Any) -> None:
+    """Set global eval handlers."""
+
+    import llama_index
+
+    llama_index.global_eval_handler = create_global_eval_handler(
+        eval_mode, **eval_params
+    )
+
+
+def create_global_eval_handler(
+    eval_mode: str, **eval_params: Any
+) -> BaseCallbackHandler:
+    """Get global eval handler."""
+    if eval_mode == "wandb":
+        handler: BaseCallbackHandler = WandbCallbackHandler(**eval_params)
+    else:
+        raise ValueError(f"Eval mode {eval_mode} not supported.")
+
+    return handler
