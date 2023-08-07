9a941f217cc4793ae934373e04b889bc75057f7c
Simon Suo <simonsdsuo@gmail.com>
2023-08-07 11:54:34 -0700

    wip

--- llama_index/text_splitter/token_splitter.py
+++ llama_index/text_splitter/token_splitter.py
@@ -1,4 +1,5 @@
 """Token splitter."""
+import logging
 from typing import Callable, List, Optional
 
 from llama_index.callbacks.base import CallbackManager
@@ -6,6 +7,8 @@ from llama_index.callbacks.schema import CBEventType, EventPayload
 from llama_index.constants import DEFAULT_CHUNK_OVERLAP, DEFAULT_CHUNK_SIZE
 from llama_index.text_splitter.types import TextSplitter
 from llama_index.utils import globals_helper
+
+_logger = logging.getLogger(__name__)
 
 
 class TokenTextSplitter(TextSplitter):
@@ -33,90 +36,17 @@ class TokenTextSplitter(TextSplitter):
         self._backup_separators = backup_separators
         self.callback_manager = callback_manager or CallbackManager([])
 
-    def _reduce_chunk_size(
-        self, start_idx: int, cur_idx: int, splits: List[str]
-    ) -> int:
-        """Reduce the chunk size by reducing cur_idx.
-
-        Return the new cur_idx.
-
-        """
-        current_doc_total = len(
-            self.tokenizer(self._separator.join(splits[start_idx:cur_idx]))
-        )
-        while current_doc_total > self._chunk_size:
-            percent_to_reduce = (
-                current_doc_total - self._chunk_size
-            ) / current_doc_total
-            num_to_reduce = int(percent_to_reduce * (cur_idx - start_idx)) + 1
-            cur_idx -= num_to_reduce
-            current_doc_total = len(
-                self.tokenizer(self._separator.join(splits[start_idx:cur_idx]))
-            )
-        return cur_idx
-
-    def _preprocess_splits(self, splits: List[str], chunk_size: int) -> List[str]:
-        """Process splits.
-
-        Specifically search for tokens that are too large for chunk size,
-        and see if we can separate those tokens more
-        (via backup separators if specified, or force chunking).
-
-        """
-        new_splits = []
-        for split in splits:
-            num_cur_tokens = len(self.tokenizer(split))
-            if num_cur_tokens <= chunk_size:
-                new_splits.append(split)
-            else:
-                cur_splits = [split]
-                if self._backup_separators:
-                    for sep in self._backup_separators:
-                        if sep in split:
-                            cur_splits = split.split(sep)
-                            break
-                else:
-                    cur_splits = [split]
-
-                cur_splits2 = []
-                for cur_split in cur_splits:
-                    num_cur_tokens = len(self.tokenizer(cur_split))
-                    if num_cur_tokens <= chunk_size:
-                        cur_splits2.extend([cur_split])
-                    else:
-                        # split cur_split according to chunk size of the token numbers
-                        cur_split_chunks = []
-                        end_idx = len(cur_split)
-                        while len(self.tokenizer(cur_split[0:end_idx])) > chunk_size:
-                            for i in range(1, end_idx):
-                                tmp_split = cur_split[0 : end_idx - i]
-                                if len(self.tokenizer(tmp_split)) <= chunk_size:
-                                    cur_split_chunks.append(tmp_split)
-                                    cur_split = cur_split[end_idx - i : end_idx]
-                                    end_idx = len(cur_split)
-                                    break
-                        cur_split_chunks.append(cur_split)
-                        cur_splits2.extend(cur_split_chunks)
-
-                new_splits.extend(cur_splits2)
-        return new_splits
-
-    def _postprocess_splits(self, docs: List[str]) -> List[str]:
-        """Post-process splits."""
-        # TODO: prune text splits, remove empty spaces
-        new_docs = []
-        for doc in docs:
-            if doc.replace(" ", "") == "":
-                continue
-            new_docs.append(doc)
-        return new_docs
-
     def split_text(self, text: str) -> List[str]:
-        """Split incoming text and return chunks."""
+        """Split text into chunks."""
+        if text == "":
+            return []
+
         with self.callback_manager.event(
             CBEventType.CHUNKING, payload={EventPayload.CHUNKS: [text]}
         ) as event:
-            chunks = self._split_text(text)
+
+            splits = self._split(text)
+            chunks = self._merge(splits)
 
             event.on_end(
                 payload={EventPayload.CHUNKS: chunks},
@@ -124,89 +54,92 @@ class TokenTextSplitter(TextSplitter):
 
         return chunks
 
-    def _split_text(self, text: str) -> List[str]:
-        """Split incoming text and return chunks with overlap size."""
-        if text == "":
-            return []
+    def _split(self, text: str) -> List[str]:
+        """Break text into splits that are smaller than chunk size.
 
-        with self.callback_manager.event(
-            CBEventType.CHUNKING, payload={EventPayload.CHUNKS: [text]}
-        ) as event:
-            # First we naively split the large input into a bunch of smaller ones.
+        The order of splitting is:
+        1. split by separator
+        2. split by backup separators (if any)
+        3. split by characters
+
+        """
+        done_splitting = False
+        if self._separator in text:
+            # split by main separator
             splits = text.split(self._separator)
-            splits = self._preprocess_splits(splits, self._chunk_size)
-            # We now want to combine these smaller pieces into medium size
-            # chunks to send to the LLM.
-            docs: List[str] = []
+            done_splitting = True
 
-            start_idx = 0
-            cur_idx = 0
-            cur_total = 0
-            prev_idx = 0  # store the previous end index
-            while cur_idx < len(splits):
-                cur_token = splits[cur_idx]
-                num_cur_tokens = max(len(self.tokenizer(cur_token)), 1)
-                if num_cur_tokens > self._chunk_size:
-                    raise ValueError(
-                        "A single term is larger than the allowed chunk size.\n"
-                        f"Term size: {num_cur_tokens}\n"
-                        f"Chunk size: {self._chunk_size}"
-                        f"Effective chunk size: {self._chunk_size}"
-                    )
-                # If adding token to current_doc would exceed the chunk size:
-                # 1. First verify with tokenizer that current_doc
-                # 1. Update the docs list
-                if cur_total + num_cur_tokens > self._chunk_size:
-                    # NOTE: since we use a proxy for counting tokens, we want to
-                    # run tokenizer across all of current_doc first. If
-                    # the chunk is too big, then we will reduce text in pieces
-                    cur_idx = self._reduce_chunk_size(start_idx, cur_idx, splits)
-                    overlap = 0
-                    # after first round, check if last chunk
-                    # ended after this chunk begins
-                    if prev_idx > 0 and prev_idx > start_idx:
-                        overlap = sum(
-                            [len(splits[i]) for i in range(start_idx, prev_idx)]
-                        )
+        if not done_splitting and self._backup_separators is not None:
+            # split by any backup separators
+            for sep in self._backup_separators:
+                if sep in text:
+                    splits = text.split(sep)
+                    done_splitting = True
+                    break
 
-                    docs.append(self._separator.join(splits[start_idx:cur_idx]))
-                    prev_idx = cur_idx
-                    # 2. Shrink the current_doc (from the front) until it is gets
-                    # smaller than the overlap size
-                    # NOTE: because counting tokens individually is an imperfect
-                    # proxy (but much faster proxy) for the total number of tokens
-                    # consumed, we need to enforce that start_idx <= cur_idx, otherwise
-                    # start_idx has a chance of going out of bounds.
-                    while cur_total > self._chunk_overlap and start_idx < cur_idx:
-                        # # call tokenizer on entire overlap
-                        # cur_total = self.tokenizer()
-                        cur_num_tokens = max(len(self.tokenizer(splits[start_idx])), 1)
-                        cur_total -= cur_num_tokens
-                        start_idx += 1
-                    # NOTE: This is a hack, make more general
-                    if start_idx == cur_idx:
-                        cur_total = 0
-                # Build up the current_doc with term d, and update the total counter
-                # with the number of the number of tokens in d, wrt self.tokenizer
+        if not done_splitting:
+            # split by characters
+            splits = list(text)
 
-                # we reassign cur_token and num_cur_tokens, because cur_idx
-                # may have changed
-                cur_token = splits[cur_idx]
-                num_cur_tokens = max(len(self.tokenizer(cur_token)), 1)
+        new_splits = []
+        for split in splits:
+            split_len = len(self.tokenizer(split))
+            if split_len <= self._chunk_size:
+                new_splits.append(split)
+            else:
+                # recursively split
+                new_splits.extend(self._split(split))
+        return new_splits
 
-                cur_total += num_cur_tokens
-                cur_idx += 1
-            overlap = 0
-            # after first round, check if last chunk ended after this chunk begins
-            if prev_idx > start_idx:
-                overlap = sum(
-                    [len(splits[i]) for i in range(start_idx, prev_idx)]
-                ) + len(range(start_idx, prev_idx))
-            docs.append(self._separator.join(splits[start_idx:cur_idx]))
+    def _merge(self, splits: List[str]) -> List[str]:
+        """Merge splits into chunks.
 
-            # run postprocessing to remove blank spaces
-            docs = self._postprocess_splits(docs)
+        The high-level idea is to keep adding splits to a chunk until we
+        exceed the chunk size, then we start a new chunk with overlap.
 
-            event.on_end(payload={EventPayload.CHUNKS: docs})
+        When we start a new chunk, we pop off the first element of the previous
+        chunk until the total length is less than the chunk size.
+        """
+        sep_len = len(self.tokenizer(self._separator))
+        chunks: List[str] = []
 
-        return docs
+        cur_chunk: List[str] = []
+        cur_len = 0
+        for split in splits:
+            split_len = len(self.tokenizer(split))
+            if split_len > self._chunk_size:
+                _logger.warning(
+                    f"Got a split of size {split_len}, ",
+                    f"larger than chunk size {self._chunk_size}.",
+                )
+
+            added_len = split_len + (sep_len if len(cur_chunk) > 0 else 0)
+            # if we exceed the chunk size after adding the new split, then
+            # we need to end the current chunk and start a new one
+            if cur_len + added_len > self._chunk_size:
+                # end the previous chunk
+                chunk = self._separator.join(cur_chunk).strip()
+                if chunk:
+                    chunks.append(chunk)
+
+                # start a new chunk with overlap
+                # keep popping off the first element of the previous chunk until:
+                #   1. the current chunk length is less than chunk overlap
+                #   2. the total length is less than chunk size
+                while (
+                    cur_len > self._chunk_overlap
+                    or cur_len + added_len > self._chunk_size
+                ):
+                    # pop off the first element
+                    first_chunk = cur_chunk.pop(0)
+                    cur_len -= len(self.tokenizer(first_chunk)) + sep_len
+
+            cur_chunk.append(split)
+            cur_len += added_len
+
+        # handle the last chunk
+        chunk = self._separator.join(cur_chunk).strip()
+        if chunk:
+            chunks.append(chunk)
+
+        return chunks
