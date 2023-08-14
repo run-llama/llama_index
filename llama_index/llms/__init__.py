fb8e75288208ce87e74c150c6533a73958897519
vikasqblocks <vikas@qblocks.cloud>
2023-08-14 19:15:55 +0530

    Add monsterapi Monster LLM

--- llama_index/llms/__init__.py
+++ llama_index/llms/__init__.py
@@ -1,21 +1,17 @@
 from llama_index.llms.anthropic import Anthropic
 from llama_index.llms.azure_openai import AzureOpenAI
-from llama_index.llms.base import (
-    ChatMessage,
-    ChatResponse,
-    ChatResponseAsyncGen,
-    ChatResponseGen,
-    CompletionResponse,
-    CompletionResponseAsyncGen,
-    CompletionResponseGen,
-    LLMMetadata,
-    MessageRole,
-)
+from llama_index.llms.base import (ChatMessage, ChatResponse,
+                                   ChatResponseAsyncGen, ChatResponseGen,
+                                   CompletionResponse,
+                                   CompletionResponseAsyncGen,
+                                   CompletionResponseGen, LLMMetadata,
+                                   MessageRole)
 from llama_index.llms.custom import CustomLLM
 from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.llms.langchain import LangChainLLM
 from llama_index.llms.llama_cpp import LlamaCPP
 from llama_index.llms.mock import MockLLM
+from llama_index.llms.monsterapi import MonsterLLM
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.palm import PaLM
 from llama_index.llms.predibase import PredibaseLLM
@@ -44,4 +40,5 @@ __all__ = [
     "CompletionResponseAsyncGen",
     "LLMMetadata",
     "Xinference",
+    "MonsterLLM"
 ]
---
+++ llama_index/llms/monsterapi.py
@@ -0,0 +1,76 @@
+from typing import Any, Callable, Dict, Optional, Sequence
+
+from llama_index.callbacks import CallbackManager
+from llama_index.constants import DEFAULT_CONTEXT_WINDOW, DEFAULT_NUM_OUTPUTS
+from llama_index.llms.base import (ChatMessage, ChatResponse,
+                                   CompletionResponse, LLMMetadata,
+                                   llm_chat_callback, llm_completion_callback)
+from llama_index.llms.custom import CustomLLM
+from llama_index.llms.generic_utils import \
+    messages_to_prompt as generic_messages_to_prompt
+
+
+class MonsterLLM(CustomLLM):
+    def __init__(
+        self,
+        model: str,
+        temperature: float = 0.75,
+        additional_kwargs: Optional[Dict[str, Any]] = None,
+        context_window: int = DEFAULT_CONTEXT_WINDOW,
+        prompt_key: str = "prompt",
+        messages_to_prompt: Optional[Callable] = None,
+        callback_manager: Optional[CallbackManager] = None,
+    ) -> None:
+        self._model = model
+        self._context_window = context_window
+        self._prompt_key = prompt_key
+        self._messages_to_prompt = messages_to_prompt or generic_messages_to_prompt
+        self.callback_manager = callback_manager or CallbackManager([])
+
+        # model kwargs
+        self._temperature = temperature
+        self._additional_kwargs = additional_kwargs or {}
+
+    @property
+    def metadata(self) -> LLMMetadata:
+        """LLM metadata."""
+        return LLMMetadata(
+            context_window=self._context_window,
+            num_output=DEFAULT_NUM_OUTPUTS,
+            model_name=self._model,
+        )
+
+    def _get_input_dict(self, prompt: str, **kwargs: Any) -> Dict[str, Any]:
+        return {
+            self._prompt_key: prompt,
+            "temperature": self._temperature,
+            **self._additional_kwargs,
+            **kwargs,
+        }
+
+    @llm_chat_callback()
+    def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
+        prompt = self._messages_to_prompt(messages)
+        completion_response = self.complete(prompt, **kwargs)
+        return completion_response
+
+    @llm_completion_callback()
+    def complete(self, prompt: str, **kwargs: Any) -> CompletionResponse:
+        try:
+            from monsterapi import MODEL_TYPES
+            from monsterapi import client as MonsterClient
+        except ImportError:
+            raise ImportError(
+                "Could not import Monster API client library."
+                "Please install it with `pip install monsterapi`"
+            )
+        llm_models_enabled = [i for i,j in MODEL_TYPES.items() if j == "LLM"]
+        if self._model not in  llm_models_enabled:
+            raise RuntimeError(f"Model: {self._model} is not supported. Please update monsterapiclient to see if any models are added. pip install --upgrade monsterapi")
+        input_dict = self._get_input_dict(prompt, **kwargs)
+        monster_client = MonsterClient()
+
+        response = monster_client.get_response(model=self._model, data=input_dict)
+        process_id = response['process_id']
+        result = monster_client.wait_and_get_result(process_id)
+        return CompletionResponse(text=result['text'])
