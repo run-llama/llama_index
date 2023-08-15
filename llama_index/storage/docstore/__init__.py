a6305c47d64b6501d9b319ba2da75faa46227532
ntflix <felix@iron59.co.uk>
2023-08-15 21:27:28 +0100

    add CosmosDBDoc/IndexStore to parent packages

--- llama_index/storage/docstore/__init__.py
+++ llama_index/storage/docstore/__init__.py
@@ -1,18 +1,21 @@
-from llama_index.storage.docstore.types import BaseDocumentStore
-from llama_index.storage.docstore.simple_docstore import SimpleDocumentStore
-from llama_index.storage.docstore.mongo_docstore import MongoDocumentStore
+from llama_index.storage.docstore.cosmosdb_docstore import CosmosDBDocStore
 from llama_index.storage.docstore.keyval_docstore import KVDocumentStore
+from llama_index.storage.docstore.mongo_docstore import MongoDocumentStore
 from llama_index.storage.docstore.redis_docstore import RedisDocumentStore
 
 # alias for backwards compatibility
-from llama_index.storage.docstore.simple_docstore import DocumentStore
-
+from llama_index.storage.docstore.simple_docstore import (
+    DocumentStore,
+    SimpleDocumentStore,
+)
+from llama_index.storage.docstore.types import BaseDocumentStore
 
 __all__ = [
     "BaseDocumentStore",
     "DocumentStore",
     "SimpleDocumentStore",
     "MongoDocumentStore",
+    "CosmosDBDocStore",
     "KVDocumentStore",
     "RedisDocumentStore",
 ]
--- llama_index/storage/index_store/__init__.py
+++ llama_index/storage/index_store/__init__.py
@@ -1,6 +1,13 @@
+from llama_index.storage.index_store.cosmosdb_index_store import CosmosDBIndexStore
 from llama_index.storage.index_store.keyval_index_store import KVIndexStore
-from llama_index.storage.index_store.simple_index_store import SimpleIndexStore
 from llama_index.storage.index_store.mongo_index_store import MongoIndexStore
 from llama_index.storage.index_store.redis_index_store import RedisIndexStore
+from llama_index.storage.index_store.simple_index_store import SimpleIndexStore
 
-__all__ = ["KVIndexStore", "SimpleIndexStore", "MongoIndexStore", "RedisIndexStore"]
+__all__ = [
+    "KVIndexStore",
+    "SimpleIndexStore",
+    "MongoIndexStore",
+    "RedisIndexStore",
+    "CosmosDBIndexStore",
+]
