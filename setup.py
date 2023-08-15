c37f70e4716e7b6e10c7ed17e8b5a007599e424d
Felix <felix@iron59.co.uk>
2023-08-15 21:46:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
     "tiktoken",
     "dataclasses_json",
     "langchain>=0.0.262",
-    "sqlalchemy>=2.0.15",
+    "sqlalchemy",
     "numpy",
     "tenacity>=8.2.0,<9.0.0",
     "openai>=0.26.4",
