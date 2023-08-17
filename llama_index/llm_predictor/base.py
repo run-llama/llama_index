1233df50e0ac781229b02f3739f5132417a2a5bc
Mustafa Eyceoz <maxusmusti@gmail.com>
2023-08-17 14:33:52 -0400

    Review feedback applied

--- llama_index/llm_predictor/base.py
+++ llama_index/llm_predictor/base.py
@@ -2,7 +2,7 @@
 
 import logging
 from abc import abstractmethod
-from typing import Any, Optional, Protocol, runtime_checkable
+from typing import Any, List, Optional, Protocol, runtime_checkable
 
 from llama_index.callbacks.base import CallbackManager
 from llama_index.llm_predictor.utils import (
@@ -11,7 +11,7 @@ from llama_index.llm_predictor.utils import (
     stream_chat_response_to_tokens,
     stream_completion_response_to_tokens,
 )
-from llama_index.llms.base import LLM, LLMMetadata
+from llama_index.llms.base import LLM, ChatMessage, LLMMetadata, MessageRole
 from llama_index.llms.generic_utils import messages_to_prompt
 from llama_index.llms.utils import LLMType, resolve_llm
 from llama_index.prompts.base import Prompt
@@ -93,21 +93,13 @@ class LLMPredictor(BaseLLMPredictor):
         """Predict."""
         if self._llm.metadata.is_chat_model:
             messages = prompt.format_messages(llm=self._llm, **prompt_args)
+            messages = self._extend_messages(messages)
             chat_response = self._llm.chat(messages)
             output = chat_response.message.content or ""
             # NOTE: this is an approximation, only for token counting
             formatted_prompt = messages_to_prompt(messages)
         else:
-            if self.system_prompt:
-                prompt.prompt_selector.default_prompt.template = (
-                    self.system_prompt
-                    + "\n\n"
-                    + prompt.prompt_selector.default_prompt.template
-                )
-            if self.query_wrapper_prompt:
-                prompt.partial_dict["query_str"] = self.query_wrapper_prompt.format(
-                    query_str=prompt.partial_dict["query_str"]
-                )
+            prompt = self._extend_prompt(prompt)
             formatted_prompt = prompt.format(llm=self._llm, **prompt_args)
             response = self._llm.complete(formatted_prompt)
             output = response.text
@@ -120,19 +112,11 @@ class LLMPredictor(BaseLLMPredictor):
         """Stream."""
         if self._llm.metadata.is_chat_model:
             messages = prompt.format_messages(llm=self._llm, **prompt_args)
+            messages = self._extend_messages(messages)
             chat_response = self._llm.stream_chat(messages)
             stream_tokens = stream_chat_response_to_tokens(chat_response)
         else:
-            if self.system_prompt:
-                prompt.prompt_selector.default_prompt.template = (
-                    self.system_prompt
-                    + "\n\n"
-                    + prompt.prompt_selector.default_prompt.template
-                )
-            if self.query_wrapper_prompt:
-                prompt.partial_dict["query_str"] = self.query_wrapper_prompt.format(
-                    query_str=prompt.partial_dict["query_str"]
-                )
+            prompt = self._extend_prompt(prompt)
             formatted_prompt = prompt.format(llm=self._llm, **prompt_args)
             stream_response = self._llm.stream_complete(formatted_prompt)
             stream_tokens = stream_completion_response_to_tokens(stream_response)
@@ -142,21 +126,13 @@ class LLMPredictor(BaseLLMPredictor):
         """Async predict."""
         if self._llm.metadata.is_chat_model:
             messages = prompt.format_messages(llm=self._llm, **prompt_args)
+            messages = self._extend_messages(messages)
             chat_response = await self._llm.achat(messages)
             output = chat_response.message.content or ""
             # NOTE: this is an approximation, only for token counting
             formatted_prompt = messages_to_prompt(messages)
         else:
-            if self.system_prompt:
-                prompt.prompt_selector.default_prompt.template = (
-                    self.system_prompt
-                    + "\n\n"
-                    + prompt.prompt_selector.default_prompt.template
-                )
-            if self.query_wrapper_prompt:
-                prompt.partial_dict["query_str"] = self.query_wrapper_prompt.format(
-                    query_str=prompt.partial_dict["query_str"]
-                )
+            prompt = self._extend_prompt(prompt)
             formatted_prompt = prompt.format(llm=self._llm, **prompt_args)
             response = await self._llm.acomplete(formatted_prompt)
             output = response.text
@@ -169,20 +145,34 @@ class LLMPredictor(BaseLLMPredictor):
         """Async stream."""
         if self._llm.metadata.is_chat_model:
             messages = prompt.format_messages(llm=self._llm, **prompt_args)
+            messages = self._extend_messages(messages)
             chat_response = await self._llm.astream_chat(messages)
             stream_tokens = await astream_chat_response_to_tokens(chat_response)
         else:
-            if self.system_prompt:
-                prompt.prompt_selector.default_prompt.template = (
-                    self.system_prompt
-                    + "\n\n"
-                    + prompt.prompt_selector.default_prompt.template
-                )
-            if self.query_wrapper_prompt:
-                prompt.partial_dict["query_str"] = self.query_wrapper_prompt.format(
-                    query_str=prompt.partial_dict["query_str"]
-                )
+            prompt = self._extend_prompt(prompt)
             formatted_prompt = prompt.format(llm=self._llm, **prompt_args)
             stream_response = await self._llm.astream_complete(formatted_prompt)
             stream_tokens = await astream_completion_response_to_tokens(stream_response)
         return stream_tokens
+
+    def _extend_prompt(self, prompt: Prompt) -> Prompt:
+        """Add system and query wrapper prompts to base prompt"""
+        if self.system_prompt:
+            prompt.prompt_selector.default_prompt.template = (
+                self.system_prompt
+                + "\n\n"
+                + prompt.prompt_selector.default_prompt.template
+            )
+        if self.query_wrapper_prompt:
+            prompt.partial_dict["query_str"] = self.query_wrapper_prompt.format(
+                query_str=prompt.partial_dict["query_str"]
+            )
+        return prompt
+
+    def _extend_messages(self, messages: List[ChatMessage]) -> List[ChatMessage]:
+        """Add system prompt to chat message list"""
+        if self.system_prompt:
+            messages = [
+                ChatMessage(role=MessageRole.SYSTEM, content=self.system_prompt)
+            ] + messages
+        return messages
