ac6466b77083e9b1d1b3f3f73e39e1bb60dacaf2
Logan Markewich <logan.markewich@live.com>
2023-08-18 15:13:46 -0600

    make all service context objects pydantic

--- llama_index/text_splitter/types.py
+++ llama_index/text_splitter/types.py
@@ -1,16 +1,19 @@
 """Text splitter implementations."""
-from typing import List, Protocol, runtime_checkable
+from abc import abstractmethod, ABC
+from pydantic import BaseModel
+from typing import List
 
 
-class TextSplitter(Protocol):
+class TextSplitter(ABC, BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+
+    @abstractmethod
     def split_text(self, text: str) -> List[str]:
         ...
 
 
-@runtime_checkable
-class MetadataAwareTextSplitter(Protocol):
-    def split_text(self, text: str) -> List[str]:
-        ...
-
+class MetadataAwareTextSplitter(TextSplitter):
+    @abstractmethod
     def split_text_metadata_aware(self, text: str, metadata_str: str) -> List[str]:
         ...
