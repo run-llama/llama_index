d9ad808b759e32156f941b816d16efdce19ebce4
Liam Moore <lmoore90@gmail.com>
2023-08-21 22:29:46 +0200

    update tests to cover description case

--- tests/tools/test_utils.py
+++ tests/tools/test_utils.py
@@ -1,6 +1,8 @@
 """Test utils."""
 from typing import List
 
+from pydantic import Field
+
 from llama_index.tools.utils import create_schema_from_function
 
 
@@ -29,3 +31,25 @@ def test_create_schema_from_function() -> None:
     SchemaCls = create_schema_from_function("test_schema", test_fn2)
     schema = SchemaCls.schema()
     assert "required" not in schema
+
+
+def test_create_schema_from_function_with_field() -> None:
+    """Test create_schema_from_function with pydantic.Field."""
+
+    def tmp_function(x: int = Field(3, description="An integer")) -> str:
+        return str(x)
+
+    schema = create_schema_from_function("TestSchema", tmp_function)
+    actual_schema = schema.schema()
+
+    assert "x" in actual_schema["properties"]
+    assert actual_schema["properties"]["x"]["type"] == "integer"
+    assert actual_schema["properties"]["x"]["default"] == 3
+    assert actual_schema["properties"]["x"]["description"] == "An integer"
+
+    # Test the created schema
+    instance = schema()
+    assert instance.x == 3
+
+    instance = schema(x=5)
+    assert instance.x == 5
