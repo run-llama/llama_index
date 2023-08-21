de48a2898ef27a12039d1eca38e084550df97d8b
Simon Suo <simonsdsuo@gmail.com>
2023-08-21 15:10:08 -0700

    wip

--- tests/prompts/test_base.py
+++ tests/prompts/test_base.py
@@ -1,15 +1,11 @@
 """Test prompts."""
 
 
+from llama_index.bridge.langchain import ConditionalPromptSelector as LangchainSelector
 from llama_index.bridge.langchain import PromptTemplate as LangchainTemplate
+from llama_index.llms import MockLLM
 from llama_index.llms.base import LLM
-from llama_index.llms.openai import OpenAI
 from llama_index.prompts import LangchainPromptTemplate, PromptTemplate
-
-
-def is_openai(llm: LLM) -> bool:
-    """Test condition."""
-    return isinstance(llm, OpenAI)
 
 
 def test_partial_format() -> None:
@@ -23,10 +19,31 @@ def test_partial_format() -> None:
     assert prompt_fmt.format(text="world") == "hello world bar"
 
 
-def test_langchain_prompt() -> None:
+def test_langchain_template() -> None:
     lc_template = LangchainTemplate.from_template("hello {text} {foo}")
     template = LangchainPromptTemplate(lc_template)
 
     template_fmt = template.partial_format(foo="bar")
     assert isinstance(template, LangchainPromptTemplate)
     assert template_fmt.format(text="world") == "hello world bar"
+
+
+def test_langchain_selector_template() -> None:
+    mock_llm = MockLLM()
+
+    def is_mock(llm: LLM):
+        return llm == mock_llm
+
+    default_lc_template = LangchainTemplate.from_template("hello {text} {foo}")
+    conditionals = [
+        (is_mock, LangchainTemplate.from_template("hello {text} {foo} mock")),
+    ]
+
+    lc_selector = LangchainSelector(
+        default_prompt=default_lc_template, conditionals=conditionals
+    )
+    template = LangchainPromptTemplate(selector=lc_selector)
+
+    template_fmt = template.partial_format(foo="bar")
+    assert isinstance(template, LangchainPromptTemplate)
+    assert template_fmt.format(llm=mock_llm, text="world") == "hello world bar mock"
