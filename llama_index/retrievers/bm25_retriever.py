97eebf8b63546e471c487f5948ea464847507bce
Ja-sonYun <killa30867@gmail.com>
2023-08-24 22:44:34 +0900

    add description about score range, and which is more relavant

--- llama_index/retrievers/bm25_retriever.py
+++ llama_index/retrievers/bm25_retriever.py
@@ -42,7 +42,6 @@ class BM25Retriever(BaseRetriever):
         tokenizer: Optional[Callable[[str], list[str]]] = None,
     ) -> "BM25Retriever":
         tokenizer = tokenizer or globals_helper.tokenizer
-
         return cls(
             index.docstore,
             tokenizer,
@@ -64,7 +63,7 @@ class BM25Retriever(BaseRetriever):
 
         scored_nodes = self._get_scored_nodes(query_bundle.query_str)
 
-        # Sort and get top_k nodes
+        # Sort and get top_k nodes, score range => 0...1, closer to 1 means more relevant
         nodes = sorted(scored_nodes, key=lambda x: x.score or 0.0, reverse=True)
         top_k_nodes = nodes[: self._similarity_top_k]
 
