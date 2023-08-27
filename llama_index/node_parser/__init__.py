28e5fadbeb28700fe4ff561b4b194f1a596eab69
Jerry Liu <jerryjliu98@gmail.com>
2023-08-27 09:00:51 -0700

    AutoMergingRetriever (#7420)

--- llama_index/node_parser/__init__.py
+++ llama_index/node_parser/__init__.py
@@ -3,10 +3,13 @@
 from llama_index.node_parser.interface import NodeParser
 from llama_index.node_parser.simple import SimpleNodeParser
 from llama_index.node_parser.sentence_window import SentenceWindowNodeParser
+from llama_index.node_parser.hierarchical import HierarchicalNodeParser, get_leaf_nodes
 
 
 __all__ = [
     "SimpleNodeParser",
     "SentenceWindowNodeParser",
     "NodeParser",
+    "HierarchicalNodeParser",
+    "get_leaf_nodes",
 ]
