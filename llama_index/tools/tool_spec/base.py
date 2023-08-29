21ea92d169e60573fabd3f142d66c6665933c626
Logan Markewich <logan.markewich@live.com>
2023-08-28 22:06:29 -0600

    linting

--- llama_index/tools/tool_spec/base.py
+++ llama_index/tools/tool_spec/base.py
@@ -100,7 +100,7 @@ class BaseToolSpec:
 
 
 def patch_sync(func_async: AsyncCallable) -> Callable:
-    def patched_sync(*args, **kwargs) -> Any:
+    def patched_sync(*args: Any, **kwargs: Any) -> Any:
         loop = asyncio.get_event_loop()
         return loop.run_until_complete(func_async(*args, **kwargs))
 
