91d9e36a5ebffbc2cd184578541804dd3fb8cf7b
Jerry Liu <jerryjliu98@gmail.com>
2023-09-03 23:04:40 -0700

    cr

--- llama_index/indices/vector_store/retrievers/retriever.py
+++ llama_index/indices/vector_store/retrievers/retriever.py
@@ -65,7 +65,7 @@ class VectorIndexRetriever(BaseRetriever):
     def similarity_top_k(self) -> int:
         """Return similarity top k."""
         return self._similarity_top_k
-    
+
     @similarity_top_k.setter
     def similarity_top_k(self, similarity_top_k: int) -> None:
         """Set similarity top k."""
