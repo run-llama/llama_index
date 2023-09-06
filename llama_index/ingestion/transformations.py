83f4e5309d0416c37c6fd84abc26b2e5671a6e06
Logan Markewich <logan.markewich@live.com>
2023-09-06 16:22:32 -0600

    linting + mypy

--- llama_index/ingestion/transformations.py
+++ llama_index/ingestion/transformations.py
@@ -4,15 +4,32 @@ This module maintains the list of transformations that are supported by the syst
 
 from typing import Any, Dict
 from enum import Enum
-from pydantic import BaseModel, Field
+
+from llama_index.bridge.pydantic import BaseModel, Field
+from llama_index.node_parser.extractors import (
+    MetadataExtractor,
+    KeywordExtractor,
+    TitleExtractor,
+    EntityExtractor,
+    MarvinMetadataExtractor,
+    SummaryExtractor,
+    QuestionsAnsweredExtractor,
+)
+from llama_index.node_parser import (
+    SimpleNodeParser,
+    SentenceWindowNodeParser,
+    HierarchicalNodeParser,
+)
 
 
+# Transform Input/Output Types
 class TransformationIOType(BaseModel):
     name: str = Field(description="Name of the input/output type")
     description: str = Field(description="Description of the input/output type")
     python_type: str = Field(description="Python type of the input/output type")
 
 
+# TODO: Figure out how to do this with an Enum class
 TransformationIOTypes = Enum(
     value="TransformationIOTypes",
     names=[
@@ -32,35 +49,20 @@ TransformationIOTypes = Enum(
                 python_type="Sequence[BaseNode]",
             ),
         ),
-        (
-            "TEXT",
-            TransformationIOType(
-                name="Text",
-                description="Text",
-                python_type="str",
-            ),
-        ),
-        (
-            "TEXT_LIST",
-            TransformationIOType(
-                name="TextList",
-                description="TextList",
-                python_type="Sequence[str]",
-            ),
-        ),
     ],
 )
 
 
+# Configured transformation schemas
 class ConfiguredTransformation(BaseModel):
     """A transformation that can be applied to data."""
 
     name: str = Field(description="Unique name of the transformation")
     description: str = Field(description="Description for the transformation")
-    input_type: TransformationIOTypes = Field(
+    input_type: TransformationIOType = Field(
         description="Input type for the transformation"
     )
-    output_type: TransformationIOTypes = Field(
+    output_type: TransformationIOType = Field(
         description="Output type for the transformation"
     )
     configuration_schema: Dict[str, Any] = Field(
@@ -69,6 +71,7 @@ class ConfiguredTransformation(BaseModel):
     )
 
 
+# TODO: Figure out how to do this with an Enum class
 SupportedTransformations = Enum(
     value="ConfiguredTransformation",
     names=[
@@ -77,8 +80,8 @@ SupportedTransformations = Enum(
             ConfiguredTransformation(
                 name="MetadataExtractor",
                 description="Applies a function to extract metadata from nodes",
-                input_type=TransformationIOTypes.NODES,
-                output_type=TransformationIOTypes.NODES,
+                input_type=TransformationIOTypes.NODES.value,
+                output_type=TransformationIOTypes.NODES.value,
             ),
         ),
         (
@@ -86,60 +89,28 @@ SupportedTransformations = Enum(
             ConfiguredTransformation(
                 name="NodeParser",
                 description="Applies a function to parse nodes from documents",
-                input_type=TransformationIOTypes.DOCUMENTS,
-                output_type=TransformationIOTypes.NODES,
-            ),
-        ),
-        (
-            "TEXT_SPLITTER",
-            ConfiguredTransformation(
-                name="TextSplitter",
-                description=(
-                    "Applies a function to split text into chunks, "
-                    "used in a NodeParser"
-                ),
-                input_type=TransformationIOTypes.TEXT,
-                output_type=TransformationIOTypes.TEXT_LIST,
+                input_type=TransformationIOTypes.DOCUMENTS.value,
+                output_type=TransformationIOTypes.NODES.value,
             ),
         ),
     ],
 )
 
 
-from llama_index.node_parser.extractors import (
-    MetadataExtractor,
-    KeywordExtractor,
-    TitleExtractor,
-    EntityExtractor,
-    MarvinMetadataExtractor,
-    SummaryExtractor,
-    QuestionsAnsweredExtractor,
-)
-from llama_index.node_parser import (
-    SimpleNodeParser,
-    SentenceWindowNodeParser,
-    HierarchicalNodeParser,
-)
-from llama_index.text_splitter import SentenceSplitter, TokenTextSplitter, CodeSplitter
-
-
+# Class name to transformation utilities
 CLASS_NAME_TO_TRANSFORM: Dict[str, ConfiguredTransformation] = {
     # Metadata Extractors
-    MetadataExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR,
-    KeywordExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR,
-    TitleExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR,
-    EntityExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR,
-    MarvinMetadataExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR,
-    SummaryExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR,
-    QuestionsAnsweredExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR,
+    MetadataExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR.value,
+    KeywordExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR.value,
+    TitleExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR.value,
+    EntityExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR.value,
+    MarvinMetadataExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR.value,  # noqa: E501
+    SummaryExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR.value,
+    QuestionsAnsweredExtractor.class_name(): SupportedTransformations.METADATA_EXTRACTOR.value,  # noqa: E501
     # Node Parsers
-    SimpleNodeParser.class_name(): SupportedTransformations.NODE_PARSER,
-    SentenceWindowNodeParser.class_name(): SupportedTransformations.NODE_PARSER,
-    HierarchicalNodeParser.class_name(): SupportedTransformations.NODE_PARSER,
-    # Text Splitters
-    SentenceSplitter.class_name(): SupportedTransformations.TEXT_SPLITTER,
-    TokenTextSplitter.class_name(): SupportedTransformations.TEXT_SPLITTER,
-    CodeSplitter.class_name(): SupportedTransformations.TEXT_SPLITTER,
+    SimpleNodeParser.class_name(): SupportedTransformations.NODE_PARSER.value,
+    SentenceWindowNodeParser.class_name(): SupportedTransformations.NODE_PARSER.value,
+    HierarchicalNodeParser.class_name(): SupportedTransformations.NODE_PARSER.value,
 }
 
 
@@ -161,36 +132,3 @@ def get_configured_transform(
     configured_transform = CLASS_NAME_TO_TRANSFORM[class_name]
     configured_transform.configuration_schema = transform_schema
     return configured_transform
-
-
-simple_node_parser = SimpleNodeParser.from_defaults()
-sentence_node_parser = SentenceWindowNodeParser.from_defaults()
-hierarchical_node_parser = HierarchicalNodeParser.from_defaults()
-
-keyword_extractor = KeywordExtractor()
-metadata_extractor = MetadataExtractor(extractors=[keyword_extractor])
-
-print(
-    "SimpleNodeParser\n",
-    get_configured_transform(simple_node_parser.to_dict()).configuration_schema,
-)
-print("------")
-print(
-    "SentenceWindowNodeParser\n",
-    get_configured_transform(sentence_node_parser.to_dict()).configuration_schema,
-)
-print("------")
-print(
-    "HierarchicalNodeParser\n",
-    get_configured_transform(hierarchical_node_parser.to_dict()).configuration_schema,
-)
-print("------")
-print(
-    "KeywordExtractor\n",
-    get_configured_transform(keyword_extractor.to_dict()).configuration_schema,
-)
-print("------")
-print(
-    "MetadataExtractor\n",
-    get_configured_transform(metadata_extractor.to_dict()).configuration_schema,
-)
