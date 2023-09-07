77e32885b238efc0c3f45fffd93b5a1294ea0f4d
rchan <rchan@turing.ac.uk>
2023-09-07 17:05:26 +0100

    add comments and remove user message when appending assistant

--- llama_index/llms/llama_utils.py
+++ llama_index/llms/llama_utils.py
@@ -27,21 +27,27 @@ def messages_to_prompt(
     system_message_str = f"{B_SYS} {system_message_str} {E_SYS}"
 
     for i in range(0, len(messages), 2):
+        # first message should always be a user
         user_message = messages[i]
         assert user_message.role == MessageRole.USER
 
         if i == 0:
+            # make sure system prompt is included at the start
             str_message = f"{BOS} {B_INST} {system_message_str} "
         else:
+            # end previous user-assistant interaction
             string_messages[-1] += f" {EOS}"
+            # no need to include system prompt
             str_message = f"{BOS} {B_INST} "
 
+        # include user message content
         str_message += f"{user_message.content} {E_INST}"
 
-        if len(messages) != i + 1:
-            assert messages[i + 1].role == MessageRole.ASSISTANT
-            assistant_message = messages[i + 1].content
-            str_message += f"{user_message.content} {E_INST} {assistant_message}"
+        if len(messages) != (i + 1):
+            # if assistant message exists, add to str_message
+            assistant_message = messages[i + 1]
+            assert assistant_message.role == MessageRole.ASSISTANT
+            str_message += assistant_message.content
 
         string_messages.append(str_message)
 
