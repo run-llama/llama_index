a90241d7a4b29719932b926b31e3bbe612dfeec4
Andrei Fajardo <andrei@nerdai.io>
2023-09-09 21:50:36 -0400

    add AsyncElasticsearchStore and its unit tests

--- llama_index/vector_stores/__init__.py
+++ llama_index/vector_stores/__init__.py
@@ -12,7 +12,10 @@ from llama_index.vector_stores.docarray import (
     DocArrayHnswVectorStore,
     DocArrayInMemoryVectorStore,
 )
-from llama_index.vector_stores.elasticsearch import ElasticsearchStore
+from llama_index.vector_stores.elasticsearch import (
+    ElasticsearchStore,
+    AsyncElasticsearchStore,
+)
 from llama_index.vector_stores.epsilla import EpsillaVectorStore
 from llama_index.vector_stores.faiss import FaissVectorStore
 from llama_index.vector_stores.lancedb import LanceDBVectorStore
@@ -36,6 +39,7 @@ from llama_index.vector_stores.weaviate import WeaviateVectorStore
 from llama_index.vector_stores.zep import ZepVectorStore
 
 __all__ = [
+    "AsyncElasticsearchStore",
     "ElasticsearchStore",
     "SimpleVectorStore",
     "RedisVectorStore",
