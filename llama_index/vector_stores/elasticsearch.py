54abb8b7ab7f80c945d55b1475c2124189abdcd9
Andrei Fajardo <andrei@nerdai.io>
2023-09-10 20:06:28 -0400

    remove AsyncElastsearchStore; adjust comments for async methods for add, delete, and query

--- llama_index/vector_stores/elasticsearch.py
+++ llama_index/vector_stores/elasticsearch.py
@@ -263,7 +263,7 @@ class ElasticsearchStore(VectorStore):
         """Create the AsyncElasticsearch index if it doesn't already exist.
 
         Args:
-            index_name: Name of the Elasticsearch index to create.
+            index_name: Name of the AsyncElasticsearch index to create.
             dims_length: Length of the embedding vectors.
         """
 
@@ -404,7 +404,7 @@ class ElasticsearchStore(VectorStore):
         Args:
             nodes: List of nodes with embeddings.
             create_index_if_not_exists: Optional. Whether to create
-                                        the Elasticsearch index if it
+                                        the AsyncElasticsearch index if it
                                         doesn't already exist.
                                         Defaults to True.
 
@@ -413,14 +413,14 @@ class ElasticsearchStore(VectorStore):
 
         Raises:
             ImportError: If elasticsearch python package is not installed.
-            BulkIndexError: If Elasticsearch bulk indexing fails.
+            BulkIndexError: If AsyncElasticsearch async_bulk indexing fails.
         """
         try:
             from elasticsearch.helpers import BulkIndexError, async_bulk
         except ImportError:
             raise ImportError(
-                "Could not import elasticsearch python package. "
-                "Please install it with `pip install elasticsearch`."
+                "Could not import elasticsearch[async] python package. "
+                "Please install it with `pip install 'elasticsearch[async]'`."
             )
 
         if len(nodes) == 0:
@@ -509,10 +509,10 @@ class ElasticsearchStore(VectorStore):
         Args:
             ref_doc_id: ID of the node to delete.
             delete_kwargs: Optional. Additional arguments to
-                        pass to Elasticsearch delete_by_query.
+                        pass to AsyncElasticsearch delete_by_query.
 
         Raises:
-            Exception: If Elasticsearch delete_by_query fails.
+            Exception: If AsyncElasticsearch delete_by_query fails.
         """
 
         try:
@@ -653,372 +653,15 @@ class ElasticsearchStore(VectorStore):
         es_filter: Optional[List[Dict]] = None,
         **kwargs: Any,
     ) -> VectorStoreQueryResult:
-        """Query index for top k most similar nodes.
+        """Asynchronous query index for top k most similar nodes.
 
         Args:
             query_embedding (List[float]): query embedding
             custom_query: Optional. custom query function that takes in the es query
                         body and returns a modified query body.
                         This can be used to add additional query
-                        parameters to the Elasticsearch query.
-            es_filter: Optional. Elasticsearch filter to apply to the
-                        query. If filter is provided in the query,
-                        this filter will be ignored.
-
-        Returns:
-            VectorStoreQueryResult: Result of the query.
-
-        Raises:
-            Exception: If Elasticsearch query fails.
-
-        """
-        query_embedding = cast(List[float], query.query_embedding)
-
-        es_query = {}
-
-        if query.filters is not None and len(query.filters.filters) > 0:
-            filter = [_to_elasticsearch_filter(query.filters)]
-        else:
-            filter = es_filter or []
-
-        if query.mode in (
-            VectorStoreQueryMode.DEFAULT,
-            VectorStoreQueryMode.HYBRID,
-        ):
-            es_query["knn"] = {
-                "filter": filter,
-                "field": self.vector_field,
-                "query_vector": query_embedding,
-                "k": query.similarity_top_k,
-                "num_candidates": query.similarity_top_k * 10,
-            }
-
-        if query.mode in (
-            VectorStoreQueryMode.TEXT_SEARCH,
-            VectorStoreQueryMode.HYBRID,
-        ):
-            es_query["query"] = {
-                "bool": {
-                    "must": {"match": {self.text_field: {"query": query.query_str}}},
-                    "filter": filter,
-                }
-            }
-
-        if query.mode == VectorStoreQueryMode.HYBRID:
-            es_query["rank"] = {"rrf": {}}
-
-        if custom_query is not None:
-            es_query = custom_query(es_query, query)
-            logger.debug(f"Calling custom_query, Query body now: {es_query}")
-
-        response = await self.async_client.search(
-            index=self.index_name,
-            **es_query,
-            size=query.similarity_top_k,
-            _source={"excludes": [self.vector_field]},
-        )
-
-        top_k_nodes = []
-        top_k_ids = []
-        top_k_scores = []
-        for hit in response["hits"]["hits"]:
-            source = hit["_source"]
-            metadata = source.get("metadata", None)
-            text = source.get(self.text_field, None)
-            node_id = hit["_id"]
-
-            try:
-                node = metadata_dict_to_node(metadata)
-                node.text = text
-            except Exception:
-                # Legacy support for old metadata format
-                logger.warning(
-                    f"Could not parse metadata from hit {hit['_source']['metadata']}"
-                )
-                node_info = source.get("node_info")
-                relationships = source.get("relationships")
-                start_char_idx = None
-                end_char_idx = None
-                if isinstance(node_info, dict):
-                    start_char_idx = node_info.get("start", None)
-                    end_char_idx = node_info.get("end", None)
-
-                node = TextNode(
-                    text=text,
-                    metadata=metadata,
-                    id_=node_id,
-                    start_char_idx=start_char_idx,
-                    end_char_idx=end_char_idx,
-                    relationships=relationships,
-                )
-            top_k_nodes.append(node)
-            top_k_ids.append(node_id)
-            top_k_scores.append(hit["_score"])
-        return VectorStoreQueryResult(
-            nodes=top_k_nodes, ids=top_k_ids, similarities=top_k_scores
-        )
-
-
-class AsyncElasticsearchStore(VectorStore):
-    """Asyncrhonious Elasticsearch vector store.
-
-    Args:
-
-        index_name: Name of the Elasticsearch index.
-        es_client: Optional. Pre-existing Elasticsearch client.
-        es_url: Optional. Elasticsearch URL.
-        es_cloud_id: Optional. Elasticsearch cloud ID.
-        es_api_key: Optional. Elasticsearch API key.
-        es_user: Optional. Elasticsearch username.
-        es_password: Optional. Elasticsearch password.
-        text_field: Optional. Name of the Elasticsearch field that stores the text.
-        vector_field: Optional. Name of the Elasticsearch field that stores the
-                    embedding.
-        batch_size: Optional. Batch size for bulk indexing. Defaults to 200.
-        distance_strategy: Optional. Distance strategy to use for similarity search.
-                        Defaults to "COSINE".
-
-    Raises:
-        ConnectionError: If Elasticsearch client cannot connect to Elasticsearch.
-        ValueError: If neither es_client nor es_url nor es_cloud_id is provided.
-
-    """
-
-    stores_text: bool = True
-
-    @classmethod
-    async def create(
-        cls,
-        index_name: str,
-        es_client: Optional[Any] = None,
-        es_url: Optional[str] = None,
-        es_cloud_id: Optional[str] = None,
-        es_api_key: Optional[str] = None,
-        es_user: Optional[str] = None,
-        es_password: Optional[str] = None,
-        text_field: str = "content",
-        vector_field: str = "embedding",
-        batch_size: int = 200,
-        distance_strategy: Optional[DISTANCE_STRATEGIES] = "COSINE",
-    ) -> "AsyncElasticsearchStore":
-        """Factory method for AsyncElasticsearchStore
-
-        NOTE: self._client has to be awaited on and can't work within __init__
-        """
-        self = AsyncElasticsearchStore()
-        self.index_name = index_name
-        self.text_field = text_field
-        self.vector_field = vector_field
-        self.batch_size = batch_size
-        self.distance_strategy = distance_strategy
-
-        if es_client is not None:
-            self._client = es_client
-        elif es_url is not None or es_cloud_id is not None:
-            self._client = await _aget_elasticsearch_client(
-                es_url=es_url,
-                username=es_user,
-                password=es_password,
-                cloud_id=es_cloud_id,
-                api_key=es_api_key,
-            )
-        else:
-            raise ValueError(
-                """Either provide a pre-existing Elasticsearch connection, \
-                or valid credentials for creating a new connection."""
-            )
-        return self
-
-    @property
-    def client(self) -> Any:
-        """Get elasticsearch client."""
-        return self._client
-
-    async def _create_index_if_not_exists(
-        self, index_name: str, dims_length: Optional[int] = None
-    ) -> None:
-        """Create the AsyncElasticsearch index if it doesn't already exist.
-
-        Args:
-            index_name: Name of the AsyncElasticsearch index to create.
-            dims_length: Length of the embedding vectors.
-        """
-
-        if await self.client.indices.exists(index=index_name):
-            logger.debug(f"Index {index_name} already exists. Skipping creation.")
-
-        else:
-            if dims_length is None:
-                raise ValueError(
-                    "Cannot create index without specifying dims_length "
-                    "when the index doesn't already exist. We infer "
-                    "dims_length from the first embedding. Check that "
-                    "you have provided an embedding function."
-                )
-
-            if self.distance_strategy == "COSINE":
-                similarityAlgo = "cosine"
-            elif self.distance_strategy == "EUCLIDEAN_DISTANCE":
-                similarityAlgo = "l2_norm"
-            elif self.distance_strategy == "DOT_PRODUCT":
-                similarityAlgo = "dot_product"
-            else:
-                raise ValueError(f"Similarity {self.distance_strategy} not supported.")
-
-            index_settings = {
-                "mappings": {
-                    "properties": {
-                        self.vector_field: {
-                            "type": "dense_vector",
-                            "dims": dims_length,
-                            "index": True,
-                            "similarity": similarityAlgo,
-                        },
-                        self.text_field: {"type": "text"},
-                        "metadata": {
-                            "properties": {
-                                "document_id": {"type": "keyword"},
-                                "doc_id": {"type": "keyword"},
-                                "ref_doc_id": {"type": "keyword"},
-                            }
-                        },
-                    }
-                }
-            }
-
-            logger.debug(
-                f"Creating index {index_name} with mappings {index_settings['mappings']}"  # noqa: E501
-            )
-            await self.client.indices.create(index=index_name, **index_settings)
-
-    async def add(
-        self,
-        nodes: List[BaseNode],
-        *,
-        create_index_if_not_exists: bool = True,
-    ) -> List[str]:
-        """Async method to add nodes to AsyncElasticsearch index.
-
-        Args:
-            nodes: List of nodes with embeddings.
-            create_index_if_not_exists: Optional. Whether to create
-                                        the Elasticsearch index if it
-                                        doesn't already exist.
-                                        Defaults to True.
-
-        Returns:
-            List of node IDs that were added to the index.
-
-        Raises:
-            ImportError: If elasticsearch python package is not installed.
-            BulkIndexError: If AsyncElasticsearch async_bulk indexing fails.
-        """
-        try:
-            from elasticsearch.helpers import BulkIndexError, async_bulk
-        except ImportError:
-            raise ImportError(
-                "Could not import elasticsearch python package. "
-                "Please install it with `pip install elasticsearch`."
-            )
-
-        if len(nodes) == 0:
-            return []
-
-        if create_index_if_not_exists:
-            dims_length = len(nodes[0].get_embedding())
-            await self._create_index_if_not_exists(
-                index_name=self.index_name, dims_length=dims_length
-            )
-
-        embeddings: List[List[float]] = []
-        texts: List[str] = []
-        metadatas: List[dict] = []
-        ids: List[str] = []
-        for node in nodes:
-            ids.append(node.node_id)
-            embeddings.append(node.get_embedding())
-            texts.append(node.get_content(metadata_mode=MetadataMode.NONE))
-            metadatas.append(node_to_metadata_dict(node, remove_text=True))
-
-        requests = []
-        return_ids = []
-
-        for i, text in enumerate(texts):
-            metadata = metadatas[i] if metadatas else {}
-            _id = ids[i] if ids else str(uuid.uuid4())
-            request = {
-                "_op_type": "index",
-                "_index": self.index_name,
-                self.vector_field: embeddings[i],
-                self.text_field: text,
-                "metadata": metadata,
-                "_id": _id,
-            }
-            requests.append(request)
-            return_ids.append(_id)
-
-        await async_bulk(
-            self.client, requests, chunk_size=self.batch_size, refresh=True
-        )
-        try:
-            success, failed = await async_bulk(
-                self.client, requests, stats_only=True, refresh=True
-            )
-            logger.debug(f"Added {success} and failed to add {failed} texts to index")
-
-            logger.debug(f"added texts {ids} to index")
-            return return_ids
-        except BulkIndexError as e:
-            logger.error(f"Error adding texts: {e}")
-            firstError = e.errors[0].get("index", {}).get("error", {})
-            logger.error(f"First error reason: {firstError.get('reason')}")
-            raise e
-
-    async def delete(self, ref_doc_id: str, **delete_kwargs: Any) -> None:
-        """Async delete node from AsyncElasticsearch index.
-
-        Args:
-            ref_doc_id: ID of the node to delete.
-            delete_kwargs: Optional. Additional arguments to
-                        pass to AsyncElasticsearch delete_by_query.
-
-        Raises:
-            Exception: If AsyncElasticsearch delete_by_query fails.
-        """
-
-        try:
-            res = await self.client.delete_by_query(
-                index=self.index_name,
-                query={"term": {"metadata.ref_doc_id": ref_doc_id}},
-                refresh=True,
-                **delete_kwargs,
-            )
-            if res["deleted"] == 0:
-                logger.warning(f"Could not find text {ref_doc_id} to delete")
-            else:
-                logger.debug(f"Deleted text {ref_doc_id} from index")
-        except Exception as e:
-            logger.error(f"Error deleting text: {ref_doc_id}")
-            raise e
-
-    async def query(
-        self,
-        query: VectorStoreQuery,
-        custom_query: Optional[
-            Callable[[Dict, Union[VectorStoreQuery, None]], Dict]
-        ] = None,
-        es_filter: Optional[List[Dict]] = None,
-        **kwargs: Any,
-    ) -> VectorStoreQueryResult:
-        """Asynchronous Query index for top k most similar nodes.
-
-        Args:
-            query_embedding (List[float]): query embedding
-            custom_query: Optional. custom query function that takes in the es query
-                        body and returns a modified query body.
-                        This can be used to add additional query
-                        parameters to the Elasticsearch query.
-            es_filter: Optional. Elasticsearch filter to apply to the
+                        parameters to the AsyncElasticsearch query.
+            es_filter: Optional. AsyncElasticsearch filter to apply to the
                         query. If filter is provided in the query,
                         this filter will be ignored.
 
@@ -1068,7 +711,7 @@ class AsyncElasticsearchStore(VectorStore):
             es_query = custom_query(es_query, query)
             logger.debug(f"Calling custom_query, Query body now: {es_query}")
 
-        response = await self.client.search(
+        response = await self.async_client.search(
             index=self.index_name,
             **es_query,
             size=query.similarity_top_k,
