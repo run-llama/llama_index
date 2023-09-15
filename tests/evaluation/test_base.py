fd15325964d3a0183a4fcecfb83ec53e2269dd8b
Simon Suo <simonsdsuo@gmail.com>
2023-09-15 13:29:31 -0700

    wip

--- tests/evaluation/test_base.py
+++ tests/evaluation/test_base.py
@@ -1,4 +1,3 @@
-
 from typing import Any, Optional, Sequence
 from llama_index.evaluation import BaseEvaluator
 from llama_index.evaluation.base import EvaluationResult
@@ -7,20 +6,21 @@ from llama_index.schema import TextNode
 
 
 class MockEvaluator(BaseEvaluator):
-    def __init__(self, 
-                 mock_score: float = 1., 
-                 mock_passing: bool = True,
-                 mock_feedback: str = "test feedback"
-        ) -> None:
+    def __init__(
+        self,
+        mock_score: float = 1.0,
+        mock_passing: bool = True,
+        mock_feedback: str = "test feedback",
+    ) -> None:
         self._mock_score = mock_score
         self._mock_passing = mock_passing
         self._mock_feedback = mock_feedback
 
     def evaluate(
-        self, 
-        query: Optional[str] = None, 
-        contexts: Optional[Sequence[str]]= None, 
-        response: Optional[str] = None, 
+        self,
+        query: Optional[str] = None,
+        response: Optional[str] = None,
+        contexts: Optional[Sequence[str]] = None,
         **kwargs: Any
     ) -> EvaluationResult:
         return EvaluationResult(
@@ -31,27 +31,25 @@ class MockEvaluator(BaseEvaluator):
             score=self._mock_score,
             feedback=self._mock_feedback,
         )
-    
+
+
 def test_evaluator_basic() -> None:
     test_evaluator = MockEvaluator()
     eval_result_0 = test_evaluator.evaluate(
-        query='test query',
-        response='test response',
-        contexts=['test context 1', 'test context 2'],
+        query="test query",
+        response="test response",
+        contexts=["test context 1", "test context 2"],
     )
 
     eval_result_1 = test_evaluator.evaluate_response(
-        query='test query',
+        query="test query",
         response=Response(
-            response='test response',
+            response="test response",
             source_nodes=[
-                NodeWithScore(node=TextNode(text='test context 1'), score=1.),
-                NodeWithScore(node=TextNode(text='test context 2'), score=1.),
+                NodeWithScore(node=TextNode(text="test context 1"), score=1.0),
+                NodeWithScore(node=TextNode(text="test context 2"), score=1.0),
             ],
-        )
+        ),
     )
 
     assert eval_result_0 == eval_result_1
-
-
-
