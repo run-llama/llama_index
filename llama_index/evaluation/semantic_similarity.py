61a64edfec434a1f115287952b54db056d11439a
Simon Suo <simonsdsuo@gmail.com>
2023-09-21 14:19:12 -0700

    wip

--- llama_index/evaluation/semantic_similarity.py
+++ llama_index/evaluation/semantic_similarity.py
@@ -1,6 +1,3 @@
-
-
-from functools import partial
 from typing import Any, Callable, Optional, Sequence
 from llama_index.evaluation.base import BaseEvaluator, EvaluationResult
 from llama_index.indices.service_context import ServiceContext
@@ -11,41 +8,47 @@ class SemanticSimilarityEvaluator(BaseEvaluator):
     """Embedding similarity evaluator.
 
     Evaluate the quality of a question answering system by
-    comparing the similarity between embeddings of the generated answer 
+    comparing the similarity between embeddings of the generated answer
     and the reference answer.
 
     Args:
         service_context (Optional[ServiceContext]): Service context.
-        similarity_threshold (float): Embedding similarity threshold for "passing". 
+        similarity_threshold (float): Embedding similarity threshold for "passing".
             Defaults to 0.8.
     """
+
     def __init__(
-        self, 
+        self,
         service_context: Optional[ServiceContext] = None,
         similarity_fn: Optional[Callable[..., float]] = None,
-        similarity_mode: Optional[SimilarityMode] = None, 
+        similarity_mode: Optional[SimilarityMode] = None,
         similarity_threshold: float = 0.8,
     ) -> None:
         self._service_context = service_context or ServiceContext.from_defaults()
         if similarity_fn is None:
             similarity_mode = similarity_mode or SimilarityMode.DEFAULT
-            self._similarity_fn = partial(similarity, mode=similarity_mode)
+            self._similarity_fn = lambda x, y: similarity(x, y, mode=similarity_mode)
         else:
             if similarity_mode is not None:
-                raise ValueError('Cannot specify both similarity_fn and similarity_mode')
+                raise ValueError(
+                    "Cannot specify both similarity_fn and similarity_mode"
+                )
             self._similarity_fn = similarity_fn
 
         self._similarity_threshold = similarity_threshold
-    
+
     async def aevaluate(
-        self, 
-        query: Optional[str] = None, 
-        response: Optional[str] = None, 
-        contexts: Optional[Sequence[str]] = None, 
+        self,
+        query: Optional[str] = None,
+        response: Optional[str] = None,
+        contexts: Optional[Sequence[str]] = None,
         reference: Optional[str] = None,
-        **kwargs: Any
+        **kwargs: Any,
     ) -> EvaluationResult:
         del query, contexts, kwargs  # Unused
+
+        if response is None or reference is None:
+            raise ValueError("Must specify both response and reference")
 
         embed_model = self._service_context.embed_model
         response_embedding = await embed_model.aget_text_embedding(response)
@@ -56,9 +59,5 @@ class SemanticSimilarityEvaluator(BaseEvaluator):
         return EvaluationResult(
             score=similarity_score,
             passing=passing,
-            feedback=f"Similarity score: {similarity_score}"
-        )   
-
-
-
-    
+            feedback=f"Similarity score: {similarity_score}",
+        )
