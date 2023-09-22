2bcc3d50d5b170fae4110d3df89a5a28097c79ae
Melih Yildiz <mzyil12@gmail.com>
2023-09-22 18:19:34 +0100

    add mising file to __init__.py

--- llama_index/storage/index_store/__init__.py
+++ llama_index/storage/index_store/__init__.py
@@ -4,6 +4,7 @@ from llama_index.storage.index_store.simple_index_store import SimpleIndexStore
 from llama_index.storage.index_store.mongo_index_store import MongoIndexStore
 from llama_index.storage.index_store.redis_index_store import RedisIndexStore
 from llama_index.storage.index_store.pickled_keyval_index_store import PickledKVIndexStore
+from llama_index.storage.index_store.simple_pickled_index_store import SimplePickledKVStore
 
 __all__ = [
     "FirestoreKVStore",
@@ -11,5 +12,6 @@ __all__ = [
     "SimpleIndexStore",
     "MongoIndexStore",
     "RedisIndexStore",
-    "PickledKVIndexStore"
+    "PickledKVIndexStore",
+    "SimplePickledKVStore"
 ]
