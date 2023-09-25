8d2f95b800752d219067a01c2e030337c3751162
Jerry Liu <jerryjliu98@gmail.com>
2023-09-25 01:30:08 -0700

    cr

--- llama_index/evaluation/dataset_generation.py
+++ llama_index/evaluation/dataset_generation.py
@@ -248,7 +248,9 @@ class DatasetGenerator:
             query_ids = query_ids[:num]
             # truncate queries, responses to the subset of query ids
             queries = {query_id: queries[query_id] for query_id in query_ids}
-            responses_dict = {query_id: responses_dict[query_id] for query_id in query_ids}
+            responses_dict = {
+                query_id: responses_dict[query_id] for query_id in query_ids
+            }
 
         return QueryResponseDataset(queries=queries, responses=responses_dict)
 
