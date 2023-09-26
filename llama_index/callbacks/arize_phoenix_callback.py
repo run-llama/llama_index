baaf21e3ee20b6c2c475468db771352e943919e0
Roger Yang <roger.yang@arize.com>
2023-09-26 12:33:11 -0700

    fix typos

--- llama_index/callbacks/arize_phoenix_callback.py
+++ llama_index/callbacks/arize_phoenix_callback.py
@@ -4,12 +4,12 @@ from llama_index.callbacks.base_handler import BaseCallbackHandler
 
 def arize_phoenix_callback_handler(**kwargs: Any) -> BaseCallbackHandler:
     try:
-        from phoenix.trace.exporter import HttpExporter
         from phoenix.trace.llama_index import OpenInferenceTraceCallbackHandler
+        from phoenix.trace.exporter import HttpExporter
     except ImportError:
         raise ImportError(
-            "To use the Arize Phoenix Tracer you need to "
-            "have the latest `arize-phoenix` python package installed. "
+            "To use the Arize Phoenix tracer you need to "
+            "have the latest `arize-phoenix` Python package installed. "
             "Please install it with `pip install -q arize-phoenix`"
         )
     if "exporter" not in kwargs:
