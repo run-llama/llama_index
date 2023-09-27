44fbf2d02307e5f94191d71b4c212f5da166c452
James Braza <jamesbraza@gmail.com>
2023-09-27 16:07:48 -0700

    Fixed broken URL for source

--- llama_index/prompts/default_prompts.py
+++ llama_index/prompts/default_prompts.py
@@ -184,7 +184,7 @@ DEFAULT_SCHEMA_EXTRACT_PROMPT = PromptTemplate(
 )
 
 # NOTE: taken from langchain and adapted
-# https://tinyurl.com/b772sd77
+# https://github.com/langchain-ai/langchain/blob/v0.0.303/libs/langchain/langchain/chains/sql_database/prompt.py
 DEFAULT_TEXT_TO_SQL_TMPL = (
     "Given an input question, first create a syntactically correct {dialect} "
     "query to run, then look at the results of the query and return the answer. "
