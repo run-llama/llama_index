865de7049895328254ec16fa47c9ac1bf0c93b1d
Jerry Liu <jerryjliu98@gmail.com>
2023-09-29 22:42:56 -0700

    cr

--- llama_index/evaluation/dataset_generation.py
+++ llama_index/evaluation/dataset_generation.py
@@ -189,6 +189,7 @@ class DatasetGenerator:
         else:
             async_module = asyncio
 
+        summary_indices: List[SummaryIndex] = []
         for node in nodes:
             if num is not None and len(queries) >= num:
                 break
@@ -211,9 +212,10 @@ class DatasetGenerator:
                 self.question_gen_query,
             )
             query_tasks.append(task)
+            summary_indices.append(index)
 
         responses = await async_module.gather(*query_tasks)
-        for response in responses:
+        for idx, response in enumerate(responses):
             result = str(response).strip().split("\n")
             cleaned_questions = [
                 re.sub(r"^\d+[\).\s]", "", question).strip() for question in result
@@ -227,6 +229,7 @@ class DatasetGenerator:
             queries.update(cur_queries)
 
             if generate_response:
+                index = summary_indices[idx]
                 qr_tasks = []
                 cur_query_items = list(cur_queries.items())
                 cur_query_keys = [query_id for query_id, _ in cur_query_items]
