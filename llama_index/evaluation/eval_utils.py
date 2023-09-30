c6baab4974be36c73e9fbe5341e5d632f1bcc27d
Jerry Liu <jerryjliu98@gmail.com>
2023-09-29 22:47:24 -0700

    cr

--- llama_index/evaluation/eval_utils.py
+++ llama_index/evaluation/eval_utils.py
@@ -50,16 +50,23 @@ def get_responses(
 
 
 def get_results_df(
-    eval_results_list: List[EvaluationResult], 
-    names: List[str], 
-    metric_keys: List[str]
+    eval_results_list: List[EvaluationResult], names: List[str], metric_keys: List[str]
 ) -> pd.DataFrame:
+    """Get results df.
+
+    Args:
+        eval_results_list (List[EvaluationResult]):
+            List of evaluation results.
+        names (List[str]):
+            Names of the evaluation results.
+        metric_keys (List[str]):
+            List of metric keys to get.
+
+    """
     metric_dict = defaultdict(list)
     metric_dict["names"] = names
     for metric_key in metric_keys:
         for eval_results in eval_results_list:
-            mean_score = np.array(
-                [r.score for r in eval_results[metric_key]]
-            ).mean()
+            mean_score = np.array([r.score for r in eval_results[metric_key]]).mean()
             metric_dict[metric_key].append(mean_score)
     return pd.DataFrame(metric_dict)
--- tests/evaluation/test_dataset_generation.py
+++ tests/evaluation/test_dataset_generation.py
@@ -6,17 +6,16 @@ from llama_index.prompts.base import PromptTemplate
 from llama_index.prompts.prompt_type import PromptType
 from llama_index.schema import TextNode
 
+
 def test_dataset_generation(
     mock_service_context: ServiceContext,
 ) -> None:
     """Test dataset generation."""
-    
-    test_nodes = [
-        TextNode(text="hello_world"),
-        TextNode(text="foo_bar")
-    ]
 
-    question_gen_prompt = PromptTemplate("""\
+    test_nodes = [TextNode(text="hello_world"), TextNode(text="foo_bar")]
+
+    question_gen_prompt = PromptTemplate(
+        """\
 Context information is below.
 ---------------------
 {context_str}
@@ -24,13 +23,15 @@ Context information is below.
 Given the context information and not prior knowledge.
 generate only questions based on the below query.
 {query_str}
-""", prompt_type=PromptType.QUESTION_ANSWER)
+""",
+        prompt_type=PromptType.QUESTION_ANSWER,
+    )
 
     dataset_generator = DatasetGenerator(
         test_nodes,
         service_context=mock_service_context,
         text_question_template=question_gen_prompt,
-        question_gen_query="gen_question"
+        question_gen_query="gen_question",
     )
     eval_dataset = dataset_generator.generate_dataset_from_nodes()
     qr_pairs = eval_dataset.qr_pairs
@@ -42,6 +43,3 @@ generate only questions based on the below query.
     assert qr_pairs[0][1] == "gen_question:hello_world:hello_world"
     assert qr_pairs[1][0] == "gen_question:foo_bar"
     assert qr_pairs[1][1] == "gen_question:foo_bar:foo_bar"
-        
-        
-        
