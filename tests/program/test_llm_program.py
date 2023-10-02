561bc85b34d40ce38be83c599206fac1f0233fb6
Logan Markewich <logan.markewich@live.com>
2023-10-02 12:06:54 -0600

    use proper kwarg

--- tests/program/test_llm_program.py
+++ tests/program/test_llm_program.py
@@ -58,7 +58,7 @@ def test_llm_program() -> None:
 def test_llm_program_with_messages() -> None:
     """Test LLM program."""
     messages = [ChatMessage(role=MessageRole.USER, content="Test")]
-    prompt = ChatPromptTemplate(messages=messages)
+    prompt = ChatPromptTemplate(message_templates=messages)
     output_parser = PydanticOutputParser(output_cls=TestModel)
     llm_program = LLMTextCompletionProgram.from_defaults(
         output_parser=output_parser,
@@ -74,7 +74,7 @@ def test_llm_program_with_messages() -> None:
 def test_llm_program_with_messages_and_chat() -> None:
     """Test LLM program."""
     messages = [ChatMessage(role=MessageRole.USER, content="Test")]
-    prompt = ChatPromptTemplate(messages=messages)
+    prompt = ChatPromptTemplate(message_templates=messages)
     output_parser = PydanticOutputParser(output_cls=TestModel)
     llm_program = LLMTextCompletionProgram.from_defaults(
         output_parser=output_parser,
