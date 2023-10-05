9e8f7a050b648c23969840407927f13a8e93c01a
James Braza <jamesbraza@gmail.com>
2023-10-05 13:57:52 -0400

    Added serialized models test case for LocalAI

--- tests/llms/test_localai.py
+++ tests/llms/test_localai.py
@@ -81,3 +81,13 @@ def test_forgetting_kwarg() -> None:
         with pytest.raises(NotImplementedError, match="/chat/completions"):
             llm.complete("A long time ago in a galaxy far, far away")
     mock_completion.assert_not_called()
+
+
+def test_serialization() -> None:
+    llm = LocalAI(model="models/placeholder.gguf", max_tokens=42, context_window=43)
+
+    serialized = llm.to_dict()
+    # Check OpenAI base class specifics
+    assert serialized["max_tokens"] == 42
+    # Check LocalAI subclass specifics
+    assert serialized["context_window"] == 43
