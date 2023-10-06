a393ac4bc13432f0e4a8f25525c09fea5d28e959
Ryan Peach <ryanpeach@users.noreply.github.com>
2023-10-06 14:27:27 -0400

    Update rake_base.py

--- llama_index/indices/keyword_table/rake_base.py
+++ llama_index/indices/keyword_table/rake_base.py
@@ -8,7 +8,7 @@ from typing import Set
 
 from llama_index.indices.keyword_table.base import BaseKeywordTableIndex
 from llama_index.indices.keyword_table.utils import rake_extract_keywords
-
+from llama_index.indices.keyword_table.base import KeywordTableRetrieverMode
 
 class RAKEKeywordTableIndex(BaseKeywordTableIndex):
     """RAKE Keyword Table Index.
@@ -20,6 +20,15 @@ class RAKEKeywordTableIndex(BaseKeywordTableIndex):
     def _extract_keywords(self, text: str) -> Set[str]:
         """Extract keywords from text."""
         return rake_extract_keywords(text, max_keywords=self.max_keywords_per_chunk)
+
+    def as_retriever(
+        self,
+        retriever_mode: Union[
+            str, KeywordTableRetrieverMode
+        ] = KeywordTableRetrieverMode.RAKE,
+        **kwargs: Any,
+    ) -> BaseRetriever:
+        return super().as_retriever(retriever_mode=retriever_mode, **kwargs)
 
 
 # legacy
