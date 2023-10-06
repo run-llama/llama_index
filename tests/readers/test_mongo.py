f98be62cb827c11e8b612397724c7ba5545c8f72
David Chiu <david20571015@gmail.com>
2023-10-06 15:26:22 +0800

    feat: adding metadata to Document

--- tests/readers/test_mongo.py
+++ tests/readers/test_mongo.py
@@ -47,3 +47,26 @@ def test_load_data_with_field_name() -> None:
         assert documents[0].get_content() == "first1second1third1"
         assert documents[1].get_content() == "first2second2third2"
         assert documents[2].get_content() == "first3second3third3"
+
+
+@pytest.mark.skipif(MongoClient is None, reason="pymongo not installed")
+def test_load_data_with_metadata_name() -> None:
+    """Test Mongo reader using passed in metadata_name."""
+    mock_cursor = [
+        {"first": "first1", "second": "second1", "third": "third1"},
+        {"first": "first2", "second": "second2", "third": "third2"},
+        {"first": "first3", "second": "second3", "third": "third3"},
+    ]
+
+    with patch("pymongo.collection.Collection.find") as mock_find:
+        mock_find.return_value = mock_cursor
+
+        reader = SimpleMongoReader("host", 1)
+        documents = reader.load_data(
+            "my_db", "my_collection", field_names=["first"], metadata_names=["second", "third"]
+        )
+
+        assert len(documents) == 3
+        assert documents[0].get_metadata_str() == "second: second1\nthird: third1"
+        assert documents[1].get_metadata_str() == "second: second2\nthird: third2"
+        assert documents[2].get_metadata_str() == "second: second3\nthird: third3"
