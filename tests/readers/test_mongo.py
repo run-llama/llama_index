b054697f25c92b9e175b5cc59957eaae2ce96905
David Chiu <david20571015@gmail.com>
2023-10-06 15:41:18 +0800

    feat: able to concatenate list fields

--- tests/readers/test_mongo.py
+++ tests/readers/test_mongo.py
@@ -30,9 +30,9 @@ def test_load_data() -> None:
 def test_load_data_with_field_name() -> None:
     """Test Mongo reader using passed in field_names."""
     mock_cursor = [
-        {"first": "first1", "second": "second1", "third": "third1"},
-        {"first": "first2", "second": "second2", "third": "third2"},
-        {"first": "first3", "second": "second3", "third": "third3"},
+        {"first": "first1", "second": ["second1", "second11"], "third": "third1"},
+        {"first": "first2", "second": ["second2", "second22"], "third": "third2"},
+        {"first": "first3", "second": ["second3", "second33"], "third": "third3"},
     ]
 
     with patch("pymongo.collection.Collection.find") as mock_find:
@@ -44,6 +44,6 @@ def test_load_data_with_field_name() -> None:
         )
 
         assert len(documents) == 3
-        assert documents[0].get_content() == "first1second1third1"
-        assert documents[1].get_content() == "first2second2third2"
-        assert documents[2].get_content() == "first3second3third3"
+        assert documents[0].get_content() == "first1second1second11third1"
+        assert documents[1].get_content() == "first2second2second22third2"
+        assert documents[2].get_content() == "first3second3second33third3"
