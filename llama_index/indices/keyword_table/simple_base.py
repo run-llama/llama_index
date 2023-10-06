f53522bab68efd2f62ff2a00f5ee7e6c27df8afa
Ryan Peach <ryanpeach@users.noreply.github.com>
2023-10-06 14:24:31 -0400

    Added retriever_mode super call to SimpleKeywordTable

--- llama_index/indices/keyword_table/simple_base.py
+++ llama_index/indices/keyword_table/simple_base.py
@@ -10,6 +10,7 @@ from typing import Set
 from llama_index.indices.keyword_table.base import BaseKeywordTableIndex
 from llama_index.indices.keyword_table.utils import simple_extract_keywords
 from llama_index.prompts.default_prompts import DEFAULT_QUERY_KEYWORD_EXTRACT_TEMPLATE
+from llama_index.indices.keyword_table.base import KeywordTableRetrieverMode
 
 DQKET = DEFAULT_QUERY_KEYWORD_EXTRACT_TEMPLATE
 
@@ -25,6 +26,14 @@ class SimpleKeywordTableIndex(BaseKeywordTableIndex):
         """Extract keywords from text."""
         return simple_extract_keywords(text, self.max_keywords_per_chunk)
 
+    def as_retriever(
+        self,
+        retriever_mode: Union[
+            str, KeywordTableRetrieverMode
+        ] = KeywordTableRetrieverMode.SIMPLE,
+        **kwargs: Any,
+    ) -> BaseRetriever:
+        return super().as_retriever(retriever_mode=retriever_mode, **kwargs)
 
 # legacy
 GPTSimpleKeywordTableIndex = SimpleKeywordTableIndex
