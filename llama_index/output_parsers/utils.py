4e5789f8e415c902cb54bb06e00b7ac5a4b9e594
Logan Markewich <logan.markewich@live.com>
2023-10-10 21:08:22 -0600

    check for negative one in marshal

--- llama_index/output_parsers/utils.py
+++ llama_index/output_parsers/utils.py
@@ -21,10 +21,11 @@ def _marshal_llm_to_json(output: str) -> str:
         A string containing a valid JSON object or array.
     """
     output = output.strip().replace("{{", "{").replace("}}", "}")
+
     left_square = output.find("[")
     left_brace = output.find("{")
 
-    if left_square < left_brace:
+    if left_square < left_brace and left_square != -1:
         left = left_square
         right = output.rfind("]")
     else:
