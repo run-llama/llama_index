92967b7ee1b26c440b9bf2f58a79356b4640d8ef
James Braza <jamesbraza@gmail.com>
2023-10-13 19:21:02 -0400

    `CompletionResponse` docs on fields (#8120)
    * Documented Fields in CompletionResponse
    
    * Documented text and additional_kwargs

--- llama_index/llms/base.py
+++ llama_index/llms/base.py
@@ -50,7 +50,17 @@ ChatResponseAsyncGen = AsyncGenerator[ChatResponse, None]
 
 # ===== Generic Model Output - Completion =====
 class CompletionResponse(BaseModel):
-    """Completion response."""
+    """
+    Completion response.
+
+    Fields:
+        text: Text content of the response if not streaming, or if streaming,
+            the current extent of streamed text.
+        additional_kwargs: Additional information on the response(i.e. token
+            counts, function calling information).
+        raw: Optional raw JSON that was parsed to populate text, if relevant.
+        delta: New text that just streamed in (only relevant when streaming).
+    """
 
     text: str
     additional_kwargs: dict = Field(default_factory=dict)
