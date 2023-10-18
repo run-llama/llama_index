8ef8d9c5fb746a868f6263b09ef77f2f28d03cdf
Jing <lscmirror@gmail.com>
2023-10-17 19:37:19 -0700

    Fix Firestore client info (#8166)

--- llama_index/storage/kvstore/firestore_kvstore.py
+++ llama_index/storage/kvstore/firestore_kvstore.py
@@ -11,7 +11,7 @@ SLASH_REPLACEMENT = "_"
 IMPORT_ERROR_MSG = (
     "`firestore` package not found, please run `pip3 install google-cloud-firestore`"
 )
-CLIENT_INFO = "LlamaIndex"
+USER_AGENT = "LlamaIndex"
 DEFAULT_FIRESTORE_DATABASE = "(default)"
 
 
@@ -28,11 +28,16 @@ class FirestoreKVStore(BaseKVStore):
     ) -> None:
         try:
             from google.cloud import firestore_v1 as firestore
+            from google.cloud.firestore_v1.services.firestore.transports.base import (
+                DEFAULT_CLIENT_INFO,
+            )
         except ImportError:
             raise ImportError(IMPORT_ERROR_MSG)
 
+        client_info = DEFAULT_CLIENT_INFO
+        client_info.user_agent = USER_AGENT
         self._db = firestore.client.Client(
-            project=project, database=database, client_info=CLIENT_INFO
+            project=project, database=database, client_info=client_info
         )
 
     def firestore_collection(self, collection: str) -> str:
