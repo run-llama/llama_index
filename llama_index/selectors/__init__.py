e17c5f5dade1c83f9f5b4fc25c1716fbd4839ad3
Jerry Liu <jerryjliu98@gmail.com>
2023-10-17 23:20:08 -0700

    cr

--- llama_index/selectors/__init__.py
+++ llama_index/selectors/__init__.py
@@ -1,9 +1,14 @@
 from llama_index.selectors.llm_selectors import LLMMultiSelector, LLMSingleSelector
 from llama_index.selectors.types import BaseSelector, SelectorResult
+from llama_index.selectors.embedding_selectors import EmbeddingSingleSelector
+from llama_index.selectors.pydantic_selectors import PydanticSingleSelector, PydanticMultiSelector
 
 __all__ = [
     "BaseSelector",
     "SelectorResult",
     "LLMSingleSelector",
     "LLMMultiSelector",
+    "EmbeddingSingleSelector",
+    "PydanticSingleSelector",
+    "PydanticMultiSelector",
 ]
