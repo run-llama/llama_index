d5006f432c0d2eabbd0fa2e5def596c75aeb175f
James Braza <jamesbraza@gmail.com>
2023-10-19 12:05:25 -0700

    Added comment on why strip is smart

--- llama_index/embeddings/huggingface_utils.py
+++ llama_index/embeddings/huggingface_utils.py
@@ -54,6 +54,8 @@ def get_query_instruct_for_model_name(model_name: str) -> str:
 def format_query(query: str, model_name: str, instruction: Optional[str] = None) -> str:
     if instruction is None:
         instruction = get_query_instruct_for_model_name(model_name)
+    # NOTE: strip() enables backdoor for defeating instruction prepend by
+    # passing empty string
     return f"{instruction} {query}".strip()
 
 
@@ -65,4 +67,6 @@ def get_text_instruct_for_model_name(model_name: str) -> str:
 def format_text(text: str, model_name: str, instruction: Optional[str] = None) -> str:
     if instruction is None:
         instruction = get_text_instruct_for_model_name(model_name)
+    # NOTE: strip() enables backdoor for defeating instruction prepend by
+    # passing empty string
     return f"{instruction} {text}".strip()
