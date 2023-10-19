0b2163ccf3b40d75c70385c7cd5e5b36edb618f6
James Braza <jamesbraza@gmail.com>
2023-10-19 13:05:13 -0700

    Cleaned up conditionals within huggingface_utils

--- llama_index/embeddings/huggingface_utils.py
+++ llama_index/embeddings/huggingface_utils.py
@@ -42,13 +42,11 @@ def get_query_instruct_for_model_name(model_name: str) -> str:
     """Get query text instruction for a given model name."""
     if model_name in INSTRUCTOR_MODELS:
         return DEFAULT_QUERY_INSTRUCTION
-    elif model_name in BGE_MODELS:
+    if model_name in BGE_MODELS:
         if "zh" in model_name:
             return DEFAULT_QUERY_BGE_INSTRUCTION_ZH
-        else:
-            return DEFAULT_QUERY_BGE_INSTRUCTION_EN
-    else:
-        return ""
+        return DEFAULT_QUERY_BGE_INSTRUCTION_EN
+    return ""
 
 
 def format_query(query: str, model_name: str, instruction: Optional[str] = None) -> str:
