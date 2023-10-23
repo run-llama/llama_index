c0e53e28f0643cf2fc626996ae47a73df8a83e78
Emanuel Ferreira <contatoferreirads@gmail.com>
2023-10-23 16:04:14 -0300

    fix(json-path): multiple expressions parsing (#8251)
    * fix(json-path): multiple expressions parsing
    
    * chore: revert return type

--- llama_index/indices/struct_store/json_query.py
+++ llama_index/indices/struct_store/json_query.py
@@ -37,15 +37,31 @@ DEFAULT_RESPONSE_SYNTHESIS_PROMPT = PromptTemplate(
 
 
 def default_output_processor(llm_output: str, json_value: JSONType) -> JSONType:
-    """Default output processor that executes the JSON Path query."""
+    """Default output processor that extracts values based on JSON Path expressions."""
+    # Split the given string into separate JSON Path expressions
+    expressions = [expr.strip() for expr in llm_output.split(",")]
+
     try:
         from jsonpath_ng.ext import parse
         from jsonpath_ng.jsonpath import DatumInContext
     except ImportError as exc:
+        IMPORT_ERROR_MSG = "You need to install jsonpath-ng to use this function!"
         raise ImportError(IMPORT_ERROR_MSG) from exc
 
-    datum: List[DatumInContext] = parse(llm_output).find(json_value)
-    return [d.value for d in datum]
+    results = {}
+
+    for expression in expressions:
+        try:
+            datum: List[DatumInContext] = parse(expression).find(json_value)
+            if datum:
+                key = expression.split(".")[
+                    -1
+                ]  # Extracting "title" from "$.title", for example
+                results[key] = datum[0].value
+        except Exception as exc:
+            raise ValueError(f"Invalid JSON Path: {expression}") from exc
+
+    return results
 
 
 class JSONQueryEngine(BaseQueryEngine):
