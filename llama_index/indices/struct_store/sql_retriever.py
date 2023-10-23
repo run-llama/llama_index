c7d61828b0118f222dee19a8ac634d30006bfac2
Jerry Liu <jerryjliu98@gmail.com>
2023-10-22 23:55:25 -0700

    cr

--- llama_index/indices/struct_store/sql_retriever.py
+++ llama_index/indices/struct_store/sql_retriever.py
@@ -164,6 +164,7 @@ class NLSQLRetriever(BaseRetriever):
         context_str_prefix (str): Prefix for context string. Defaults to None.
         service_context (ServiceContext): Service context. Defaults to None.
         return_raw (bool): Whether to return plain-text dump of SQL results, or parsed into Nodes.
+        handle_sql_errors (bool): Whether to handle SQL errors. Defaults to True.
 
     """
 
@@ -178,6 +179,7 @@ class NLSQLRetriever(BaseRetriever):
         sql_parser_mode: SQLParserMode = SQLParserMode.DEFAULT,
         service_context: Optional[ServiceContext] = None,
         return_raw: bool = True,
+        handle_sql_errors: bool = True,
         **kwargs: Any,
     ) -> None:
         """Initialize params."""
@@ -191,6 +193,7 @@ class NLSQLRetriever(BaseRetriever):
         self._text_to_sql_prompt = text_to_sql_prompt or DEFAULT_TEXT_TO_SQL_PROMPT
         self._sql_parser_mode = sql_parser_mode
         self._sql_parser = self._load_sql_parser(sql_parser_mode, self._service_context)
+        self._handle_sql_errors = handle_sql_errors
 
     def _load_sql_parser(
         self, sql_parser_mode: SQLParserMode, service_context: ServiceContext
@@ -251,9 +254,19 @@ class NLSQLRetriever(BaseRetriever):
         )
         # assume that it's a valid SQL query
         logger.debug(f"> Predicted SQL query: {sql_query_str}")
-        retrieved_nodes, metadata = self._sql_retriever.retrieve_with_metadata(
-            sql_query_str
-        )
+        try:
+            retrieved_nodes, metadata = self._sql_retriever.retrieve_with_metadata(
+                sql_query_str
+            )
+        except BaseException as e:
+            # if handle_sql_errors is True, then return error message
+            if self._handle_sql_errors:
+                err_node = TextNode(text=f"Error: {e!s}")
+                retrieved_nodes = [NodeWithScore(node=err_node)]
+                metadata = {}
+            else:
+                raise
+
         return retrieved_nodes, {"sql_query": sql_query_str, **metadata}
 
     async def aretrieve_with_metadata(
@@ -279,9 +292,19 @@ class NLSQLRetriever(BaseRetriever):
         )
         # assume that it's a valid SQL query
         logger.debug(f"> Predicted SQL query: {sql_query_str}")
-        retrieved_nodes, metadata = await self._sql_retriever.aretrieve_with_metadata(
-            sql_query_str
-        )
+        try:
+            (
+                retrieved_nodes,
+                metadata,
+            ) = await self._sql_retriever.aretrieve_with_metadata(sql_query_str)
+        except BaseException as e:
+            # if handle_sql_errors is True, then return error message
+            if self._handle_sql_errors:
+                err_node = TextNode(text=f"Error: {e!s}")
+                retrieved_nodes = [NodeWithScore(node=err_node)]
+                metadata = {}
+            else:
+                raise
         return retrieved_nodes, {"sql_query": sql_query_str, **metadata}
 
     def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
