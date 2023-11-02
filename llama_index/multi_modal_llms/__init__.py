376ec09e051e14675c1e2c93f5e0299564aa4a56
haotian zhang <haotianzhang@haotians-MacBook-Pro-3.local>
2023-11-02 14:58:50 -0400

    fix

--- llama_index/multi_modal_llms/__init__.py
+++ llama_index/multi_modal_llms/__init__.py
@@ -1,5 +1,13 @@
+from llama_index.multi_modal_llms.base import (
+    MultiModalCompletionResponse,
+    MultiModalCompletionResponseGen,
+    MultiModalLLMMetadata,
+)
 from llama_index.multi_modal_llms.fuyu import Fuyu
 
 __all__ = [
     "Fuyu",
+    "MultiModalCompletionResponse",
+    "MultiModalCompletionResponseGen",
+    "MultiModalLLMMetadata",
 ]
