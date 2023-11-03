14c9994a111e8e5f552c1221dca8c45de466f13d
Jeremy Dyer <jdye64@gmail.com>
2023-11-03 13:43:06 -0400

    Add TensorRTLLM to __init__.py

--- llama_index/llms/__init__.py
+++ llama_index/llms/__init__.py
@@ -34,6 +34,7 @@ from llama_index.llms.palm import PaLM
 from llama_index.llms.portkey import Portkey
 from llama_index.llms.predibase import PredibaseLLM
 from llama_index.llms.replicate import Replicate
+from llama_index.llms.trt_llm import TensorRTLLM
 from llama_index.llms.vertex import Vertex
 from llama_index.llms.watsonx import WatsonX
 from llama_index.llms.xinference import Xinference
@@ -75,6 +76,7 @@ __all__ = [
     "Portkey",
     "PredibaseLLM",
     "Replicate",
+    "TensorRTLLM"
     "WatsonX",
     "Xinference",
     "Vertex",
