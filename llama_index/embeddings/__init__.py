def285ef655f6ca2ca195902650c1a8a83a03c74
haotian zhang <haotianzhang@haotians-MacBook-Pro-3.local>
2023-11-05 17:49:44 -0500

    Initialize CLIP image embeddings

--- llama_index/embeddings/__init__.py
+++ llama_index/embeddings/__init__.py
@@ -7,6 +7,7 @@ from llama_index.embeddings.adapter import (
 from llama_index.embeddings.base import SimilarityMode
 from llama_index.embeddings.bedrock import BedrockEmbedding
 from llama_index.embeddings.clarifai import ClarifaiEmbedding
+from llama_index.embeddings.clip import ClipEmbedding
 from llama_index.embeddings.cohereai import CohereEmbedding
 from llama_index.embeddings.elasticsearch import (
     ElasticsearchEmbedding,
@@ -33,6 +34,7 @@ __all__ = [
     "AdapterEmbeddingModel",
     "BedrockEmbedding",
     "ClarifaiEmbedding",
+    "ClipEmbedding",
     "CohereEmbedding",
     "DEFAULT_HUGGINGFACE_EMBEDDING_MODEL",
     "ElasticsearchEmbedding",
