860126864e55b5f090b1e9c8c28d817103dd2dbf
Jerry Liu <jerryjliu98@gmail.com>
2023-11-04 22:45:44 -0700

    cr

---
+++ llama_index/param_tuner/__init__.py
@@ -0,0 +1,14 @@
+from llama_index.param_tuner.base import (
+    BaseParamTuner,
+    ParamTuner,
+    AsyncParamTuner,
+    RayTuneParamTuner
+)
+
+
+__all__ = [
+    "BaseParamTuner",
+    "ParamTuner",
+    "AsyncParamTuner",
+    "RayTuneParamTuner"
+]
--- llama_index/param_tuner/base.py
+++ llama_index/param_tuner/base.py
@@ -2,11 +2,13 @@
 
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Awaitable
+from llama_index.bridge.pydantic import PrivateAttr
 from pydantic import BaseModel, Field
 from copy import deepcopy
 from typing import Callable
 from llama_index.utils import get_tqdm_iterable
+import asyncio
 
 
 class RunResult(BaseModel):
@@ -52,9 +54,6 @@ def generate_param_combinations(param_dict: Dict[str, Any]) -> List[Dict[str, An
 
 class BaseParamTuner(BaseModel):
     """Base param tuner."""
-    param_fn: Callable[[Dict[str, Any]], RunResult] = Field(
-        ..., description="Function to run with parameters."
-    )
     param_dict: Dict[str, Any] = Field(
         ..., description="A dictionary of parameters to iterate over."
     )
@@ -66,6 +65,14 @@ class BaseParamTuner(BaseModel):
     @abstractmethod
     def tune(self) -> TunedResult:
         """Tune parameters."""
+
+    async def atune(self) -> TunedResult:
+        """Async Tune parameters.
+
+        Override if you implement a native async method.
+        
+        """
+        return self.tune()
 
 
 class ParamTuner(BaseParamTuner):
@@ -81,19 +88,9 @@ class ParamTuner(BaseParamTuner):
         fixed_param_dict(Dict): A dictionary of fixed parameters passed to each job.
     
     """
-
-    # def __init__(
-    #     self, 
-    #     param_fn: Callable[[Dict[str, Any]], RunResult],
-    #     param_dict: Dict, 
-    #     fixed_param_dict: Dict,
-    #     show_progress: bool = False
-    # ) -> None:
-    #     """Init params."""
-    #     self._param_dict = param_dict
-    #     self._fixed_param_dict = fixed_param_dict
-    #     self._param_fn = param_fn
-    #     self._show_progress = show_progress
+    param_fn: Callable[[Dict[str, Any]], RunResult] = Field(
+        ..., description="Function to run with parameters."
+    )
 
     def tune(self) -> TunedResult:
         """Run tuning."""
@@ -125,7 +122,83 @@ class ParamTuner(BaseParamTuner):
         )
 
         return TunedResult(run_results=sorted_run_results, best_idx=0)
-            
+
+
+class AsyncParamTuner(BaseParamTuner):
+    """Async Parameter tuner.
+    
+    Args:
+        param_dict(Dict): A dictionary of parameters to iterate over.
+            Example param_dict:
+            {
+                "num_epochs": [10, 20],
+                "batch_size": [8, 16, 32],
+            }
+        fixed_param_dict(Dict): A dictionary of fixed parameters passed to each job.
+        aparam_fn (Callable): An async function to run with parameters.
+        num_workers (int): Number of workers to use.
+    
+    """
+    aparam_fn: Callable[[Dict[str, Any]], Awaitable[RunResult]] = Field(
+        ..., description="Async function to run with parameters."
+    )
+    num_workers: int = Field(
+        2, description="Number of workers to use."
+    )
+
+    _semaphore: asyncio.Semaphore = PrivateAttr()
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Init params."""
+        super().__init__(*args, **kwargs)
+        self._semaphore = asyncio.Semaphore(self.num_workers)
+
+    async def atune(self) -> TunedResult:
+        """Run tuning."""
+        # each key in param_dict is a parameter to tune, each val
+        # is a list of values to try
+        # generate combinations of parameters from the param_dict
+        param_combinations = generate_param_combinations(self.param_dict)
+
+        # for each combination, run the job with the arguments
+        # in args_dict
+
+        async def aparam_fn_worker(
+            semaphore: asyncio.Semaphore,
+            full_param_dict: Dict[str, Any],
+        ) -> RunResult:
+            """Async param fn worker."""
+            async with semaphore:
+                return await self.aparam_fn(full_param_dict)
+        
+        all_run_results = []
+        run_jobs = []
+        for param_combination in param_combinations:
+            full_param_dict = {
+                **self.fixed_param_dict,
+                **param_combination,
+            }
+            run_jobs.append(aparam_fn_worker(self._semaphore, full_param_dict))
+            # run_jobs.append(self.aparam_fn(full_param_dict))
+
+        if self.show_progress:
+            from tqdm.asyncio import tqdm_asyncio
+
+            all_run_results = await tqdm_asyncio.gather(*run_jobs)
+        else:
+            all_run_results = await asyncio.gather(*run_jobs)
+
+        # sort the results by score
+        sorted_run_results = sorted(
+            all_run_results, key=lambda x: x.score, reverse=True
+        )
+
+        return TunedResult(run_results=sorted_run_results, best_idx=0)
+
+    def tune(self) -> TunedResult:
+        """Run tuning."""
+        return asyncio.run(self.atune())
+
 
 class RayTuneParamTuner(BaseParamTuner):
     """Parameter tuner powered by Ray Tune.
@@ -140,6 +213,9 @@ class RayTuneParamTuner(BaseParamTuner):
         fixed_param_dict(Dict): A dictionary of fixed parameters passed to each job.
     
     """
+    param_fn: Callable[[Dict[str, Any]], RunResult] = Field(
+        ..., description="Function to run with parameters."
+    )
 
     run_config_dict: Optional[dict] = Field(
         default=None, description="Run config dict for Ray Tune."
