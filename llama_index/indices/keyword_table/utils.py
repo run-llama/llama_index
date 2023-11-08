e8c1408123dc1dc401032842b8d53f01e096ab46
Ryan Peach <ryanpeach@me.com>
2023-11-08 17:15:05 -0500

    Made an index example

--- llama_index/indices/keyword_table/utils.py
+++ llama_index/indices/keyword_table/utils.py
@@ -13,7 +13,7 @@ def simple_extract_keywords(
     text_chunk: str, max_keywords: Optional[int] = None, filter_stopwords: bool = True
 ) -> Set[str]:
     """Extract keywords with simple algorithm."""
-    tokens = [t.strip().lower() for t in re.findall(r"\w+", text_chunk)]
+    tokens = [t.strip().lower() for t in re.findall(r"\S+", text_chunk)]
     if filter_stopwords:
         tokens = [t for t in tokens if t not in globals_helper.stopwords]
     value_counts = pd.Series(tokens).value_counts()
--- llama_index/node_parser/__init__.py
+++ llama_index/node_parser/__init__.py
@@ -8,11 +8,13 @@ from llama_index.node_parser.hierarchical import (
 from llama_index.node_parser.interface import NodeParser
 from llama_index.node_parser.sentence_window import SentenceWindowNodeParser
 from llama_index.node_parser.simple import SimpleNodeParser
+from llama_index.node_parser.code_hierarchy import CodeHierarchyNodeParser
 from llama_index.node_parser.unstructured_element import (
     UnstructuredElementNodeParser,
 )
 
 __all__ = [
+    "CodeHierarchyNodeParser",
     "SimpleNodeParser",
     "SentenceWindowNodeParser",
     "NodeParser",
