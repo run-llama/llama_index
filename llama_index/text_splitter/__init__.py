14dcdf9c644efe09d7702d67df52e2736a0f0efa
Logan Markewich <logan.markewich@live.com>
2023-11-08 15:00:19 -0600

    imports

--- llama_index/text_splitter/__init__.py
+++ llama_index/text_splitter/__init__.py
@@ -1,9 +1,9 @@
 # TODO: Deprecated import support for old text splitters
-from llama_index.node_parser.text.code import CodeSplitter as CodeSplitter
+from llama_index.node_parser.text.code import CodeSplitter
 from llama_index.node_parser.text.sentence import (
-    SentenceSplitter as SentenceSplitter,
+    SentenceSplitter,
 )
-from llama_index.node_parser.text.token import TokenTextSplitter as TokenTextSplitter
+from llama_index.node_parser.text.token import TokenTextSplitter
 
 __all__ = [
     "SentenceSplitter",
