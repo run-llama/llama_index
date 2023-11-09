f4292b228ec77042404763359eaec81439c4f955
Logan Markewich <logan.markewich@live.com>
2023-11-09 15:24:08 -0600

    more linting

--- tests/ingestion/test_cache.py
+++ tests/ingestion/test_cache.py
@@ -22,7 +22,9 @@ def test_cache() -> None:
     new_nodes = transformation([node])
     cache.put(hash, new_nodes)
 
-    assert cache.get(hash)[0].get_content() == new_nodes[0].get_content()
+    cache_hit = cache.get(hash)
+    assert cache_hit is not None
+    assert cache_hit[0].get_content() == new_nodes[0].get_content()
 
     new_hash = get_transformation_hash(new_nodes, transformation)
     assert cache.get(new_hash) is None
