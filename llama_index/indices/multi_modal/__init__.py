18b15b66f360f3ff48020b4cfc1dd5758a66e026
Haotian Zhang <socool.king@gmail.com>
2023-11-08 21:35:41 -0500

    Add Multi Modal Retriever (#8787)
    * Initialize Multi Modal Retrieval
    
    * cr
    
    * cr
    
    * cr
    
    * add async
    
    * add seperate top k
    
    * fix linting
    
    ---------
    
    Co-authored-by: haotian zhang <haotianzhang@haotians-MacBook-Pro-3.local>
    Co-authored-by: Logan Markewich <logan.markewich@live.com>

--- llama_index/indices/multi_modal/__init__.py
+++ llama_index/indices/multi_modal/__init__.py
@@ -1,7 +1,9 @@
 """Vector-store based data structures."""
 
 from llama_index.indices.multi_modal.base import MultiModalVectorStoreIndex
+from llama_index.indices.multi_modal.retriever import MutliModalVectorIndexRetriever
 
 __all__ = [
     "MultiModalVectorStoreIndex",
+    "MutliModalVectorIndexRetriever",
 ]
