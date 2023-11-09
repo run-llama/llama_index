42000abaad74a23c7d66324e32bd94429d296b42
Andrei Fajardo <andrei@nerdai.io>
2023-11-08 23:05:45 -0500

    have to_openai_function just call to_openai_tool

--- llama_index/llms/openai_utils.py
+++ llama_index/llms/openai_utils.py
@@ -280,12 +280,7 @@ def to_openai_function(pydantic_class: Type[BaseModel]) -> Dict[str, Any]:
 
     Convert pydantic class to OpenAI function.
     """
-    schema = pydantic_class.schema()
-    return {
-        "name": schema["title"],
-        "description": schema["description"],
-        "parameters": pydantic_class.schema(),
-    }
+    return to_openai_tool(pydantic_class)
 
 
 def to_openai_tool(pydantic_class: Type[BaseModel]) -> Dict[str, Any]:
