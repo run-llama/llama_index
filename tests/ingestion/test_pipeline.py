b582b26411818414229eaf311082eb992e0f699f
Simon Suo <simonsdsuo@gmail.com>
2023-11-08 17:56:30 -0800

    wip

--- tests/ingestion/test_pipeline.py
+++ tests/ingestion/test_pipeline.py
@@ -1,4 +1,3 @@
-import pytest
 from llama_index.embeddings import OpenAIEmbedding
 from llama_index.extractors import KeywordExtractor
 from llama_index.ingestion.pipeline import IngestionPipeline
@@ -10,7 +9,6 @@ from llama_index.schema import Document
 
 def test_build_pipeline() -> None:
     pipeline = IngestionPipeline(
-        name="Test",
         reader=ReaderConfig(
             reader=StringIterableReader(), reader_kwargs={"texts": ["This is a test."]}
         ),
@@ -23,12 +21,10 @@ def test_build_pipeline() -> None:
     )
 
     assert len(pipeline.transformations) == 3
-    assert pipeline.name == "Test"
 
 
-def test_run_local_pipeline() -> None:
+def test_run_pipeline() -> None:
     pipeline = IngestionPipeline(
-        name="Test",
         reader=ReaderConfig(
             reader=StringIterableReader(), reader_kwargs={"texts": ["This is a test."]}
         ),
@@ -39,65 +35,7 @@ def test_run_local_pipeline() -> None:
         ],
     )
 
-    nodes = pipeline.run_local()
+    nodes = pipeline.run()
 
-    assert len(nodes) == 2
-    assert len(nodes[0].metadata) > 0
-
-
-@pytest.mark.integration()
-def test_register() -> None:
-    pipeline = IngestionPipeline(
-        name="Test",
-        reader=ReaderConfig(
-            reader=StringIterableReader(), reader_kwargs={"texts": ["This is a test."]}
-        ),
-        documents=[Document.example()],
-        transformations=[
-            SentenceAwareNodeParser(),
-            KeywordExtractor(llm=MockLLM()),
-        ],
-    )
-
-    pipeline_id = pipeline.register()
-
-    # update pipeline
-    updated_pipeline = IngestionPipeline(
-        name="Test",
-        reader=ReaderConfig(
-            reader=StringIterableReader(),
-            reader_kwargs={"texts": ["This is another test."]},
-        ),
-        documents=[Document.example()],
-        transformations=[
-            SentenceAwareNodeParser(),
-            KeywordExtractor(llm=MockLLM()),
-        ],
-    )
-
-    new_pipeline_id = updated_pipeline.register()
-
-    # make sure we are updating the same pipeline instead of creating a new one
-    assert pipeline_id == new_pipeline_id
-
-
-@pytest.mark.integration()
-def test_from_pipeline_name() -> None:
-    pipeline = IngestionPipeline(
-        name="Test",
-        reader=ReaderConfig(
-            reader=StringIterableReader(), reader_kwargs={"texts": ["This is a test."]}
-        ),
-        documents=[Document.example()],
-        transformations=[
-            SentenceAwareNodeParser(),
-            KeywordExtractor(llm=MockLLM()),
-        ],
-    )
-
-    pipeline.register()
-
-    new_pipeline = IngestionPipeline.from_pipeline_name("Test")
-    nodes = new_pipeline.run_local()
     assert len(nodes) == 2
     assert len(nodes[0].metadata) > 0
