8027508f1b02768a938aefa7362605308d9f32ba
Vydruth <Vydruth@users.noreply.github.com>
2023-11-09 18:51:44 -0800

    Issue:346 - Allow multiple Langchain prompt template keys
    https://github.com/run-llama/llama_index/issues/346

--- llama_index/langchain_helpers/memory_wrapper.py
+++ llama_index/langchain_helpers/memory_wrapper.py
@@ -15,20 +15,6 @@ from llama_index.schema import Document
 from llama_index.utils import get_new_id
 
 
-def get_prompt_input_key(inputs: Dict[str, Any], memory_variables: List[str]) -> str:
-    """Get prompt input key.
-
-    Copied over from langchain.
-
-    """
-    # "stop" is a special key that can be passed as input but is not used to
-    # format the prompt.
-    prompt_input_keys = list(set(inputs).difference([*memory_variables, "stop"]))
-    if len(prompt_input_keys) != 1:
-        raise ValueError(f"One input key expected got {prompt_input_keys}")
-    return prompt_input_keys[0]
-
-
 class GPTIndexMemory(Memory):
     """Langchain memory wrapper (for LlamaIndex).
 
@@ -38,7 +24,7 @@ class GPTIndexMemory(Memory):
         memory_key (str): Key for memory. Defaults to "history".
         index (BaseIndex): LlamaIndex instance.
         query_kwargs (Dict[str, Any]): Keyword arguments for LlamaIndex query.
-        input_key (Optional[str]): Input key. Defaults to None.
+        input_keys (Optional[List[str]]): Input keys. Defaults to None.
         output_key (Optional[str]): Output key. Defaults to None.
 
     """
@@ -49,46 +35,48 @@ class GPTIndexMemory(Memory):
     index: BaseIndex
     query_kwargs: Dict = Field(default_factory=dict)
     output_key: Optional[str] = None
-    input_key: Optional[str] = None
+    input_keys: Optional[List[str]] = None
 
     @property
     def memory_variables(self) -> List[str]:
         """Return memory variables."""
         return [self.memory_key]
 
-    def _get_prompt_input_key(self, inputs: Dict[str, Any]) -> str:
-        if self.input_key is None:
-            prompt_input_key = get_prompt_input_key(inputs, self.memory_variables)
+    def _get_prompt_input_keys(self, inputs: Dict[str, Any]) -> List[str]:
+        if self.input_keys is None:
+            prompt_input_keys = list(set(inputs).difference([self.memory_key, "stop"]))
         else:
-            prompt_input_key = self.input_key
-        return prompt_input_key
+            prompt_input_keys = self.input_keys
+        return prompt_input_keys
 
     def load_memory_variables(self, inputs: Dict[str, Any]) -> Dict[str, str]:
         """Return key-value pairs given the text input to the chain."""
-        prompt_input_key = self._get_prompt_input_key(inputs)
-        query_str = inputs[prompt_input_key]
-
-        # TODO: wrap in prompt
-        # TODO: add option to return the raw text
-        # NOTE: currently it's a hack
-        query_engine = self.index.as_query_engine(**self.query_kwargs)
-        response = query_engine.query(query_str)
-        return {self.memory_key: str(response)}
+        prompt_input_keys = self._get_prompt_input_keys(inputs)
+        response_dict = {}
+        for key in prompt_input_keys:
+            query_str = inputs[key]
+            query_engine = self.index.as_query_engine(**self.query_kwargs)
+            response = query_engine.query(query_str)
+            response_dict[key] = str(response)
+        return response_dict
 
     def save_context(self, inputs: Dict[str, Any], outputs: Dict[str, str]) -> None:
         """Save the context of this model run to memory."""
-        prompt_input_key = self._get_prompt_input_key(inputs)
-        if self.output_key is None:
-            if len(outputs) != 1:
-                raise ValueError(f"One output key expected, got {outputs.keys()}")
-            output_key = next(iter(outputs.keys()))
-        else:
-            output_key = self.output_key
-        human = f"{self.human_prefix}: " + inputs[prompt_input_key]
-        ai = f"{self.ai_prefix}: " + outputs[output_key]
-        doc_text = f"{human}\n{ai}"
-        doc = Document(text=doc_text)
-        self.index.insert(doc)
+        prompt_input_keys = self._get_prompt_input_keys(inputs)
+        for key in prompt_input_keys:
+            if self.output_key is None:
+                if key in outputs and len(outputs) == 1:
+                    output_key = key
+                else:
+                    raise ValueError(f"One output key expected for input key '{key}'")
+            else:
+                output_key = self.output_key
+
+            human = f"{self.human_prefix}: " + inputs[key]
+            ai = f"{self.ai_prefix}: " + outputs[output_key]
+            doc_text = f"{human}\n{ai}"
+            doc = Document(text=doc_text)
+            self.index.insert(doc)
 
     def clear(self) -> None:
         """Clear memory contents."""
@@ -107,7 +95,7 @@ class GPTIndexChatMemory(BaseChatMemory):
         memory_key (str): Key for memory. Defaults to "history".
         index (BaseIndex): LlamaIndex instance.
         query_kwargs (Dict[str, Any]): Keyword arguments for LlamaIndex query.
-        input_key (Optional[str]): Input key. Defaults to None.
+        input_keys (Optional[List[str]]): Input keys. Defaults to None.
         output_key (Optional[str]): Output key. Defaults to None.
 
     """
@@ -118,7 +106,7 @@ class GPTIndexChatMemory(BaseChatMemory):
     index: BaseIndex
     query_kwargs: Dict = Field(default_factory=dict)
     output_key: Optional[str] = None
-    input_key: Optional[str] = None
+    input_keys: Optional[List[str]] = None
 
     return_source: bool = False
     id_to_message: Dict[str, BaseMessage] = Field(default_factory=dict)
@@ -128,68 +116,69 @@ class GPTIndexChatMemory(BaseChatMemory):
         """Return memory variables."""
         return [self.memory_key]
 
-    def _get_prompt_input_key(self, inputs: Dict[str, Any]) -> str:
-        if self.input_key is None:
-            prompt_input_key = get_prompt_input_key(inputs, self.memory_variables)
+    def _get_prompt_input_keys(self, inputs: Dict[str, Any]) -> List[str]:
+        if self.input_keys is None:
+            prompt_input_keys = list(set(inputs).difference([self.memory_key, "stop"]))
         else:
-            prompt_input_key = self.input_key
-        return prompt_input_key
+            prompt_input_keys = self.input_keys
+        return prompt_input_keys
 
     def load_memory_variables(self, inputs: Dict[str, Any]) -> Dict[str, str]:
         """Return key-value pairs given the text input to the chain."""
-        prompt_input_key = self._get_prompt_input_key(inputs)
-        query_str = inputs[prompt_input_key]
-
-        query_engine = self.index.as_query_engine(**self.query_kwargs)
-        response_obj = query_engine.query(query_str)
-        if self.return_source:
-            source_nodes = response_obj.source_nodes
-            if self.return_messages:
-                # get source messages from ids
-                source_ids = [sn.node.node_id for sn in source_nodes]
-                source_messages = [
-                    m for id, m in self.id_to_message.items() if id in source_ids
-                ]
-                # NOTE: type List[BaseMessage]
-                response: Any = source_messages
+        response_dict = {}
+        for key in self._get_prompt_input_keys(inputs):
+            query_str = inputs[key]
+            query_engine = self.index.as_query_engine(**self.query_kwargs)
+            response_obj = query_engine.query(query_str)
+            if self.return_source:
+                source_nodes = response_obj.source_nodes
+                if self.return_messages:
+                    # get source messages from ids
+                    source_ids = [sn.node.node_id for sn in source_nodes]
+                    source_messages = [
+                        m for id, m in self.id_to_message.items() if id in source_ids
+                    ]
+                    response: Any = source_messages
+                else:
+                    source_texts = [sn.node.get_content() for sn in source_nodes]
+                    response = "\n\n".join(source_texts)
             else:
-                source_texts = [sn.node.get_content() for sn in source_nodes]
-                response = "\n\n".join(source_texts)
-        else:
-            response = str(response_obj)
-        return {self.memory_key: response}
+                response = str(response_obj)
+            response_dict[key] = response
+        return response_dict
 
     def save_context(self, inputs: Dict[str, Any], outputs: Dict[str, str]) -> None:
         """Save the context of this model run to memory."""
-        prompt_input_key = self._get_prompt_input_key(inputs)
-        if self.output_key is None:
-            if len(outputs) != 1:
-                raise ValueError(f"One output key expected, got {outputs.keys()}")
-            output_key = next(iter(outputs.keys()))
-        else:
-            output_key = self.output_key
+        for key in self._get_prompt_input_keys(inputs):
+            if self.output_key is None:
+                if key in outputs and len(outputs) == 1:
+                    output_key = key
+                else:
+                    raise ValueError(f"One output key expected for input key '{key}'")
+            else:
+                output_key = self.output_key
 
-        # a bit different than existing langchain implementation
-        # because we want to track id's for messages
-        human_message = HumanMessage(content=inputs[prompt_input_key])
-        human_message_id = get_new_id(set(self.id_to_message.keys()))
-        ai_message = AIMessage(content=outputs[output_key])
-        ai_message_id = get_new_id(
-            set(self.id_to_message.keys()).union({human_message_id})
-        )
+            # a bit different than existing langchain implementation
+            # because we want to track id's for messages
+            human_message = HumanMessage(content=inputs[key])
+            human_message_id = get_new_id(set(self.id_to_message.keys()))
+            ai_message = AIMessage(content=outputs[output_key])
+            ai_message_id = get_new_id(
+                set(self.id_to_message.keys()).union({human_message_id})
+            )
 
-        self.chat_memory.messages.append(human_message)
-        self.chat_memory.messages.append(ai_message)
+            self.chat_memory.messages.append(human_message)
+            self.chat_memory.messages.append(ai_message)
 
-        self.id_to_message[human_message_id] = human_message
-        self.id_to_message[ai_message_id] = ai_message
+            self.id_to_message[human_message_id] = human_message
+            self.id_to_message[ai_message_id] = ai_message
 
-        human_txt = f"{self.human_prefix}: " + inputs[prompt_input_key]
-        ai_txt = f"{self.ai_prefix}: " + outputs[output_key]
-        human_doc = Document(text=human_txt, id_=human_message_id)
-        ai_doc = Document(text=ai_txt, id_=ai_message_id)
-        self.index.insert(human_doc)
-        self.index.insert(ai_doc)
+            human_txt = f"{self.human_prefix}: " + inputs[key]
+            ai_txt = f"{self.ai_prefix}: " + outputs[output_key]
+            human_doc = Document(text=human_txt, id_=human_message_id)
+            ai_doc = Document(text=ai_txt, id_=ai_message_id)
+            self.index.insert(human_doc)
+            self.index.insert(ai_doc)
 
     def clear(self) -> None:
         """Clear memory contents."""
