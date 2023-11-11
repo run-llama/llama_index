1ff602edc4b532f7f478b33c8abc40428fed4fa7
ravi03071991 <ravi03071991@gmail.com>
2023-11-11 16:46:56 +0530

    Add cohere reranker funetuner

--- llama_index/finetuning/types.py
+++ llama_index/finetuning/types.py
@@ -44,3 +44,15 @@ class BaseCrossEncoderFinetuningEngine(ABC):
         self, model_name: str, top_n: int = 3
     ) -> SentenceTransformerRerank:
         """Gets fine-tuned Cross-Encoder model as re-ranker."""
+
+
+class BaseCohereRerankerFinetuningEngine(ABC):
+    """Base Cross Encoder Finetuning Engine."""
+
+    @abstractmethod
+    def finetune(self) -> None:
+        """Goes off and does stuff."""
+
+    @abstractmethod
+    def get_finetuned_model(self, model: Any) -> str:
+        """Gets finetuned model."""
