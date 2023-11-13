d898f1a1cf18d88857569fcc9f2d8201068d40db
rostrovsky <rafal.ostrovsky@gmail.com>
2023-11-13 17:33:26 +0100

    safe funcs

--- llama_index/exec_utils.py
+++ llama_index/exec_utils.py
@@ -4,67 +4,75 @@ from typing import Any, Mapping
 from types import CodeType
 from _typeshed import ReadableBuffer
 
-ALLOWED_IMPORTS = {'math', 'time'}
+ALLOWED_IMPORTS = {"math", "time"}
+
 
 def _restricted_import(name, globals=None, locals=None, fromlist=(), level=0):
     if name in ALLOWED_IMPORTS:
         return __import__(name, globals, locals, fromlist, level)
     raise ImportError(f"Import of module '{name}' is not allowed")
 
+
 ALLOWED_BUILTINS = {
-    'abs': abs,
-    'all': all,
-    'any': any,
-    'ascii': ascii,
-    'bin': bin,
-    'bool': bool,
-    'bytearray': bytearray,
-    'bytes': bytes,
-    'chr': chr,
-    'complex': complex,
-    'divmod': divmod,
-    'enumerate': enumerate,
-    'filter': filter,
-    'float': float,
-    'format': format,
-    'frozenset': frozenset,
-    'getattr': getattr,
-    'hasattr': hasattr,
-    'hash': hash,
-    'hex': hex,
-    'int': int,
-    'isinstance': isinstance,
-    'issubclass': issubclass,
-    'iter': iter,
-    'len': len,
-    'list': list,
-    'map': map,
-    'max': max,
-    'min': min,
-    'next': next,
-    'oct': oct,
-    'ord': ord,
-    'pow': pow,
-    'print': print,
-    'range': range,
-    'repr': repr,
-    'reversed': reversed,
-    'round': round,
-    'set': set,
-    'setattr': setattr,
-    'slice': slice,
-    'sorted': sorted,
-    'str': str,
-    'sum': sum,
-    'tuple': tuple,
-    'type': type,
-    'zip': zip,
+    "abs": abs,
+    "all": all,
+    "any": any,
+    "ascii": ascii,
+    "bin": bin,
+    "bool": bool,
+    "bytearray": bytearray,
+    "bytes": bytes,
+    "chr": chr,
+    "complex": complex,
+    "divmod": divmod,
+    "enumerate": enumerate,
+    "filter": filter,
+    "float": float,
+    "format": format,
+    "frozenset": frozenset,
+    "getattr": getattr,
+    "hasattr": hasattr,
+    "hash": hash,
+    "hex": hex,
+    "int": int,
+    "isinstance": isinstance,
+    "issubclass": issubclass,
+    "iter": iter,
+    "len": len,
+    "list": list,
+    "map": map,
+    "max": max,
+    "min": min,
+    "next": next,
+    "oct": oct,
+    "ord": ord,
+    "pow": pow,
+    "print": print,
+    "range": range,
+    "repr": repr,
+    "reversed": reversed,
+    "round": round,
+    "set": set,
+    "setattr": setattr,
+    "slice": slice,
+    "sorted": sorted,
+    "str": str,
+    "sum": sum,
+    "tuple": tuple,
+    "type": type,
+    "zip": zip,
     # Constants
-    'True': True,
-    'False': False,
-    'None': None,
-    '__import__': _restricted_import
+    "True": True,
+    "False": False,
+    "None": None,
+    "__import__": _restricted_import,
 }
+
+
+def _get_restricted_globals(external_globals):
+    restricted_globals = copy.deepcopy(ALLOWED_BUILTINS)
+    restricted_globals.update(__globals)
+    return restricted_globals
 
 
 def safe_eval(
@@ -72,7 +80,10 @@ def safe_eval(
     __globals: dict[str, Any] | None = None,
     __locals: Mapping[str, object] | None = None,
 ) -> Any:
-    pass
+    """
+    eval within safe global context
+    """
+    eval(__source, _get_restricted_globals(__globals), __locals)
 
 
 def safe_exec(
@@ -80,4 +91,7 @@ def safe_exec(
     __globals: dict[str, Any] | None = None,
     __locals: Mapping[str, object] | None = None,
 ) -> None:
-    pass
+    """
+    eval within safe global context
+    """
+    exec(__source, _get_restricted_globals(__globals), __locals)
