d2d561abef4279a558a8e5b3e2b0b8e1691e36dd
haotian zhang <haotianzhang@haotians-MacBook-Pro-3.local>
2023-11-15 14:50:22 -0800

    cr

--- llama_index/schema.py
+++ llama_index/schema.py
@@ -709,12 +709,14 @@ class QueryBundle(DataClassJsonMixin):
     Args:
         query_str (str): the original user-specified query string.
             This is currently used by all non embedding-based queries.
-        embedding_strs (list[str]): list of strings used for embedding the query.
+        custom_embedding_strs (list[str]): list of strings used for embedding the query.
             This is currently used by all embedding-based queries.
         embedding (list[float]): the stored embedding for the query.
     """
 
     query_str: str
+    # using single image path as query input
+    image_path: Optional[str] = None
     custom_embedding_strs: Optional[List[str]] = None
     embedding: Optional[List[float]] = None
 
@@ -727,6 +729,13 @@ class QueryBundle(DataClassJsonMixin):
             return [self.query_str]
         else:
             return self.custom_embedding_strs
+
+    @property
+    def embedding_image(self) -> List[any]:
+        """Use image path for image retrieval."""
+        if self.image_path is None:
+            return []
+        return [self.image_path]
 
 
 QueryType = Union[str, QueryBundle]
