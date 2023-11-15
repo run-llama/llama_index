3719b48d234da5de94f7544080bf906817c771d7
Andrei Fajardo <andrei@nerdai.io>
2023-11-15 10:47:23 -0500

    relax assertion

--- llama_index/indices/multi_modal/retriever.py
+++ llama_index/indices/multi_modal/retriever.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import Any, Dict, List, Optional
 
 from llama_index.constants import DEFAULT_SIMILARITY_TOP_K
+from llama_index.embeddings.base import BaseEmbedding
 from llama_index.indices.multi_modal.base import MultiModalVectorStoreIndex
 from llama_index.indices.vector_store.retrievers.retriever import VectorIndexRetriever
 from llama_index.schema import NodeWithScore, QueryBundle
@@ -50,7 +51,7 @@ class MultiModalVectorIndexRetriever(VectorIndexRetriever):
         # separate image vector store for image retrieval
         self._image_vector_store = self._index.image_vector_store
 
-        # assert isinstance(self._index.image_embed_model, MultiModalEmbedding)
+        assert isinstance(self._index.image_embed_model, BaseEmbedding)
         self._image_embed_model = self._index.image_embed_model
 
         self._service_context = self._index.service_context
