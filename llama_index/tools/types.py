0dfd3ec64069f1e257955be77ca120ce4d6b488c
Jerry Liu <jerryjliu98@gmail.com>
2023-11-18 21:04:23 -0800

    cr

--- llama_index/tools/types.py
+++ llama_index/tools/types.py
@@ -186,23 +186,3 @@ def adapt_to_async_tool(tool: BaseTool) -> AsyncBaseTool:
         return tool
     else:
         return BaseToolAsyncAdapter(tool)
-
-
-def call_tool_with_error_handling(
-    tool: BaseTool, input_dict: Dict, error_message: Optional[str] = None
-) -> ToolOutput:
-    """Call tool with error handling.
-
-    Input is a dictionary with args and kwargs
-
-    """
-    try:
-        return tool(**input_dict)
-    except Exception as e:
-        error_message = error_message or str(e)
-        return ToolOutput(
-            content=error_message,
-            tool_name=tool.metadata.name,
-            raw_input={"kwargs": input_dict},
-            raw_output=e,
-        )
