d9cd43734d4f45100cd351abe7f74aaa6bd162db
Logan Markewich <logan.markewich@live.com>
2023-11-20 16:16:13 -0600

    add CLI tool

---
+++ llama_index/llama_pack/command_line.py
@@ -0,0 +1,44 @@
+import argparse
+
+from llama_index.llama_pack.download import LLAMA_HUB_URL, download_llama_pack
+
+
+def main():
+    # Create a parser for downloading llama-packs
+    parser = argparse.ArgumentParser(description="Download llama-packs")
+    parser.add_argument(
+        "llama_pack_class",
+        type=str,
+        help=(
+            "The name of the llama-pack class you want to download, "
+            "such as `GmailOpenAIAgentPack`."
+        ),
+    )
+    parser.add_argument(
+        "download_dir",
+        type=str,
+        help="Custom dirpath to download the pack into.",
+    )
+    parser.add_argument(
+        "--llama_hub_url",
+        type=str,
+        default=LLAMA_HUB_URL,
+        help="URL to llama hub.",
+    )
+    parser.add_argument(
+        "--refresh_cache",
+        action="store_true",
+        help=(
+            "If true, the local cache will be skipped and the pack "
+            "will be fetched directly from the remote repo."
+        ),
+    )
+
+    args = parser.parse_args()
+    download_llama_pack(
+        llama_pack_class=args.llama_pack_class,
+        download_dir=args.download_dir,
+        llama_hub_url=args.llama_hub_url,
+        refresh_cache=args.refresh_cache,
+    )
+    print(f"Successfully downloaded {args.llama_pack_class} to {args.download_dir}")
