441f1323f4a6e2d2c8cb65f0fe71ce4e53c3d3c8
haotian zhang <haotianzhang@haotians-MacBook-Pro-3.local>
2023-11-21 15:21:28 -0500

    cr

--- llama_index/embeddings/base.py
+++ llama_index/embeddings/base.py
@@ -132,45 +132,6 @@ class BaseEmbedding(TransformComponent):
             )
         return query_embedding
 
-    # def _get_image_embedding(self, img_file_path: ImageType) -> Embedding:
-    #     """
-    #     Embed the input image synchronously.
-
-    #     Subclasses should implement this method. Reference get_image_embedding's
-    #     docstring for more information.
-    #     """
-
-    # async def _aget_image_embedding(self, img_file_path: ImageType) -> Embedding:
-    #     """
-    #     Embed the input image asynchronously.
-
-    #     Subclasses should implement this method. Reference get_query_embedding's
-    #     docstring for more information.
-    #     """
-
-    # def get_image_embedding(self, img_file_path: ImageType) -> Embedding:
-    #     """
-    #     Embed the input image.
-
-    #     When embedding a query, depending on the model, a special instruction
-    #     can be prepended to the raw query string. For example, "Represent the
-    #     question for retrieving supporting documents: ". If you're curious,
-    #     other examples of predefined instructions can be found in
-    #     embeddings/huggingface_utils.py.
-    #     """
-    #     with self.callback_manager.event(
-    #         CBEventType.EMBEDDING, payload={EventPayload.SERIALIZED: self.to_dict()}
-    #     ) as event:
-    #         image_embedding = self._get_image_embedding(img_file_path)
-
-    #         event.on_end(
-    #             payload={
-    #                 EventPayload.CHUNKS: [img_file_path],
-    #                 EventPayload.EMBEDDINGS: [image_embedding],
-    #             },
-    #         )
-    #     return image_embedding
-
     async def aget_query_embedding(self, query: str) -> Embedding:
         """Get query embedding."""
         with self.callback_manager.event(
