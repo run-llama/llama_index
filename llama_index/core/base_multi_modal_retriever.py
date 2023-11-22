541e3b1b982bbc54183d398883962ea592d2046b
Haotian Zhang <socool.king@gmail.com>
2023-11-22 18:21:14 -0500

    Image to Image Retrieval with example (#9045)
    * Image to Image Retrieval with exmaple
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    ---------
    
    Co-authored-by: haotian zhang <haotianzhang@haotians-MacBook-Pro-3.local>

--- llama_index/core/base_multi_modal_retriever.py
+++ llama_index/core/base_multi_modal_retriever.py
@@ -13,15 +13,27 @@ class MultiModalRetriever(BaseRetriever, BaseImageRetriever):
 
     @abstractmethod
     def text_retrieve(self, str_or_query_bundle: QueryType) -> List[NodeWithScore]:
-        """Retrieve text nodes given query.
+        """Retrieve text nodes given text query.
 
         Implemented by the user.
 
         """
 
     @abstractmethod
-    def image_retrieve(self, str_or_query_bundle: QueryType) -> List[NodeWithScore]:
-        """Retrieve image nodes given query.
+    def text_to_image_retrieve(
+        self, str_or_query_bundle: QueryType
+    ) -> List[NodeWithScore]:
+        """Retrieve image nodes given text query.
+
+        Implemented by the user.
+
+        """
+
+    @abstractmethod
+    def image_to_image_retrieve(
+        self, str_or_query_bundle: QueryType
+    ) -> List[NodeWithScore]:
+        """Retrieve image nodes given image query.
 
         Implemented by the user.
 
@@ -31,17 +43,27 @@ class MultiModalRetriever(BaseRetriever, BaseImageRetriever):
     async def atext_retrieve(
         self, str_or_query_bundle: QueryType
     ) -> List[NodeWithScore]:
-        """Async Retrieve text nodes given query.
+        """Async Retrieve text nodes given text query.
 
         Implemented by the user.
 
         """
 
     @abstractmethod
-    async def aimage_retrieve(
+    async def atext_to_image_retrieve(
         self, str_or_query_bundle: QueryType
     ) -> List[NodeWithScore]:
-        """Async Retrieve image nodes given query.
+        """Async Retrieve image nodes given text query.
+
+        Implemented by the user.
+
+        """
+
+    @abstractmethod
+    async def aimage_to_image_retrieve(
+        self, str_or_query_bundle: QueryType
+    ) -> List[NodeWithScore]:
+        """Async Retrieve image nodes given image query.
 
         Implemented by the user.
 
