aeb9f1733f4eaa13dc85e5236ed9c1639198eb67
Logan Markewich <logan.markewich@live.com>
2023-11-30 15:01:09 -0600

    fix eval tests

--- tests/evaluation/test_platform_eval.py
+++ tests/evaluation/test_platform_eval.py
@@ -4,8 +4,8 @@ from llama_index_client.client import PlatformApi
 
 from llama_index.evaluation.eval_utils import upload_eval_dataset
 
-base_url = os.environ["PLATFORM_BASE_URL"]
-api_key = os.environ["PLATFORM_API_KEY"]
+base_url = os.environ.get("PLATFORM_BASE_URL", None)
+api_key = os.environ.get("PLATFORM_API_KEY", None)
 
 @pytest.mark.skipif(
         not base_url or not api_key, 
@@ -19,7 +19,7 @@ def test_upload_eval_dataste() -> None:
         overwrite=True,
     )
 
-    client = PlatformApi(base_url=os.environ["PLATFORM_BASE_URL"])
+    client = PlatformApi(base_url=base_url, api_key=api_key)
     eval_dataset = client.eval.get_dataset(dataset_id=eval_dataset_id)
     assert eval_dataset.name == "test_dataset"
 
