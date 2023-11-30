06b25793de1bf8fbd624741810fe2a12a6e14f69
Haotian Zhang <socool.king@gmail.com>
2023-11-29 21:46:33 -0500

    Init More Advanced Metadata Filter for Vector Store (#9216)
    * Init Advanced Metadata Filters for Vector Stores
    
    * Init Advanced Metadata filter
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * legacy filters support
    
    * basic metadata legacy tests
    
    * update imports
    
    * fix tests
    
    * cr
    
    * cr
    
    ---------
    
    Co-authored-by: haotian zhang <haotianzhang@haotians-MacBook-Pro-3.local>
    Co-authored-by: Logan Markewich <logan.markewich@live.com>

--- llama_index/vector_stores/mongodb.py
+++ llama_index/vector_stores/mongodb.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 def _to_mongodb_filter(standard_filters: MetadataFilters) -> Dict:
     """Convert from standard dataclass to filter dict."""
     filters = {}
-    for filter in standard_filters.filters:
+    for filter in standard_filters.legacy_filters():
         filters[filter.key] = filter.value
     return filters
 
