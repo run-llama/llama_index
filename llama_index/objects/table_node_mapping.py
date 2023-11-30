33220ed713f3c946010c4a63d11972322e3d0d99
Logan Markewich <logan.markewich@live.com>
2023-11-29 19:53:59 -0600

    fix bad merge

--- llama_index/objects/table_node_mapping.py
+++ llama_index/objects/table_node_mapping.py
@@ -92,24 +92,3 @@ class SQLTableNodeMapping(BaseObjectNodeMapping[SQLTableSchema]):
         raise NotImplementedError(
             "This object node mapping does not support persist method."
         )
-
-    @property
-    def obj_node_mapping(self) -> Dict[int, Any]:
-        """The mapping data structure between node and object."""
-        raise NotImplementedError("Subclasses should implement this!")
-
-    def persist(
-        self, persist_dir: str = ..., obj_node_mapping_fname: str = ...
-    ) -> None:
-        """Persist objs."""
-        raise NotImplementedError("Subclasses should implement this!")
-
-    @classmethod
-    def from_persist_dir(
-        cls,
-        persist_dir: str = DEFAULT_PERSIST_DIR,
-        obj_node_mapping_fname: str = DEFAULT_PERSIST_FNAME,
-    ) -> "SQLTableNodeMapping":
-        raise NotImplementedError(
-            "This object node mapping does not support persist method."
-        )
