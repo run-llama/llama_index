345b956489a86ea02ea0a002f58d9dbfb674de33
Logan Markewich <logan.markewich@live.com>
2023-11-30 15:33:02 -0600

    more linting for managed

--- llama_index/indices/managed/base.py
+++ llama_index/indices/managed/base.py
@@ -87,6 +87,7 @@ class BaseManagedIndex(BaseIndex[IndexDict], ABC):
         service_context: Optional[ServiceContext] = None,
         show_progress: bool = False,
         remote_pipeline_name: Optional[str] = None,
+        from_pipeline_name: Optional[str] = None,
         project_name: str = DEFAULT_PROJECT_NAME,
         **kwargs: Any,
     ) -> IndexType:
