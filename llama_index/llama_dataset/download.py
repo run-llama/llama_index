2598a5a6b4051d675cdfb0a32959c6d85cf404df
Andrei Fajardo <andrei@nerdai.io>
2023-12-01 12:33:28 -0500

    wip

--- llama_index/llama_dataset/download.py
+++ llama_index/llama_dataset/download.py
@@ -1,17 +1,13 @@
 from typing import List, Tuple, Type
 
 from llama_index import Document
-from llama_index.download.download_utils import (
+from llama_index.download.dataset import (
+    LLAMA_DATASETS_LFS_URL,
+    LLAMA_DATASETS_SOURCE_FILES_GITHUB_TREE_URL,
     LLAMA_DATASETS_URL,
-    LLAMA_HUB_URL,
-<<<<<<< HEAD
-    MODULE_TYPE,
-=======
-    LLAMA_RAG_DATASET_FILENAME,
->>>>>>> 474aa922 (add modifications to allow for pdf download)
-    download_llama_module,
-    track_download,
+    download_llama_dataset as download,
 )
+from llama_index.download.download_utils import MODULE_TYPE, track_download
 from llama_index.llama_dataset.base import BaseLlamaDataset
 from llama_index.llama_dataset.rag import LabelledRagDataset
 from llama_index.readers import SimpleDirectoryReader
@@ -20,8 +16,9 @@ from llama_index.readers import SimpleDirectoryReader
 def download_llama_dataset(
     llama_dataset_class: str,
     download_dir: str,
-    llama_hub_url: str = LLAMA_HUB_URL,
     llama_datasets_url: str = LLAMA_DATASETS_URL,
+    llama_datasets_lfs_url: str = LLAMA_DATASETS_LFS_URL,
+    llama_datasets_source_files_tree_url: str = LLAMA_DATASETS_SOURCE_FILES_GITHUB_TREE_URL,
     show_progress: bool = False,
 ) -> Tuple[Type[BaseLlamaDataset], List[Document]]:
     """Download a single LlamaDataset from Llama Hub.
@@ -36,23 +33,23 @@ def download_llama_dataset(
     Returns:
         A Loader.
     """
-    filenames: Tuple[str, List[str]] = download_llama_module(
+    filenames: Tuple[str, List[str]] = download(
         llama_dataset_class,
-        is_dataset=True,
-        llama_hub_url=llama_hub_url,
         llama_datasets_url=llama_datasets_url,
+        llama_datasets_lfs_url=llama_datasets_lfs_url,
+        llama_datasets_source_files_tree_url=llama_datasets_source_files_tree_url,
         refresh_cache=True,
         custom_path=download_dir,
-        library_path="llama_datasets/library.json",
+        library_path="library.json",
         disable_library_cache=True,
         override_path=True,
         show_progress=show_progress,
     )
-    rag_dataset_filename, source_filenames = filenames
+    rag_dataset_filename, source_files_dir = filenames
     track_download(llama_dataset_class, MODULE_TYPE.DATASETS)
     return (
         LabelledRagDataset.from_json(rag_dataset_filename),
-        SimpleDirectoryReader(
-            input_dir=download_dir, exclude=[LLAMA_RAG_DATASET_FILENAME, "__init__.py"]
-        ).load_data(show_progress=show_progress),
+        SimpleDirectoryReader(input_dir=source_files_dir).load_data(
+            show_progress=show_progress
+        ),
     )
