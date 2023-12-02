b7c0a563da596dad76132f8add2e735a4478425d
bindi <pankhudi@gmail.com>
2023-12-01 16:56:49 -0800

    fix some linting issue

--- llama_index/download/download_utils.py
+++ llama_index/download/download_utils.py
@@ -358,12 +358,12 @@ def track_download(
     module_class: str,
     module_type: str
 ) -> None:
-    """Tracks number of downloads via Llamahub proxy
+    """Tracks number of downloads via Llamahub proxy.
     Args:
         module_class: The name of the llama module being downloaded, e.g.,`GmailOpenAIAgentPack`.
         module_type: Can be "loader", "tool", "llamapack", or "datasets"
     """
     try: 
         requests.post(LLAMAHUB_ANALYTICS_PROXY_SERVER, json={"type": module_type, "plugin": module_class})
-    except: 
-        logger.info(f"Error tracking downloads for {module_class}")
+    except Exception as e: 
+        logger.info(f"Error tracking downloads for {module_class} : {e}")
