042bbaddf1fe201252e39815272f62408b647f21
Haotian Zhang <socool.king@gmail.com>
2023-12-01 22:36:55 -0500

    cr

--- llama_index/vector_stores/pinecone.py
+++ llama_index/vector_stores/pinecone.py
@@ -61,8 +61,6 @@ def _transform_pinecone_filter_operator(operator: str) -> str:
         return "$lte"
     elif operator == "in":
         return "$in"
-    elif operator == "nin":
-        return "$nin"
     else:
         raise ValueError(f"Filter operator {operator} not supported")
 
