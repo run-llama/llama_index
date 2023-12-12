c3026236bec88d317469bf784c0595883cf8fc7e
Jerry Liu <jerryjliu98@gmail.com>
2023-12-11 17:28:17 -0800

    cr

--- llama_index/question_gen/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Question gen."""
-
-from llama_index.question_gen.openai_generator import OpenAIQuestionGenerator
-from llama_index.question_gen.llm_generators import LLMQuestionGenerator
-
-__all__ = ["OpenAIQuestionGenerator", "LLMQuestionGenerator"]
