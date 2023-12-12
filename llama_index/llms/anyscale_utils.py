c854b0423820809c7673bcd298f30206376cee4b
Kyle Huang <kyle@anyscale.com>
2023-12-12 11:01:05 -0800

    Add Anyscale embedding model

--- llama_index/llms/anyscale_utils.py
+++ llama_index/llms/anyscale_utils.py
@@ -1,21 +1,32 @@
-from typing import Any, Dict, List, Sequence
+from typing import Any, Dict, List, Sequence, Optional, Tuple
 
 from llama_index.llms.base import ChatMessage, MessageRole
+from llama_index.llms.generic_utils import get_from_param_or_env
+
+DEFAULT_ANYSCALE_API_BASE = "https://api.endpoints.anyscale.com/v1"
+DEFAULT_ANYSCALE_API_VERSION = ""
 
 LLAMA_MODELS = {
     "meta-llama/Llama-2-7b-chat-hf": 4096,
     "meta-llama/Llama-2-13b-chat-hf": 4096,
     "meta-llama/Llama-2-70b-chat-hf": 4096,
     "codellama/CodeLlama-34b-Instruct-hf": 16384,
+    "Meta-Llama/Llama-Guard-7b": 4096,
 }
 
 MISTRAL_MODELS = {
-    "mistralai/Mistral-7B-Instruct-v0.1": 4096,
+    "mistralai/Mistral-7B-Instruct-v0.1": 16384,
+    "Open-Orca/Mistral-7B-OpenOrca": 8192,
+}
+
+ZEPHYR_MODELS = {
+    "HuggingFaceH4/zephyr-7b-beta":16384,
 }
 
 ALL_AVAILABLE_MODELS = {
     **LLAMA_MODELS,
     **MISTRAL_MODELS,
+    **ZEPHYR_MODELS,
 }
 
 DISCONTINUED_MODELS: Dict[str, int] = {}
@@ -75,3 +86,31 @@ def messages_to_anyscale_prompt(messages: Sequence[ChatMessage]) -> List[Dict]:
         raise ValueError("Got empty list of messages.")
 
     return [_message_to_anyscale_prompt(message) for message in messages]
+
+
+def resolve_anyscale_credentials(
+    api_key: Optional[str] = None,
+    api_base: Optional[str] = None,
+    api_version: Optional[str] = None,
+) -> Tuple[Optional[str], str, str]:
+    """ "Resolve OpenAI credentials.
+
+    The order of precedence is:
+    1. param
+    2. env
+    3. openai module
+    4. default
+    """
+    # resolve from param or env
+    api_key = get_from_param_or_env("api_key", api_key, "ANYSCALE_API_KEY", "")
+    api_base = get_from_param_or_env("api_base", api_base, "ANYSCALE_API_BASE", "")
+    api_version = get_from_param_or_env(
+        "api_version", api_version, "ANYSCALE_API_VERSION", ""
+    )
+
+    # resolve from openai module or default
+    final_api_key = api_key or ""
+    final_api_base = api_base or DEFAULT_ANYSCALE_API_BASE
+    final_api_version = api_version or DEFAULT_ANYSCALE_API_VERSION
+
+    return final_api_key, str(final_api_base), final_api_version
