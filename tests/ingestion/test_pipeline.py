7fe3d84af86c6d63605b5b3dbebd995713d79ff9
Alex Yang <himself65@outlook.com>
2023-12-11 19:49:14 -0600

    fix: test

--- tests/ingestion/test_pipeline.py
+++ tests/ingestion/test_pipeline.py
@@ -6,6 +6,7 @@ from llama_index.node_parser import SentenceSplitter
 from llama_index.readers import ReaderConfig, StringIterableReader
 from llama_index.schema import Document
 from llama_index.storage.docstore import SimpleDocumentStore
+from tests.conftest import CachedOpenAIApiKeys
 
 
 # clean up folders after tests
@@ -16,19 +17,21 @@ def teardown_function() -> None:
 
 
 def test_build_pipeline() -> None:
-    pipeline = IngestionPipeline(
-        reader=ReaderConfig(
-            reader=StringIterableReader(), reader_kwargs={"texts": ["This is a test."]}
-        ),
-        documents=[Document.example()],
-        transformations=[
-            SentenceSplitter(),
-            KeywordExtractor(llm=MockLLM()),
-            OpenAIEmbedding(api_key="fake"),
-        ],
-    )
+    with CachedOpenAIApiKeys():
+        pipeline = IngestionPipeline(
+            reader=ReaderConfig(
+                reader=StringIterableReader(),
+                reader_kwargs={"texts": ["This is a test."]},
+            ),
+            documents=[Document.example()],
+            transformations=[
+                SentenceSplitter(),
+                KeywordExtractor(llm=MockLLM()),
+                OpenAIEmbedding(),
+            ],
+        )
 
-    assert len(pipeline.transformations) == 3
+        assert len(pipeline.transformations) == 3
 
 
 def test_run_pipeline() -> None:
