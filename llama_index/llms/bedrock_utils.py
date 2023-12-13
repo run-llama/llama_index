ce8e0736e53e9689dfa7af2b02973b63f557cf5f
Christophe Bornet <cbornet@hotmail.com>
2023-12-13 16:38:29 +0100

    Changes following review

--- llama_index/llms/bedrock_utils.py
+++ llama_index/llms/bedrock_utils.py
@@ -149,6 +149,8 @@ PROVIDERS = {
 
 def get_provider(model: str) -> Provider:
     provider_name = model.split(".")[0]
+    if provider_name not in PROVIDERS:
+        raise ValueError(f"Provider {provider_name} for model {model} is not supported")
     return PROVIDERS[provider_name]
 
 
