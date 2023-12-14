bfc03be385f62620a59f99a78e27be7f4ce1b83d
Logan Markewich <logan.markewich@live.com>
2023-12-14 16:11:04 -0600

    linting

--- llama_index/vector_stores/neo4jvector.py
+++ llama_index/vector_stores/neo4jvector.py
@@ -65,7 +65,7 @@ def _get_search_index_query(hybrid: bool) -> str:
     )
 
 
-def remove_lucene_chars(text: Optional[str]) -> List[str]:
+def remove_lucene_chars(text: Optional[str]) -> Optional[str]:
     """Remove Lucene special characters."""
     if not text:
         return None
