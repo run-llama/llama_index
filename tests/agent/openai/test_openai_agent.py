8c7b84be44dd7286aa2c81b51f05af25c892600d
Jerry Liu <jerryjliu98@gmail.com>
2023-12-14 16:14:49 -0800

    cr

--- tests/agent/openai/test_openai_agent.py
+++ tests/agent/openai/test_openai_agent.py
@@ -1,14 +1,17 @@
-from typing import Any, List, Sequence
+from typing import Any, AsyncGenerator, Generator, List, Sequence
 from unittest.mock import MagicMock, patch
 
 import pytest
-from llama_index.agent.openai_agent import OpenAIAgent, call_tool_with_error_handling
-from llama_index.chat_engine.types import AgentChatResponse
+from llama_index.agent.openai.base import OpenAIAgent
+from llama_index.agent.openai.step import call_tool_with_error_handling
+from llama_index.chat_engine.types import AgentChatResponse, StreamingAgentChatResponse
+from llama_index.llms.base import ChatMessage, ChatResponse
 from llama_index.llms.mock import MockLLM
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.types import ChatMessage, ChatResponse
 from llama_index.tools.function_tool import FunctionTool
 from openai.types.chat.chat_completion import ChatCompletion, Choice
+from openai.types.chat.chat_completion_chunk import ChatCompletionChunk, ChoiceDelta
 from openai.types.chat.chat_completion_message import ChatCompletionMessage
 
 
@@ -34,6 +37,73 @@ def mock_chat_completion(*args: Any, **kwargs: Any) -> ChatCompletion:
             )
         ],
     )
+
+
+def mock_chat_stream(
+    *args: Any, **kwargs: Any
+) -> Generator[ChatCompletionChunk, None, None]:
+    if "functions" in kwargs:
+        if not kwargs["functions"]:
+            raise ValueError("functions must not be empty")
+
+    yield ChatCompletionChunk(
+        id="chatcmpl-abc123",
+        object="chat.completion.chunk",
+        created=1677858242,
+        model="gpt-3.5-turbo-0301",
+        usage={"prompt_tokens": 13, "completion_tokens": 7, "total_tokens": 20},
+        choices=[
+            Choice(
+                message=ChatCompletionMessage(
+                    role="assistant", content="\n\nThis is a test!"
+                ),
+                finish_reason="stop",
+                index=0,
+                delta=ChoiceDelta(
+                    role="assistant",
+                    content="\n\nThis is a test!",
+                ),
+            )
+        ],
+    )
+
+
+async def mock_achat_completion(*args: Any, **kwargs: Any) -> ChatCompletion:
+    return mock_chat_completion(*args, **kwargs)
+
+
+async def mock_achat_stream(
+    *args: Any, **kwargs: Any
+) -> AsyncGenerator[ChatCompletionChunk, None]:
+    async def _mock_achat_stream(
+        *args: Any, **kwargs: Any
+    ) -> AsyncGenerator[ChatCompletionChunk, None]:
+        if "functions" in kwargs:
+            if not kwargs["functions"]:
+                raise ValueError("functions must not be empty")
+
+        yield ChatCompletionChunk(
+            id="chatcmpl-abc123",
+            object="chat.completion.chunk",
+            created=1677858242,
+            model="gpt-3.5-turbo-0301",
+            usage={"prompt_tokens": 13, "completion_tokens": 7, "total_tokens": 20},
+            choices=[
+                Choice(
+                    message=ChatCompletionMessage(
+                        role="assistant", content="\n\nThis is a test!"
+                    ),
+                    finish_reason="stop",
+                    index=0,
+                    delta=ChoiceDelta(
+                        role="assistant",
+                        content="\n\nThis is a test!",
+                    ),
+                )
+            ],
+        )
+
+    return _mock_achat_stream(*args, **kwargs)
 
 
 @pytest.fixture()
@@ -85,6 +155,62 @@ def test_chat_basic(MockSyncOpenAI: MagicMock, add_tool: FunctionTool) -> None:
     response = agent.chat("What is 1 + 1?")
     assert isinstance(response, AgentChatResponse)
     assert response.response == "\n\nThis is a test!"
+
+
+@patch("llama_index.llms.openai.AsyncOpenAI")
+@pytest.mark.asyncio()
+async def test_achat_basic(MockAsyncOpenAI: MagicMock, add_tool: FunctionTool) -> None:
+    mock_instance = MockAsyncOpenAI.return_value
+    mock_instance.chat.completions.create.return_value = mock_achat_completion()
+
+    llm = OpenAI(model="gpt-3.5-turbo")
+
+    agent = OpenAIAgent.from_tools(
+        tools=[add_tool],
+        llm=llm,
+    )
+    response = await agent.achat("What is 1 + 1?")
+    assert isinstance(response, AgentChatResponse)
+    assert response.response == "\n\nThis is a test!"
+
+
+@patch("llama_index.llms.openai.SyncOpenAI")
+def test_stream_chat_basic(MockSyncOpenAI: MagicMock, add_tool: FunctionTool) -> None:
+    mock_instance = MockSyncOpenAI.return_value
+    mock_instance.chat.completions.create.side_effect = mock_chat_stream
+
+    llm = OpenAI(model="gpt-3.5-turbo")
+
+    agent = OpenAIAgent.from_tools(
+        tools=[add_tool],
+        llm=llm,
+    )
+    response = agent.stream_chat("What is 1 + 1?")
+    assert isinstance(response, StreamingAgentChatResponse)
+    # str() strips newline values
+    assert str(response) == "This is a test!"
+
+
+@patch("llama_index.llms.openai.AsyncOpenAI")
+@pytest.mark.asyncio()
+async def test_astream_chat_basic(
+    MockAsyncOpenAI: MagicMock, add_tool: FunctionTool
+) -> None:
+    mock_instance = MockAsyncOpenAI.return_value
+    mock_instance.chat.completions.create.side_effect = mock_achat_stream
+
+    llm = OpenAI(model="gpt-3.5-turbo")
+
+    agent = OpenAIAgent.from_tools(
+        tools=[add_tool],
+        llm=llm,
+    )
+    response_stream = await agent.astream_chat("What is 1 + 1?")
+    async for response in response_stream.async_response_gen():
+        pass
+    assert isinstance(response_stream, StreamingAgentChatResponse)
+    # str() strips newline values
+    assert response == "\n\nThis is a test!"
 
 
 @patch("llama_index.llms.openai.SyncOpenAI")
