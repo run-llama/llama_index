7bc074d0713202077acf7df598a9d279f96af4e8
Jerry Liu <jerryjliu98@gmail.com>
2023-12-16 21:45:59 -0800

    cr

--- tests/agent/executor/test_base.py
+++ tests/agent/executor/test_base.py
@@ -142,6 +142,10 @@ def test_agent() -> None:
     assert str(step_output.output) == "counter: 1"
     assert step_output.is_last is False
 
+    # test list task, get task
+    assert len(agent_runner.list_tasks()) == 1
+    assert agent_runner.get_task(task_id=task.task_id) == task
+
     # test run step again
     step_output = agent_runner.run_step(task=task)
     assert task.extra_state["counter"] == 2
