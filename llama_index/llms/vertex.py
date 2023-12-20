d42165f41dc3aefb704140b269dd651b601ac113
Nicolò Boschi <boschi1997@gmail.com>
2023-12-20 19:02:53 +0100

    Add support for Google Gemini models via VertexAI (#9624)
    * Add support for Google Gemini models via VertexAI
    
    * fix
    
    * fix import
    
    * add more tests
    
    * nb
    
    * fix
    
    * lint
    
    * fix lint

--- llama_index/llms/vertex.py
+++ llama_index/llms/vertex.py
@@ -18,6 +18,7 @@ from llama_index.llms.types import (
     LLMMetadata,
     MessageRole,
 )
+from llama_index.llms.vertex_gemini_utils import is_gemini_model
 from llama_index.llms.vertex_utils import (
     CHAT_MODELS,
     CODE_CHAT_MODELS,
@@ -25,6 +26,7 @@ from llama_index.llms.vertex_utils import (
     TEXT_MODELS,
     _parse_chat_history,
     _parse_examples,
+    _parse_message,
     acompletion_with_retry,
     completion_with_retry,
     init_vertexai,
@@ -47,6 +49,7 @@ class Vertex(LLM):
     iscode: bool = Field(
         default=False, description="Flag to determine if current model is a Code Model"
     )
+    _is_gemini: bool = PrivateAttr()
     _client: Any = PrivateAttr()
     _chat_client: Any = PrivateAttr()
 
@@ -74,6 +77,7 @@ class Vertex(LLM):
         additional_kwargs = additional_kwargs or {}
         callback_manager = callback_manager or CallbackManager([])
 
+        self._is_gemini = False
         if model in CHAT_MODELS:
             from vertexai.language_models import ChatModel
 
@@ -92,8 +96,14 @@ class Vertex(LLM):
             from vertexai.language_models import TextGenerationModel
 
             self._client = TextGenerationModel.from_pretrained(model)
+        elif is_gemini_model(model):
+            from llama_index.llms.vertex_gemini_utils import create_gemini_client
+
+            self._client = create_gemini_client(model)
+            self._chat_client = self._client
+            self._is_gemini = True
         else:
-            raise (ValueError("Model Not Found Please Check the model name"))
+            raise (ValueError(f"Model {model} not found, please verify the model name"))
 
         super().__init__(
             temperature=temperature,
@@ -141,8 +151,8 @@ class Vertex(LLM):
 
     @llm_chat_callback()
     def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
-        question = messages[-1].content
-        chat_history = _parse_chat_history(messages[:-1])
+        question = _parse_message(messages[-1], self._is_gemini)
+        chat_history = _parse_chat_history(messages[:-1], self._is_gemini)
         chat_params = {**chat_history}
 
         kwargs = kwargs if kwargs else {}
@@ -165,9 +175,10 @@ class Vertex(LLM):
             prompt=question,
             chat=True,
             stream=False,
+            is_gemini=self._is_gemini,
             params=chat_params,
             max_retries=self.max_retries,
-            **params
+            **params,
         )
 
         return ChatResponse(
@@ -183,7 +194,11 @@ class Vertex(LLM):
             raise (ValueError("candidate_count is not supported by the codey model's"))
 
         completion = completion_with_retry(
-            self._client, prompt, max_retries=self.max_retries, **params
+            self._client,
+            prompt,
+            max_retries=self.max_retries,
+            is_gemini=self._is_gemini,
+            **params,
         )
         return CompletionResponse(text=completion.text, raw=completion.__dict__)
 
@@ -191,8 +206,8 @@ class Vertex(LLM):
     def stream_chat(
         self, messages: Sequence[ChatMessage], **kwargs: Any
     ) -> ChatResponseGen:
-        question = messages[-1].content
-        chat_history = _parse_chat_history(messages[:-1])
+        question = _parse_message(messages[-1], self._is_gemini)
+        chat_history = _parse_chat_history(messages[:-1], self._is_gemini)
         chat_params = {**chat_history}
         kwargs = kwargs if kwargs else {}
         params = {**self._model_kwargs, **kwargs}
@@ -212,19 +227,17 @@ class Vertex(LLM):
             prompt=question,
             chat=True,
             stream=True,
+            is_gemini=self._is_gemini,
             params=chat_params,
             max_retries=self.max_retries,
-            **params
+            **params,
         )
 
         def gen() -> ChatResponseGen:
             content = ""
             role = MessageRole.ASSISTANT
             for r in response:
-                if "text" in r.__dict__:
-                    content_delta = r.text
-                else:
-                    content_delta = ""
+                content_delta = r.text
                 content += content_delta
                 yield ChatResponse(
                     message=ChatMessage(role=role, content=content),
@@ -245,8 +258,9 @@ class Vertex(LLM):
             client=self._client,
             prompt=prompt,
             stream=True,
+            is_gemini=self._is_gemini,
             max_retries=self.max_retries,
-            **params
+            **params,
         )
 
         def gen() -> CompletionResponseGen:
@@ -264,8 +278,8 @@ class Vertex(LLM):
     async def achat(
         self, messages: Sequence[ChatMessage], **kwargs: Any
     ) -> ChatResponse:
-        question = messages[-1].content
-        chat_history = _parse_chat_history(messages[:-1])
+        question = _parse_message(messages[-1], self._is_gemini)
+        chat_history = _parse_chat_history(messages[:-1], self._is_gemini)
         chat_params = {**chat_history}
         kwargs = kwargs if kwargs else {}
         params = {**self._model_kwargs, **kwargs}
@@ -283,9 +297,10 @@ class Vertex(LLM):
             client=self._chat_client,
             prompt=question,
             chat=True,
+            is_gemini=self._is_gemini,
             params=chat_params,
             max_retries=self.max_retries,
-            **params
+            **params,
         )
         ##this is due to a bug in vertex AI we have to await twice
         if self.iscode:
@@ -302,7 +317,11 @@ class Vertex(LLM):
         if self.iscode and "candidate_count" in params:
             raise (ValueError("candidate_count is not supported by the codey model's"))
         completion = await acompletion_with_retry(
-            client=self._client, prompt=prompt, max_retries=self.max_retries, **params
+            client=self._client,
+            prompt=prompt,
+            max_retries=self.max_retries,
+            is_gemini=self._is_gemini,
+            **params,
         )
         return CompletionResponse(text=completion.text)
 
