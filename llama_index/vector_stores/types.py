c7bf170944b0dae0888ca564767231703360ac57
Jerry Liu <jerryjliu98@gmail.com>
2023-12-20 20:31:34 -0800

    cr

--- llama_index/vector_stores/types.py
+++ llama_index/vector_stores/types.py
@@ -93,11 +93,14 @@ class MetadataFilter(BaseModel):
     operator: FilterOperator = FilterOperator.EQ
 
 
-# TODO: Deprecate ExactMatchFilter and use MetadataFilter instead
-# Keep class for now so that AutoRetriever can still work with old vector stores
-class ExactMatchFilter(BaseModel):
-    key: str
-    value: Union[StrictInt, StrictFloat, StrictStr]
+# # TODO: Deprecate ExactMatchFilter and use MetadataFilter instead
+# # Keep class for now so that AutoRetriever can still work with old vector stores
+# class ExactMatchFilter(BaseModel):
+#     key: str
+#     value: Union[StrictInt, StrictFloat, StrictStr]
+
+# set ExactMatchFilter to MetadataFilter
+ExactMatchFilter = MetadataFilter
 
 
 class MetadataFilters(BaseModel):
