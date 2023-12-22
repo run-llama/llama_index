24a13e9603848530577bbf9961f997e386317f27
Berke <berkecanrizai1@gmail.com>
2023-12-22 12:08:33 +0100

    add: server to retrievers

--- llama_index/retrievers/__init__.py
+++ llama_index/retrievers/__init__.py
@@ -31,7 +31,10 @@ from llama_index.indices.vector_store.retrievers import (
 from llama_index.retrievers.auto_merging_retriever import AutoMergingRetriever
 from llama_index.retrievers.bm25_retriever import BM25Retriever
 from llama_index.retrievers.fusion_retriever import QueryFusionRetriever
-from llama_index.retrievers.pathway_retriever import PathwayRetriever
+from llama_index.retrievers.pathway_retriever import (
+    PathwayRetriever,
+    PathwayVectorServer,
+)
 from llama_index.retrievers.recursive_retriever import RecursiveRetriever
 from llama_index.retrievers.router_retriever import RouterRetriever
 from llama_index.retrievers.transform_retriever import TransformRetriever
@@ -60,6 +63,7 @@ __all__ = [
     "VectaraRetriever",
     "YouRetriever",
     "PathwayRetriever",
+    "PathwayVectorServer",
     "QueryFusionRetriever",
     # SQL
     "SQLRetriever",
