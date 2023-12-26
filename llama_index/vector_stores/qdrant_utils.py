8bcf117117645eb9290f62f752e39e4c958c6e81
Logan <logan.markewich@live.com>
2023-12-26 11:02:12 -0600

    Fix qdrant fusion fn, add tests (#9707)

--- llama_index/vector_stores/qdrant_utils.py
+++ llama_index/vector_stores/qdrant_utils.py
@@ -93,27 +93,47 @@ def relative_score_fusion(
 
     # normalize sparse similarities from 0 to 1
     sparse_similarities = [x[0] for x in sparse_result_tuples]
-    max_sparse_sim = max(sparse_similarities)
-    min_sparse_sim = min(sparse_similarities)
-    sparse_similarities = [
-        (x - min_sparse_sim) / (max_sparse_sim - min_sparse_sim)
-        for x in sparse_similarities
-    ]
-    sparse_per_node = {
-        sparse_result_tuples[i][1].node_id: x for i, x in enumerate(sparse_similarities)
-    }
+
+    sparse_per_node = {}
+    if len(sparse_similarities) > 0:
+        max_sparse_sim = max(sparse_similarities)
+        min_sparse_sim = min(sparse_similarities)
+
+        # avoid division by zero
+        if max_sparse_sim == min_sparse_sim:
+            sparse_similarities = [max_sparse_sim] * len(sparse_similarities)
+        else:
+            sparse_similarities = [
+                (x - min_sparse_sim) / (max_sparse_sim - min_sparse_sim)
+                for x in sparse_similarities
+            ]
+
+        sparse_per_node = {
+            sparse_result_tuples[i][1].node_id: x
+            for i, x in enumerate(sparse_similarities)
+        }
 
     # normalize dense similarities from 0 to 1
     dense_similarities = [x[0] for x in dense_result_tuples]
-    max_dense_sim = max(dense_similarities)
-    min_dense_sim = min(dense_similarities)
-    dense_similarities = [
-        (x - min_dense_sim) / (max_dense_sim - min_dense_sim)
-        for x in dense_similarities
-    ]
-    dense_per_node = {
-        dense_result_tuples[i][1].node_id: x for i, x in enumerate(dense_similarities)
-    }
+
+    dense_per_node = {}
+    if len(dense_similarities) > 0:
+        max_dense_sim = max(dense_similarities)
+        min_dense_sim = min(dense_similarities)
+
+        # avoid division by zero
+        if max_dense_sim == min_dense_sim:
+            dense_similarities = [max_dense_sim] * len(dense_similarities)
+        else:
+            dense_similarities = [
+                (x - min_dense_sim) / (max_dense_sim - min_dense_sim)
+                for x in dense_similarities
+            ]
+
+        dense_per_node = {
+            dense_result_tuples[i][1].node_id: x
+            for i, x in enumerate(dense_similarities)
+        }
 
     # fuse the scores
     fused_similarities = []
