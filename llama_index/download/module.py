a4bb17c50e8bf3c4074230590e8c5cf7b87f821b
Jerry Liu <jerryjliu98@gmail.com>
2023-12-28 18:01:19 -0800

    cr

--- llama_index/download/module.py
+++ llama_index/download/module.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 import sys
 from enum import Enum
-from importlib import util
+from importlib import util, import_module
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Union
 
@@ -198,6 +198,7 @@ def download_llama_module(
     use_gpt_index_import: bool = False,
     disable_library_cache: bool = False,
     override_path: bool = False,
+    skip_load: bool = False,
 ) -> Any:
     """Download a module from LlamaHub.
 
@@ -247,19 +248,27 @@ def download_llama_module(
         base_file_name=base_file_name,
         override_path=override_path,
     )
+    if skip_load:
+        return None
 
     # loads the module into memory
     if override_path:
+        # base_path = str(dirpath)
+        # sys.path.append(base_path)
         path = f"{dirpath}/{base_file_name}"
         module_path = _convert_path_to_module(path)
+        # module_path = Path(path).stem
         spec = util.spec_from_file_location(
             module_path, location=path
         )
         if spec is None:
             raise ValueError(f"Could not find file: {path}.")
     else:
+        # base_path = f"{dirpath}/{module_id}"
+        # sys.path.append(base_path)
         path = f"{dirpath}/{module_id}/{base_file_name}"
         module_path = _convert_path_to_module(path)
+        # module_path = Path(path).stem
         spec = util.spec_from_file_location(
             module_path, location=path
         )
@@ -267,6 +276,17 @@ def download_llama_module(
             raise ValueError(
                 f"Could not find file: {path}."
             )
+
+    # instead of using module_from_spec as below, try to import
+    # given that we added to sys.path using importlib
+
+    # module_import_path = Path(base_path).stem + "." + base_file_name[:-3]
+    # print(base_path)
+    # print(module_import_path)
+    # module = import_module(module_import_path)
+
+    # print(path)
+    # print(module_path)
 
     module = util.module_from_spec(spec)
     spec.loader.exec_module(module)  # type: ignore
--- llama_index/llama_pack/download.py
+++ llama_index/llama_pack/download.py
@@ -13,6 +13,8 @@ def download_llama_pack(
     llama_pack_class: str,
     download_dir: str,
     llama_hub_url: str = LLAMA_HUB_URL,
+    refresh_cache: bool = True,
+    skip_load: bool = False,
 ) -> Type[BaseLlamaPack]:
     """Download a single LlamaPack from Llama Hub.
 
@@ -29,11 +31,12 @@ def download_llama_pack(
     pack_cls = download_llama_module(
         llama_pack_class,
         llama_hub_url=llama_hub_url,
-        refresh_cache=True,
+        refresh_cache=refresh_cache,
         custom_path=download_dir,
         library_path="llama_packs/library.json",
         disable_library_cache=True,
         override_path=True,
+        skip_load=skip_load,
     )
     if not issubclass(pack_cls, BaseLlamaPack):
         raise ValueError(f"Tool class {pack_cls} must be a subclass of BaseToolSpec.")
