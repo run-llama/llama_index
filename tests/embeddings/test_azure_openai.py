8d51bf823392b7ba4442dcd81ff75c320fc24d6f
Logan Markewich <logan.markewich@live.com>
2023-12-31 11:29:37 -0600

    linting

--- tests/embeddings/test_azure_openai.py
+++ tests/embeddings/test_azure_openai.py
@@ -1,13 +1,15 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 import httpx
-
 from llama_index.embeddings import AzureOpenAIEmbedding
 
 
 @patch("llama_index.embeddings.azure_openai.AzureOpenAI")
 def test_custom_http_client(azure_openai_mock: MagicMock):
-    """Verify that a custom http_client set for AzureOpenAIEmbedding gets passed on to the implementation from OpenAI"""
+    """
+    Verify that a custom http_client set for AzureOpenAIEmbedding.
+    Should get passed on to the implementation from OpenAI.
+    """
     custom_http_client = httpx.Client()
     embedding = AzureOpenAIEmbedding(http_client=custom_http_client)
     embedding.get_text_embedding(text="foo bar")
--- tests/llms/test_azure_openai.py
+++ tests/llms/test_azure_openai.py
@@ -1,15 +1,17 @@
-from unittest.mock import MagicMock
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import httpx
-
 from llama_index.llms import AzureOpenAI
+
 from tests.llms.test_openai import mock_chat_completion_v1
 
 
 @patch("llama_index.llms.azure_openai.SyncAzureOpenAI")
 def test_custom_http_client(sync_azure_openai_mock: MagicMock):
-    """Verify that a custom http_client set for AzureOpenAI gets passed on to the implementation from OpenAI"""
+    """
+    Verify that a custom http_client set for AzureOpenAI.
+    Should get passed on to the implementation from OpenAI.
+    """
     custom_http_client = httpx.Client()
     mock_instance = sync_azure_openai_mock.return_value
     # Valid mocked result required to not run into another error
