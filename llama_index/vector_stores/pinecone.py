8da30fee50e6f199064585fb07191f4a8cf2ee12
aulorbe <aulorbe@gmail.com>
2024-01-04 14:30:57 -0800

    Revert "Reverting back to using get_distribution b/c pinecone client recently had __version__ removed, unclear why."
    This reverts commit 472991a155c3db4a96b15aa47bec0a50f70d06bb.

--- llama_index/vector_stores/pinecone.py
+++ llama_index/vector_stores/pinecone.py
@@ -224,8 +224,7 @@ class PineconeVectorStore(BasePydanticVectorStore):
             self._pinecone_index = cast(pinecone.Index, pinecone_index)  # type: ignore[name-defined]
         else:
             # Pinecone client version >= 3.0.0 has breaking changes to initialization signature
-            pinecone_client_version = get_distribution("pinecone-client").version[:-5]
-            if version.parse(pinecone_client_version) >= version.parse("3.0.0"):
+            if hasattr(pinecone, "version") and pinecone.version >= "3.0.0":
                 if index_name is None:
                     raise ValueError(
                         "Must specify index_name if not directly passing in client."
