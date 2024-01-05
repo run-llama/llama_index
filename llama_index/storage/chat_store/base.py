d4d541d135ac3a26f3fe7e860c316a5c0655cd4f
Logan Markewich <logan.markewich@live.com>
2024-01-05 11:07:45 -0600

    add chat_store concept for storing chat history

---
+++ llama_index/storage/chat_store/base.py
@@ -0,0 +1,43 @@
+"""Base interface class for storing chat history per user."""
+from abc import abstractmethod
+from typing import List, Optional
+
+from llama_index.llms import ChatMessage
+from llama_index.schema import BaseComponent
+
+
+class BaseChatStore(BaseComponent):
+    @classmethod
+    def class_name(cls) -> str:
+        """Get class name."""
+        return "BaseChatStore"
+
+    @abstractmethod
+    def set_messages(self, key: str, messages: List[ChatMessage]) -> None:
+        """Set messages for a key."""
+        ...
+
+    @abstractmethod
+    def get_messages(self, key: str) -> List[ChatMessage]:
+        """Get messages for a key."""
+        ...
+
+    @abstractmethod
+    def add_message(self, key: str, message: ChatMessage) -> None:
+        """Add a message for a key."""
+        ...
+
+    @abstractmethod
+    def delete_messages(self, key: str) -> None:
+        """Delete messages for a key."""
+        ...
+
+    @abstractmethod
+    def undo_last_message(self, key: str) -> Optional[ChatMessage]:
+        """Undo last message for a key."""
+        ...
+
+    @abstractmethod
+    def get_keys(self) -> List[str]:
+        """Get all keys."""
+        ...
