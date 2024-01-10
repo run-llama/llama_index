7f44ad9dff6e4c27f7547118eecc5b045b5d6b3c
aulorbe <aulorbe@gmail.com>
2024-01-10 12:50:23 -0800

    add more tests

--- tests/vector_stores/test_pinecone.py
+++ tests/vector_stores/test_pinecone.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
 
+import pytest
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 
 
@@ -50,14 +51,59 @@ class TestPineconeVectorStore(unittest.TestCase):
             self.assertIsInstance(store._pinecone_index, MockPineconePods.Index)
 
     def test_with_pod_based_false(self) -> None:
-        # Testing with serverless configuration
+        # Testing with serverless configuration (False is default, so no need to pass param)
         with patch(
             "llama_index.vector_stores.pinecone.PineconeVectorStore._initialize_pinecone_client",
             return_value=MockPineconeServerless.Pinecone("dummy_key").Index(
                 "dummy_index"
             ),
         ):
+            store = PineconeVectorStore(api_key="dummy_key", index_name="dummy_index")
+            self.assertIsInstance(store._pinecone_index, MockPineconeServerless.Index)
+
+    def test_str_value_for_pinecone_index_param_does_not_match_str_value_for_index_name_param(
+        self,
+    ) -> None:
+        str_pinecone_index = "some-string-representing-an-existing-pinecone-index"
+        with patch(
+            "llama_index.vector_stores.pinecone.PineconeVectorStore._initialize_pinecone_client",
+            return_value=MockPineconeServerless.Pinecone("dummy_key").Index(
+                "dummy_index"
+            ),
+        ):  # MockPineconeServerless vs MockPineconePods is arbitrary for this test
+            with pytest.raises(ValueError) as e:
+                store = PineconeVectorStore(
+                    pinecone_index=str_pinecone_index,
+                    api_key="dummy_key",
+                    index_name="dummy_index",
+                    use_pod_based=False,
+                )
+                assert (
+                    e.value.message
+                    == "The string value for `pinecone_index` must match the string value for `index_name`."
+                )
+
+    def test_str_value_for_pinecone_index_param_gets_transformed_into_index_obj(
+        self,
+    ) -> None:
+        str_pinecone_index = "some-string-representing-an-existing-pinecone-index"
+        with patch(
+            "llama_index.vector_stores.pinecone.PineconeVectorStore._initialize_pinecone_client",
+            return_value=MockPineconeServerless.Pinecone("dummy_key").Index(
+                str_pinecone_index
+            ),
+        ):  # MockPineconeServerless vs MockPineconePods is arbitrary for this test
             store = PineconeVectorStore(
-                api_key="dummy_key", index_name="dummy_index", use_pod_based=False
+                pinecone_index=str_pinecone_index,
+                api_key="dummy_key",
+                index_name=str_pinecone_index,
             )
             self.assertIsInstance(store._pinecone_index, MockPineconeServerless.Index)
+
+    def test_pinecone_vector_store_without_passing_index_name(self) -> None:
+        with pytest.raises(ValueError) as e:
+            PineconeVectorStore()
+            assert (
+                e.value.message
+                == "index_name is required for Pinecone client initialization"
+            )
