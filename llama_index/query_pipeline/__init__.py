d2b325247cd7a8febcab48205891e96daeb398cf
Jerry Liu <jerryjliu98@gmail.com>
2024-01-09 20:40:44 -0800

    cr

--- llama_index/query_pipeline/__init__.py
+++ llama_index/query_pipeline/__init__.py
@@ -2,8 +2,11 @@
 
 from llama_index.core.query_pipeline.query_component import (
     CustomQueryComponent,
-    InputComponent,
     QueryComponent,
+)
+from llama_index.core.query_pipeline.components import (
+    InputComponent,
+    FnComponent,
 )
 from llama_index.query_pipeline.query import InputKeys, OutputKeys, QueryPipeline
 
@@ -14,4 +17,5 @@ __all__ = [
     "QueryComponent",
     "CustomQueryComponent",
     "InputComponent",
+    "FnComponent"
 ]
