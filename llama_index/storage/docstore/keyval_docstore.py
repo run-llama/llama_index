4b8d100ab3569caec9d4d3a80533dcd42cc56454
Jordan Parker <jordanhparker6@gmail.com>
2024-01-11 22:13:41 +1000

    :sparkles: adding async support to the docstore

--- llama_index/storage/docstore/keyval_docstore.py
+++ llama_index/storage/docstore/keyval_docstore.py
@@ -150,6 +150,91 @@ class KVDocumentStore(BaseDocumentStore):
                         node_key, metadata, collection=self._metadata_collection
                     )
 
+    async def aadd_documents(
+        self,
+        nodes: Sequence[BaseNode],
+        allow_update: bool = True,
+        batch_size: Optional[int] = None,
+        store_text: bool = True,
+    ) -> None:
+        """Add a document to the store.
+
+        Args:
+            docs (List[BaseDocument]): documents
+            allow_update (bool): allow update of docstore from document
+
+        """
+        batch_size = batch_size or self._batch_size
+        if batch_size > 1:
+            if store_text:
+                await self._kvstore.aput_all(
+                    [(node.node_id, doc_to_json(node)) for node in nodes],
+                    collection=self._node_collection,
+                )
+            ref_docs, metadatas = [], []
+            for node in nodes:
+                metadata = {"doc_hash": node.hash}
+                if isinstance(node, TextNode) and node.ref_doc_id is not None:
+                    ref_doc_info = (
+                        await self.aget_ref_doc_info(node.ref_doc_id) or RefDocInfo()
+                    )
+                    if node.node_id not in ref_doc_info.node_ids:
+                        ref_doc_info.node_ids.append(node.node_id)
+                    if not ref_doc_info.metadata:
+                        ref_doc_info.metadata = node.metadata or {}
+                    metadata["ref_doc_id"] = node.ref_doc_id
+                    ref_docs.append((node.node_id, ref_doc_info.to_dict()))
+                metadatas.append((node.node_id, metadata))
+            await self._kvstore.aput_all(
+                ref_docs,
+                collection=self._ref_doc_collection,
+            )
+            await self._kvstore.aput_all(
+                metadatas,
+                collection=self._metadata_collection,
+            )
+        else:
+            for node in nodes:
+                # NOTE: doc could already exist in the store, but we overwrite it
+                if not allow_update and await self.adocument_exists(node.node_id):
+                    raise ValueError(
+                        f"node_id {node.node_id} already exists. "
+                        "Set allow_update to True to overwrite."
+                    )
+                node_key = node.node_id
+                data = doc_to_json(node)
+
+                if store_text:
+                    await self._kvstore.aput(
+                        node_key, data, collection=self._node_collection
+                    )
+
+                # update doc_collection if needed
+                metadata = {"doc_hash": node.hash}
+                if isinstance(node, TextNode) and node.ref_doc_id is not None:
+                    ref_doc_info = (
+                        await self.aget_ref_doc_info(node.ref_doc_id) or RefDocInfo()
+                    )
+                    if node.node_id not in ref_doc_info.node_ids:
+                        ref_doc_info.node_ids.append(node.node_id)
+                    if not ref_doc_info.metadata:
+                        ref_doc_info.metadata = node.metadata or {}
+                    await self._kvstore.aput(
+                        node.ref_doc_id,
+                        ref_doc_info.to_dict(),
+                        collection=self._ref_doc_collection,
+                    )
+
+                    # update metadata with map
+                    metadata["ref_doc_id"] = node.ref_doc_id
+                    await self._kvstore.aput(
+                        node_key, metadata, collection=self._metadata_collection
+                    )
+                else:
+                    await self._kvstore.aput(
+                        node_key, metadata, collection=self._metadata_collection
+                    )
+
     def get_document(self, doc_id: str, raise_error: bool = True) -> Optional[BaseNode]:
         """Get a document from the store.
 
@@ -166,9 +251,45 @@ class KVDocumentStore(BaseDocumentStore):
                 return None
         return json_to_doc(json)
 
+    async def aget_document(
+        self, doc_id: str, raise_error: bool = True
+    ) -> Optional[BaseNode]:
+        """Get a document from the store.
+
+        Args:
+            doc_id (str): document id
+            raise_error (bool): raise error if doc_id not found
+
+        """
+        json = await self._kvstore.aget(doc_id, collection=self._node_collection)
+        if json is None:
+            if raise_error:
+                raise ValueError(f"doc_id {doc_id} not found.")
+            else:
+                return None
+        return json_to_doc(json)
+
     def get_ref_doc_info(self, ref_doc_id: str) -> Optional[RefDocInfo]:
         """Get the RefDocInfo for a given ref_doc_id."""
         ref_doc_info = self._kvstore.get(
+            ref_doc_id, collection=self._ref_doc_collection
+        )
+        if not ref_doc_info:
+            return None
+
+        # TODO: deprecated legacy support
+        if "doc_ids" in ref_doc_info:
+            ref_doc_info["node_ids"] = ref_doc_info.get("doc_ids", [])
+            ref_doc_info.pop("doc_ids")
+
+            ref_doc_info["metadata"] = ref_doc_info.get("extra_info", {})
+            ref_doc_info.pop("extra_info")
+
+        return RefDocInfo(**ref_doc_info)
+
+    async def aget_ref_doc_info(self, ref_doc_id: str) -> Optional[RefDocInfo]:
+        """Get the RefDocInfo for a given ref_doc_id."""
+        ref_doc_info = await self._kvstore.aget(
             ref_doc_id, collection=self._ref_doc_collection
         )
         if not ref_doc_info:
@@ -203,13 +324,42 @@ class KVDocumentStore(BaseDocumentStore):
 
         return all_ref_doc_infos
 
+    async def aget_all_ref_doc_info(self) -> Optional[Dict[str, RefDocInfo]]:
+        """Get a mapping of ref_doc_id -> RefDocInfo for all ingested documents."""
+        ref_doc_infos = await self._kvstore.aget_all(
+            collection=self._ref_doc_collection
+        )
+        if ref_doc_infos is None:
+            return None
+
+        # TODO: deprecated legacy support
+        all_ref_doc_infos = {}
+        for doc_id, ref_doc_info in ref_doc_infos.items():
+            if "doc_ids" in ref_doc_info:
+                ref_doc_info["node_ids"] = ref_doc_info.get("doc_ids", [])
+                ref_doc_info.pop("doc_ids")
+
+                ref_doc_info["metadata"] = ref_doc_info.get("extra_info", {})
+                ref_doc_info.pop("extra_info")
+                all_ref_doc_infos[doc_id] = RefDocInfo(**ref_doc_info)
+
+        return all_ref_doc_infos
+
     def ref_doc_exists(self, ref_doc_id: str) -> bool:
         """Check if a ref_doc_id has been ingested."""
         return self.get_ref_doc_info(ref_doc_id) is not None
 
+    async def aref_doc_exists(self, ref_doc_id: str) -> bool:
+        """Check if a ref_doc_id has been ingested."""
+        return await self.aget_ref_doc_info(ref_doc_id) is not None
+
     def document_exists(self, doc_id: str) -> bool:
         """Check if document exists."""
         return self._kvstore.get(doc_id, self._node_collection) is not None
+
+    async def adocument_exists(self, doc_id: str) -> bool:
+        """Check if document exists."""
+        return await self._kvstore.aget(doc_id, self._node_collection) is not None
 
     def _remove_ref_doc_node(self, doc_id: str) -> None:
         """Helper function to remove node doc_id from ref_doc_collection."""
@@ -241,6 +391,40 @@ class KVDocumentStore(BaseDocumentStore):
 
             self._kvstore.delete(ref_doc_id, collection=self._metadata_collection)
 
+    async def _aremove_ref_doc_node(self, doc_id: str) -> None:
+        """Helper function to remove node doc_id from ref_doc_collection."""
+        metadata = await self._kvstore.aget(
+            doc_id, collection=self._metadata_collection
+        )
+        if metadata is None:
+            return
+
+        ref_doc_id = metadata.get("ref_doc_id", None)
+
+        if ref_doc_id is None:
+            return
+
+        ref_doc_info = await self._kvstore.aget(
+            ref_doc_id, collection=self._ref_doc_collection
+        )
+
+        if ref_doc_info is not None:
+            ref_doc_obj = RefDocInfo(**ref_doc_info)
+
+            ref_doc_obj.node_ids.remove(doc_id)
+
+            # delete ref_doc from collection if it has no more doc_ids
+            if len(ref_doc_obj.node_ids) > 0:
+                await self._kvstore.aput(
+                    ref_doc_id,
+                    ref_doc_obj.to_dict(),
+                    collection=self._ref_doc_collection,
+                )
+
+            await self._kvstore.adelete(
+                ref_doc_id, collection=self._metadata_collection
+            )
+
     def delete_document(
         self, doc_id: str, raise_error: bool = True, remove_ref_doc_node: bool = True
     ) -> None:
@@ -250,6 +434,21 @@ class KVDocumentStore(BaseDocumentStore):
 
         delete_success = self._kvstore.delete(doc_id, collection=self._node_collection)
         _ = self._kvstore.delete(doc_id, collection=self._metadata_collection)
+
+        if not delete_success and raise_error:
+            raise ValueError(f"doc_id {doc_id} not found.")
+
+    async def adelete_document(
+        self, doc_id: str, raise_error: bool = True, remove_ref_doc_node: bool = True
+    ) -> None:
+        """Delete a document from the store."""
+        if remove_ref_doc_node:
+            await self._aremove_ref_doc_node(doc_id)
+
+        delete_success = await self._kvstore.adelete(
+            doc_id, collection=self._node_collection
+        )
+        _ = await self._kvstore.adelete(doc_id, collection=self._metadata_collection)
 
         if not delete_success and raise_error:
             raise ValueError(f"doc_id {doc_id} not found.")
@@ -269,14 +468,46 @@ class KVDocumentStore(BaseDocumentStore):
         self._kvstore.delete(ref_doc_id, collection=self._metadata_collection)
         self._kvstore.delete(ref_doc_id, collection=self._ref_doc_collection)
 
+    async def adelete_ref_doc(self, ref_doc_id: str, raise_error: bool = True) -> None:
+        """Delete a ref_doc and all it's associated nodes."""
+        ref_doc_info = await self.aget_ref_doc_info(ref_doc_id)
+        if ref_doc_info is None:
+            if raise_error:
+                raise ValueError(f"ref_doc_id {ref_doc_id} not found.")
+            else:
+                return
+
+        for doc_id in ref_doc_info.node_ids:
+            await self.adelete_document(
+                doc_id, raise_error=False, remove_ref_doc_node=False
+            )
+
+        await self._kvstore.adelete(ref_doc_id, collection=self._metadata_collection)
+        await self._kvstore.adelete(ref_doc_id, collection=self._ref_doc_collection)
+
     def set_document_hash(self, doc_id: str, doc_hash: str) -> None:
         """Set the hash for a given doc_id."""
         metadata = {"doc_hash": doc_hash}
         self._kvstore.put(doc_id, metadata, collection=self._metadata_collection)
 
+    async def aset_document_hash(self, doc_id: str, doc_hash: str) -> None:
+        """Set the hash for a given doc_id."""
+        metadata = {"doc_hash": doc_hash}
+        await self._kvstore.aput(doc_id, metadata, collection=self._metadata_collection)
+
     def get_document_hash(self, doc_id: str) -> Optional[str]:
         """Get the stored hash for a document, if it exists."""
         metadata = self._kvstore.get(doc_id, collection=self._metadata_collection)
+        if metadata is not None:
+            return metadata.get("doc_hash", None)
+        else:
+            return None
+
+    async def aget_document_hash(self, doc_id: str) -> Optional[str]:
+        """Get the stored hash for a document, if it exists."""
+        metadata = await self._kvstore.aget(
+            doc_id, collection=self._metadata_collection
+        )
         if metadata is not None:
             return metadata.get("doc_hash", None)
         else:
@@ -287,6 +518,17 @@ class KVDocumentStore(BaseDocumentStore):
         hashes = {}
         for doc_id in self._kvstore.get_all(collection=self._metadata_collection):
             hash = self.get_document_hash(doc_id)
+            if hash is not None:
+                hashes[hash] = doc_id
+        return hashes
+
+    async def aget_all_document_hashes(self) -> Dict[str, str]:
+        """Get the stored hash for all documents."""
+        hashes = {}
+        for doc_id in await self._kvstore.aget_all(
+            collection=self._metadata_collection
+        ):
+            hash = await self.aget_document_hash(doc_id)
             if hash is not None:
                 hashes[hash] = doc_id
         return hashes
