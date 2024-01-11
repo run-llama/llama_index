4eeaae76c98adb9d77ca62ea7824c93ea6ad5b1b
Logan Markewich <logan.markewich@live.com>
2024-01-11 15:01:49 -0600

    wip

--- llama_index/core/base_retriever.py
+++ llama_index/core/base_retriever.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, List, Optional
 from llama_index.bridge.pydantic import Field
 from llama_index.callbacks.base import CallbackManager
 from llama_index.callbacks.schema import CBEventType, EventPayload
+from llama_index.core.base_query_engine import BaseQueryEngine
 from llama_index.core.query_pipeline.query_component import (
     ChainableMixin,
     InputKeys,
@@ -13,15 +14,27 @@ from llama_index.core.query_pipeline.query_component import (
     validate_and_convert_stringable,
 )
 from llama_index.prompts.mixin import PromptDictType, PromptMixin, PromptMixinType
-from llama_index.schema import NodeWithScore, QueryBundle, QueryType
+from llama_index.schema import (
+    BaseNode,
+    IndexNode,
+    NodeWithScore,
+    QueryBundle,
+    QueryType,
+    TextNode,
+)
 from llama_index.service_context import ServiceContext
 
 
 class BaseRetriever(ChainableMixin, PromptMixin):
     """Base retriever."""
 
-    def __init__(self, callback_manager: Optional[CallbackManager] = None) -> None:
+    def __init__(
+        self,
+        callback_manager: Optional[CallbackManager] = None,
+        object_map: Optional[Dict] = None,
+    ) -> None:
         self.callback_manager = callback_manager or CallbackManager()
+        self.object_map = object_map or {}
 
     def _check_callback_manager(self) -> None:
         """Check callback manager."""
@@ -38,6 +51,59 @@ class BaseRetriever(ChainableMixin, PromptMixin):
 
     def _update_prompts(self, prompts: PromptDictType) -> None:
         """Update prompts."""
+
+    def _retrieve_from_object(
+        self,
+        obj: Any,
+        query_bundle: QueryBundle,
+        score: float,
+    ) -> List[NodeWithScore]:
+        """Retrieve nodes from object."""
+        if isinstance(obj, NodeWithScore):
+            return [obj]
+        elif isinstance(obj, BaseNode):
+            return [NodeWithScore(node=obj, score=score)]
+        elif isinstance(obj, BaseQueryEngine):
+            response = obj.query(query_bundle)
+            return [NodeWithScore(node=TextNode(text=str(response)), score=score)]
+        elif isinstance(obj, BaseRetriever):
+            return obj.retrieve(query_bundle)
+        elif isinstance(obj, QueryComponent):
+            component_keys = obj.input_keys.required_keys
+            if len(component_keys) > 1:
+                raise ValueError(
+                    f"QueryComponent {obj} has more than one input key: {component_keys}"
+                )
+            elif len(component_keys) == 0:
+                component_response = obj.run_component()
+            else:
+                kwargs = {next(iter(component_keys)): query_bundle.query_str}
+                component_response = obj.run_component(**kwargs)
+
+            result_output = str(next(iter(component_response.values())))
+            return [NodeWithScore(node=TextNode(text=result_output), score=score)]
+        else:
+            raise ValueError(f"Object {obj} is not retrievable.")
+
+    def _handle_recursive_retrieval(
+        self, query_bundle: QueryBundle, nodes: List[NodeWithScore]
+    ) -> List[NodeWithScore]:
+        retrieved_nodes: List[NodeWithScore] = []
+        for n in nodes:
+            node = n.node
+            if isinstance(node, IndexNode):
+                obj = self.object_map.get(node.index_id, None)
+                if obj is not None:
+                    retrieved_nodes.extend(
+                        self._retrieve_from_object(
+                            obj, query_bundle=query_bundle, score=node.score
+                        )
+                    )
+            else:
+                retrieved_nodes.append(n)
+                continue
+
+        return retrieved_nodes
 
     def retrieve(self, str_or_query_bundle: QueryType) -> List[NodeWithScore]:
         """Retrieve nodes given query.
