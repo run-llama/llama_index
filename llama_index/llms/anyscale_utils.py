cef5539767c2807faf410c5f4f8ab5efa14d1d4f
kYLe <kyle@anyscale.com>
2024-01-11 13:59:06 -0600

    Add Anyscale Embedding model support (#9470)
    * Add Anyscale embedding model
    
    * format updatae
    
    * remove debug print
    
    * removing debug print
    
    * add anyscale example
    
    * fornat
    
    * format
    
    * format
    
    * format
    
    * add basic embedding example
    
    * format
    
    * format
    
    * ruff for ipynb
    
    * ipynb modify
    
    * black
    
    * cr
    
    ---------
    
    Co-authored-by: Logan <logan.markewich@live.com>
    Co-authored-by: Haotian Zhang <socool.king@gmail.com>

--- llama_index/llms/anyscale_utils.py
+++ llama_index/llms/anyscale_utils.py
@@ -1,29 +1,41 @@
-from typing import Any, Dict, List, Sequence
+from typing import Any, Dict, List, Optional, Sequence, Tuple
 
 from llama_index.core.llms.types import ChatMessage, MessageRole
+from llama_index.llms.generic_utils import get_from_param_or_env
+
+DEFAULT_ANYSCALE_API_BASE = "https://api.endpoints.anyscale.com/v1"
+DEFAULT_ANYSCALE_API_VERSION = ""
 
 LLAMA_MODELS = {
     "meta-llama/Llama-2-7b-chat-hf": 4096,
     "meta-llama/Llama-2-13b-chat-hf": 4096,
     "meta-llama/Llama-2-70b-chat-hf": 4096,
     "codellama/CodeLlama-34b-Instruct-hf": 16384,
+    "Meta-Llama/Llama-Guard-7b": 4096,
 }
 
 MISTRAL_MODELS = {
     "mistralai/Mistral-7B-Instruct-v0.1": 16384,
+    "Open-Orca/Mistral-7B-OpenOrca": 8192,
     "mistralai/Mixtral-8x7B-Instruct-v0.1": 32768,
+}
+
+ZEPHYR_MODELS = {
+    "HuggingFaceH4/zephyr-7b-beta": 16384,
 }
 
 ALL_AVAILABLE_MODELS = {
     **LLAMA_MODELS,
     **MISTRAL_MODELS,
+    **ZEPHYR_MODELS,
 }
 
 DISCONTINUED_MODELS: Dict[str, int] = {}
 
 
 def anyscale_modelname_to_contextsize(modelname: str) -> int:
-    """Calculate the maximum number of tokens possible to generate for a model.
+    """
+    Calculate the maximum number of tokens possible to generate for a model.
 
     Args:
         modelname: The modelname we want to know the context size for.
@@ -76,3 +88,32 @@ def messages_to_anyscale_prompt(messages: Sequence[ChatMessage]) -> List[Dict]:
         raise ValueError("Got empty list of messages.")
 
     return [_message_to_anyscale_prompt(message) for message in messages]
+
+
+def resolve_anyscale_credentials(
+    api_key: Optional[str] = None,
+    api_base: Optional[str] = None,
+    api_version: Optional[str] = None,
+) -> Tuple[Optional[str], str, str]:
+    """
+    "Resolve OpenAI credentials.
+
+    The order of precedence is:
+    1. param
+    2. env
+    3. openai module
+    4. default
+    """
+    # resolve from param or env
+    api_key = get_from_param_or_env("api_key", api_key, "ANYSCALE_API_KEY", "")
+    api_base = get_from_param_or_env("api_base", api_base, "ANYSCALE_API_BASE", "")
+    api_version = get_from_param_or_env(
+        "api_version", api_version, "ANYSCALE_API_VERSION", ""
+    )
+
+    # resolve from openai module or default
+    final_api_key = api_key or ""
+    final_api_base = api_base or DEFAULT_ANYSCALE_API_BASE
+    final_api_version = api_version or DEFAULT_ANYSCALE_API_VERSION
+
+    return final_api_key, str(final_api_base), final_api_version
