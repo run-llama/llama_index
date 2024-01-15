0942b633e411cd09cb9108945ac6c2041dd10947
Haotian Zhang <socool.king@gmail.com>
2024-01-15 14:45:07 -0500

    cr

--- llama_index/postprocessor/rankGPT_rerank.py
+++ llama_index/postprocessor/rankGPT_rerank.py
@@ -1,8 +1,5 @@
-import copy
 import logging
 from typing import Any, Dict, List, Optional, Sequence
-
-import tiktoken
 
 from llama_index.bridge.pydantic import Field
 from llama_index.llms import ChatMessage, ChatResponse, OpenAI
@@ -36,6 +33,7 @@ class RankGPTRerank(BaseNodePostprocessor):
     api_key: str = Field(default=None, description="The OpenAI API key.", exclude=True)
     api_base: str = Field(description="The base URL for OpenAI API.")
     api_version: str = Field(description="The API version for OpenAI API.")
+    token_counter: TokenCounter = Field(description="token counter.")
 
     def __init__(
         self,
@@ -45,6 +43,7 @@ class RankGPTRerank(BaseNodePostprocessor):
         api_key: Optional[str] = None,
         api_base: Optional[str] = None,
         api_version: Optional[str] = None,
+        token_counter: Optional[TokenCounter] = None,
         **kwargs: Any,
     ) -> None:
         api_key, api_base, api_version = resolve_openai_credentials(
@@ -52,6 +51,7 @@ class RankGPTRerank(BaseNodePostprocessor):
             api_base=api_base,
             api_version=api_version,
         )
+        token_counter = TokenCounter()
         super().__init__(
             top_n=top_n,
             model=model,
@@ -59,9 +59,9 @@ class RankGPTRerank(BaseNodePostprocessor):
             api_key=api_key,
             api_base=api_base,
             api_version=api_version,
+            token_counter=token_counter,
             **kwargs,
         )
-        self._token_counter = TokenCounter()
 
     @classmethod
     def class_name(cls) -> str:
@@ -113,41 +113,6 @@ class RankGPTRerank(BaseNodePostprocessor):
     def _get_post_prompt(self, query: str, num: int) -> str:
         return f"Search Query: {query}. \nRank the {num} passages above based on their relevance to the search query. The passages should be listed in descending order using identifiers. The most relevant passages should be listed first. The output format should be [] > [], e.g., [1] > [2]. Only response the ranking results, do not say any word or explain."
 
-    def _num_tokens_from_messages(
-        self, messages: Sequence[ChatMessage], model: str = "gpt-3.5-turbo-0301"
-    ) -> int:
-        """Returns the number of tokens used by a list of messages."""
-        if model == "gpt-3.5-turbo":
-            return self._num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
-        elif model == "gpt-4":
-            return self._num_tokens_from_messages(messages, model="gpt-4-0314")
-        elif model == "gpt-3.5-turbo-0301":
-            tokens_per_message = (
-                4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
-            )
-            # tokens_per_name = -1  # if there's a name, the role is omitted
-        elif model == "gpt-4-0314":
-            tokens_per_message = 3
-            # tokens_per_name = 1
-        else:
-            tokens_per_message, tokens_per_name = 0, 0
-
-        try:
-            encoding = tiktoken.get_encoding(model)
-        except Exception as e:
-            logger.warning(
-                f"Encountered exception when loading encoding for given model: {e}"
-            )
-            encoding = tiktoken.get_encoding("cl100k_base")
-
-        num_tokens = 0
-        for message in messages:
-            num_tokens += tokens_per_message
-            if message.content is not None:
-                num_tokens += len(encoding.encode(str(message.content)))
-        num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
-        return num_tokens
-
     def create_permutation_instruction(self, item: Dict[str, Any]) -> List[ChatMessage]:
         query = item["query"]
         num = len(item["hits"])
@@ -170,10 +135,8 @@ class RankGPTRerank(BaseNodePostprocessor):
             messages.append(
                 ChatMessage(role="user", content=self._get_post_prompt(query, num))
             )
-            for m in messages:
-                print(m)
             if (
-                self._token_counter.estimate_tokens_in_messages(messages)
+                self.token_counter.estimate_tokens_in_messages(messages)
                 <= openai_modelname_to_contextsize(self.model) - 200
             ):
                 break
@@ -203,15 +166,12 @@ class RankGPTRerank(BaseNodePostprocessor):
         return new_response
 
     def _receive_permutation(self, item: Dict[str, Any], permutation: str) -> List[int]:
-        rank_start = 0
         rank_end = len(item["hits"])
 
         response = self._clean_response(permutation)
         response_list = [int(x) - 1 for x in response.split()]
         response_list = self._remove_duplicate(response_list)
-        original_rank = copy.deepcopy(item["hits"][rank_start:rank_end])
-        # original_rank = [tt for tt in range(len(cut_range))]
-        response_list = [ss for ss in response_list if ss in original_rank]
+        response_list = [ss for ss in response_list if ss in range(rank_end)]
         return response_list + [
-            tt for tt in original_rank if tt not in response_list
+            tt for tt in range(rank_end) if tt not in response_list
         ]  # add the rest of the rank
