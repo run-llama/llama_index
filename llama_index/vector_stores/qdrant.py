4b7ebdc9d11e8b96141bd3a169a8bd9a243634f0
Simon Suo <simonsdsuo@gmail.com>
2024-01-19 20:31:13 -0800

    wip

--- llama_index/vector_stores/qdrant.py
+++ llama_index/vector_stores/qdrant.py
@@ -517,7 +517,7 @@ class QdrantVectorStore(BasePydanticVectorStore):
                             name="text-dense",
                             vector=query_embedding,
                         ),
-                        limit=sparse_top_k,
+                        limit=query.similarity_top_k,
                         filter=query_filter,
                         with_payload=True,
                     ),
