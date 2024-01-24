run-llama/llama_index

commit 5a0ae84909043693f4920fc37e2b09f3668bac40

feat: extract title for nodes based on refId (#10209)

llama_index/extractors/metadata_extractors.py
@@ -19,7 +19,6 @@ The prompts used to generate the metadata are specifically aimed to help
 disambiguate the document or subsection from other similar documents or subsections.
 (similar with contrastive learning)
 """
-from functools import reduce
 from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, cast
 
 from llama_index.async_utils import DEFAULT_NUM_WORKERS, run_jobs
@@ -100,45 +99,60 @@ class TitleExtractor(BaseExtractor):
         return "TitleExtractor"
 
     async def aextract(self, nodes: Sequence[BaseNode]) -> List[Dict]:
-        nodes_to_extract_title: List[BaseNode] = []
+        nodes_to_extract_title = self.filter_nodes(nodes)
 
+        if not nodes_to_extract_title:
+            return []
+
+        nodes_by_doc_id = self.separate_nodes_by_ref_id(nodes_to_extract_title)
+        titles_by_doc_id = await self.extract_titles(nodes_by_doc_id)
+
+        return [{"document_title": titles_by_doc_id[node.ref_doc_id]} for node in nodes]
+
+    def filter_nodes(self, nodes: Sequence[BaseNode]) -> List[BaseNode]:
+        filtered_nodes: List[BaseNode] = []
         for node in nodes:
-            if len(nodes_to_extract_title) >= self.nodes:
+            if len(filtered_nodes) >= self.nodes:
                 break
             if self.is_text_node_only and not isinstance(node, TextNode):
                 continue
-            nodes_to_extract_title.append(node)
+            filtered_nodes.append(node)
+        return filtered_nodes
 
-        if len(nodes_to_extract_title) == 0:
-            # Could not extract title
-            return []
+    def separate_nodes_by_ref_id(self, nodes: Sequence[BaseNode]) -> Dict:
+        separated_items: Dict[Optional[str], List[BaseNode]] = {}
 
+        for node in nodes:
+            key = node.ref_doc_id
+            if key not in separated_items:
+                separated_items[key] = []
+
+            separated_items[key].append(node)
+
+        return separated_items
+
+    async def extract_titles(self, nodes_by_doc_id: Dict) -> Dict:
+        titles_by_doc_id = {}
+        for key, nodes in nodes_by_doc_id.items():
+            title_candidates = await self.get_title_candidates(nodes)
+            combined_titles = ", ".join(title_candidates)
+            titles_by_doc_id[key] = await self.llm.apredict(
+                PromptTemplate(template=self.combine_template),
+                context_str=combined_titles,
+            )
+        return titles_by_doc_id
+
+    async def get_title_candidates(self, nodes: List[BaseNode]) -> List[str]:
         title_jobs = [
             self.llm.apredict(
                 PromptTemplate(template=self.node_template),
                 context_str=cast(TextNode, node).text,
             )
-            for node in nodes_to_extract_title
+            for node in nodes
         ]
-        title_candidates = await run_jobs(
+        return await run_jobs(
             title_jobs, show_progress=self.show_progress, workers=self.num_workers
         )
-
-        if len(nodes_to_extract_title) > 1:
-            titles = reduce(
-                lambda x, y: x + "," + y, title_candidates[1:], title_candidates[0]
-            )
-
-            title = await self.llm.apredict(
-                PromptTemplate(template=self.combine_template),
-                context_str=titles,
-            )
-        else:
-            title = title_candidates[
-                0
-            ]  # if single node, just use the title from that node
-
-        return [{"document_title": title.strip(' \t\n\r"')} for _ in nodes]
 
 
 class KeywordExtractor(BaseExtractor):

commit 3a5de4ec38ce1df839a48ddc17d83db2f97c6ea9

fixed the bug introduced by https://github.com/run-llama/llama_index/pull/10209
added pypdf dependencies

revised logic to fix the metadata extractor bug

poetry lock

poetry lock

just update pypdf for poetry

llama_index/extractors/metadata_extractors.py
@@ -99,21 +99,13 @@ class TitleExtractor(BaseExtractor):
         return "TitleExtractor"
 
     async def aextract(self, nodes: Sequence[BaseNode]) -> List[Dict]:
-        nodes_to_extract_title = self.filter_nodes(nodes)
-
-        if not nodes_to_extract_title:
-            return []
-
-        nodes_by_doc_id = self.separate_nodes_by_ref_id(nodes_to_extract_title)
+        nodes_by_doc_id = self.separate_nodes_by_ref_id(nodes)
         titles_by_doc_id = await self.extract_titles(nodes_by_doc_id)
-
         return [{"document_title": titles_by_doc_id[node.ref_doc_id]} for node in nodes]
 
     def filter_nodes(self, nodes: Sequence[BaseNode]) -> List[BaseNode]:
         filtered_nodes: List[BaseNode] = []
         for node in nodes:
-            if len(filtered_nodes) >= self.nodes:
-                break
             if self.is_text_node_only and not isinstance(node, TextNode):
                 continue
             filtered_nodes.append(node)
@@ -127,7 +119,8 @@ class TitleExtractor(BaseExtractor):
             if key not in separated_items:
                 separated_items[key] = []
 
-            separated_items[key].append(node)
+            if len(separated_items[key]) < self.nodes:
+                separated_items[key].append(node)
 
         return separated_items
 

pyproject.toml
@@ -121,6 +121,7 @@ mypy = "0.991"
 pre-commit = "3.2.0"
 pylint = "2.15.10"
 pymongo = "^4.5.0"  # needed for tests
+pypdf = "*"
 pytest = "7.2.1"
 pytest-asyncio = "0.21.0"
 pytest-dotenv = "0.5.2"

tests/extractors/test_metadata_extractor.py
@@ -1,0 +1,69 @@
+"""Test dataset generation."""
+
+import tempfile
+import typing
+import urllib.request
+
+from llama_index import SimpleDirectoryReader
+from llama_index.extractors import (
+    QuestionsAnsweredExtractor,
+    TitleExtractor,
+)
+from llama_index.ingestion import IngestionPipeline
+from llama_index.llms import OpenAI
+from llama_index.text_splitter import TokenTextSplitter
+
+
+def two_random_integers(range_limit: int) -> typing.Tuple[int, int]:
+    import random
+
+    index1 = random.randint(0, range_limit - 1)
+    index2 = index1
+    while index2 == index1:
+        index2 = random.randint(0, range_limit - 1)
+    return index1, index2
+
+
+def test_metadata_extractor() -> None:
+    """Test metadata extraction."""
+    llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo", max_tokens=512)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        urllib.request.urlretrieve(
+            "https://www.dropbox.com/scl/fi/6dlqdk6e2k1mjhi8dee5j/uber.pdf?rlkey=2jyoe49bg2vwdlz30l76czq6g&dl=1",
+            f"{tmpdirname}/10k-132.pdf",
+        )
+        urllib.request.urlretrieve(
+            "https://www.dropbox.com/scl/fi/qn7g3vrk5mqb18ko4e5in/lyft.pdf?rlkey=j6jxtjwo8zbstdo4wz3ns8zoj&dl=1",
+            f"{tmpdirname}/10k-vFinal.pdf",
+        )
+
+        # Note the uninformative document file name, which may be a common scenario in a production setting
+        uber_docs = SimpleDirectoryReader(
+            input_files=[f"{tmpdirname}/10k-132.pdf"]
+        ).load_data()
+        uber_front_pages = uber_docs[0:3]
+        uber_content = uber_docs[63:69]
+        uber_docs = uber_front_pages + uber_content
+
+        text_splitter = TokenTextSplitter(
+            separator=" ", chunk_size=512, chunk_overlap=128
+        )
+
+        extractors = [
+            TitleExtractor(nodes=5, llm=llm),
+            QuestionsAnsweredExtractor(questions=3, llm=llm),
+        ]
+
+        transformations = [text_splitter, *extractors]
+
+        pipeline = IngestionPipeline(transformations=transformations)
+
+        uber_nodes = pipeline.run(documents=uber_docs)
+
+        assert len(uber_nodes) == 21
+        index1, index2 = two_random_integers(len(uber_nodes))
+        assert (
+            uber_nodes[index1].metadata["document_title"]
+            != uber_nodes[index2].metadata["document_title"]
+        )