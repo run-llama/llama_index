db1a445696061186a4c085c2cdaa4d43bf631b76
Jerry Liu <jerryjliu98@gmail.com>
2024-01-24 22:58:54 -0800

    cr

--- docs/optimizing/custom_modules.md
+++ docs/optimizing/custom_modules.md
@@ -17,6 +17,7 @@ This guide centralizes all the resources around writing custom modules in LlamaI
 - [Custom Embedding Model](custom_embeddings)
 
 ## Custom Output Parsers
+
 - [Custom Output Parsers](/examples/output_parsing/llm_program.ipynb)
 
 ## Custom Transformations
--- llama_index/output_parsers/__init__.py
+++ llama_index/output_parsers/__init__.py
@@ -1,15 +1,15 @@
 """Output parsers."""
 
+from llama_index.output_parsers.base import ChainableOutputParser
 from llama_index.output_parsers.guardrails import GuardrailsOutputParser
 from llama_index.output_parsers.langchain import LangchainOutputParser
 from llama_index.output_parsers.pydantic import PydanticOutputParser
 from llama_index.output_parsers.selection import SelectionOutputParser
-from llama_index.output_parsers.base import ChainableOutputParser
 
 __all__ = [
     "GuardrailsOutputParser",
     "LangchainOutputParser",
     "PydanticOutputParser",
     "SelectionOutputParser",
-    "ChainableOutputParser"
+    "ChainableOutputParser",
 ]
