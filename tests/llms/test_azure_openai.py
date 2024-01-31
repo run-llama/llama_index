19e41c8f22b734c7cd78f2468ce9ecfe4977eec0
Ancelin Serre <ancelin.serre@se.com>
2024-01-31 16:56:06 +0100

    fix: add default value for _azure_ad_token (#10245)

--- tests/llms/test_azure_openai.py
+++ tests/llms/test_azure_openai.py
@@ -22,3 +22,24 @@ def test_custom_http_client(sync_azure_openai_mock: MagicMock) -> None:
     kwargs = sync_azure_openai_mock.call_args.kwargs
     assert "http_client" in kwargs
     assert kwargs["http_client"] == custom_http_client
+
+
+@patch("llama_index.llms.azure_openai.SyncAzureOpenAI")
+def test_azure_open_ai_with_azure_ad(sync_azure_openai_mock: MagicMock) -> None:
+    """
+    Verify that a custom http_client set for AzureOpenAI.
+    Should get passed on to the implementation from OpenAI.
+    Use azure ad.
+    """
+    custom_http_client = httpx.Client()
+    mock_instance = sync_azure_openai_mock.return_value
+    # Valid mocked result required to not run into another error
+    mock_instance.chat.completions.create.return_value = mock_chat_completion_v1()
+    azure_openai = AzureOpenAI(
+        engine="foo bar", http_client=custom_http_client, use_azure_ad=True
+    )
+    azure_openai.complete("test prompt")
+    sync_azure_openai_mock.assert_called()
+    kwargs = sync_azure_openai_mock.call_args.kwargs
+    assert "http_client" in kwargs
+    assert kwargs["http_client"] == custom_http_client
