7cf131de7903d8ecfeb35fae69978695330e7b78
Andrei Fajardo <andrei@nerdai.io>
2024-02-06 00:29:07 -0500

    passing

--- llama-index-integrations/callbacks/llama-index-callbacks-aim/tests/test_aim_callback.py
+++ llama-index-integrations/callbacks/llama-index-callbacks-aim/tests/test_aim_callback.py
@@ -2,6 +2,6 @@ from llama_index.callbacks.aim.base import AimCallback
 from llama_index.core.callbacks.base_handler import BaseCallbackHandler
 
 
-def test_handler_callable():
-    handler = AimCallback()
-    assert isinstance(handler, BaseCallbackHandler)
+def test_class():
+    names_of_base_classes = [b.__name__ for b in AimCallback.__mro__]
+    assert BaseCallbackHandler.__name__ in names_of_base_classes
