7d444d1ed46840fc2d2d0c9af3b95d0a4d1b35a3
Andrei Fajardo <andrei@nerdai.io>
2024-02-06 00:37:17 -0500

    init

---
+++ llama-index-integrations/callbacks/llama-index-callbacks-argilla/llama_index/callbacks/argilla/__init__.py
@@ -0,0 +1,4 @@
+from llama_index.callbacks.argilla.base import argilla_callback_handler
+
+
+__all__ = ["argilla_callback_handler"]
--- llama-index-integrations/callbacks/llama-index-callbacks-argilla/tests/test_callbacks_argilla.py
+++
@@ -1,7 +0,0 @@
-from llama_index.callbacks.argilla.base import argilla_callback_handler
-from llama_index.core.callbacks.base_handler import BaseCallbackHandler
-
-
-def test_handler_callable():
-    handler = argilla_callback_handler()
-    assert isinstance(handler, BaseCallbackHandler)
