5f6eba3de0d91e16560a76505ac41c7079e21e8e
Andrei Fajardo <andrei@nerdai.io>
2024-02-08 02:40:12 -0500

    test_torch_imports commented out

--- llama-index-legacy/tests/finetuning/test_base.py
+++ llama-index-legacy/tests/finetuning/test_base.py
@@ -1,19 +1,15 @@
 """Test finetuning engine."""
 
-import pkgutil
 
-import pytest
-
-
-def test_torch_imports() -> None:
-    """Test that torch is an optional dependency."""
-    # importing fine-tuning modules should be ok
-    from llama_index.legacy.finetuning import OpenAIFinetuneEngine  # noqa
-
-    # if torch isn't installed, then these should fail
-    if pkgutil.find_loader("torch") is None:
-        with pytest.raises(ModuleNotFoundError):
-            pass
-    else:
-        # else, importing these should be ok
-        pass
+# def test_torch_imports() -> None:
+#     """Test that torch is an optional dependency."""
+#     # importing fine-tuning modules should be ok
+#     from llama_index.legacy.finetuning import OpenAIFinetuneEngine
+#
+#     # if torch isn't installed, then these should fail
+#     if pkgutil.find_loader("torch") is None:
+#         with pytest.raises(ModuleNotFoundError):
+#             pass
+#     else:
+#         # else, importing these should be ok
+#         pass
