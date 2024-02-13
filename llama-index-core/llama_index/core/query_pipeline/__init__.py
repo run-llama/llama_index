13fa7f43cccad04c918f108ac18155fda48c2338
Jerry Liu <jerryjliu98@gmail.com>
2024-02-12 16:37:30 -0800

    cr

--- llama-index-core/llama_index/core/query_pipeline/__init__.py
+++ llama-index-core/llama_index/core/query_pipeline/__init__.py
@@ -10,7 +10,12 @@ from llama_index.core.query_pipeline.components.function import FnComponent
 from llama_index.core.query_pipeline.components.input import InputComponent
 from llama_index.core.query_pipeline.components.router import RouterComponent
 from llama_index.core.query_pipeline.components.tool_runner import ToolRunnerComponent
-from llama_index.core.query_pipeline.query import QueryPipeline, Link, ChainableMixin, QueryComponent
+from llama_index.core.query_pipeline.query import (
+    QueryPipeline,
+    Link,
+    ChainableMixin,
+    QueryComponent,
+)
 
 __all__ = [
     "AgentFnComponent",
@@ -25,5 +30,5 @@ __all__ = [
     "QueryComponent",
     "Link",
     "ChainableMixin",
-    "QueryComponent"
+    "QueryComponent",
 ]
