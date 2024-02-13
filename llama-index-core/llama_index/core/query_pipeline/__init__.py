6e0cf1830787ff98e1daa6cabdc2715a48676730
Jerry Liu <jerryjliu98@gmail.com>
2024-02-12 16:41:44 -0800

    fix query pipeline agent (#10608)

--- llama-index-core/llama_index/core/query_pipeline/__init__.py
+++ llama-index-core/llama_index/core/query_pipeline/__init__.py
@@ -2,13 +2,20 @@
 from llama_index.core.query_pipeline.components.agent import (
     AgentFnComponent,
     AgentInputComponent,
+    CustomAgentComponent,
+    QueryComponent,
 )
 from llama_index.core.query_pipeline.components.argpacks import ArgPackComponent
 from llama_index.core.query_pipeline.components.function import FnComponent
 from llama_index.core.query_pipeline.components.input import InputComponent
 from llama_index.core.query_pipeline.components.router import RouterComponent
 from llama_index.core.query_pipeline.components.tool_runner import ToolRunnerComponent
-from llama_index.core.query_pipeline.query import QueryPipeline
+from llama_index.core.query_pipeline.query import (
+    QueryPipeline,
+    Link,
+    ChainableMixin,
+    QueryComponent,
+)
 
 __all__ = [
     "AgentFnComponent",
@@ -19,4 +26,9 @@ __all__ = [
     "RouterComponent",
     "ToolRunnerComponent",
     "QueryPipeline",
+    "CustomAgentComponent",
+    "QueryComponent",
+    "Link",
+    "ChainableMixin",
+    "QueryComponent",
 ]
