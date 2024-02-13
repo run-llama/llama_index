ab34f9abbab9f0b1ae92b686b569c1947d76f2cf
Ryan Peach <ryanpeach@me.com>
2024-02-13 14:42:05 -0500

    CodeHierarchyNodeParser

---
+++ llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/code/README.md
@@ -0,0 +1,91 @@
+# CodeHierarchyNodeParser
+
+The `CodeHierarchyNodeParser` is useful to split long code files into more reasonable chunks. What this will do is create a "Hierarchy" of sorts, where sections of the code are made more reasonable by replacing the scope body with short comments telling the LLM to search for a referenced node if it wants to read that context body. This is called skeletonization, and is toggled by setting `skeleton` to `True` which it is by default.
+
+Nodes in this hierarchy will be split based on scope, like function, class, or method scope, and will have links to their children and parents so the LLM can traverse the tree.
+
+```python
+from llama_index.node_parser.code_hierarchy import CodeHierarchyNodeParser
+from llama_index.text_splitter.code_splitter import CodeSplitter
+
+split_nodes = CodeHierarchyNodeParser(
+    language="python",
+    # You can further parameterize the CodeSplitter to split the code
+    # into "chunks" that match your context window size using
+    # chunck_lines and max_chars parameters, here we just use the defaults
+    code_splitter=CodeSplitter(language="python"),
+)
+```
+
+A full example can be found [here in combination with `CodeSplitter`](./CodeHierarchyNodeParserUsage.ipynb).
+
+# Repo Maps
+
+Generate a map of a repository's structure and contents. This is useful for the LLM to understand the structure of a codebase, and to be able to reference specific files or directories.
+
+For example:
+
+- code_hierarchy
+  - \_SignatureCaptureType
+  - \_SignatureCaptureOptions
+  - \_ScopeMethod
+  - \_CommentOptions
+  - \_ScopeItem
+  - \_ChunkNodeOutput
+  - CodeHierarchyNodeParser
+    - class_name
+    - **init**
+    - \_get_node_name
+      - recur
+    - \_get_node_signature
+      - find_start
+      - find_end
+    - \_chunk_node
+    - get_code_hierarchy_from_nodes
+      - get_subdict
+      - recur_inclusive_scope
+      - dict_to_markdown
+    - \_parse_nodes
+    - \_get_indentation
+    - \_get_comment_text
+    - \_create_comment_line
+    - \_get_replacement_text
+    - \_skeletonize
+    - \_skeletonize_list
+      - recur
+
+# Query Engine & Langchain Tool
+
+Generates a langchain tool with the following name and description:
+
+```
+name: "Code Search"
+description:
+    Search the tool by any element in this list,
+    or any uuid found in the code,
+    to get more information about that element.
+
+    {repo_map}
+```
+
+# Adding new languages
+
+To add a new language you need to edit `_DEFAULT_SIGNATURE_IDENTIFIERS` in `code_hierarchy.py`.
+
+The docstrings are infomative as how you ought to do this and its nuances, it should work for most languages.
+
+Please **test your new language** by adding a new file to `tests/file/code/` and testing all your edge cases.
+
+People often ask "how do I find the Node Types I need for a new language?" The best way is to use breakpoints.
+I have added a comment `TIP: This is a wonderful place to put a debug breakpoint` in the `code_hierarchy.py` file, put a breakpoint there, input some code in the desired language, and step through it to find the name
+of the node you want to capture.
+
+The code as it is should handle any language which:
+
+1. expects you to indent deeper scopes
+2. has a way to comment, either full line or between delimiters
+
+## Future
+
+I'm considering adding all the languages from [aider](https://github.com/paul-gauthier/aider/tree/main/aider/queries)
+by incorporating `.scm` files instead of `_SignatureCaptureType`, `_SignatureCaptureOptions`, and `_DEFAULT_SIGNATURE_IDENTIFIERS`
---
+++ llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/code/index.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+from typing import Dict, Sequence, Set, Tuple
+from llama_index import PromptTemplate
+from llama_index.langchain_helpers.agents import LlamaIndexTool
+from llama_index.query_engine import CustomQueryEngine
+from llama_index.schema import BaseNode
+import re
+from llama_hub.file.code.code_hierarchy import CodeHierarchyNodeParser
+
+
+class CodeHierarchyKeywordQueryEngine(CustomQueryEngine):
+    """A keyword table made specifically to work with the code hierarchy node parser."""
+
+    nodes: Sequence[BaseNode]
+    index: Dict[str, Tuple[int, BaseNode]] | None = None
+    tool_instructions: PromptTemplate = PromptTemplate(
+        template="""
+        Search the tool by any element in this list,
+        or any uuid found in the code,
+        to get more information about that element.
+
+        {repo_map}
+        """
+    )
+
+    def _setup_index(
+        self,
+    ) -> None:
+        """Initialize the index."""
+        self.index = {}
+        for node in self.nodes:
+            keys = self._extract_keywords_from_node(node)
+            for key in keys:
+                self.index[key] = (node.metadata["start_byte"], node.text)
+
+    def _extract_keywords_from_node(self, node: BaseNode) -> Set[str]:
+        """Determine the keywords associated with the node in the index."""
+        keywords = self._extract_uuid_from_node(node)
+        keywords |= self._extract_module_from_node(node)
+        keywords |= self._extract_name_from_node(node)
+        return {k for k in keywords}
+
+    def _extract_uuid_from_node(self, node) -> Set[str]:
+        """Extract the uuid from the node."""
+        return {node.node_id}
+
+    def _extract_module_from_node(self, node) -> Set[str]:
+        """Extract the module name from the node."""
+        keywords = set()
+        if not node.metadata["inclusive_scopes"]:
+            path = Path(node.metadata["filepath"])
+            name = path.name
+            name = re.sub(r"\..*$", "", name)
+            if name in self.index:
+                its_start_byte, _ = self.index[name]
+                if node.metadata["start_byte"] < its_start_byte:
+                    keywords.add(name)
+            else:
+                keywords.add(name)
+        return keywords
+
+    def _extract_name_from_node(self, node) -> Set[str]:
+        """Extract the name and signature from the node."""
+        keywords = set()
+        if node.metadata["inclusive_scopes"]:
+            name = node.metadata["inclusive_scopes"][-1]["name"]
+            start_byte = node.metadata["start_byte"]
+            if name in self.index:
+                its_start_byte, _ = self.index[name]
+                if start_byte < its_start_byte:
+                    keywords.add(name)
+            else:
+                keywords.add(name)
+        return keywords
+
+    def custom_query(self, query: str) -> str:
+        """Query the index. Only use exact matches."""
+        if self.index is None:
+            self._setup_index()
+        return self.index.get(str(query), (0, ""))[1]
+
+    def as_langchain_tool(
+        self,
+        include_repo_map: bool = True,
+        repo_map_depth: int = -1,
+        **tool_kwargs,
+    ) -> LlamaIndexTool:
+        """
+        Return the index as a langchain tool.
+        Set a repo map depth of -1 to include all nodes.
+        otherwise set the depth to the desired max depth.
+        """
+        repo_map = {}
+        if include_repo_map:
+            repo_map = CodeHierarchyNodeParser.get_code_hierarchy_from_nodes(
+                self.nodes, max_depth=repo_map_depth
+            )
+        return LlamaIndexTool(
+            name="Code Search",
+            description=self.tool_instructions.format(repo_map=repo_map),
+            query_engine=self,
+            **tool_kwargs,
+        )
