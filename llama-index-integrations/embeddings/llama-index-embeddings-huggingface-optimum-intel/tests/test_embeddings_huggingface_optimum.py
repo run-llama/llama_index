d8462f37b7ac859cef44dbc74b138f6a241bd6b3
Daniel Fleischer <daniel.fleischer@intel.com>
2024-02-14 11:32:43 +0200

    Test

--- llama-index-integrations/embeddings/llama-index-embeddings-huggingface-optimum-intel/tests/test_embeddings_huggingface_optimum.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-huggingface-optimum-intel/tests/test_embeddings_huggingface_optimum.py
@@ -1,7 +1,7 @@
 from llama_index.core.base.embeddings.base import BaseEmbedding
-from llama_index.embeddings.huggingface_optimum import OptimumEmbedding
+from llama_index.embeddings.huggingface_optimum_intel import IntelEmbedding
 
 
-def test_optimumembedding_class():
-    names_of_base_classes = [b.__name__ for b in OptimumEmbedding.__mro__]
+def test_optimum_intel_embedding_class():
+    names_of_base_classes = [b.__name__ for b in IntelEmbedding.__mro__]
     assert BaseEmbedding.__name__ in names_of_base_classes
