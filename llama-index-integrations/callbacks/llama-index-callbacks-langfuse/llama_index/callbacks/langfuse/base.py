82908874d9337c63d3017feaa381b1590189a88f
Hassieb Pakzad <hassieb.pakzad@gmail.com>
2024-02-26 10:39:27 +0100

    feat: passes sdk_integration to llama-index callback handler

--- llama-index-integrations/callbacks/llama-index-callbacks-langfuse/llama_index/callbacks/langfuse/base.py
+++ llama-index-integrations/callbacks/llama-index-callbacks-langfuse/llama_index/callbacks/langfuse/base.py
@@ -6,4 +6,6 @@ from langfuse.llama_index import LlamaIndexCallbackHandler
 
 
 def langfuse_callback_handler(**eval_params: Any) -> BaseCallbackHandler:
-    return LlamaIndexCallbackHandler(**eval_params)
+    return LlamaIndexCallbackHandler(
+        **eval_params, sdk_integration="llama-index_set-global-handler"
+    )
