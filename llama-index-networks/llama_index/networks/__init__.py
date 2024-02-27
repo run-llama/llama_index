2e63d5edd2f825fb3d789646fec8d9c64a1bdc35
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2024-02-27 01:55:13 -0500

    llama-index-networks package (#11413)
    * lint
    
    * pants tailor
    
    * remove unnecessary files/folders
    
    * make api_key an optional field
    
    * make secret field optional as well
    
    * top level imports
    
    * prerelease version bump
    
    * remove misleading env variables
    
    * override gitignore to add data to demo
    
    * add note in demo README that you can use  instead of
    
    * update demo contributor poetry.locks pyproject.toml
    
    * published v0.1.0; updated demo deps

---
+++ llama-index-networks/llama_index/networks/__init__.py
@@ -0,0 +1,16 @@
+from llama_index.networks.contributor import (
+    ContributorClient,
+    ContributorClientSettings,
+    ContributorService,
+    ContributorServiceSettings,
+)
+
+from llama_index.networks.query_engine import NetworkQueryEngine
+
+__all__ = [
+    "ContributorClient",
+    "ContributorClientSettings",
+    "ContributorService",
+    "ContributorServiceSettings",
+    "NetworkQueryEngine",
+]
