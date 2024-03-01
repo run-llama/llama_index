bb49de86c15a86307cff84ee2ccbe396f8ccd0e1
abhiram1809 <53875874+abhiram1809@users.noreply.github.com>
2024-03-01 20:56:00 +0530

    Revert "Basic Clean Up"
    This reverts commit cafdfb681819ce3508c91d5a50e3f18609a6722a.

--- llama-index-integrations/readers/llama-index-readers-google/llama_index/readers/google/sheets/base.py
+++ llama-index-integrations/readers/llama-index-readers-google/llama_index/readers/google/sheets/base.py
@@ -168,6 +168,8 @@ class GoogleSheetsReader(BasePydanticReader):
                 dataframes.append(df)
         return dataframes
 
+        # return pd.DataFrame(response.get("values", []))
+
     def _get_credentials(self) -> Any:
         """Get valid user credentials from storage.
 
