871bd836525d8aba67bdd7e5d9049a00d7906b82
Sasha <sashashengyt@gmail.com>
2024-03-01 17:43:47 -0800

    query understanding agent pack

--- llama-index-packs/llama-index-packs-query-understanding-agent/llama_index/packs/query_understanding_agent/step.py
+++ llama-index-packs/llama-index-packs-query-understanding-agent/llama_index/packs/query_understanding_agent/step.py
@@ -1,5 +1,7 @@
-from pydantic import PrivateAttr
+from llama_index.core.bridge.pydantic import Field, BaseModel, PrivateAttr
 from typing import Any, Dict, List, Optional
+from dataclasses import dataclass, field
+
 import uuid
 
 from llama_index.core.agent.types import (
@@ -11,6 +13,7 @@ from llama_index.core.agent.custom.simple import CustomSimpleAgentWorker
 from llama_index.core.callbacks import (
     trace_method,
 )
+from llama_index.core.chat_engine.types import AgentChatResponse
 
 
 from llama_index.core.agent import CustomSimpleAgentWorker, Task
@@ -21,24 +24,56 @@ from llama_index.core.output_parsers import PydanticOutputParser
 from llama_index.core.query_engine import RouterQueryEngine
 from llama_index.core.prompts import ChatPromptTemplate
 from llama_index.core.selectors import PydanticSingleSelector
-from pydantic import Field, BaseModel
 
 from llama_index.core.llms import ChatMessage, MessageRole
 
 DEFAULT_PROMPT_STR = """
-Given previous question/response pairs, please determine if an error has occurred in the response, and suggest \
-    a modified question that will not trigger the error.
+Given the question, the tools provided and response, please determine if the question is clear enough to provide the response (with the help of the tools).
+If the question is not clear enough, provide a clarifying_question for user to clarify. Provide a clarifying question that only need plan text answers. DO NOT ASK the user a similar question to the one the user asks you.
+If the response is sufficient, then return has_error: false, and requires_human_input: false. No need to make the response perfect, as long as it is sufficient.
 
-Examples of modified questions:
-- The question itself is modified to elicit a non-erroneous response
-- The question is augmented with context that will help the downstream system better answer the question.
-- The question is augmented with examples of negative responses, or other negative questions.
+Given the questions and tools, here are several ways to clarify questions:
+- If there are multiple tools for each timeframe, then if the timeframe is not specified in the question, we need to ask to be clear on the timeframe.
+- Be clear on which subjects the user is referring to, ask which subject the user is referring to if there are a tool for each subject
 
-An error means that either an exception has triggered, or the response is completely irrelevant to the question.
+Example 1:
+Tools:
+A useful tool for financial documents for uber in 2022
+A useful tool for financial documents for lyft in 2022
+
+Question: What is the company's financial stats in 2022?
+Response: Uber's financial stats is 20k in the year 2022
+
+Answer:
+{{"requires_human_input": true, "has_error": true, "clarifying_question": "Which company are you referring to?", "explanation": "Given the tools and the question, it is not clear which company the user is referring to"}}
+
+Example 2:
+Tools:
+A useful tool for financial documents for uber in 2022
+A useful tool for financial documents for lyft in 2022
+
+Question: What is the Uber's financial stats in 2022?
+Response: Uber's financial stats is 20k in the year 2022
+
+Answer:
+{{"requires_human_input": false, "has_error": false, "clarifying_question": "", "explanation": "It is quite clear that the user is referring to uber and the year 2022"}}
+
+Tools:
+{tools}
+
+Question: {query_str}
+Response: {response_str}
 
 Please return the evaluation of the response in the following JSON format.
-
+Answer:
 """
+
+
+@dataclass
+class AgentChatComplexResponse(AgentChatResponse):
+    """Agent chat response with metadata."""
+
+    output: Dict[str, Any] = field(default_factory=dict)
 
 
 def get_chat_prompt_template(
@@ -57,20 +92,20 @@ def get_chat_prompt_template(
 class ResponseEval(BaseModel):
     """Evaluation of whether the response has an error."""
 
-    has_error: bool = Field(..., description="Whether the response has an error")
-    requires_human_input: bool = Field(
-        ...,
-        description="Whether the response needs human input, if human input is required, it means it is not erroneous",
-    )
     clarifying_question: str = Field(
-        ..., description="The suggested new question, if human input is required"
+        ..., description="The clarifying question, if human input is required"
     )
     explanation: str = Field(
         ...,
         description=(
-            "The explanation for the error as well as for the clarifying question."
+            "The explanation for the error OR for the clarifying question."
             "Can include the direct stack trace as well."
         ),
+    )
+    has_error: bool = Field(..., description="Whether the response has an error")
+    requires_human_input: bool = Field(
+        ...,
+        description="Whether the response needs human input. If true, the clarifying question should be provided.",
     )
 
 
@@ -123,9 +158,24 @@ class QueryUnderstandingAgentWorker(CustomSimpleAgentWorker):
         """Initialize state."""
         return {"count": 0, "current_reasoning": []}
 
+    def _run_llm_program(self, prompt, state):
+        for _ in range(3):
+            try:
+                return LLMTextCompletionProgram.from_defaults(
+                    output_cls=ResponseEval,
+                    output_parser=PydanticOutputParser(output_cls=ResponseEval),
+                    prompt_template_str=prompt,
+                    verbose=True,
+                    llm=self.llm,
+                )
+            except Exception as e:
+                print(f"Attempt failed with error: {e}")
+                continue
+        raise Exception("Failed to run LLM program after 3 attempts")
+
     def _run_step(
         self, state: Dict[str, Any], task: Task, input: Optional[str] = None
-    ) -> Tuple[Dict, bool]:
+    ) -> Tuple[AgentChatComplexResponse, bool]:
         """Run step.
 
         Returns:
@@ -153,17 +203,13 @@ class QueryUnderstandingAgentWorker(CustomSimpleAgentWorker):
 
         # Then, check for errors
         # dynamically create pydantic program for structured output extraction based on template
-        chat_prompt_tmpl = get_chat_prompt_template(
-            self.prompt_str, state["current_reasoning"]
-        )
-        llm_program = LLMTextCompletionProgram.from_defaults(
-            output_parser=PydanticOutputParser(output_cls=ResponseEval),
-            prompt=chat_prompt_tmpl,
-            llm=self.llm,
-        )
+        tools = "\n".join([a.description for a in self._router_query_engine._metadatas])
+        llm_program = self._run_llm_program(self.prompt_str, state)
 
         # run program, look at the result
-        response_eval = llm_program(query_str=new_input, response_str=str(response))
+        response_eval = llm_program(
+            query_str=new_input, response_str=str(response), tools=tools
+        )
 
         if self.verbose:
             print(f"> Question: {new_input}")
@@ -173,22 +219,31 @@ class QueryUnderstandingAgentWorker(CustomSimpleAgentWorker):
         # return response
         if response_eval.requires_human_input:
             return (
-                {
-                    "response": response,
-                    "type": "requires_human_input",
-                    "clarifying_question": str(response_eval.clarifying_question),
-                    "has_error": response_eval.has_error,
-                    "explanation": response_eval.explanation,
-                },
+                AgentChatComplexResponse(
+                    response=response,
+                    output={
+                        "type": "requires_human_input",
+                        "clarifying_question": str(response_eval.clarifying_question),
+                        "has_error": response_eval.has_error,
+                        "explanation": response_eval.explanation,
+                    },
+                ),
                 True,
             )
 
-        return {"response": response}, not response_eval.has_error
+        return AgentChatComplexResponse(response=response), not response_eval.has_error
 
     @trace_method("run_step")
     def run_step(self, step: TaskStep, task: Task, **kwargs: Any) -> TaskStepOutput:
         """Run step."""
         output, is_done = self._run_step(step.step_state, task, input=step.input)
+        if output.output and output.output["type"] == "requires_human_input":
+            raise HumanInputRequiredException(
+                message=output.output["clarifying_question"],
+                task_id=task.task_id,
+                step=step,
+            )
+
         response = self._get_task_step_response(output, step, is_done)
         # sync step state with task state
         task.extra_state.update(step.step_state)
