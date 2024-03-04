677238c99d2301063e320bfba8e509d76e940f1c
thegunslingers <thegunslingers@users.noreply.github.com>
2024-03-04 17:25:57 -0500

    format and lint

--- llama-index-core/llama_index/core/readers/json.py
+++ llama-index-core/llama_index/core/readers/json.py
@@ -84,7 +84,9 @@ class JSONReader(BaseReader):
         self.ensure_ascii = ensure_ascii
         self.is_jsonl = is_jsonl
 
-    def load_data(self, input_file: str, extra_info: Optional[Dict] = None) -> List[Document]:
+    def load_data(
+        self, input_file: str, extra_info: Optional[Dict] = None
+    ) -> List[Document]:
         """Load data from the input file."""
         with open(input_file, encoding="utf-8") as f:
             load_data = []
@@ -107,7 +109,9 @@ class JSONReader(BaseReader):
                     useful_lines = [
                         line for line in lines if not re.match(r"^[{}\[\],]*$", line)
                     ]
-                    documents.append(Document(text="\n".join(useful_lines), metadata=extra_info))
+                    documents.append(
+                        Document(text="\n".join(useful_lines), metadata=extra_info)
+                    )
                 elif self.levels_back is not None:
                     # If levels_back is set, we make the embeddings contain the labels
                     # from further up the JSON tree
@@ -120,5 +124,7 @@ class JSONReader(BaseReader):
                             self.ensure_ascii,
                         )
                     ]
-                    documents.append(Document(text="\n".join(lines), metadata=extra_info))
+                    documents.append(
+                        Document(text="\n".join(lines), metadata=extra_info)
+                    )
             return documents
