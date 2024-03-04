d9651f4e859228ffb5e790c9823d8e05ca32d0dd
ravi03071991 <ravi03071991@gmail.com>
2024-03-04 22:29:51 +0530

    Add bedrock support code

--- llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/utils.py
+++ llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/utils.py
@@ -2,6 +2,9 @@ from typing import Dict, Sequence, Tuple
 
 from llama_index.core.base.llms.types import ChatMessage, MessageRole
 
+HUMAN_PREFIX = "\n\nHuman:"
+ASSISTANT_PREFIX = "\n\nAssistant:"
+
 CLAUDE_MODELS: Dict[str, int] = {
     "claude-instant-1": 100000,
     "claude-instant-1.2": 100000,
@@ -35,3 +38,34 @@ def messages_to_anthropic_messages(
             message = {"role": message.role.value, "content": message.content}
             anthropic_messages.append(message)
     return anthropic_messages, system_prompt
+
+
+# Function used in bedrock
+def _message_to_anthropic_prompt(message: ChatMessage) -> str:
+    if message.role == MessageRole.USER:
+        prompt = f"{HUMAN_PREFIX} {message.content}"
+    elif message.role == MessageRole.ASSISTANT:
+        prompt = f"{ASSISTANT_PREFIX} {message.content}"
+    elif message.role == MessageRole.SYSTEM:
+        prompt = f"{message.content}"
+    elif message.role == MessageRole.FUNCTION:
+        raise ValueError(f"Message role {MessageRole.FUNCTION} is not supported.")
+    else:
+        raise ValueError(f"Unknown message role: {message.role}")
+
+    return prompt
+
+
+def messages_to_anthropic_prompt(messages: Sequence[ChatMessage]) -> str:
+    if len(messages) == 0:
+        raise ValueError("Got empty list of messages.")
+
+    # NOTE: make sure the prompt ends with the assistant prefix
+    if messages[-1].role != MessageRole.ASSISTANT:
+        messages = [
+            *list(messages),
+            ChatMessage(role=MessageRole.ASSISTANT, content=""),
+        ]
+
+    str_list = [_message_to_anthropic_prompt(message) for message in messages]
+    return "".join(str_list)
