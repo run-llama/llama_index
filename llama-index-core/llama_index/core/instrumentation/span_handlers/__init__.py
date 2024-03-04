f5bd17ffbe5582bffe7056f3b771e719b55a72df
Andrei Fajardo <andrei@nerdai.io>
2024-03-04 11:31:36 -0500

    add SimpleSpanHandler to top-level

--- llama-index-core/llama_index/core/instrumentation/span_handlers/__init__.py
+++ llama-index-core/llama_index/core/instrumentation/span_handlers/__init__.py
@@ -1,5 +1,6 @@
 from llama_index.core.instrumentation.span_handlers.base import BaseSpanHandler
 from llama_index.core.instrumentation.span_handlers.null import NullSpanHandler
+from llama_index.core.instrumentation.span_handlers.simple import SimpleSpanHandler
 
 
-__all__ = ["BaseSpanHandler", "NullSpanHandler"]
+__all__ = ["BaseSpanHandler", "NullSpanHandler", "SimpleSpanHandler"]
