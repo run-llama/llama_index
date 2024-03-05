dbb72b99f21b885ed263551d5fff016061d03aa8
DJC <85006613+DJC-GO-SOLO@users.noreply.github.com>
2024-03-05 22:53:50 +0800

    Update base.py

--- llama-index-packs/llama-index-packs-searchain/llama_index/packs/searchain/base.py
+++ llama-index-packs/llama-index-packs-searchain/llama_index/packs/searchain/base.py
@@ -44,7 +44,7 @@ def _have_seen_or_not(model_cross_encoder,query_item,query_seen_list,query_type)
 
 
 class SearChainPack(BaseLlamaPack):
-    """Simple short form Self-RAG pack."""
+    """Simple short form SearChain pack."""
 
     def __init__(
         self,
@@ -56,7 +56,8 @@ class SearChainPack(BaseLlamaPack):
         **kwargs: Any,
     ) -> None:
         """Init params."""
-        self.crossencoder = CrossEncoder(crossencoder_name_or_path,device=device)
+        self.device = device
+        self.crossencoder = CrossEncoder(crossencoder_name_or_path,device=self.device)
         self.documents = SimpleDirectoryReader(data_path).load_data()
         self.index = VectorStoreIndex.from_documents(self.documents)
         self.query_engine = self.index.as_query_engine()
@@ -65,9 +66,10 @@ class SearChainPack(BaseLlamaPack):
         self.dprtokenizer = DPRReaderTokenizer.from_pretrained(dprtokenizer_path)
         self.dprmodel = DPRReader.from_pretrained(dprmodel_path)
         self.dprmodel.eval()
-        self.dprmodel.to(device)
+        self.dprmodel.to(self.device)
     def _get_answer(self,query,texts,title):
-        encoded_inputs = self.tokenizer(
+        print('texts:'+texts)
+        encoded_inputs = self.dprtokenizer(
             questions=[query],
             titles=[title],
             texts=[texts],
@@ -87,97 +89,89 @@ class SearChainPack(BaseLlamaPack):
         answer = self.dprtokenizer.decode(predict_answer_tokens)
         return answer,relevance_logits
 
-    def _ir(self,query):
-        continue_label = True
-        query_seen_list = []
-        # break_flag = False
-        while continue_label:
-            continue_label = False
-            print('recv query is {}'.format(query))
-            if query == 'end':
-                # break_flag = True
-                break
-            query_list = query.split('\n')
-            message = ''
-            for idx in range(len(query_list)):
-                query_item = query_list[idx]
-                if 'Query' in query_item and ']:' in query_item:
-                    temp = query_item.split(']')
-                    if len(temp) < 2:
-                        continue
-                    query_type = temp[0]
-                    query_item = temp[1]
-                    if ':' in query_item:
-                        query_item = query_item[1:]
-                    print('solving: '+query_item)
-                    if not _have_seen_or_not(self.crossencoder,query_item,query_seen_list,query_type):
-                        now_reference = {}
-                        query_seen_list.append(query_item)
-                        response = self.query_engine.query(query_item)
+    def _ir(self,query,query_seen_list):
+        flag_contibue_label = False
+        #print('recv query is {}'.format(query))
+        query_list = query.split('\n')
+        message = ''
+        for idx in range(len(query_list)):
+            query_item = query_list[idx]
+            if 'Query' in query_item and ']:' in query_item:
+                temp = query_item.split(']')
+                if len(temp) < 2:
+                    continue
+                query_type = temp[0]
+                query_item = temp[1]
+                if ':' in query_item:
+                    query_item = query_item[1:]
+                #print('solving: '+query_item)
+                if not _have_seen_or_not(self.crossencoder,query_item,query_seen_list,query_type):
+                    now_reference = {}
+                    query_seen_list.append(query_item)
+                    response = str(self.query_engine.query(query_item))
+#                   print('query_item:'+query_item)
+#                   print('top1_passage:'+response)
+#                   print(response)
 
-                        answer,relevance_score = self.get_answer(query=query_item,texts='',title=response)
-                        now_reference['query'] = query_item
-                        now_reference['answer'] = answer
-                        now_reference['reference'] = response
-                        now_reference['ref_score'] = relevance_score
-                        now_reference['idx'] = response
+                    answer,relevance_score = self._get_answer(query=query_item,texts='',title=response)
+                    now_reference['query'] = query_item
+                    now_reference['answer'] = answer
+                    now_reference['reference'] = response
+                    now_reference['ref_score'] = relevance_score
+                    now_reference['idx'] = response
 
-                        print('answer is '+answer)
-                        print('reference is'+response)
-                        print('score is {}'.format(relevance_score))
-
-                        print('query_type is '+query_type)
-                        if 'Unsolved' in query_type:
-                            message = '[Unsolved Query]:{}<SEP>[Answer]:{}<SEP>[Reference]:{}<SEP>'.format(query_item,
-                                                                                                               answer,
-                                                                                                               response)
-                            print(message)
-                            continue_label = True
-                            if relevance_score > 1.5:
-                                good_cite += 1
-                            break
-                        elif relevance_score > 1.5:
-                            good_cite += 1
-                            answer_start_idx = idx+1
-                            predict_answer = ''
-                            while answer_start_idx < len(query_list):
-                                if 'Answer' in query_list[answer_start_idx]:
-                                    predict_answer = query_list[answer_start_idx]
-                                    break
-                                answer_start_idx += 1
-                            print('predict answer is '+predict_answer)
-                            match_label = _match_or_not(prediction=predict_answer,ground_truth=answer)
-                            if match_label:
-                                continue
-                            else:
-                                message = '[Query]:{}<SEP>[Answer]:{}<SEP>[Reference]:{}<SEP>'.format(query_item,
-                                                                                            answer,
-                                                                                            response)
-                                print(message)
-                                continue_label = True
+                    # print('answer is '+answer)
+                    # print('reference is'+response)
+                    # print('score is {}'.format(relevance_score))
+                    # print('query_type is '+query_type)
+                    
+                    if 'Unsolved' in query_type:
+                        message = '[Unsolved Query]:{}<SEP>[Answer]:{}<SEP>[Reference]:{}<SEP>'.format(query_item,answer,response)
+                        #print(message)
+                        flag_contibue_label = True
+                        break
+                    elif relevance_score > 1.5:
+                        answer_start_idx = idx+1
+                        predict_answer = ''
+                        while answer_start_idx < len(query_list):
+                            if 'Answer' in query_list[answer_start_idx]:
+                                predict_answer = query_list[answer_start_idx]
                                 break
-        if continue_label:
-            return message
-        else:
-            return('end')
+                            answer_start_idx += 1
+                        #print('predict answer is '+predict_answer)
+                        match_label = _match_or_not(prediction=predict_answer,ground_truth=answer)
+                        if match_label:
+                            continue
+                        else:
+                            message = '[Query]:{}<SEP>[Answer]:{}<SEP>[Reference]:{}<SEP>'.format(query_item,answer,response)
+                            #print(message)
+                            flag_contibue_label = True
+                            break
+        return message,flag_contibue_label,query_seen_list
+
+    def _extract(self,message_keys_list):
+        text = message_keys_list
+        idx = len(text)
+        while idx > 0:
+            idx = idx - 1
+            item = text[idx]  
+            if item.role == 'assistant' and 'Final Content' in item.content:
+                list_item = item.content.split('\n')
+                for sp in list_item:
+                    if 'Final Content' in sp:
+                        return item.content
+        return 'Sorry, I still cannot solve this question!'
 
     def excute(self,data_path,start_idx):
         data = open(data_path, 'r')
         for k, example in enumerate(data):
             if k < start_idx:
                 continue
-            print(k)
+            #print(k)
             example = json.loads(example)
             q = example['question']
-            answer = example['answer']
             round_count = 0
-            message_keys_list = [ChatMessage(role="user", content="""Construct a global reasoning chain for this complex [Question] : " {} " You should generate a query to the search engine based on
-                        what you already know at each step of the reasoning chain, starting with [Query].
-                        If you know the answer for [Query], generate it starting with [Answer].
-                        You can try to generate the final answer for the [Question] by referring to the [Query]-[Answer] pairs, starting with [Final
-                        Content].
-                        If you don't know the answer, generate a query to search engine based on what you already know and do not know, starting with
-                        [Unsolved Query].
+            message_keys_list = [ChatMessage(role="user", content="""Construct a global reasoning chain for this complex [Question] : " {} " You should generate a query to the search engine based on what you already know at each step of the reasoning chain, starting with [Query]. If you know the answer for [Query], generate it starting with [Answer]. You can try to generate the final answer for the [Question] by referring to the [Query]-[Answer] pairs, starting with [Final Content]. If you don't know the answer, generate a query to search engine based on what you already know and do not know, starting with [Unsolved Query].
                         For example:
                         [Question]: "Where do greyhound buses that are in the birthplace of Spirit If...'s performer leave from? "
                         [Query 1]: Who is the performer of Spirit If... ?
@@ -195,60 +189,48 @@ class SearChainPack(BaseLlamaPack):
                         [Unsolved Query]: Where do greyhound buses in Toronto leave from?
                         If you know the answer:
                         [Answer 3]: Toronto Coach Terminal.
-                        [Final Content]: The performer of Spirit If... is Kevin Drew [1]. Kevin Drew was born in Toronto [2]. Greyhound buses in
-                        Toronto leave from Toronto
-                        Coach Terminal [3]. So the final answer is Toronto Coach Terminal.
+                        [Final Content]: The performer of Spirit If... is Kevin Drew [1]. Kevin Drew was born in Toronto [2]. Greyhound buses in Toronto leave from Toronto Coach Terminal [3]. So the final answer is Toronto Coach Terminal.
                         
                         [Question]:"Which magazine was started first Arthur’s Magazine or First for Women?"
                         [Query 1]: When was Arthur’s Magazine started?
                         [Answer 1]: 1844.
                         [Query 2]: When was First for Women started?
                         [Answer 2]: 1989
-                        [Final Content]: Arthur’s Magazine started in 1844 [1]. First for Women started in 1989 [2]. So Arthur’s Magazine was started
-                        first. So the answer is Arthur’s Magazi
-                        [Question]: {}
-                    """.format(q,q))]
+                        [Final Content]: Arthur’s Magazine started in 1844 [1]. First for Women started in 1989 [2]. So Arthur’s Magazine was started first. So the answer is Arthur’s Magazi
+                        [Question]: {}""".format(q,q))]
             feedback_answer = 'continue'
             predict_answer = ''
+            query_seen_list = []
             while round_count < 5 and not feedback_answer == 'end':
-                print('round is {}'.format(round_count))
-                try:
-                    time.sleep(0.5)
-                    rsp = self.llm.chat(message_keys_list)
-                    round_count += 1
-                    input_str = rsp.get("choices")[0]["message"]["content"]
-                    message_keys_list.append(ChatMessage(role='assistant',content=input_str))
-                    print('solving......')
-                    predict_answer += input_str
-                    feedback = self._ir(input_str)
-                    print('send message {}'.format(input_str))
-                    print('feedback is '+feedback)
-                    if feedback == 'end':
-                        break
-                    #[Query]:xxxx<SEP>[Answer]:xxxx<SEP>[Reference]:xxxx<SEP>
-                    feedback_list = feedback.split('<SEP>')
-                    if not 'Unsolved Query' in feedback:
-                        new_prompt = """
-                        According to this Reference, the answer for "{}" should be "{}",  
-                        you can change your answer based on the Reference and continue constructing the reasoning chain to give the final answer for [Question]:{}
-                        Reference: {}
-                        """.format(feedback_list[0],feedback_list[1],q,feedback_list[2])
-                    else:
-                        new_prompt = """
-                        According to this Reference, the answer for "{}" should be "{}",  
-                        you can give your answer based on the Reference and continue constructing the reasoning chain to give the final answer for [Question]：{}
-                        Reference: {}
-                        """.format(feedback_list[0],feedback_list[1],q,feedback_list[2])
-                    message_keys_list.append(ChatMessage(role='user',content=new_prompt))
-                except:
-                    print('start_idx is {}'.format(k))
-                    # sock.send('end'.encode())
-                    # sock.close()
-                    return k
-            if not feedback_answer == 'end':
-                # sock.send('end'.encode())
-                print()
-            # sock.close()
-            print(message_keys_list)
+                #print('round is {}'.format(round_count))
+                time.sleep(0.5)
+                rsp = self.llm.chat(message_keys_list)
+                round_count += 1
+                input_str = str(rsp.message.content)
+                message_keys_list.append(ChatMessage(role='assistant',content=input_str))
+                #print('solving......')
+                predict_answer += input_str
+        
+                message,flag_contibue_label,query_seen_list = self._ir(input_str,query_seen_list)
+                if flag_contibue_label:
+                    feedback = message
+                else:
+                    feedback = 'end'
+                
+                #print('send message {}'.format(input_str))
+                #print('feedback is '+feedback)
+                if feedback == 'end':
+                    break
+                #[Query]:xxxx<SEP>[Answer]:xxxx<SEP>[Reference]:xxxx<SEP>
+                feedback_list = feedback.split('<SEP>')
+                if not 'Unsolved Query' in feedback:
+                    new_prompt = \
+                    """Reference: {} According to this Reference, the answer for "{}" should be "{}", you can change your answer based on the Reference and continue constructing the reasoning chain to give the final answer for [Question]:{}""".format(feedback_list[0],feedback_list[1],q,feedback_list[2])
+                else:
+                    new_prompt = \
+                    """Reference: {} According to this Reference, the answer for "{}" should be "{}", you can give your answer based on the Reference and continue constructing the reasoning chain to give the final answer for [Question]：{} """.format(feedback_list[0],feedback_list[1],q,feedback_list[2])
+                message_keys_list.append(ChatMessage(role='user',content=new_prompt))
+            result = self._extract(message_keys_list)
+            print(result)
 
         return -1
