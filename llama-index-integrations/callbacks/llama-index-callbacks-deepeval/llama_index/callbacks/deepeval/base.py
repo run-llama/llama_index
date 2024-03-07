1ed4536d5a0e667144fe1658134098c977863ecc
Logan Markewich <logan.markewich@live.com>
2024-03-07 16:38:47 -0600

    linting

--- llama-index-integrations/callbacks/llama-index-callbacks-deepeval/llama_index/callbacks/deepeval/base.py
+++ llama-index-integrations/callbacks/llama-index-callbacks-deepeval/llama_index/callbacks/deepeval/base.py
@@ -4,5 +4,6 @@ from llama_index.core.callbacks.base_handler import BaseCallbackHandler
 
 from deepeval.integrations.llama_index.callback import LlamaIndexCallbackHandler
 
+
 def deepeval_callback_handler(**eval_params: Any) -> BaseCallbackHandler:
     return LlamaIndexCallbackHandler(**eval_params)
