67b7bcdb85a6575ff258857103ed48ad7471525a
Suman Michael <sumanmichael01@gmail.com>
2024-03-08 19:51:44 +0530

    added table comment in sql query

--- llama-index-core/llama_index/core/utilities/sql_wrapper.py
+++ llama-index-core/llama_index/core/utilities/sql_wrapper.py
@@ -150,10 +150,15 @@ class SQLDatabase:
     def get_single_table_info(self, table_name: str) -> str:
         """Get table info for a single table."""
         # same logic as table_info, but with specific table names
-        template = (
-            "Table '{table_name}' has columns: {columns}, "
-            "and foreign keys: {foreign_keys}."
-        )
+        template = "Table '{table_name}' has columns: {columns}, "
+
+        table_comment = self._inspector.get_table_comment(
+            table_name, schema=self._schema
+        )["text"]
+        if table_comment:
+            template += f"with comment: ({table_comment}) "
+
+        template += "and foreign keys: {foreign_keys}."
         columns = []
         for column in self._inspector.get_columns(table_name, schema=self._schema):
             if column.get("comment"):
