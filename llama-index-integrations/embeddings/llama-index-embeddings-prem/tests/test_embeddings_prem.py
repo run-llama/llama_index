ff9af4216f8c5ac895ca950ac708f1a24554d14d
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-08 13:10:17 +0530

    replaced llama-index mistralai test file to prem's test file

--- llama-index-integrations/embeddings/llama-index-embeddings-prem/tests/test_embeddings_prem.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-prem/tests/test_embeddings_prem.py
@@ -1,7 +1,7 @@
 from llama_index.core.base.embeddings.base import BaseEmbedding
-from llama_index.embeddings.mistralai import MistralAIEmbedding
+from llama_index.embeddings.prem import PremAIEmbeddings
 
 
 def test_embedding_class():
-    emb = MistralAIEmbedding(api_key="fake-key")
+    emb = PremAIEmbeddings(api_key="fake-key")
     assert isinstance(emb, BaseEmbedding)
