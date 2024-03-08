e9eb9243760d9e8c0ce41dc10cdc33bc99058cfe
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-08 13:09:08 +0530

    embedding function for text, query and documents

--- llama-index-integrations/embeddings/llama-index-embeddings-prem/llama_index/embeddings/prem/base.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-prem/llama_index/embeddings/prem/base.py
@@ -3,28 +3,31 @@
 from typing import Any, List, Optional
 
 from llama_index.core.base.embeddings.base import (
-    DEFAULT_EMBED_BATCH_SIZE,
     BaseEmbedding,
 )
 from llama_index.core.bridge.pydantic import PrivateAttr
 from llama_index.core.callbacks.base import CallbackManager
 from llama_index.core.base.llms.generic_utils import get_from_param_or_env
+from llama_index.core.bridge.pydantic import Field
 
 from premai import Prem
 
 
 class PremAIEmbeddings(BaseEmbedding):
-    """Class for PremAI embeddings.
+    """Class for PremAI embeddings."""
 
-    Args:
-        model_name (str): Model for embedding.
-            Defaults to "mistral-embed".
+    project_id: int = Field(
+        description=(
+            "The project ID in which the experiments or deployements are carried out. can find all your projects here: https://app.premai.io/projects/"
+        )
+    )
 
-        api_key (Optional[str]): API key to access the model. Defaults to None.
-    """
+    model_name: str = Field(
+        description=("The Embedding model to choose from"),
+    )
 
     # Instance variables initialized via Pydantic's mechanism
-    _premai_client: Any = PrivateAttr()
+    _premai_client: "Prem" = PrivateAttr()
 
     def __init__(
         self,
@@ -41,7 +44,7 @@ class PremAIEmbeddings(BaseEmbedding):
                 "You must provide an API key to use PremAI. "
                 "You can either pass it in as an argument or set it `PREMAI_API_KEY`."
             )
-        self._mistralai_client = Prem(api_key=api_key)
+        self._premlai_client = Prem(api_key=api_key)
         super().__init__(
             project_id=project_id,
             model_name=model_name,
@@ -55,26 +58,24 @@ class PremAIEmbeddings(BaseEmbedding):
 
     def _get_query_embedding(self, query: str) -> List[float]:
         """Get query embedding."""
-        return (
-            self._mistralai_client.embeddings(model=self.model_name, input=[query])
-            .data[0]
-            .embedding
+        embedding_response = self._premai_client.embeddings.create(
+            project_id=self.project_id, model=self.model_name, input=query
         )
+        return embedding_response.data[0].embedding
 
     async def _aget_query_embedding(self, query: str) -> List[float]:
         raise NotImplementedError("Async calls are not available in this version.")
 
     def _get_text_embedding(self, text: str) -> List[float]:
         """Get text embedding."""
-        return (
-            self._mistralai_client.embeddings(model=self.model_name, input=[text])
-            .data[0]
-            .embedding
+        embedding_response = self._premai_client.embeddings.create(
+            project_id=self.project_id, model=self.model_name, input=[text]
         )
+        return embedding_response.data[0].embedding
 
     def _get_text_embeddings(self, texts: List[str]) -> List[List[float]]:
         """Get text embeddings."""
-        embedding_response = self._mistralai_client.embeddings(
-            model=self.model_name, input=texts
+        embeddings = self._premai_client.embeddings.create(
+            self, model=self.model_name, project_id=self.project_id, input=texts
         ).data
-        return [embed.embedding for embed in embedding_response]
+        return [embedding.embedding for embedding in embeddings]
