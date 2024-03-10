76246538e79cd44fe7aa1b18978e76d9278c5ed3
Andrei Fajardo <andrei@nerdai.io>
2024-03-10 14:11:29 -0400

    rename pack

---
+++ llama-index-packs/llama-index-packs-diff-private-examples-gen/llama_index/packs/diff_private_simple_dataset/__init__.py
@@ -0,0 +1,4 @@
+from llama_index.packs.diff_private_simple_dataset import DiffPrivateSimpleDatasetPack
+
+
+__all__ = ["DiffPrivateSimpleDatasetPack"]
---
+++ llama-index-packs/llama-index-packs-diff-private-examples-gen/llama_index/packs/diff_private_simple_dataset/base.py
@@ -0,0 +1,9 @@
+from llama_index.core.llama_pack.base import BaseLlamaPack
+
+
+class DiffPrivateSimpleDatasetPack(BaseLlamaPack):
+    """A pack for creating differentially private simple llama-dataset."""
+
+    def run():
+        """Main run method."""
+        return
