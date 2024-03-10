fcaf202a49c1c21ea003fb453899b5bf72d26a8e
Izuki Matsuba <imatsuba@omers.com>
2024-03-10 00:16:12 -0500

    NetworkRetriever class and contributor files

---
+++ llama-index-networks/llama_index/networks/contributor/retriever/__init__.py
@@ -0,0 +1,16 @@
+from llama_index.networks.contributor.retriever.service import (
+    ContributorService,
+    ContributorServiceSettings,
+)
+from llama_index.networks.contributor.retriever.client import (
+    ContributorClient,
+    ContributorClientSettings,
+)
+
+
+__all__ = [
+    "ContributorService",
+    "ContributorServiceSettings",
+    "ContributorClient",
+    "ContributorClientSettings",
+]
--- llama-index-networks/llama_index/networks/network/retriever.py
+++ llama-index-networks/llama_index/networks/network/retriever.py
@@ -1,8 +1,15 @@
+from typing import List, Optional
+import asyncio
+
 from llama_index.core.base.base_retriever import BaseRetriever
+from llama_index.core.async_utils import run_async_tasks
+from llama_index.core.callbacks.base import CallbackManager
+from llama_index.core.schema import TextNode, NodeWithScore
+from llama_index.networks.contributor.query_engine import ContributorClient
 
 
 class NetworkRetriever(BaseRetriever):
-    """..."""
+    """The network Retriever"""
 
     def __init__(
         self,
@@ -10,7 +17,34 @@ class NetworkRetriever(BaseRetriever):
         callback_manager: Optional[CallbackManager] = None,
     ) -> None:
         self._contributors = contributors
-        self._response_synthesizer = response_synthesizer or get_response_synthesizer(
-            llm=Settings.llm, callback_manager=Settings.callback_manager
-        )
         super().__init__(callback_manager=callback_manager)
+
+    def _retrieve(self, query: str) -> List[NodeWithScore]:
+        
+        results = []
+        async_tasks = [
+            contributor.aquery(query) for contributor in self._contributors
+        ]
+        results = run_async_tasks(async_tasks)
+
+        nodes = [
+            NodeWithScore(
+                node=TextNode(text=el.response), score=el.metadata["score"]
+            )
+            for el in results
+        ]
+        return nodes
+
+    async def _aretrieve(self, query: str) -> List[NodeWithScore]:
+        async_tasks = [
+            contributor.aquery(query) for contributor in self._contributors
+        ]
+        results = await asyncio.gather(*async_tasks)
+
+        nodes = [
+            NodeWithScore(
+                node=TextNode(text=el.response), score=el.metadata["score"]
+            )
+            for el in results
+        ]
+        return nodes
