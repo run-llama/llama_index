1a507cce73466ebb0ec32c4b2df5a90c8a82214b
Andrei Fajardo <andrei@nerdai.io>
2024-03-11 13:42:51 -0400

    fix network retriever to use query bundle; passing unit test

--- llama-index-networks/llama_index/networks/network/retriever.py
+++ llama-index-networks/llama_index/networks/network/retriever.py
@@ -6,6 +6,7 @@ from llama_index.core.callbacks.base import CallbackManager
 from llama_index.core.schema import NodeWithScore
 from llama_index.core.indices.query.schema import QueryBundle
 from llama_index.networks.contributor.retriever import ContributorClient
+from functools import reduce
 
 
 class NetworkRetriever(BaseRetriever):
@@ -19,17 +20,18 @@ class NetworkRetriever(BaseRetriever):
         self._contributors = contributors
         super().__init__(callback_manager=callback_manager)
 
-    def _retrieve(self, query: str) -> List[NodeWithScore]:
-        results = []
-        query_bundle = QueryBundle(query_str=query)
+    def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
         async_tasks = [
             contributor.aretrieve(query_bundle) for contributor in self._contributors
         ]
-        return run_async_tasks(async_tasks)
+        contributor_results: List[List[NodeWithScore]] = run_async_tasks(async_tasks)
+        return reduce(lambda x, y: x + y, contributor_results)
 
-    async def _aretrieve(self, query: str) -> List[NodeWithScore]:
-        query_bundle = QueryBundle(query_str=query)
+    async def _aretrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
         async_tasks = [
             contributor.aretrieve(query_bundle) for contributor in self._contributors
         ]
-        return await asyncio.gather(*async_tasks)
+        contributor_results: List[List[NodeWithScore]] = await asyncio.gather(
+            *async_tasks
+        )
+        return reduce(lambda x, y: x + y, contributor_results)
--- llama-index-networks/tests/network/test_retriever.py
+++ llama-index-networks/tests/network/test_retriever.py
@@ -1,10 +1,10 @@
 from llama_index.networks.network.retriever import NetworkRetriever
-from llama_index.core.base.response.schema import NodeWithScore
+from llama_index.core.schema import TextNode, NodeWithScore, QueryBundle
 from unittest.mock import patch
 
 
 async def return_nodes():
-    return [NodeWithScore(node={"id_": "mock_node", "metadata": {}}, score=0.9)]
+    return [NodeWithScore(node=TextNode(text="mock_node"), score=0.9)]
 
 
 @patch("llama_index.networks.contributor.retriever.client.ContributorClient")
@@ -14,13 +14,14 @@ def test_network_retriever(mock_contributor):
     network_retriever = NetworkRetriever(contributors=[mock_contributor])
 
     # act
-    result = network_retriever.retrieve("What's the scenario?")
+    query_bundle = QueryBundle(query_str="What's the scenario?")
+    result = network_retriever.retrieve(query_bundle)
 
     # assert
     mock_contributor.aretrieve.assert_called_once()
     args, _ = mock_contributor.aretrieve.call_args
-    query_str = args[0]
-    assert query_str == "What's the scenario?"
+    query_bundle = args[0]
+    assert query_bundle.query_str == "What's the scenario?"
     assert len(result) == 1
-    assert result[0].node["id_"] == "mock_node"
+    assert result[0].node.text == "mock_node"
     assert result[0].score == 0.9
