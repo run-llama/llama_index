d98e66a969cb2223a2548b8fdac15fb9c4b2edaf
Andrei Fajardo <andrei@nerdai.io>
2024-03-12 15:07:40 -0400

    add more dynamic few shot examples

--- llama-index-packs/llama-index-packs-diff-private-examples-gen/llama_index/packs/diff_private_simple_dataset/templates.py
+++ llama-index-packs/llama-index-packs-diff-private-examples-gen/llama_index/packs/diff_private_simple_dataset/templates.py
@@ -150,6 +150,18 @@ ZERO_SHOT_COMPLETION_TEMPLATE = (
 )
 zero_shot_completion_template = PromptTemplate(ZERO_SHOT_COMPLETION_TEMPLATE)
 
+SINGLE_EXAMPLE_TEMPLATE = (
+    "{label_heading}: {example_label}\n{text_heading}: {example_text}\n\n"
+)
+single_example_template = PromptTemplate(SINGLE_EXAMPLE_TEMPLATE)
+
+
+FEW_SHOT_COMPLETION_TEMPLATE = (
+    "{instruction}\n\n"
+    "{few_shot_examples}"
+    "{label_heading}: {label}\n{text_heading}: {synthetic_text}"
+)
+few_shot_completion_template = PromptTemplate(FEW_SHOT_COMPLETION_TEMPLATE)
 
 THREE_SHOT_COMPLETION_TEMPLATE = (
     "{instruction}\n"
