0e465e5ca6bf31c90a5ec414aa6e5f1961241913
ravi03071991 <ravi03071991@gmail.com>
2024-03-14 02:41:48 +0530

    Add Anthropic haiku model

--- llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/utils.py
+++ llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/utils.py
@@ -13,6 +13,7 @@ CLAUDE_MODELS: Dict[str, int] = {
     "claude-2.1": 200000,
     "claude-3-opus-20240229": 180000,
     "claude-3-sonnet-20240229": 180000,
+    "claude-3-haiku-20240307": 180000,
 }
 
 
