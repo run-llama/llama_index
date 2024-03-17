a5e240c3ad70ccc3be7763bc385dadc55d12e46e
Andrei Fajardo <andrei@nerdai.io>
2024-03-16 21:11:08 -0400

    report the EventHandler itself

--- llama-index-packs/llama-index-packs-diff-private-simple-dataset/examples/symptom_2_disease/event_handler.py
+++ llama-index-packs/llama-index-packs-diff-private-simple-dataset/examples/symptom_2_disease/event_handler.py
@@ -2,6 +2,7 @@ from typing import Dict
 from llama_index.core.instrumentation.event_handlers import BaseEventHandler
 from llama_index.packs.diff_private_simple_dataset.events import (
     SyntheticExampleStartEvent,
+    SyntheticExampleEndEvent,
     LLMEmptyResponseEvent,
     EmptyIntersectionEvent,
 )
@@ -14,12 +15,10 @@ class TooManyProblemsEncounteredError(Exception):
 
 class DiffPrivacyEventHandler(BaseEventHandler):
     synthetic_example_starts: int = 0
-    errors: int = 0
+    synthetic_example_ends: int = 0
+    llm_empty_responses: int = 0
+    empty_intersections: int = 0
     critical_threshold: int = 250
-    error_counts: Dict = {
-        "LLMEmptyResponseEvent": 0,
-        "EmptyIntersectionEvent": 0,
-    }
 
     @classmethod
     def class_name(cls) -> str:
@@ -28,20 +27,30 @@ class DiffPrivacyEventHandler(BaseEventHandler):
 
     def handle(self, event) -> None:
         """Logic for handling event."""
+
         if isinstance(event, SyntheticExampleStartEvent):
             self.synthetic_example_starts += 1
-        elif isinstance(event, (LLMEmptyResponseEvent, EmptyIntersectionEvent)):
-            self.errors += 1
-            self.error_counts[event.class_name()] = (
-                self.error_counts[event.class_name()] + 1
-            )
-            error_report = {
-                "synthetic_example_starts": self.synthetic_example_starts,
-                "error_counts": self.error_counts,
-            }
+        elif isinstance(event, LLMEmptyResponseEvent):
+            self.llm_empty_responses += 1
+
             with open("error_report.json", "w") as f:
-                json.dump(error_report, f)
-            if self.errors > self.critical_threshold:
+                json.dump(self.dict(), f)
+
+            if (
+                self.llm_empty_responses + self.empty_intersections
+            ) > self.critical_threshold:
+                raise TooManyProblemsEncounteredError(
+                    "There were too many errors encountered."
+                )
+        elif isinstance(event, EmptyIntersectionEvent):
+            self.empty_intersections += 1
+
+            with open("error_report.json", "w") as f:
+                json.dump(self.dict(), f)
+
+            if (
+                self.llm_empty_responses + self.empty_intersections
+            ) > self.critical_threshold:
                 raise TooManyProblemsEncounteredError(
                     "There were too many errors encountered."
                 )
