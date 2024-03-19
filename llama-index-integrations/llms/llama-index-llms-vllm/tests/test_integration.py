d5404bb2214e7f653936c8129e185703b7344a16
Mikhail Khludnev <mkhl@apache.org>
2024-03-19 23:35:30 +0300

    ignore suite

--- llama-index-integrations/llms/llama-index-llms-vllm/tests/test_integration.py
+++ llama-index-integrations/llms/llama-index-llms-vllm/tests/test_integration.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 
+import pytest
 from llama_index.core.base.llms.types import MessageRole, ChatMessage, ChatResponse
 
 from llama_index.core.base.llms.types import CompletionResponse
@@ -22,104 +23,105 @@ def local_vllm():
     )
 
 
-# replace to local_vllm(), it requires vllm installed and fail ..stream.. tests due to Not Implemented
-vllm = remote_vllm()
+@pytest.mark.skip(reason="requires remotely running `vllm.entrypoints.api_server`")
+class TestVllmIntegration():
+    # replace to local_vllm(), it requires vllm installed and fail ..stream.. tests due to Not Implemented
+    vllm = remote_vllm()
+
+    def test_completion(self):
+        completion = self.vllm.complete("When AI hype is over?")
+        assert (
+            isinstance(completion, CompletionResponse)
+            and isinstance(completion.text, str)
+            and len(completion.text) > 0
+        )
 
 
-def test_completion():
-    completion = vllm.complete("When AI hype is over?")
-    assert (
-        isinstance(completion, CompletionResponse)
-        and isinstance(completion.text, str)
-        and len(completion.text) > 0
-    )
+    def test_acompletion(self):
+        completion = asyncio.run(self.vllm.acomplete("When AI hype is over?"))
+        assert (
+            isinstance(completion, CompletionResponse)
+            and isinstance(completion.text, str)
+            and len(completion.text) > 0
+        )
 
 
-def test_acompletion():
-    completion = asyncio.run(vllm.acomplete("When AI hype is over?"))
-    assert (
-        isinstance(completion, CompletionResponse)
-        and isinstance(completion.text, str)
-        and len(completion.text) > 0
-    )
+    def test_chat(self):
+        from llama_index.core.base.llms.types import ChatMessage
 
-
-def test_chat():
-    from llama_index.core.base.llms.types import ChatMessage
-
-    chat = vllm.chat(
-        [ChatMessage(content="When AI hype is over?", role=MessageRole.USER)]
-    )
-    assert (
-        isinstance(chat.message, ChatMessage)
-        and chat.message.role == MessageRole.ASSISTANT
-        and isinstance(chat.message.content, str)
-        and len(chat.message.content) > 0
-    )
-
-
-def test_achat():
-    from llama_index.core.base.llms.types import ChatMessage
-
-    chat = asyncio.run(
-        vllm.achat(
+        chat = self.vllm.chat(
             [ChatMessage(content="When AI hype is over?", role=MessageRole.USER)]
         )
-    )
-    assert (
-        isinstance(chat.message, ChatMessage)
-        and chat.message.role == MessageRole.ASSISTANT
-        and isinstance(chat.message.content, str)
-        and len(chat.message.content) > 0
-    )
+        assert (
+            isinstance(chat.message, ChatMessage)
+            and chat.message.role == MessageRole.ASSISTANT
+            and isinstance(chat.message.content, str)
+            and len(chat.message.content) > 0
+        )
 
 
-def test_stream_completion():
-    prompt = "When AI hype is over?"
-    completion = list(vllm.stream_complete(prompt))[-1]
-    assert isinstance(completion, CompletionResponse)
-    assert completion.text.count(prompt) == 1
-    print(completion)
+    def test_achat(self):
+        from llama_index.core.base.llms.types import ChatMessage
 
-
-def test_astream_completion():
-    prompt = "When AI hype is over?"
-
-    async def concat2():
-        return [c async for c in await vllm.astream_complete(prompt)]
-
-    completion = asyncio.run(concat2())[-1]
-    assert isinstance(completion, CompletionResponse)
-    assert completion.text.count(prompt) == 1
-    print(completion)
-
-
-def test_stream_chat():
-    prompt = "When AI hype is over?"
-    chat = list(vllm.stream_chat([ChatMessage(content=prompt, role=MessageRole.USER)]))[
-        -1
-    ]
-    assert isinstance(chat, ChatResponse)
-    assert isinstance(chat.message, ChatMessage)
-    assert chat.message.role == MessageRole.ASSISTANT
-    assert chat.message.content.count(prompt) == 1
-    print(chat)
-
-
-def test_astream_chat():
-    prompt = "When AI hype is over?"
-
-    async def concat2():
-        return [
-            c
-            async for c in await vllm.astream_chat(
-                [ChatMessage(content=prompt, role=MessageRole.USER)]
+        chat = asyncio.run(
+            self.vllm.achat(
+                [ChatMessage(content="When AI hype is over?", role=MessageRole.USER)]
             )
-        ]
+        )
+        assert (
+            isinstance(chat.message, ChatMessage)
+            and chat.message.role == MessageRole.ASSISTANT
+            and isinstance(chat.message.content, str)
+            and len(chat.message.content) > 0
+        )
 
-    chat = asyncio.run(concat2())[-1]
-    assert isinstance(chat, ChatResponse)
-    assert isinstance(chat.message, ChatMessage)
-    assert chat.message.role == MessageRole.ASSISTANT
-    assert chat.message.content.count(prompt) == 1
-    print(chat)
+
+    def test_stream_completion(self):
+        prompt = "When AI hype is over?"
+        completion = list(self.vllm.stream_complete(prompt))[-1]
+        assert isinstance(completion, CompletionResponse)
+        assert completion.text.count(prompt) == 1
+        print(completion)
+
+
+    def test_astream_completion(self):
+        prompt = "When AI hype is over?"
+
+        async def concat():
+            return [c async for c in await self.vllm.astream_complete(prompt)]
+
+        completion = asyncio.run(concat())[-1]
+        assert isinstance(completion, CompletionResponse)
+        assert completion.text.count(prompt) == 1
+        print(completion)
+
+
+    def test_stream_chat(self):
+        prompt = "When AI hype is over?"
+        chat = list(self.vllm.stream_chat([ChatMessage(content=prompt, role=MessageRole.USER)]))[
+            -1
+        ]
+        assert isinstance(chat, ChatResponse)
+        assert isinstance(chat.message, ChatMessage)
+        assert chat.message.role == MessageRole.ASSISTANT
+        assert chat.message.content.count(prompt) == 1
+        print(chat)
+
+
+    def test_astream_chat(self):
+        prompt = "When AI hype is over?"
+
+        async def concat():
+            return [
+                c
+                async for c in await self.vllm.astream_chat(
+                    [ChatMessage(content=prompt, role=MessageRole.USER)]
+                )
+            ]
+
+        chat = asyncio.run(concat())[-1]
+        assert isinstance(chat, ChatResponse)
+        assert isinstance(chat.message, ChatMessage)
+        assert chat.message.role == MessageRole.ASSISTANT
+        assert chat.message.content.count(prompt) == 1
+        print(chat)
