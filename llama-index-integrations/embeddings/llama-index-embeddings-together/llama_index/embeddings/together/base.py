20312ec44677e740f532535dd63855212dbe591a
Vaishakh Raveendran <107925721+vaishakhRaveendran@users.noreply.github.com>
2024-03-21 01:13:42 +0530

    Formatted base.py

--- llama-index-integrations/embeddings/llama-index-embeddings-together/llama_index/embeddings/together/base.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-together/llama_index/embeddings/together/base.py
@@ -7,6 +7,7 @@ import requests
 from llama_index.core.base.embeddings.base import BaseEmbedding, Embedding
 from llama_index.core.bridge.pydantic import Field
 
+
 class TogetherEmbedding(BaseEmbedding):
     api_base: str = Field(
         default="https://api.together.xyz/v1",
@@ -31,7 +32,7 @@ class TogetherEmbedding(BaseEmbedding):
             api_base=api_base,
             **kwargs,
         )
-            
+
     def _generate_embedding(self, text: str, model_api_string: str) -> Embedding:
         """Generate embeddings from Together API.
         Args:
@@ -40,7 +41,7 @@ class TogetherEmbedding(BaseEmbedding):
         Returns:
             embeddings: a list of float numbers. Embeddings correspond to your given text.
         """
-        
+
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
@@ -56,23 +57,22 @@ class TogetherEmbedding(BaseEmbedding):
             )
             if response.status_code != 200:
                 if response.status_code == 429:
-                    """ Rate limit exceeded, wait for reset """
+                    """Rate limit exceeded, wait for reset"""
                     reset_time = int(response.headers.get("X-RateLimit-Reset", 0))
                     if reset_time > 0:
                         time.sleep(reset_time)
                         continue
                     else:
-                        """ Rate limit reset time has passed, retry immediately """
+                        """Rate limit reset time has passed, retry immediately"""
                         continue
-                        
+
                 """ Handle other non-200 status codes """
                 raise ValueError(
                     f"Request failed with status code {response.status_code}: {response.text}"
                 )
 
             return response.json()["data"][0]["embedding"]
-        
-    
+
     async def _agenerate_embedding(self, text: str, model_api_string: str) -> Embedding:
         """Async generate embeddings from Together API.
         Args:
@@ -96,22 +96,21 @@ class TogetherEmbedding(BaseEmbedding):
                 )
                 if response.status_code != 200:
                     if response.status_code == 429:
-                        """Rate limit exceeded, wait for reset """
+                        """Rate limit exceeded, wait for reset"""
                         reset_time = int(response.headers.get("X-RateLimit-Reset", 0))
-                        if reset_time > 0: 
+                        if reset_time > 0:
                             await asyncio.sleep(reset_time)
                             continue
                         else:
-                            """ Rate limit reset time has passed, retry immediately """
+                            """Rate limit reset time has passed, retry immediately"""
                             continue
-                            
+
                     """ Handle other non-200 status codes"""
                     raise ValueError(
                         f"Request failed with status code {response.status_code}: {response.text}"
                     )
 
                 return response.json()["data"][0]["embedding"]
-    
 
     def _get_text_embedding(self, text: str) -> Embedding:
         """Get text embedding."""
