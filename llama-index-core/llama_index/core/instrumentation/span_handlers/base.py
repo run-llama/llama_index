fb163f53dd8e1c252be753ab9caa90c70539338b
Roger Yang <roger.yang@arize.com>
2024-03-28 08:57:54 -0700

    wip

--- llama-index-core/llama_index/core/instrumentation/span_handlers/base.py
+++ llama-index-core/llama_index/core/instrumentation/span_handlers/base.py
@@ -1,3 +1,4 @@
+import inspect
 from abc import abstractmethod
 from typing import Any, Dict, Generic, Optional, TypeVar
 
@@ -22,52 +23,100 @@ class BaseSpanHandler(BaseModel, Generic[T]):
         """Class name."""
         return "BaseSpanHandler"
 
-    def span_enter(self, *args, id: str, **kwargs) -> None:
+    def span_enter(
+        self,
+        *args: Any,
+        id_: str,
+        bound_args: inspect.BoundArguments,
+        instance: Optional[Any] = None,
+        **kwargs: Any,
+    ) -> None:
         """Logic for entering a span."""
-        if id in self.open_spans:
+        if id_ in self.open_spans:
             pass  # should probably raise an error here
         else:
             # TODO: thread safe?
             span = self.new_span(
-                *args, id=id, parent_span_id=self.current_span_id, **kwargs
+                id_=id_,
+                bound_args=bound_args,
+                instance=instance,
+                parent_span_id=self.current_span_id,
             )
             if span:
-                self.open_spans[id] = span
-                self.current_span_id = id
+                self.open_spans[id_] = span
+                self.current_span_id = id_
 
-    def span_exit(self, *args, id: str, result: Optional[Any] = None, **kwargs) -> None:
+    def span_exit(
+        self,
+        *args: Any,
+        id_: str,
+        bound_args: inspect.BoundArguments,
+        instance: Optional[Any] = None,
+        result: Optional[Any] = None,
+        **kwargs: Any,
+    ) -> None:
         """Logic for exiting a span."""
-        span = self.prepare_to_exit_span(*args, id=id, result=result, **kwargs)
+        span = self.prepare_to_exit_span(
+            id_=id_, bound_args=bound_args, instance=instance, result=result
+        )
         if span:
-            if self.current_span_id == id:
-                self.current_span_id = self.open_spans[id].parent_id
-            del self.open_spans[id]
+            if self.current_span_id == id_:
+                self.current_span_id = self.open_spans[id_].parent_id
+            del self.open_spans[id_]
 
-    def span_drop(self, *args, id: str, err: Optional[Exception], **kwargs) -> None:
+    def span_drop(
+        self,
+        *args: Any,
+        id_: str,
+        bound_args: inspect.BoundArguments,
+        instance: Optional[Any] = None,
+        err: Optional[BaseException] = None,
+        **kwargs: Any,
+    ) -> None:
         """Logic for dropping a span i.e. early exit."""
-        span = self.prepare_to_drop_span(*args, id=id, err=err, **kwargs)
+        span = self.prepare_to_drop_span(
+            id_=id_, bound_args=bound_args, instance=instance, err=err
+        )
         if span:
-            if self.current_span_id == id:
-                self.current_span_id = self.open_spans[id].parent_id
-            del self.open_spans[id]
+            if self.current_span_id == id_:
+                self.current_span_id = self.open_spans[id_].parent_id
+            del self.open_spans[id_]
 
     @abstractmethod
     def new_span(
-        self, *args, id: str, parent_span_id: Optional[str], **kwargs
+        self,
+        *args: Any,
+        id_: str,
+        bound_args: inspect.BoundArguments,
+        instance: Optional[Any] = None,
+        parent_span_id: Optional[str] = None,
+        **kwargs: Any,
     ) -> Optional[T]:
         """Create a span."""
         ...
 
     @abstractmethod
     def prepare_to_exit_span(
-        self, *args, id: str, result: Optional[Any] = None, **kwargs
+        self,
+        *args: Any,
+        id_: str,
+        bound_args: inspect.BoundArguments,
+        instance: Optional[Any] = None,
+        result: Optional[Any] = None,
+        **kwargs: Any,
     ) -> Optional[T]:
         """Logic for preparing to exit a span."""
         ...
 
     @abstractmethod
     def prepare_to_drop_span(
-        self, *args, id: str, err: Optional[Exception], **kwargs
+        self,
+        *args: Any,
+        id_: str,
+        bound_args: inspect.BoundArguments,
+        instance: Optional[Any] = None,
+        err: Optional[BaseException] = None,
+        **kwargs: Any,
     ) -> Optional[T]:
         """Logic for preparing to drop a span."""
         ...
