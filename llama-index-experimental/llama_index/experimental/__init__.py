e0a7e4e0b0368497c04fa5c1a26ad4fc81b946b7
Logan Markewich <logan.markewich@live.com>
2024-03-29 15:15:11 -0600

    move pandas to experimental

---
+++ llama-index-experimental/llama_index/experimental/__init__.py
@@ -0,0 +1,6 @@
+from llama_index.experimental.query_engine.pandas.pandas_query_engine import (
+    PandasQueryEngine,
+)
+from llama_index.experimental.param_tuner.base import ParamTuner
+
+__all__ = ["PandasQueryEngine", "ParamTuner"]
