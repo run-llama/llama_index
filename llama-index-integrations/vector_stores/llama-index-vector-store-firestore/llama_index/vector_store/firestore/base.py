7e38a2a27655266a28bcdfaa21cbb8632010ca8a
Mais <mais.alheraki@gmail.com>
2024-04-04 04:59:49 +0300

    feat: support filters in query

--- llama-index-integrations/vector_stores/llama-index-vector-store-firestore/llama_index/vector_store/firestore/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-store-firestore/llama_index/vector_store/firestore/base.py
@@ -5,18 +5,26 @@ An index that is built on top of an existing vector store.
 """
 
 import logging
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Union
 
 import more_itertools
 from google.cloud.firestore import Client
 from google.cloud.firestore_v1.base_vector_query import DistanceMeasure
 from google.cloud.firestore_v1.document import DocumentSnapshot
 from google.cloud.firestore_v1.vector import Vector
+from google.cloud.firestore_v1 import FieldFilter, Or, And
+from google.cloud.firestore_v1.base_query import StructuredQuery
+
 from llama_index.core.schema import BaseNode
 from llama_index.core.vector_stores.types import (
     BasePydanticVectorStore,
     VectorStoreQuery,
     VectorStoreQueryResult,
+    MetadataFilters,
+    MetadataFilter,
+    ExactMatchFilter,
+    FilterCondition,
+    FilterOperator,
 )
 from llama_index.core.vector_stores.utils import (
     metadata_dict_to_node,
@@ -29,12 +37,56 @@ DEFAULT_TOP_K = 10
 _logger = logging.getLogger(__name__)
 
 
-# def _to_firestore_filter(standard_filters: MetadataFilters) -> Any:
-#     """Convert from standard dataclass to filter dict."""
-#     filters = {}
-#     for f in standard_filters.filters():
+def _to_firestore_operator(
+    operator: FilterOperator,
+) -> StructuredQuery.FieldFilter.Operator:
+    """Convert from standard operator to Firestore operator."""
+    if operator == FilterOperator.EQ:
+        return "=="
+    if operator == FilterOperator.NE:
+        return "!="
+    if operator == FilterOperator.GT:
+        return ">"
+    if operator == FilterOperator.GTE:
+        return ">="
+    if operator == FilterOperator.LT:
+        return "<"
+    if operator == FilterOperator.LTE:
+        return "<="
+    if operator == FilterOperator.IN:
+        return "in"
+    if operator == FilterOperator.NIN:
+        return "not-in"
+    if operator == FilterOperator.CONTAINS:
+        return "array_contains"
 
-#     return filters
+    raise ValueError(f"Operator {operator} not supported in Firestore.")
+
+
+def _to_firestore_filter(
+    standard_filters: MetadataFilters,
+) -> Union[FieldFilter, Or, And, List[FieldFilter]]:
+    """Convert from standard dataclass to filter dict."""
+    firestore_filters = []
+
+    # Convert standard filters to Firestore filters
+    # Firestore filters are FieldFilter or And/Or filters
+    for f in standard_filters.filters:
+        if isinstance(f, MetadataFilter):
+            firestore_filters.append(
+                FieldFilter(f.key, _to_firestore_operator(f.operator), f.value)
+            )
+        elif isinstance(f, ExactMatchFilter):
+            firestore_filters.append(FieldFilter(f.key, "==", f.value))
+
+    if len(firestore_filters) == 1:
+        return firestore_filters[0]
+    if standard_filters.condition == FilterCondition.AND:
+        return And(filters=firestore_filters)
+    if standard_filters.condition == FilterCondition.OR:
+        return Or(filters=firestore_filters)
+
+    return firestore_filters
 
 
 class FirestoreVectorStore(BasePydanticVectorStore):
@@ -147,9 +199,14 @@ class FirestoreVectorStore(BasePydanticVectorStore):
     ) -> List[DocumentSnapshot]:
         _filters = kwargs.get("filters")
         _logger.debug("Querying Firestore with filters: %s", _filters)
-        _wfilters = None
 
-        results = self._client.collection(self.collection_name).find_nearest(
+        wfilters = None
+        collection = self._client.collection(self.collection_name)
+
+        if _filters is not None:
+            wfilters = collection.where(filter=_filters)
+
+        results = (wfilters or collection).find_nearest(
             vector_field=self.embedding_key,
             query_vector=Vector(query),
             distance_measure=self.distance_strategy,
