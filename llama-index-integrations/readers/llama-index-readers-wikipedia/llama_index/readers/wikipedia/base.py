3c1d582e0ba490fc7666e5d7a0c09a1524cbed77
Anirban Basu <anirbanbasu@users.noreply.github.com>
2024-04-05 17:01:07 +0900

    feat: Added the ability to connect to Wikipedia in a language other than English.
    Wikipedia is available in multiple languages. Some articles are in specific languages only. Valid language codes are: https://en.wikipedia.org/wiki/List_of_Wikipedias.

--- llama-index-integrations/readers/llama-index-readers-wikipedia/llama_index/readers/wikipedia/base.py
+++ llama-index-integrations/readers/llama-index-readers-wikipedia/llama_index/readers/wikipedia/base.py
@@ -27,14 +27,19 @@ class WikipediaReader(BasePydanticReader):
     def class_name(cls) -> str:
         return "WikipediaReader"
 
-    def load_data(self, pages: List[str], **load_kwargs: Any) -> List[Document]:
+    def load_data(self, pages: List[str], lang_code: str = 'en', **load_kwargs: Any) -> List[Document]:
         """Load data from the input directory.
 
         Args:
             pages (List[str]): List of pages to read.
-
+            lang_code (str): Language code for Wikipedia. Defaults to English. Valid Wikipedia language codes
+            can be found at https://en.wikipedia.org/wiki/List_of_Wikipedias.
         """
         import wikipedia
+
+        if lang_code.lower() != 'en':
+            # Sets, without checking the validity of, the language code for Wikipedia.
+            wikipedia.set_lang(lang_code)
 
         results = []
         for page in pages:
