368011606823b4f92e7422af14714fe00515679c
Mingyang Li <tslmy@outlook.com>
2024-04-09 19:37:35 -0700

    Avoid "Reached max iterations" errors by telling Agent how many steps are allowed

--- llama-index-core/llama_index/core/agent/react/step.py
+++ llama-index-core/llama_index/core/agent/react/step.py
@@ -456,6 +456,10 @@ class ReActAgentWorker(BaseAgentWorker):
         input_chat = self._react_chat_formatter.format(
             tools,
             chat_history=task.memory.get() + task.extra_state["new_memory"].get_all(),
+            # Let LLM know that it can only take at most this many steps to complete the task.
+            # - `//2`: An observation step also takes as an iteration.
+            # - `-1`: The final output reasoning step needs to take a spot.
+            allowance=(self._max_iterations - 1) // 2,
             current_reasoning=task.extra_state["current_reasoning"],
         )
 
@@ -495,6 +499,10 @@ class ReActAgentWorker(BaseAgentWorker):
         input_chat = self._react_chat_formatter.format(
             tools,
             chat_history=task.memory.get() + task.extra_state["new_memory"].get_all(),
+            # Let LLM know that it can only take at most this many steps to complete the task.
+            # - `//2`: An observation step also takes as an iteration.
+            # - `-1`: The final output reasoning step needs to take a spot.
+            allowance=(self._max_iterations - 1) // 2,
             current_reasoning=task.extra_state["current_reasoning"],
         )
         # send prompt
@@ -533,6 +541,10 @@ class ReActAgentWorker(BaseAgentWorker):
         input_chat = self._react_chat_formatter.format(
             tools,
             chat_history=task.memory.get() + task.extra_state["new_memory"].get_all(),
+            # Let LLM know that it can only take at most this many steps to complete the task.
+            # - `//2`: An observation step also takes as an iteration.
+            # - `-1`: The final output reasoning step needs to take a spot.
+            allowance=(self._max_iterations - 1) // 2,
             current_reasoning=task.extra_state["current_reasoning"],
         )
 
@@ -597,6 +609,10 @@ class ReActAgentWorker(BaseAgentWorker):
         input_chat = self._react_chat_formatter.format(
             tools,
             chat_history=task.memory.get() + task.extra_state["new_memory"].get_all(),
+            # Let LLM know that it can only take at most this many steps to complete the task.
+            # - `//2`: An observation step also takes as an iteration.
+            # - `-1`: The final output reasoning step needs to take a spot.
+            allowance=(self._max_iterations - 1) // 2,
             current_reasoning=task.extra_state["current_reasoning"],
         )
 
