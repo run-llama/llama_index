71eb6e96f99a5b4ec84f8895c05c36bf2fc417d6
Logan Markewich <logan.markewich@live.com>
2024-04-09 22:01:35 -0600

    fix transformations bug

--- llama-index-core/llama_index/core/indices/labelled_property_graph/transformations.py
+++ llama-index-core/llama_index/core/indices/labelled_property_graph/transformations.py
@@ -65,7 +65,9 @@ class ExtractTripletsFromText(TransformComponent):
         except ValueError:
             triplets = []
 
-        node.metadata["triplets"] = triplets
+        existing_triplets = node.metadata.get("triplets", [])
+        existing_triplets.extend(triplets)
+        node.metadata["triplets"] = existing_triplets
 
         return node
 
@@ -84,32 +86,36 @@ class ExtractTripletsFromNodeRelations(TransformComponent):
     def __call__(self, nodes: List[BaseNode], **kwargs: Any) -> List[BaseNode]:
         """Extract triplets from node relationships."""
         for node in nodes:
-            node.metadata["triplets"] = []
+            triplets = []
 
             if node.source_node:
-                node.metadata["triplets"].append(
-                    (node.id_, NodeRelationship.SOURCE.value, node.source_node.node_id)
+                triplets.append(
+                    (node.id_, str(NodeRelationship.SOURCE), node.source_node.node_id)
                 )
 
             if node.parent_node:
-                node.metadata["triplets"].append(
-                    (node.id_, NodeRelationship.PARENT.value, node.parent_node.node_id)
+                triplets.append(
+                    (node.id_, str(NodeRelationship.PARENT), node.parent_node.node_id)
                 )
 
             if node.prev_node:
-                node.metadata["triplets"].append(
-                    (node.id_, NodeRelationship.PREVIOUS.value, node.prev_node.node_id)
+                triplets.append(
+                    (node.id_, str(NodeRelationship.PREVIOUS), node.prev_node.node_id)
                 )
 
             if node.next_node:
-                node.metadata["triplets"].append(
-                    (node.id_, NodeRelationship.NEXT.value, node.next_node.node_id)
+                triplets.append(
+                    (node.id_, str(NodeRelationship.NEXT), node.next_node.node_id)
                 )
 
             if node.child_nodes:
                 for child_node in node.child_nodes:
-                    node.metadata["triplets"].append(
-                        (node.id_, NodeRelationship.CHILD.value, child_node.node_id)
+                    triplets.append(
+                        (node.id_, str(NodeRelationship.CHILD), child_node.node_id)
                     )
+
+            existing_triplets = node.metadata.get("triplets", [])
+            existing_triplets.extend(triplets)
+            node.metadata["triplets"] = existing_triplets
 
         return nodes
