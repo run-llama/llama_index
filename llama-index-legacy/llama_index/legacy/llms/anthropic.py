7a8a1a57db2f56dd35c27112de6c161072ea50af
Ashish Bhatia <ashishb@ashishb.net>
2024-04-14 03:11:11 -0700

    Update anthropic.py

--- llama-index-legacy/llama_index/legacy/llms/anthropic.py
+++ llama-index-legacy/llama_index/legacy/llms/anthropic.py
@@ -95,11 +95,17 @@ class Anthropic(LLM):
         callback_manager = callback_manager or CallbackManager([])
 
         self._client = anthropic.Anthropic(
-            api_key=api_key, base_url=base_url, timeout=timeout, max_retries=max_retries,
+            api_key=api_key, 
+            base_url=base_url, 
+            timeout=timeout, 
+            max_retries=max_retries,
             default_headers=default_headers,
         )
         self._aclient = anthropic.AsyncAnthropic(
-            api_key=api_key, base_url=base_url, timeout=timeout, max_retries=max_retries,
+            api_key=api_key,
+            base_url=base_url,
+            timeout=timeout,
+            max_retries=max_retries,
             default_headers=default_headers,
         )
 
