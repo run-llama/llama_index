a0d42c8eb1ac76f0587ad45e4c25e362dfc45ec5
Emanuel Ferreira <contatoferreirads@gmail.com>
2024-04-15 11:02:09 -0300

    feat: add retry on pypdf

--- llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/docs/base.py
+++ llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/docs/base.py
@@ -11,6 +11,8 @@ import zlib
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
 from fsspec import AbstractFileSystem
 from llama_index.core.readers.base import BaseReader
 from llama_index.core.readers.file.base import get_default_fs, is_default_fs
@@ -18,6 +20,9 @@ from llama_index.core.schema import Document
 
 logger = logging.getLogger(__name__)
 
+WAIT_MIN_SECONDS = 1
+WAIT_MAX_SECONDS = 10
+RETRY_TIMES = 3
 
 class PDFReader(BaseReader):
     """PDF parser."""
@@ -28,6 +33,10 @@ class PDFReader(BaseReader):
         """
         self.return_full_document = return_full_document
 
+    @retry(
+        wait=wait_random_exponential(min=WAIT_MIN_SECONDS, max=WAIT_MAX_SECONDS),
+        stop=stop_after_attempt(RETRY_TIMES),
+    )
     def load_data(
         self,
         file: Path,
