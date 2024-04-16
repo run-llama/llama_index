758bc48bf520f3f4ca6d44196ecce5d2b8d15145
Mais <mais.alheraki@gmail.com>
2024-04-16 09:20:53 +0300

    test: mock client for utils

--- llama-index-integrations/vector_stores/llama-index-vector-store-firestore/tests/test_utils.py
+++ llama-index-integrations/vector_stores/llama-index-vector-store-firestore/tests/test_utils.py
@@ -1,9 +1,11 @@
+from typing import Any
 from unittest import TestCase
 from unittest.mock import patch
+from google.cloud.firestore import Client
 
 import pytest
 
-from llama_index.vector_store.firestore.utils import client_with_user_agent
+from llama_index.vector_store.firestore.utils import USER_AGENT
 
 
 @pytest.fixture(autouse=True, name="test_case")
@@ -12,11 +14,21 @@ def init_test_case() -> TestCase:
     return TestCase()
 
 
-def test_user_agent_with_correct_version(test_case: TestCase) -> None:
+@pytest.fixture(autouse=True, name="mock_client")
+def mock_firestore_client() -> Any:
+    """Returns a mock Firestore client."""
+    with patch("google.cloud.firestore.Client") as mock_client_cls:
+        with patch("importlib.metadata.version", return_value="0.1.0", autospec=True):
+            mock_client = mock_client_cls.return_value
+            mock_client._client_info.user_agent = USER_AGENT
+            yield mock_client
+
+
+def test_user_agent_with_correct_version(
+    test_case: TestCase, mock_client: Client
+) -> None:
     """Test that the user agent includes the correct version."""
-    with patch("importlib.metadata.version", return_value="0.1.0"):
-        client = client_with_user_agent()
-        test_case.assertEqual(
-            client._client_info.user_agent,
-            "llama-index-vector-store-firestore-python:vectorstore0.1.0",
-        )
+    test_case.assertEqual(
+        mock_client._client_info.user_agent,
+        "llama-index-vector-store-firestore-python:vectorstore0.1.0",
+    )
