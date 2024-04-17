830e6462c704255cb7669eb988017aacbbe49525
Zenodia Charpy <zcharpy@nvidia.com>
2024-04-17 12:22:37 +0200

    merged LLOC-29 and LLOc-77, able to switch mode between nim and catalog
    Signed-off-by: Zenodia Charpy <zcharpy@nvidia.com>

--- llama-index-integrations/postprocessor/llama-index-postprocessor-nvidia-rerank/llama_index/postprocessor/nvidia_rerank/base.py
+++ llama-index-integrations/postprocessor/llama-index-postprocessor-nvidia-rerank/llama_index/postprocessor/nvidia_rerank/base.py
@@ -8,43 +8,28 @@ from llama_index.core.schema import NodeWithScore, QueryBundle
 import requests
 from openai import OpenAI as SyncOpenAI
 from openai import AsyncOpenAI
+from enum import Enum
+
+
+class ModelType(Enum):
+    NVIDIAAPICatalog = "catalog"
+    NIM = "nim"
 
 
 
-DEFAULT_PLAYGROUND_MODEL = "nv-rerank-qa-mistral-4b:1"
+DEFAULT_PLAYGROUND_MODEL = "nv-rerank-qa-mistral-4b:1" 
 BASE_PLAYGROUND_URL = "https://ai.api.nvidia.com/v1/retrieval/nvidia/reranking"
+
 DEFAULT_TOP_N = 2
-
-
-def get_from_param_or_env(
-    key: str,
-    param: Optional[str] = None,
-    env_key: Optional[str] = None,
-    default: Optional[str] = None,
-) -> str:
-    """Get a value from a param or an environment variable."""
-    if param is not None:
-        return param
-    elif env_key and env_key in os.environ and os.environ[env_key]:
-        return os.environ[env_key]
-    elif default is not None:
-        return default
-    else:
-        raise ValueError(
-            f"Did not find {key}, please add an environment variable"
-            f" `{env_key}` which contains it, or pass"
-            f"  `{key}` as a named parameter."
-        )    
-
 
 
 class NVIDIARerank(BaseNodePostprocessor):
     """NVIDIA's API Catalog Reranker Connector"""
-    model: str = Field(
+    model: Optional[str] = Field(
                     default=DEFAULT_PLAYGROUND_MODEL,
                     description="The NVIDIA API Catalog reranker to use.",
                 )
-    top_n: int = Field(
+    top_n: Optional[int] = Field(
                     default=2,
                     description="The default value for top_n is 2",
                 )
@@ -52,28 +37,53 @@ class NVIDIARerank(BaseNodePostprocessor):
                     default=BASE_PLAYGROUND_URL,
                     description="The default API Catalog reranker's url",
                 ),
+    type_mode : Optional[str] = Field(
+                    default="catalog" ,
+                    description="Default to NVIDIA API Catalog reranker to use, support mode switching",
+                )
     _api_key: Any = PrivateAttr()
+
     def __init__(
         self,
+        type_mode : str = "catalog" ,
         model: str = DEFAULT_PLAYGROUND_MODEL,
         top_n: int = DEFAULT_TOP_N,
         timeout: float = 120,
         max_retries: int = 5,
         api_key: Optional[str] = None,
         url : str = BASE_PLAYGROUND_URL,
-    ) -> None:      
-
-        self._api_key = get_from_param_or_env(
-            "api_key", api_key, "NVIDIA_API_KEY", ""
-        )
-
-        if not self._api_key:
-            raise ValueError(
-                "The NVIDIA API key must be provided as an environment variable or as a parameter."
-            )
-               
-        super().__init__(top_n=top_n, model=model, url=url)
+    ) -> None:
         
+        super().__init__(top_n=top_n, model=model, url=url)
+        self.model = model 
+        self.top_n = top_n
+        self.type_mode = type_mode
+        self.url = url
+        if type_mode=='nim':
+            self.mode(type_mode=type_mode)
+        
+
+    def mode(self, type_mode=type_mode, base_url = url):
+        
+        if type_mode == "nim" :             
+            self.model = DEFAULT_PLAYGROUND_MODEL
+            self.url = base_url + '/ranking'
+            self.type_mode = 'nim'
+            self.top_n = DEFAULT_TOP_N
+        elif type_mode == "catalog" :
+            try:
+                api_key = os.environ["NVIDIA_API_KEY"]
+                self._api_key = api_key
+                self.top_n = DEFAULT_TOP_N
+                self.url = BASE_PLAYGROUND_URL
+                self.model = DEFAULT_PLAYGROUND_MODEL
+                self.type_mode="catalog"                
+            except:
+                raise ValueError(
+                "Did not find NVIDIA_API_KEY, please add an environment variable"   ) 
+        else:
+            print("currently support only 'nim' backend or NVIDIA API Catalog's 'catalog' as mode !")
+
 
 
     @classmethod
@@ -92,11 +102,6 @@ class NVIDIARerank(BaseNodePostprocessor):
         if len(nodes) == 0:
             return []
         
-        headers = {
-            "Authorization": f"Bearer {self._api_key}",
-            "Accept": "application/json",
-        }
-
         session = requests.Session()
 
         with self.callback_manager.event(
@@ -115,8 +120,18 @@ class NVIDIARerank(BaseNodePostprocessor):
                 "query": {"text": query_bundle.query_str},
                 "passages":texts,
             }
-            
-            response = session.post(self.url, headers=headers, json=payloads)
+            if self.type_mode =='nim':
+                response=requests.post(self.url,json=payloads)
+            elif self.type_mode =='catalog':
+                headers = {
+                "Authorization": f"Bearer {self._api_key}",
+                "Accept": "application/json",
+            }
+                response = session.post(self.url, headers=headers, json=payloads)
+            else:
+                raise ValueError(
+                f"Currently there are only 2 backends supported, default backend is catalog , your current backend is {self.type_mode}"                )    
+
 
             try :
                 assert response.status_code == 200                
