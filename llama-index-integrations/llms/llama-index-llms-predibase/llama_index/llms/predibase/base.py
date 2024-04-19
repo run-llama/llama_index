58dbcd6ae3f39d3bd0f995b98f08a66527e5b4cd
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2024-04-19 16:03:29 -0700

    Add implementation of Predibase with SDK-v2 (in a backward-compatible way).

--- llama-index-integrations/llms/llama-index-llms-predibase/llama_index/llms/predibase/base.py
+++ llama-index-integrations/llms/llama-index-llms-predibase/llama_index/llms/predibase/base.py
@@ -33,7 +33,7 @@ class PredibaseLLM(CustomLLM):
     of a fine-tuned LLM adapter, whose base model is the `model` parameter; the
     fine-tuned adapter must be compatible with its base model; otherwise, an
     error is raised.  If the fine-tuned adapter is hosted at Predibase,
-    `adapter_version` can be specified (omitting it gives the latest version).
+    `adapter_version` must be specified.
 
     Examples:
         `pip install llama-index-llms-predibase`
@@ -125,26 +125,31 @@ class PredibaseLLM(CustomLLM):
             output_parser=output_parser,
         )
 
-        self._client = self.initialize_client(predibase_api_key)
+        self._client: Union["PredibaseClient", "Predibase"] = self.initialize_client(
+            predibase_api_key
+        )
 
     @staticmethod
-    def initialize_client(predibase_api_key: str) -> Any:
+    def initialize_client(
+        predibase_api_key: str,
+    ) -> Union["PredibaseClient", "Predibase"]:
         try:
-            from predibase import PredibaseClient
-            from predibase.pql import get_session
-            from predibase.pql.api import Session
+            if PredibaseLLM._is_deprecated_sdk_version():
+                from predibase import PredibaseClient
+                from predibase.pql import get_session
+                from predibase.pql.api import Session
 
-            session: Session = get_session(
-                token=predibase_api_key,
-                gateway="https://api.app.predibase.com/v1",
-                serving_endpoint="serving.app.predibase.com",
-            )
-            return PredibaseClient(session=session)
-        except ImportError as e:
-            raise ImportError(
-                "Could not import Predibase Python package. "
-                "Please install it with `pip install predibase`."
-            ) from e
+                session: Session = get_session(
+                    token=predibase_api_key,
+                    gateway="https://api.app.predibase.com/v1",
+                    serving_endpoint="serving.app.predibase.com",
+                )
+                return PredibaseClient(session=session)
+
+            from predibase import Predibase
+
+            os.environ["PREDIBASE_GATEWAY"] = "https://api.app.predibase.com"
+            return Predibase(api_token=predibase_api_key)
         except ValueError as e:
             raise ValueError("Your API key is not correct. Please try again") from e
 
@@ -165,18 +170,6 @@ class PredibaseLLM(CustomLLM):
     def complete(
         self, prompt: str, formatted: bool = False, **kwargs: Any
     ) -> "CompletionResponse":
-        from predibase.pql.api import ServerResponseError
-        from predibase.resource.llm.interface import (
-            HuggingFaceLLM,
-            LLMDeployment,
-        )
-        from predibase.resource.llm.response import GeneratedResponse
-        from predibase.resource.model import Model
-
-        base_llm_deployment: LLMDeployment = self._client.LLM(
-            uri=f"pb://deployments/{self.model_name}"
-        )
-
         options: Dict[str, Union[str, float]] = copy.deepcopy(kwargs)
         options.update(
             {
@@ -185,36 +178,131 @@ class PredibaseLLM(CustomLLM):
             }
         )
 
-        result: GeneratedResponse
-        if self.adapter_id:
-            """
-            Attempt to retrieve the fine-tuned adapter from a Predibase repository.
-            If absent, then load the fine-tuned adapter from a HuggingFace repository.
-            """
-            adapter_model: Union[Model, HuggingFaceLLM]
-            try:
-                adapter_model = self._client.get_model(
-                    name=self.adapter_id,
-                    version=self.adapter_version,
-                    model_id=None,
-                )
-            except ServerResponseError:
-                # Predibase does not recognize the adapter ID (query HuggingFace).
-                adapter_model = self._client.LLM(uri=f"hf://{self.adapter_id}")
-            result = base_llm_deployment.with_adapter(model=adapter_model).generate(
-                prompt=prompt,
-                options=options,
+        response_text: str
+
+        if PredibaseLLM._is_deprecated_sdk_version():
+            from predibase.pql.api import ServerResponseError
+            from predibase.resource.llm.interface import (
+                HuggingFaceLLM,
+                LLMDeployment,
             )
-        else:
-            result = base_llm_deployment.generate(
-                prompt=prompt,
-                options=options,
+            from predibase.resource.llm.response import GeneratedResponse
+            from predibase.resource.model import Model
+
+            base_llm_deployment: LLMDeployment = self._client.LLM(
+                uri=f"pb://deployments/{self.model_name}"
             )
 
-        return CompletionResponse(text=result.response)
+            result: GeneratedResponse
+            if self.adapter_id:
+                """
+                Attempt to retrieve the fine-tuned adapter from a Predibase repository.
+                If absent, then load the fine-tuned adapter from a HuggingFace repository.
+                """
+                adapter_model: Union[Model, HuggingFaceLLM]
+                try:
+                    adapter_model = self._client.get_model(
+                        name=self.adapter_id,
+                        version=self.adapter_version,
+                        model_id=None,
+                    )
+                except ServerResponseError:
+                    # Predibase does not recognize the adapter ID (query HuggingFace).
+                    adapter_model = self._client.LLM(uri=f"hf://{self.adapter_id}")
+                result = base_llm_deployment.with_adapter(model=adapter_model).generate(
+                    prompt=prompt,
+                    options=options,
+                )
+            else:
+                result = base_llm_deployment.generate(
+                    prompt=prompt,
+                    options=options,
+                )
+            response_text = result.response
+        else:
+            import requests
+            from lorax.client import Client as LoraxClient
+            from lorax.errors import GenerationError
+            from lorax.types import Response
+
+            lorax_client: LoraxClient = self._client.deployments.client(
+                deployment_ref=self.model_name
+            )
+
+            response: Response
+            if self.adapter_id:
+                """
+                Attempt to retrieve the fine-tuned adapter from a Predibase repository.
+                If absent, then load the fine-tuned adapter from a HuggingFace repository.
+                """
+                if self.adapter_version:
+                    # Since the adapter version is provided, query the Predibase repository.
+                    pb_adapter_id: str = f"{self.adapter_id}/{self.adapter_version}"
+                    try:
+                        response = lorax_client.generate(
+                            prompt=prompt,
+                            adapter_id=pb_adapter_id,
+                            **options,
+                        )
+                    except GenerationError as ge:
+                        raise ValueError(
+                            f'An adapter with the ID "{pb_adapter_id}" cannot be found in the Predibase repository of fine-tuned adapters.'
+                        ) from ge
+                else:
+                    # The adapter version is omitted, hence look for the adapter ID in the HuggingFace repository.
+                    try:
+                        response = lorax_client.generate(
+                            prompt=prompt,
+                            adapter_id=self.adapter_id,
+                            adapter_source="hub",
+                            **options,
+                        )
+                    except GenerationError as ge:
+                        raise ValueError(
+                            f'An adapter with the ID "{self.adapter_id}" cannot be found in a HuggingFace repository.'
+                        ) from ge
+            else:
+                try:
+                    response = lorax_client.generate(
+                        prompt=prompt,
+                        **options,
+                    )
+                except requests.JSONDecodeError as jde:
+                    raise ValueError(
+                        f"""An LLM with the deployment ID "{self.model_name}" cannot be found at Predibase \
+(please refer to "https://docs.predibase.com/user-guide/inference/models" for the list of supported models).
+"""
+                    ) from jde
+            response_text = response.generated_text
+
+        return CompletionResponse(text=response_text)
 
     @llm_completion_callback()
     def stream_complete(
         self, prompt: str, formatted: bool = False, **kwargs: Any
     ) -> "CompletionResponseGen":
         raise NotImplementedError
+
+    @staticmethod
+    def _is_deprecated_sdk_version() -> bool:
+        try:
+            import semantic_version
+            from semantic_version.base import Version
+            from predibase.version import __version__ as current_version
+
+            sdk_semver_deprecated: Version = semantic_version.Version(
+                version_string="2024.4.6"
+            )
+            sdk_semver_current: Version = semantic_version.Version(
+                version_string=current_version
+            )
+
+            return not (
+                (sdk_semver_current > sdk_semver_deprecated)
+                or ("+dev" in current_version)
+            )
+        except ImportError as e:
+            raise ImportError(
+                "Could not import Predibase Python package. "
+                "Please install it with `pip install semantic_version predibase`."
+            ) from e
