d718034f4dbab7edc6b81313fa19ca9615d336a8
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2024-04-19 11:13:06 +0000

    updated browserbase integration

--- llama-index-integrations/readers/llama-index-readers-web/llama_index/readers/web/browserbase_web/base.py
+++ llama-index-integrations/readers/llama-index-readers-web/llama_index/readers/web/browserbase_web/base.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from typing import Iterator, List
+from typing import Optional, Iterator, Sequence
 from llama_index.core.readers.base import BaseReader
 from llama_index.core.schema import Document
 
@@ -9,11 +9,14 @@ logger = logging.getLogger(__name__)
 
 
 class BrowserbaseWebReader(BaseReader):
-    """Browserbase Web Reader"""
+    """Load pre-rendered web pages using a headless browser hosted on Browserbase.
+     Depends on `browserbase` package.
+     Get your API key from https://browserbase.com
+     """
 
     def __init__(
         self,
-        api_key: str = os.environ["BROWSERBASE_KEY"],
+        api_key: Optional[str] = None,
     ) -> None:
         try:
             from browserbase import Browserbase
@@ -22,10 +25,11 @@ class BrowserbaseWebReader(BaseReader):
                 "`browserbase` package not found, please run `pip install browserbase`"
             )
 
+        api_key = api_key or os.environ["BROWSERBASE_API_KEY"]
         self.browserbase = Browserbase(api_key=api_key)
 
-    def lazy_load_data(self, urls: List[str], text_content: bool = False) -> Iterator[Document]:
-        """Load pages using Browserbase Web Reader"""
+    def lazy_load_data(self, urls: Sequence[str], text_content: bool = False) -> Iterator[Document]:
+        """Load pages from URLs"""
         pages = self.browserbase.load_urls(urls, text_content)
 
         for i, page in enumerate(pages):
@@ -39,4 +43,4 @@ class BrowserbaseWebReader(BaseReader):
 
 if __name__ == "__main__":
     reader = BrowserbaseWebReader()
-    logger.info(reader.load_data(urls=["https://example.com"]))
+    logger.warn(reader.load_data(urls=["https://example.com"]))
