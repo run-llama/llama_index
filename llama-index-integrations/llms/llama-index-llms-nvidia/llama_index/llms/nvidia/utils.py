540e9bde8112e79e9cbbf6555163aa6607cb8e48
chris-alexiuk-nvidia <calexiuk@nvidia.com>
2024-04-19 14:03:14 -0400

    Added new model references

--- llama-index-integrations/llms/llama-index-llms-nvidia/llama_index/llms/nvidia/utils.py
+++ llama-index-integrations/llms/llama-index-llms-nvidia/llama_index/llms/nvidia/utils.py
@@ -1,12 +1,15 @@
 from typing import Dict
 
 API_CATALOG_MODELS: Dict[str, int] = {
-    "mistralai/mistral-7b-instruct-v0.2" : 16384,
-    "mistralai/mixtral-8x7b-instruct-v0.1" : 16384,
-    "google/gemma-7b" : 4096,
+    "mistralai/mistral-7b-instruct-v0.2": 16384,
+    "mistralai/mixtral-8x7b-instruct-v0.1": 16384,
+    "google/gemma-7b": 4096,
+    "google/gemma-2b": 4096,
+    "google/codegemma-7b": 4096,
     "meta/codellama-70b": 1024,
     "meta/llama2-70b": 1024,
 }
+
 
 def playground_modelname_to_contextsize(modelname: str) -> int:
     if modelname not in API_CATALOG_MODELS:
