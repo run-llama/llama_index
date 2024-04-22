3995e0fd868e49fe83444b8c3de066d3093984d0
Zenodia Charpy <zcharpy@nvidia.com>
2024-04-22 14:48:11 +0200

    WIP resolving Matthew' opened review threads
    Signed-off-by: Zenodia Charpy <zcharpy@nvidia.com>

--- llama-index-integrations/postprocessor/llama-index-postprocessor-nvidia-rerank/llama_index/postprocessor/nvidia_rerank/base.py
+++ llama-index-integrations/postprocessor/llama-index-postprocessor-nvidia-rerank/llama_index/postprocessor/nvidia_rerank/base.py
@@ -8,18 +8,38 @@ from llama_index.core.schema import NodeWithScore, QueryBundle
 import requests
 
 
-DEFAULT_PLAYGROUND_MODEL = "nv-rerank-qa-mistral-4b:1" 
-BASE_PLAYGROUND_URL = "https://ai.api.nvidia.com/v1/retrieval/nvidia/reranking"
+DEFAULT_MODEL = "nv-rerank-qa-mistral-4b:1" 
+BASE_URL = "https://ai.api.nvidia.com/v1/retrieval/nvidia/reranking"
 
 DEFAULT_TOP_N = 2
  
-model_lookup = {"catalog":[DEFAULT_PLAYGROUND_MODEL], "nim":[DEFAULT_PLAYGROUND_MODEL]}
+model_lookup = {"nvidia":[DEFAULT_MODEL], "nim":[DEFAULT_MODEL]}
 
+
+def get_from_param_or_env(
+    key: str,
+    param: Optional[str] = None,
+    env_key: Optional[str] = None,
+    default: Optional[str] = None,
+) -> str:
+    """Get a value from a param or an environment variable."""
+    if param is not None:
+        return param
+    elif env_key and env_key in os.environ and os.environ[env_key]:
+        return os.environ[env_key]
+    elif default is not None:
+        return default
+    else:
+        raise ValueError(
+            f"Did not find {key}, please add an environment variable"
+            f" `{env_key}` which contains it, or pass"
+            f"  `{key}` as a named parameter."
+        )
 
 class NVIDIARerank(BaseNodePostprocessor):
     """NVIDIA's API Catalog Reranker Connector"""
     model: Optional[str] = Field(
-                    default=DEFAULT_PLAYGROUND_MODEL,
+                    default=DEFAULT_MODEL,
                     description="The NVIDIA API Catalog reranker to use.",
                 )
     top_n: Optional[int] = Field(
@@ -27,77 +47,75 @@ class NVIDIARerank(BaseNodePostprocessor):
                     description="The default value for top_n is 2",
                 )
     url: Optional[str] =Field(
-                    default=BASE_PLAYGROUND_URL,
+                    default=BASE_URL,
                     description="The default API Catalog reranker's url",
                 ),
-    type_mode : Optional[str] = Field(
-                    default="catalog" ,
+    _mode : Optional[str] = Field(
+                    default="nvidia" ,
                     description="Default to NVIDIA API Catalog reranker to use, support mode switching",
                 )
     _api_key: Any = PrivateAttr()
+    _mode : Any = PrivateAttr()
 
     def __init__(
         self,
-        type_mode : str = "catalog" ,
-        model: str = DEFAULT_PLAYGROUND_MODEL,
-        top_n: int = DEFAULT_TOP_N,
-        timeout: float = 120,
-        max_retries: int = 5,
-        api_key: Optional[str] = None,
-        url : str = BASE_PLAYGROUND_URL,
-    ) :
+        _mode : Optional[str]=None ,
+        model: str = DEFAULT_MODEL,
+        top_n: int = DEFAULT_TOP_N,      
+        url : str = BASE_URL,
+        _api_key: Optional[str] = None,
+    ) : 
         
         super().__init__(top_n=top_n, model=model, url=url)
         self.model = model 
         self.top_n = top_n
-        self.type_mode = type_mode
+        self._api_key = None
         self.url = url
-        if type_mode=='nim':
-            self.mode(type_mode=type_mode)
+        self._mode = None
+        
+        
         
     def get_available_models():
         return model_lookup.items()
 
-    def mode(self, type_mode=type_mode, base_url = url , model=model , top_n = top_n):
+    def mode(self, mode :str = None, base_url :str = url , model :str =model , top_n : int = top_n , api_key :str = None):
         if isinstance(self, str):
             raise ValueError("Please construct the model before calling mode()")
         out = self
-        if type_mode in ["catalog"]:
+        if mode in ["nvidia"]:
             key_var = "NVIDIA_API_KEY"
-            api_key = os.getenv(key_var)
+            my_key = get_from_param_or_env("api_key", api_key, "NVIDIA_API_KEY", "")
+
+            #api_key = os.getenv(key_var)
             
             if not api_key.startswith("nvapi-"):
                 raise ValueError(f"No {key_var} in env/fed as api_key. (nvapi-...)")
-        if type_mode in ["openai"]:
-            key_var = "OPENAI_API_KEY"
-            if not api_key or not api_key.startswith("sk-"):
-                api_key = os.getenv(key_var) 
-            if not api_key.startswith("sk-"):
-                raise ValueError(f"No {key_var} in env/fed as api_key. (sk-...)")
-
-        out.type_mode = type_mode
+        
+        out._mode = mode
         
 
-        if type_mode == "catalog":
+        if mode == "nvidia":
             ## NVIDIA API Catalog Integration: OpenAPI-spec gateway over NVCF endpoints
             out.top_n = top_n
             out.url = base_url 
             ## API Catalog is early, so no models list yet. Undercut to nvcf for now.
-            out.model = model_lookup[type_mode][0]
-            out._api_key = api_key
+            out.model = model_lookup[mode][0]
+            out._api_key = my_key
             
 
-        elif type_mode == "nim":
+        elif mode == "nim":
             ## OpenAPI-style specs to connect to NeMo Inference Microservices etc.
             ## Most generic option, requires specifying base_url            
             out.top_n = top_n
+            if base_url.endswith('/ranking'):
+                raise ValueError(f"Incorrect url format {base_url}, you do not need to extend '/ranking' at the end, as an example, here is a valid url format :http://.../v1/")
             out.url = base_url +'/ranking'
             ## API Catalog is early, so no models list yet. Undercut to nvcf for now.
-            out.model = model_lookup[type_mode][0]
+            out.model = model_lookup[mode][0]
 
         else:
-            options = ["catalog", "nim"]
-            raise ValueError(f"Unknown mode: `{type_mode}`. Expected one of {options}.")       
+            options = ["nvidia", "nim"]
+            raise ValueError(f"Unknown mode: `{_mode}`. Expected one of {options}.")       
 
         return out
     
@@ -110,9 +128,8 @@ class NVIDIARerank(BaseNodePostprocessor):
         nodes: List[NodeWithScore],
         query_bundle: Optional[QueryBundle] = None,
     ) -> List[NodeWithScore]:
-        try:
-            assert query_bundle is not None
-        except :            
+        
+        if query_bundle is None:
             raise ValueError("Missing query bundle in extra info. Please do not give empty query!")
         if len(nodes) == 0:
             return []
@@ -130,26 +147,23 @@ class NVIDIARerank(BaseNodePostprocessor):
                 EventPayload.TOP_K: self.top_n,
             },
         ) as event:
-            texts = [{"text": node.node.get_content()} for node in nodes]
             
             payloads = {
                 "model": model,
                 "query": {"text": query_bundle.query_str},
-                "passages":texts,
+                "passages":[{"text": node.node.get_content()} for node in nodes],
             }
-            if self.type_mode =='nim':
+            if self._mode =='nim':
                 current_url=self.url
                 response=requests.post(current_url,json=payloads)
-            elif self.type_mode =='catalog':
+            elif self._mode =='nvidia':
                 current_url = self.url[0].default
                 headers = {
                 "Authorization": f"Bearer {self._api_key}",
                 "Accept": "application/json",
             }
                 response = session.post(current_url, headers=headers, json=payloads)
-            else:
-                raise ValueError(
-                f"Currently there are only 2 backends supported, default backend is catalog , your current backend is {self.type_mode}"                )    
+                response.raise_for_status()         
 
             
             try :
