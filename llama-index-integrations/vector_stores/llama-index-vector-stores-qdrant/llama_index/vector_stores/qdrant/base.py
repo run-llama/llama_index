e85e5fd724e14e64edd69db1aa55bddd5e7a70c3
Javier Torres <javierandrestorresreyes@gmail.com>
2024-04-25 14:49:07 -0500

    Add nested filters support for Qdrant and Pinecone (#13113)
    nested filters in qdrant and pinecone

--- llama-index-integrations/vector_stores/llama-index-vector-stores-qdrant/llama_index/vector_stores/qdrant/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-qdrant/llama_index/vector_stores/qdrant/base.py
@@ -18,6 +18,9 @@ from llama_index.core.vector_stores.types import (
     VectorStoreQuery,
     VectorStoreQueryMode,
     VectorStoreQueryResult,
+    MetadataFilters,
+    FilterOperator,
+    FilterCondition,
 )
 from llama_index.core.vector_stores.utils import (
     legacy_metadata_dict_to_node,
@@ -768,6 +771,94 @@ class QdrantVectorStore(BasePydanticVectorStore):
 
         return VectorStoreQueryResult(nodes=nodes, similarities=similarities, ids=ids)
 
+    def _build_subfilter(self, filters: MetadataFilters) -> Filter:
+        conditions = []
+        for subfilter in filters.filters:
+            # only for exact match
+            if isinstance(subfilter, MetadataFilters) and len(subfilter.filters) > 0:
+                conditions.append(self._build_subfilter(subfilter))
+            elif not subfilter.operator or subfilter.operator == FilterOperator.EQ:
+                if isinstance(subfilter.value, float):
+                    conditions.append(
+                        FieldCondition(
+                            key=subfilter.key,
+                            range=Range(
+                                gte=subfilter.value,
+                                lte=subfilter.value,
+                            ),
+                        )
+                    )
+                else:
+                    conditions.append(
+                        FieldCondition(
+                            key=subfilter.key,
+                            match=MatchValue(value=subfilter.value),
+                        )
+                    )
+            elif subfilter.operator == FilterOperator.LT:
+                conditions.append(
+                    FieldCondition(
+                        key=subfilter.key,
+                        range=Range(lt=subfilter.value),
+                    )
+                )
+            elif subfilter.operator == FilterOperator.GT:
+                conditions.append(
+                    FieldCondition(
+                        key=subfilter.key,
+                        range=Range(gt=subfilter.value),
+                    )
+                )
+            elif subfilter.operator == FilterOperator.GTE:
+                conditions.append(
+                    FieldCondition(
+                        key=subfilter.key,
+                        range=Range(gte=subfilter.value),
+                    )
+                )
+            elif subfilter.operator == FilterOperator.LTE:
+                conditions.append(
+                    FieldCondition(
+                        key=subfilter.key,
+                        range=Range(lte=subfilter.value),
+                    )
+                )
+            elif subfilter.operator == FilterOperator.TEXT_MATCH:
+                conditions.append(
+                    FieldCondition(
+                        key=subfilter.key,
+                        match=MatchText(text=subfilter.value),
+                    )
+                )
+            elif subfilter.operator == FilterOperator.NE:
+                conditions.append(
+                    FieldCondition(
+                        key=subfilter.key,
+                        match=MatchExcept(**{"except": [subfilter.value]}),
+                    )
+                )
+            elif subfilter.operator == FilterOperator.IN:
+                # match any of the values
+                # https://qdrant.tech/documentation/concepts/filtering/#match-any
+                if isinstance(subfilter.value, List):
+                    values = [str(val) for val in subfilter.value]
+                else:
+                    values = str(subfilter.value).split(",")
+
+                conditions.append(
+                    FieldCondition(
+                        key=subfilter.key,
+                        match=MatchAny(any=values),
+                    )
+                )
+
+        filter = Filter()
+        if filters.condition == FilterCondition.AND:
+            filter.must = conditions
+        elif filters.condition == FilterCondition.OR:
+            filter.should = conditions
+        return filter
+
     def _build_query_filter(self, query: VectorStoreQuery) -> Optional[Any]:
         if not query.doc_ids and not query.query_str:
             return None
@@ -793,80 +884,8 @@ class QdrantVectorStore(BasePydanticVectorStore):
         # filtering cannot handle longer queries and can effectively filter our all the
         # nodes. See: https://github.com/jerryjliu/llama_index/pull/1181
 
-        if query.filters is None:
-            return Filter(must=must_conditions)
-
-        for subfilter in query.filters.filters:
-            # only for exact match
-            if not subfilter.operator or subfilter.operator == "==":
-                if isinstance(subfilter.value, float):
-                    must_conditions.append(
-                        FieldCondition(
-                            key=subfilter.key,
-                            range=Range(
-                                gte=subfilter.value,
-                                lte=subfilter.value,
-                            ),
-                        )
-                    )
-                else:
-                    must_conditions.append(
-                        FieldCondition(
-                            key=subfilter.key,
-                            match=MatchValue(value=subfilter.value),
-                        )
-                    )
-            elif subfilter.operator == "<":
-                must_conditions.append(
-                    FieldCondition(
-                        key=subfilter.key,
-                        range=Range(lt=subfilter.value),
-                    )
-                )
-            elif subfilter.operator == ">":
-                must_conditions.append(
-                    FieldCondition(
-                        key=subfilter.key,
-                        range=Range(gt=subfilter.value),
-                    )
-                )
-            elif subfilter.operator == ">=":
-                must_conditions.append(
-                    FieldCondition(
-                        key=subfilter.key,
-                        range=Range(gte=subfilter.value),
-                    )
-                )
-            elif subfilter.operator == "<=":
-                must_conditions.append(
-                    FieldCondition(
-                        key=subfilter.key,
-                        range=Range(lte=subfilter.value),
-                    )
-                )
-            elif subfilter.operator == "text_match":
-                must_conditions.append(
-                    FieldCondition(
-                        key=subfilter.key,
-                        match=MatchText(text=subfilter.value),
-                    )
-                )
-            elif subfilter.operator == "!=":
-                must_conditions.append(
-                    FieldCondition(
-                        key=subfilter.key,
-                        match=MatchExcept(**{"except": [subfilter.value]}),
-                    )
-                )
-            elif subfilter.operator == "in":
-                # match any of the values
-                # https://qdrant.tech/documentation/concepts/filtering/#match-any
-                must_conditions.append(
-                    FieldCondition(
-                        key=subfilter.key,
-                        match=MatchAny(any=str(subfilter.value).split(",")),
-                    )
-                )
+        if query.filters and query.filters.filters:
+            must_conditions.append(self._build_subfilter(query.filters))
 
         return Filter(must=must_conditions)
 
