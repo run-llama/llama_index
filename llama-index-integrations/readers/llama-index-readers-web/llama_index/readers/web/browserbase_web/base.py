2f6fb674d6a00ead88115fb1e4893a7c551df738
Logan Markewich <logan.markewich@live.com>
2024-05-01 21:35:13 -0600

    linting

--- llama-index-integrations/readers/llama-index-readers-web/llama_index/readers/web/browserbase_web/base.py
+++ llama-index-integrations/readers/llama-index-readers-web/llama_index/readers/web/browserbase_web/base.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from typing import Optional, Iterator, Sequence
 from llama_index.core.readers.base import BaseReader
@@ -9,10 +8,12 @@ logger = logging.getLogger(__name__)
 
 
 class BrowserbaseWebReader(BaseReader):
-    """Load pre-rendered web pages using a headless browser hosted on Browserbase.
-     Depends on `browserbase` package.
-     Get your API key from https://browserbase.com
-     """
+    """BrowserbaseWebReader.
+
+    Load pre-rendered web pages using a headless browser hosted on Browserbase.
+    Depends on `browserbase` package.
+    Get your API key from https://browserbase.com
+    """
 
     def __init__(
         self,
@@ -27,8 +28,10 @@ class BrowserbaseWebReader(BaseReader):
 
         self.browserbase = Browserbase(api_key=api_key)
 
-    def lazy_load_data(self, urls: Sequence[str], text_content: bool = False) -> Iterator[Document]:
-        """Load pages from URLs"""
+    def lazy_load_data(
+        self, urls: Sequence[str], text_content: bool = False
+    ) -> Iterator[Document]:
+        """Load pages from URLs."""
         pages = self.browserbase.load_urls(urls, text_content)
 
         for i, page in enumerate(pages):
