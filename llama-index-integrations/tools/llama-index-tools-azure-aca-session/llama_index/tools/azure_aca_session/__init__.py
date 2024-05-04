9eea2bb69e8d0ebe3bf5b835e8e06aa4d66755b2
harrli <harrli@microsoft.com>
2024-05-04 14:05:04 -0700

    Updated example and renamed project

--- llama-index-integrations/tools/llama-index-tools-azure-aca-session/llama_index/tools/azure_aca_session/__init__.py
+++
@@ -1,4 +0,0 @@
-## init
-from llama_index.tools.azure_aca_session.base import AzureACASessionToolSpec
-
-__all__ = ["AzureACASessionToolSpec"]
---
+++ llama-index-integrations/tools/llama-index-tools-azure-dynamic-session/llama_index/tools/azure_dynamic_session/__init__.py
@@ -0,0 +1,4 @@
+## init
+from llama_index.tools.azure_dynamic_session.base import AzureDynamicSessionToolSpec
+
+__all__ = ["AzureDynamicSessionToolSpec"]
