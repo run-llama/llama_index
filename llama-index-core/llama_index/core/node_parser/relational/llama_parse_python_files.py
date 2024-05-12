41c22680db70d682e529a5d270bc93884fbbd36c
Jaime Sancho <jimysanchomolero@gmail.com>
2024-05-12 15:51:01 +0200

    feat: add llama_parse_python_files to create the nodes from a python repository

--- llama-index-core/llama_index/core/node_parser/relational/llama_parse_python_files.py
+++ llama-index-core/llama_index/core/node_parser/relational/llama_parse_python_files.py
@@ -1,21 +1,35 @@
-try:
-  from pychunk.chunkers.python_chunker import PythonChunker
-  from pychunk.parser.llama_index_parser import LlamaIndexParser
-  from pychunk.nodes.base import BaseNode as PychunkBaseNode
-except ModuleNotFoundError as :
-  raise e("You need to install pychunk first: pip install rag-pychunk")
+from typing import List
 
-from llama_index.core.schema import NodeRelationship, BaseNode
+try:
+    from pychunk.chunkers.python_chunker import PythonChunker
+    from pychunk.parser.llama_index_parser import LlamaIndexParser
+except ModuleNotFoundError as e:
+    raise e("You need to install pychunk first: pip install rag-pychunk")
+
+from llama_index.core.schema import NodeRelationship, BaseNode, RelatedNodeInfo
 from llama_index.core.node_parser.interface import NodeParser
 
 
 class LlamaParsePythonFile(NodeParser):
-
-  def get_nodes_from_python_files(self, files: List[str]) -> List[BaseNode]:
-    ...
     
-  def _create_relationsihps_of_nodes(self, nodes: List[PyhunkBaseNode]) -> List[BaseNode]:
-    ...
+    def get_nodes_from_python_files(self, files: List[str]) -> List[BaseNode]:
+        """Convert pychunk nodes into LlamaIndex BaseNode"""
+        _chunker = PythonChunker(files_path=files)
+        _nodes = _chunker.find_relationships()
+        nodes = []
+        for _, nodes_of_file in _nodes.items():
+          nodes.extend(list(nodes_of_file.values()))
+          
+        base_nodes = LlamaIndexParser(nodes=nodes).parse_to_llama_index()
 
-llama_index_parser = LlamaIndexParser(nodes=list(all_nodes.values()))
-llama_index_nodes = llama_index_parser.parse_to_llama_index()
+        for node in base_nodes:
+            metadata = node.metadata
+            other_relationships = metadata.get('other_relationships')
+            if other_relationships is None or len(other_relationships) < 1:
+                continue
+            node.relationships[NodeRelationship.REFERENCE] = [RelatedNodeInfo(node_id=id) for id in other_relationships]
+
+        return base_nodes
+      
+    def _parse_nodes(self):
+      ...
