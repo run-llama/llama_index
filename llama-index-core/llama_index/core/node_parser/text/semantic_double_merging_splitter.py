353844f268dff55c971a93f189edd6979050a397
Konrad Rucińsk <konrad.rucinski@bitpeak.pl>
2024-05-15 23:54:50 +0200

    finish algorithm;

--- llama-index-core/llama_index/core/node_parser/text/semantic_double_merging_splitter.py
+++ llama-index-core/llama_index/core/node_parser/text/semantic_double_merging_splitter.py
@@ -58,13 +58,14 @@ class SemanticDoubleMergingSplitterNodeParser(NodeParser):
     """Semantic double merging text splitter.
 
     Splits a document into Nodes, with each node being a group of semantically related sentences.
-
+    TODO
     Args:
         # buffer_size (int): number of sentences to group together when evaluating semantic similarity
         # embed_model: (BaseEmbedding): embedding model to use
         sentence_splitter (Optional[Callable]): splits text into sentences
         include_metadata (bool): whether to include metadata in nodes
         include_prev_next_rel (bool): whether to include prev/next relationships
+        connecting_range (int): How many chunks "ahead" beyond the nearest neighbor should the algorithm check during the second pass (possible options are 1 or 2)
     """
 
     language_config: LanguageConfig = Field(
@@ -81,7 +82,7 @@ class SemanticDoubleMergingSplitterNodeParser(NodeParser):
         ),
     )
 
-    appending_treshold: float = Field(
+    appending_threshold: float = Field(
         default=0.8,
         description=(
             "The value of semantic similarity that must be exceeded between a "
@@ -104,12 +105,19 @@ class SemanticDoubleMergingSplitterNodeParser(NodeParser):
         description="Maximum size of returned chunk (number of characters)"
     )
 
+    merging_range: int = Field(
+        default=1,
+        description=("How many chunks 'ahead' beyond the nearest neighbor"
+                    "should the algorithm check during the second pass"
+                    "(possible options are 1 or 2")
+    )
+
+
     sentence_splitter: Callable[[str], List[str]] = Field(
         default_factory=split_by_sentence_tokenizer,
         description="The text splitter to use when splitting documents.",
         exclude=True,
     )
-
 
     @classmethod
     def class_name(cls) -> str:
@@ -124,6 +132,7 @@ class SemanticDoubleMergingSplitterNodeParser(NodeParser):
         appending_threshold: Optional[float] = 0.8,
         merging_threshold: Optional[float] = 0.8,
         max_chunk_size: Optional[int] = 1000,
+        merging_range: Optional[int] = 1,
         sentence_splitter: Optional[Callable[[str], List[str]]] = None,
         original_text_metadata_key: str = DEFAULT_OG_TEXT_METADATA_KEY,
         include_metadata: bool = True,
@@ -143,6 +152,7 @@ class SemanticDoubleMergingSplitterNodeParser(NodeParser):
             appending_threshold=appending_threshold,
             merging_threshold=merging_threshold,
             max_chunk_size=max_chunk_size,
+            merging_range=merging_range,
             sentence_splitter=sentence_splitter,
             original_text_metadata_key=original_text_metadata_key,
             include_metadata=include_metadata,
@@ -158,6 +168,157 @@ class SemanticDoubleMergingSplitterNodeParser(NodeParser):
         show_progress: bool = False,
         **kwargs: Any,
     ) -> List[BaseNode]:
-        #TODO
-        pass
+        """Parse document into nodes."""
+        all_nodes: List[BaseNode] = []
+        nodes_with_progress = get_tqdm_iterable(nodes, show_progress, "Parsing nodes")
 
+        for node in nodes_with_progress:
+            nodes = self.build_semantic_nodes_from_documents([node])
+            all_nodes.extend(nodes)
+
+        return all_nodes
+    
+    def build_semantic_nodes_from_documents(
+        self,
+        documents: Sequence[Document],
+    ) -> List[BaseNode]:
+        
+        """Build window nodes from documents."""
+        all_nodes: List[BaseNode] = []
+
+        for doc in documents:
+
+            text = doc.text
+            sentences = self.sentence_splitter(text)
+            initial_chunks = self._create_initial_chunks(sentences)
+            chunks = self._merge_initial_chunks(initial_chunks)
+
+            nodes = build_nodes_from_splits(
+                chunks,
+                doc,
+                id_func=self.id_func,
+            )
+            all_nodes.extend(nodes)
+        
+        return all_nodes
+    
+
+    def _create_initial_chunks(self, sentences: List[str]) -> List[str]:
+        initial_chunks: list[str] = []
+        chunk = sentences[0]  # ""
+        new = True
+
+        for sentence in sentences[1:]:
+            if new:
+                # check if 2 sentences got anything in common
+
+                if (
+                    self.language_config.nlp(self._clean_text_advanced(chunk)).similarity(
+                        self.language_config.nlp(self._clean_text_advanced(sentence))
+                    )
+                    < self.initial_threshold
+                ):  
+                    #if not then leave first sentence as separate chunk
+                    initial_chunks.append(chunk)
+                    chunk = sentence
+                    continue
+
+                chunk_sentences = [chunk]
+
+                chunk_sentences.append(sentence)
+                chunk = " ".join(chunk_sentences)
+
+                last_sentences = " ".join(chunk_sentences[-2:])
+                new = False
+            
+            elif (
+                self.language_config.nlp(self._clean_text_advanced(last_sentences)).similarity(
+                    self.language_config.nlp(self._clean_text_advanced(sentence))
+                )
+                > self.appending_threshold and not len(chunk) > self.max_chunk_size
+            ):  
+                # elif nlp(last_sentences).similarity(nlp(sentence)) > self.treshold:
+                chunk_sentences.append(sentence)
+                last_sentences = " ".join(chunk_sentences[-2:])
+                chunk += " " + sentence
+            else:
+                initial_chunks.append(chunk)
+                chunk = sentence  # ""
+                new = True
+        initial_chunks.append(chunk)
+
+        # if self.debug:
+        #     print("Chunks after 1st pass:")
+        #     for c in res:
+        #         print(c)
+        
+        return initial_chunks
+
+
+    def _merge_initial_chunks(self, initial_chunks: list[str]) -> List[str]:
+        chunks: list[str] = []
+        skip = 0
+        current = initial_chunks[0]
+
+        # TODO avoid connecting 1st chunk with 3rd if 2nd one is above some value, or if its length is above some value
+
+        for i in range(1, len(initial_chunks)):
+            # avoid connecting same chunk multiple times
+            if skip > 0:
+                skip -= 1
+                continue
+
+            current_nlp = self.language_config.nlp(self._clean_text_advanced(current))
+
+            if len(current) > self.max_chunk_size:
+                chunks.append(current)
+                current = initial_chunks[i]
+
+            #check if 1st and 2nd chunk should be connected
+            elif (
+                current_nlp.similarity(self.language_config.nlp(self._clean_text_advanced(initial_chunks[i])))
+                > self.merging_threshold
+            ):
+                current += " " + initial_chunks[i]
+
+            #check if 1st and 3rd chunk are similar, if yes then merge 1st, 2nd, 3rd together
+            elif (
+                i <= len(initial_chunks) - 2
+                and current_nlp.similarity(
+                    self.language_config.nlp(self._clean_text_advanced(initial_chunks[i + 1]))
+                )
+                > self.merging_threshold
+            ):
+                current += " " + initial_chunks[i] + " " + initial_chunks[i + 1]
+                skip = 1
+            
+            #check if 1st and 4th chunk are smilar, if yes then merge 1st, 2nd, 3rd and 4th together
+            elif (
+                i < len(initial_chunks) - 2
+                and current_nlp.similarity(
+                    self.language_config.nlp(self._clean_text_advanced(initial_chunks[i + 2]))
+                )
+                > self.merging_threshold and self.merging_range == 2
+            ):
+                current += " " + initial_chunks[i] + " " + initial_chunks[i + 1] + " " + initial_chunks[i + 2]
+                skip = 2
+            
+            else:
+                chunks.append(current)
+                current = initial_chunks[i]
+        
+        chunks.append(current)
+        return chunks
+
+    
+    def _clean_text_advanced(self, text: str) -> str:
+        text = text.lower()
+        # Remove urls
+        text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE)
+        # Remove punctuations
+        text = text.translate(str.maketrans("", "", string.punctuation))
+        # Remove stopwords
+        tokens = word_tokenize(text)
+        filtered_words = [w for w in tokens if w not in self.stop_words]
+
+        return " ".join(filtered_words)
