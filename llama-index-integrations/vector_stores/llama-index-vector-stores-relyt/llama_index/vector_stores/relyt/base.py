d6104ee9f363093a0233151adb4657ab883b3fc1
jingsi <jingsi@leadincloud.com>
2024-05-16 12:52:00 +0800

    modify apis

--- llama-index-integrations/vector_stores/llama-index-vector-stores-relyt/llama_index/vector_stores/relyt/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-relyt/llama_index/vector_stores/relyt/base.py
@@ -15,14 +15,22 @@ from llama_index.core.vector_stores.utils import (
     metadata_dict_to_node,
     node_to_metadata_dict,
 )
-from pgvecto_rs.sdk import PGVectoRs, Record
+
+try:
+    from sqlalchemy.orm import declarative_base
+except ImportError:
+    from sqlalchemy.ext.declarative import declarative_base
+
+from psycopg2.extensions import JSONB
 from pydantic import StrictStr
-from sqlalchemy import text
+from sqlalchemy import text, TEXT, Table, Column, String, create_engine, Engine, insert
 
 logger = logging.getLogger(__name__)
 import_err_msg = (
     '`pgvecto_rs.sdk` package not found, please run `pip install "pgvecto_rs[sdk]"`'
 )
+
+Base = declarative_base()  # type: Any
 
 
 class RelytVectorStore(BasePydanticVectorStore):
@@ -59,15 +67,23 @@ class RelytVectorStore(BasePydanticVectorStore):
 
     stores_text = True
 
-    _client: "PGVectoRs" = PrivateAttr()
+    _client: Engine = PrivateAttr()
     _collection_name: str = PrivateAttr()
+    _enable_vector_index: bool = PrivateAttr()
+    _dimension: int = PrivateAttr()
 
     def __init__(
-        self, client: "PGVectoRs", collection_name: str, enable_vector_index: bool
+        self,
+        connection_url: str,
+        collection_name: str,
+        dimension: int,
+        enable_vector_index: bool,
     ) -> None:
-        self._client: PGVectoRs = client
-        self._collection_name = collection_name
+        self._client = create_engine(connection_url)
+        self._collection_name = f"collection_{collection_name}"
+        self._dimension = dimension
         self._enable_vector_index = enable_vector_index
+        self.init_table()
         self.init_index()
         super().__init__()
 
@@ -75,9 +91,33 @@ class RelytVectorStore(BasePydanticVectorStore):
     def class_name(cls) -> str:
         return "RelytStore"
 
+    def init_table(self):
+        with self._client.connect() as conn:
+            with conn.begin():
+                table_query = text(
+                    f"""
+                        SELECT 1
+                        FROM pg_class
+                        WHERE relname = '{self._collection_name}';
+                    """
+                )
+                result = conn.execute(table_query).scalar()
+                if not result:
+                    table_statement = text(
+                        f"""
+                            CREATE TABLE {self._collection_name} (
+                                id TEXT PRIMARY KEY DEFAULT (md5(random()::text || clock_timestamp()::text)::uuid),
+                                embedding vector({self.embedding_dimension}),
+                                text TEXT,
+                                meta JSONB
+                            ) USING heap;
+                        """
+                    )
+                    conn.execute(table_statement)
+
     def init_index(self):
         index_name = f"idx_{self._collection_name}_embedding"
-        with self._client._engine.connect() as conn:
+        with self._client.connect() as conn:
             with conn.begin():
                 index_query = text(
                     f"""
@@ -91,7 +131,7 @@ class RelytVectorStore(BasePydanticVectorStore):
                     index_statement = text(
                         f"""
                             CREATE INDEX {index_name}
-                            ON collection_{self._collection_name}
+                            ON {self._collection_name}
                             USING vectors (embedding vector_l2_ops)
                             WITH (options = $$
                             optimizing.optimizing_threads = 10
@@ -115,7 +155,7 @@ class RelytVectorStore(BasePydanticVectorStore):
                 result = conn.execute(index_query).scalar()
                 if not result:
                     index_statement = text(
-                        f""" CREATE INDEX {index_name} ON collection_{self._collection_name} USING gin (meta); """
+                        f""" CREATE INDEX {index_name} ON {self._collection_name} USING gin (meta); """
                     )
                     conn.execute(index_statement)
 
@@ -127,18 +167,71 @@ class RelytVectorStore(BasePydanticVectorStore):
         self,
         nodes: List[BaseNode],
     ) -> List[str]:
-        records = [
-            Record(
-                id=node.id_,
-                text=node.get_content(metadata_mode=MetadataMode.NONE),
-                meta=node_to_metadata_dict(node, remove_text=True),
-                embedding=node.get_embedding(),
-            )
-            for node in nodes
-        ]
+        # records = [
+        #     Record(
+        #         id=node.id_,
+        #         text=node.get_content(metadata_mode=MetadataMode.NONE),
+        #         meta=node_to_metadata_dict(node, remove_text=True),
+        #         embedding=node.get_embedding(),
+        #     )
+        #     for node in nodes
+        # ]
+        #
+        # self._client.insert(records)
+        # return [node.id_ for node in nodes]
+        from pgvecto_rs.sqlalchemy import Vector
 
-        self._client.insert(records)
-        return [node.id_ for node in nodes]
+        ids, content, embeddings, meta = zip(
+            *[
+                (
+                    node.get_metadata_str(),
+                    node.get_content(metadata_mode=MetadataMode.NONE),
+                    node.get_embedding(),
+                    node_to_metadata_dict(
+                        node, remove_text=True, flat_metadata=self.flat_metadata
+                    ),
+                )
+                for node in nodes
+            ]
+        )
+
+        # Define the table schema
+        chunks_table = Table(
+            self._collection_name,
+            Base.metadata,
+            Column("id", TEXT, primary_key=True),
+            Column("embedding", Vector(self._dimension)),
+            Column("text", String, nullable=True),
+            Column("meta", JSONB, nullable=True),
+            extend_existing=True,
+        )
+
+        chunks_table_data = []
+        with self._client.connect() as conn:
+            with conn.begin():
+                for document, metadata, chunk_id, embedding in zip(
+                    content, meta, ids, embeddings
+                ):
+                    chunks_table_data.append(
+                        {
+                            "id": chunk_id,
+                            "embedding": embedding,
+                            "text": document,
+                            "meta": metadata,
+                        }
+                    )
+
+                    # Execute the batch insert when the batch size is reached
+                    if len(chunks_table_data) == 500:
+                        conn.execute(insert(chunks_table).values(chunks_table_data))
+                        # Clear the chunks_table_data list for the next batch
+                        chunks_table_data.clear()
+
+                # Insert any remaining records that didn't make up a full batch
+                if chunks_table_data:
+                    conn.execute(insert(chunks_table).values(chunks_table_data))
+
+        return ids
 
     def delete(self, filters: str, **delete_kwargs: Any) -> None:
         if filters is None:
@@ -146,13 +239,18 @@ class RelytVectorStore(BasePydanticVectorStore):
 
         filter_condition = f"WHERE {filters}"
 
-        with self._client._engine.connect() as conn:
+        with self._client.connect() as conn:
             with conn.begin():
-                sql_query = f""" DELETE FROM collection_{self._collection_name} {filter_condition}"""
+                sql_query = (
+                    f""" DELETE FROM {self._collection_name} {filter_condition}"""
+                )
                 conn.execute(text(sql_query))
 
     def drop(self) -> None:
-        self._client.drop()
+        drop_statement = text(f"DROP TABLE IF EXISTS {self._collection_name};")
+        with self._client.connect() as conn:
+            with conn.begin():
+                conn.execute(drop_statement)
 
     def to_postgres_operator(self, operator: FilterOperator) -> str:
         if operator == FilterOperator.EQ:
@@ -222,10 +320,10 @@ class RelytVectorStore(BasePydanticVectorStore):
                 "Please 'pip install sqlalchemy>=1.4'."
             )
 
-        embedding = VectorStoreQuery.query_embedding
-        k = VectorStoreQuery.similarity_top_k
+        embedding = query.query_embedding
+        k = query.similarity_top_k
         filter_condition = ""
-        filters = VectorStoreQuery.filters
+        filters = query.filters
 
         if filters is not None:
             filter_condition += f"WHERE {self.transformer_filter(filters)}"
@@ -244,7 +342,7 @@ class RelytVectorStore(BasePydanticVectorStore):
         params = {"embedding": embedding_str, "k": k}
 
         # Execute the query and fetch the results
-        with self.engine.connect() as conn:
+        with self._client.connect() as conn:
             results: Sequence[Row] = conn.execute(text(sql_query), params).fetchall()
 
         nodes = [
