7037b1383068d728fc7d781073e4745fffac86ce
raghavdixit99 <dixitraghav99@gmail.com>
2024-05-23 20:11:30 +0100

    add util

---
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-lancedb/llama_index/vector_stores/lancedb/util.py
@@ -0,0 +1,20 @@
+from llama_index.core.vector_stores.types import (
+    FilterOperator,
+)
+
+sql_operator_mapper = {
+    FilterOperator.EQ: " = ",
+    FilterOperator.GT: " > ",
+    FilterOperator.GTE: " >= ",
+    FilterOperator.LTE: " <= ",
+    FilterOperator.TEXT_MATCH: " LIKE ",
+    FilterOperator.NE: " NOT LIKE ",
+    FilterOperator.IN: " IN ",
+}
+
+reranker_mapper = {
+    "default": "LinearCombinationReranker",
+    "cohere": "CohereReranker",
+    "colbert": "ColbertReranker",
+    "crossencoder": "CrossEncoderReranker",
+}
