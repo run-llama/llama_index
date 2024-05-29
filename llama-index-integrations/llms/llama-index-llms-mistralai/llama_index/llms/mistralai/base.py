9a648f5ef934b9c913daf5add80584db2e69bc88
ravi03071991 <ravi03071991@gmail.com>
2024-05-29 21:10:10 +0530

    Update codestral with fill in middle endpoint

--- llama-index-integrations/llms/llama-index-llms-mistralai/llama_index/llms/mistralai/base.py
+++ llama-index-integrations/llms/llama-index-llms-mistralai/llama_index/llms/mistralai/base.py
@@ -31,6 +31,7 @@ from llama_index.core.types import BaseOutputParser, PydanticProgramMode
 from llama_index.core.llms.function_calling import FunctionCallingLLM
 from llama_index.llms.mistralai.utils import (
     is_mistralai_function_calling_model,
+    is_mistralai_code_model,
     mistralai_modelname_to_contextsize,
 )
 
@@ -444,3 +445,19 @@ class MistralAI(FunctionCallingLLM):
             )
 
         return tool_selections
+
+    def fill_in_middle(
+        self, prompt: str, suffix: str, stop: List[str] = ["\n\n"]
+    ) -> CompletionResponse:
+        if not is_mistralai_code_model(self.model):
+            raise ValueError(
+                "Please provide code model from MistralAI. Currently supported code model is 'codestral-latest'."
+            )
+
+        response = self._client.completion(
+            model=self.model, prompt=prompt, suffix=suffix, stop=stop
+        )
+
+        return CompletionResponse(
+            text=response.choices[0].message.content, raw=dict(response)
+        )
