b765122ef7d45f3f42b8e5ce2b038c4b447e1d8c
Andrei Fajardo <andrei@nerdai.io>
2024-05-30 23:40:31 -0400

    scaffold

--- llama-index-integrations/agent/llama-index-agent-multi-hop/llama_index/agent/multi_hop/step.py
+++ llama-index-integrations/agent/llama-index-agent-multi-hop/llama_index/agent/multi_hop/step.py
@@ -3,7 +3,7 @@
 import logging
 import uuid
 import re
-from typing import Any, Coroutine, List, Optional, Sequence, Type, TypeAlias
+from typing import Any, Coroutine, List, Optional, Sequence, Tuple, Type, TypeAlias
 
 from llama_index.core.agent.types import (
     BaseAgentWorker,
@@ -13,12 +13,17 @@ from llama_index.core.bridge.pydantic import BaseModel, Field, validator, create
 from llama_index.core.callbacks import (
     CallbackManager,
 )
+from llama_index.core.chat_engine.types import (
+    AgentChatResponse,
+)
 from llama_index.core.llms.llm import LLM
-from llama_index.core.base.base_query_engine import BaseQueryEngine
+from llama_index.core.prompts import PromptTemplate
+from llama_index.core.base.base_retriever import BaseRetriever
 from llama_index.core.memory.chat_memory_buffer import ChatMemoryBuffer
 from llama_index.core.settings import Settings
 from llama_index.core.tools import BaseTool
-from llama_index.core.tools.query_engine import QueryEngineTool
+
+from llama_index.agent.multi_hop.data_extraction import DataExtractionAgentWorker
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
@@ -32,6 +37,16 @@ def get_function_by_name(tools: List[BaseTool], name: str) -> BaseTool:
     if name not in name_to_tool:
         raise ValueError(f"Tool with name {name} not found")
     return name_to_tool[name]
+
+
+DATA_REQUIREMENTS_PROMPT_TEMPLATE = """
+You are responsible for identifying the data requirements for
+sufficiently answering the query provided below. Make sure to
+also include the direct object of the query itself as a data field
+requirement.
+
+{query}
+"""
 
 
 class DataRequirements(BaseModel):
@@ -52,15 +67,24 @@ class DataRequirements(BaseModel):
             raise ValueError("There must be a description for every data field.")
         return v
 
-    def to_structured_context(self) -> Type[StructuredContext]:
+    def get_structured_context_cls(
+        self,
+    ) -> Tuple[Type[StructuredContext], List[Type[StructuredContext]]]:
         """Generate a custom pydantic model for StructuredContext."""
         data_fields = {}
+        sub_structured_context_classes = []
         for name, desc in zip(self.data_field_names, self.data_field_descriptions):
+            this_data_field = {}
             # strip out punctuation
             name = re.sub(r"[^\w\s]", "", name)
             # lower and replace space with _
             name = name.lower().replace(" ", "_")
-            data_fields[name] = (Optional[str], Field(default=None, description=desc))
+            this_data_field[name] = (
+                Optional[str],
+                Field(default=None, description=desc),
+            )
+            data_fields.update(this_data_field)
+            sub_structured_context_classes.append(create_model(""))
 
         StructuredContext = create_model("StructuredContext", **data_fields)
         StructuredContext.__doc__ = (
@@ -69,28 +93,43 @@ class DataRequirements(BaseModel):
         return StructuredContext
 
 
+def _get_query_str_from_structured_context_cls(
+    structured_context_cls: Type[StructuredContext],
+) -> str:
+    """Generate query string from a structure context class."""
+    query_str = ""
+    for props in structured_context_cls.schema()["properties"].values():
+        query_str += f"{props['description']}\n\n"
+    return query_str
+
+
 class MultiHopAgentWorker(BaseAgentWorker):
     """MultiHop Agent Worker."""
 
     def __init__(
         self,
         llm: LLM,
-        query_engine_tool: QueryEngineTool,
+        retrievers: List[BaseRetriever],
         tools: Optional[Sequence[BaseTool]] = None,
         verbose: bool = False,
         callback_manager: Optional[CallbackManager] = None,
     ) -> None:
         """Init params."""
         self.llm = llm
-        self.query_engine_tool = query_engine_tool
+        self.retrievers = retrievers
         self.tools = tools
         self.verbose = verbose
         self.callback_manager = callback_manager or CallbackManager([])
+        self._data_extraction_agent_worker = (
+            DataExtractionAgentWorker.from_tools_and_retrievers(
+                tools=tools, retrievers=retrievers
+            )
+        )
 
     @classmethod
     def from_defaults(
         cls,
-        query_engine: BaseQueryEngine,
+        retrievers: List[BaseRetriever],
         llm: Optional[LLM] = None,
         tools: Optional[Sequence[BaseTool]] = None,
         verbose: bool = False,
@@ -101,13 +140,9 @@ class MultiHopAgentWorker(BaseAgentWorker):
         if callback_manager is not None:
             llm.callback_manager = callback_manager
 
-        query_engine_tool = QueryEngineTool.from_defaults(
-            query_engine=query_engine, **kwargs
-        )
-
         return cls(
             llm=llm,
-            query_engine_tool=query_engine_tool,
+            retrievers=retrievers,
             tools=tools,
             verbose=verbose,
             callback_manager=callback_manager,
@@ -132,13 +167,42 @@ class MultiHopAgentWorker(BaseAgentWorker):
 
     def run_step(self, step: TaskStep, task: Task, **kwargs: Any) -> TaskStepOutput:
         # generate structured data model to get data requirements based on input
+        data_requirements: DataRequirements = self.llm.structured_predict(
+            DataRequirements,
+            PromptTemplate(DATA_REQUIREMENTS_PROMPT_TEMPLATE),
+            query=task.input,
+        )
+        structured_context_cls = data_requirements.get_structured_context_cls()
 
-        # retrieve relevant documents from index
+        # generate sub-task for each data requirement
+        sub_task_extra_state = {"structured_context_obj": structured_context_cls()}
+        data_extraction_agent = self._data_extraction_agent_worker.as_agent(
+            extra_state=sub_task_extra_state
+        )
+        data_extraction_response = data_extraction_agent.chat(
+            "Perform data extraction task."
+        )
 
-        # perform data extraction using retrieved context from RAG
+        # try parsing as a structured context obj
+        final_structured_context = structured_context_cls.parse_obj(
+            data_extraction_response.content
+        )
 
         # perform final response synthesis
-        return super().run_step(step, task, **kwargs)
+        structured_context_augmented_query = ...
+        result = self.llm.chat(structured_context_augmented_query)
+
+        agent_response = AgentChatResponse(
+            response=str(result.message),
+            sources=task.extra_state["data_extraction"]["sources"],
+        )
+
+        return TaskStepOutput(
+            output=agent_response,
+            task_step=step,
+            is_last=True,
+            next_steps=[],
+        )
 
     def stream_step(self, step: TaskStep, task: Task, **kwargs: Any) -> TaskStepOutput:
         return super().stream_step(step, task, **kwargs)
