383b536e8f13436a8d32f067f9f33f6785ec13dc
Ravi Theja <ravi03071991@gmail.com>
2024-06-04 00:13:51 +0200

    Add Nomic multi modal embeddings (#13920)

--- llama-index-integrations/embeddings/llama-index-embeddings-nomic/tests/test_embeddings_nomic.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-nomic/tests/test_embeddings_nomic.py
@@ -1,7 +1,7 @@
-from llama_index.core.base.embeddings.base import BaseEmbedding
+from llama_index.core.embeddings import MultiModalEmbedding
 from llama_index.embeddings.nomic import NomicEmbedding
 
 
 def test_embedding_class():
     names_of_base_classes = [b.__name__ for b in NomicEmbedding.__mro__]
-    assert BaseEmbedding.__name__ in names_of_base_classes
+    assert MultiModalEmbedding.__name__ in names_of_base_classes
