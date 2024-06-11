6fbfef256d122657a30391526d25f0fc89ab3b03
Fernando Silva <fernandonsilva16@gmail.com>
2024-06-10 22:16:59 -0300

    fix(huggingface): fix deprecation notice

--- llama-index-integrations/embeddings/llama-index-embeddings-huggingface/llama_index/embeddings/huggingface/base.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-huggingface/llama_index/embeddings/huggingface/base.py
@@ -31,10 +31,6 @@ DEFAULT_HUGGINGFACE_LENGTH = 512
 logger = logging.getLogger(__name__)
 
 
-@deprecated(
-    "Deprecated in favor of `TextGenerationInferenceEmbedding` from `llama-index-embeddings-text-generation-inference` which should be used instead.",
-    action="always",
-)
 class HuggingFaceEmbedding(BaseEmbedding):
     max_length: int = Field(
         default=DEFAULT_HUGGINGFACE_LENGTH, description="Maximum length of input.", gt=0
@@ -154,6 +150,10 @@ class HuggingFaceEmbedding(BaseEmbedding):
         return self._embed(texts, prompt_name="text")
 
 
+@deprecated(
+    "Deprecated in favor of `HuggingFaceInferenceAPIEmbedding` from `llama-index-embeddings-huggingface-api` which should be used instead.",
+    action="always",
+)
 class HuggingFaceInferenceAPIEmbedding(BaseEmbedding):  # type: ignore[misc]
     """
     Wrapper on the Hugging Face's Inference API for embeddings.
