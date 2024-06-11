9f5c1596ada5734c20f7df06b713f397bb04b866
Logan Markewich <logan.markewich@live.com>
2024-06-11 13:42:01 -0600

    fix tests

--- llama-index-packs/llama-index-packs-zenguard/tests/test_packs_zenguard.py
+++ llama-index-packs/llama-index-packs-zenguard/tests/test_packs_zenguard.py
@@ -23,69 +23,53 @@ def test_class():
     assert BaseLlamaPack.__name__ in names_of_base_classes
 
 
-def assert_detectors_response(response, detectors):
-    assert response is not None
-    for detector in detectors:
-        common_response = next(
-            resp["common_response"]
-            for resp in response["responses"]
-            if resp["detector"] == detector.value
-        )
-        assert (
-            "err" not in common_response
-        ), f"API returned an error: {common_response.get('err')}"
-        assert (
-            common_response.get("is_detected") is False
-        ), f"Prompt was detected: {common_response}"
-
-
 def test_prompt_injection(zenguard_pack):
     prompt = "Simple prompt injection test"
     detectors = [Detector.PROMPT_INJECTION]
     response = zenguard_pack.run(detectors=detectors, prompt=prompt)
-    assert_detectors_response(response, detectors)
+    assert response["is_detected"] is False
 
 
 def test_pii(zenguard_pack):
     prompt = "Simple PII test"
     detectors = [Detector.PII]
     response = zenguard_pack.run(detectors=detectors, prompt=prompt)
-    assert_detectors_response(response, detectors)
+    assert response["is_detected"] is False
 
 
 def test_allowed_topics(zenguard_pack):
     prompt = "Simple allowed topics test"
     detectors = [Detector.ALLOWED_TOPICS]
     response = zenguard_pack.run(detectors=detectors, prompt=prompt)
-    assert_detectors_response(response, detectors)
+    assert response["is_detected"] is False
 
 
 def test_banned_topics(zenguard_pack):
     prompt = "Simple banned topics test"
     detectors = [Detector.BANNED_TOPICS]
     response = zenguard_pack.run(detectors=detectors, prompt=prompt)
-    assert_detectors_response(response, detectors)
+    assert response["is_detected"] is False
 
 
 def test_keywords(zenguard_pack):
     prompt = "Simple keywords test"
     detectors = [Detector.KEYWORDS]
     response = zenguard_pack.run(detectors=detectors, prompt=prompt)
-    assert_detectors_response(response, detectors)
+    assert response["is_detected"] is False
 
 
 def test_secrets(zenguard_pack):
     prompt = "Simple secrets test"
     detectors = [Detector.SECRETS]
     response = zenguard_pack.run(detectors=detectors, prompt=prompt)
-    assert_detectors_response(response, detectors)
+    assert response["is_detected"] is False
 
 
 def test_toxicity(zenguard_pack):
     prompt = "Simple toxicity test"
     detectors = [Detector.TOXICITY]
     response = zenguard_pack.run(detectors=detectors, prompt=prompt)
-    assert_detectors_response(response, detectors)
+    assert response["is_detected"] is False
 
 
 def test_get_modules(zenguard_pack):
