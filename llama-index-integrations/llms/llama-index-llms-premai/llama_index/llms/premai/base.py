a5a85851d872a8cdd7daa3d2334f0e7730b3f07d
Anindyadeep <proanindyadeep@gmail.com>
2024-06-12 18:55:15 +0530

    Feat: Prem Template feature in llama-index

--- llama-index-integrations/llms/llama-index-llms-premai/llama_index/llms/premai/base.py
+++ llama-index-integrations/llms/llama-index-llms-premai/llama_index/llms/premai/base.py
@@ -177,21 +177,57 @@ class PremAI(LLM):
                 all_kwargs.pop(key, None)
         return all_kwargs
 
-    @llm_chat_callback()
-    def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
-        all_kwargs = self._get_all_kwargs(**{**self.additional_kwargs, **kwargs})
+    def _prepare_messages_before_chat(
+        self, messages: Sequence[ChatMessage], **kwargs: Any
+    ):
         chat_messages = []
+        all_kwargs = self._get_all_kwargs(**{**self.additional_kwargs, **kwargs})
 
         for message in messages:
             if "system_prompt" in all_kwargs and message.role.value == "system":
                 continue
-
             elif "system_prompt" not in all_kwargs and message.role.value == "system":
                 all_kwargs["system_prompt"] = message.content
-            else:
+            elif message.role.value == "assistant":
                 chat_messages.append(
                     {"role": message.role.value, "content": message.content}
                 )
+            elif message.role.value == "user":
+                if "template_id" not in kwargs:
+                    chat_messages.append(
+                        {"role": message.role.value, "content": message.content}
+                    )
+                else:
+                    template_id = kwargs["template_id"]
+                    assert template_id is not None and template_id != "", ValueError(
+                        "template_id can not be None or '' when passed in kwargs"
+                    )
+                    assert "id" in message.additional_kwargs, KeyError(
+                        "When using Prem templates in llama-index, ensure you have 'id' key ",
+                        "in message.additional_kwargs. This id act as the template variable. ",
+                        "If you do not have template variable then use only system prompt",
+                    )
+
+                    chat_messages.append(
+                        {
+                            "role": message.role.value,
+                            "template_id": template_id,
+                            "params": {
+                                message.additional_kwargs["id"]: message.content
+                            },
+                        }
+                    )
+
+            else:
+                raise ValueError("role can be either 'system', 'user' or 'assistant'")
+        return chat_messages, all_kwargs
+
+    @llm_chat_callback()
+    def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
+        chat_messages, all_kwargs = self._prepare_messages_before_chat(
+            messages=messages, **kwargs
+        )
+
         response = self._client.chat.completions.create(
             project_id=self.project_id, messages=chat_messages, **all_kwargs
         )
@@ -219,19 +255,9 @@ class PremAI(LLM):
     def stream_chat(
         self, messages: Sequence[ChatMessage], **kwargs: Any
     ) -> ChatResponseGen:
-        all_kwargs = self._get_all_kwargs(**kwargs)
-        chat_messages = []
-
-        for message in messages:
-            if "system_prompt" in all_kwargs and message.role.value == "system":
-                continue
-
-            elif "system_prompt" not in all_kwargs and message.role.value == "system":
-                all_kwargs["system_prompt"] = message.content
-            else:
-                chat_messages.append(
-                    {"role": message.role.value, "content": message.content}
-                )
+        chat_messages, all_kwargs = self._prepare_messages_before_chat(
+            messages=messages, **kwargs
+        )
 
         response_generator = self._client.chat.completions.create(
             project_id=self.project_id,
