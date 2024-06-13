2f989847793f17e4165f8c38eaef7e0f4fa78ad9
raghavdixit99 <dixitraghav99@gmail.com>
2024-06-14 00:46:32 +0100

    typo fix, version bump

--- llama-index-integrations/vector_stores/llama-index-vector-stores-lancedb/llama_index/vector_stores/lancedb/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-lancedb/llama_index/vector_stores/lancedb/base.py
@@ -33,6 +33,7 @@ from llama_index.core.vector_stores.utils import (
 from pandas import DataFrame
 
 import lancedb
+import lancedb.remote.table  # type: ignore
 
 _logger = logging.getLogger(__name__)
 
@@ -342,13 +343,17 @@ class LanceDBVectorStore(BasePydanticVectorStore):
             data.append(append_data)
             ids.append(node.node_id)
 
-            if self._table is None:
-                self._table = self._connection.create_table(
-                    self._table_name, data, mode=self.mode
-                )
+        if self._table is None:
+            self._table = self._connection.create_table(
+                self._table_name, data, mode=self.mode
+            )
+        else:
+            if self.api_key is None:
+                self._table.add(data, mode=self.mode)
+            else:
+                self._table.add(data)
 
-            self._table.add(data, mode=self.mode)
-            self._fts_index = None  # reset fts index
+        self._fts_index = None  # reset fts index
 
         return ids
 
