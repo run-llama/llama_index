907117266f628ca64ebb7e6d72368858064e80c3
Cyrus Leung <cyrus.tl.leung@gmail.com>
2024-06-16 02:05:15 +0800

    [Core][Retrieval] Add precision and recall metrics (#14170)
    * Add precision and recall metric
    
    * Remove code not in the original notebook

--- llama-index-core/llama_index/core/evaluation/retrieval/metrics.py
+++ llama-index-core/llama_index/core/evaluation/retrieval/metrics.py
@@ -142,6 +142,100 @@ class MRR(BaseRetrievalMetric):
         return RetrievalMetricResult(score=mrr_score)
 
 
+class Precision(BaseRetrievalMetric):
+    """Precision metric.
+
+    The `K`-value in `Precision@K` usually corresponds to `top_k` of the retriever.
+
+    Attributes:
+        metric_name (str): The name of the metric.
+    """
+
+    metric_name: ClassVar[str] = "precision"
+
+    def compute(
+        self,
+        query: Optional[str] = None,
+        expected_ids: Optional[List[str]] = None,
+        retrieved_ids: Optional[List[str]] = None,
+        expected_texts: Optional[List[str]] = None,
+        retrieved_texts: Optional[List[str]] = None,
+    ) -> RetrievalMetricResult:
+        """Compute precision based on the provided inputs and selected method.
+
+        Parameters:
+            query (Optional[str]): The query string (not used in the current implementation).
+            expected_ids (Optional[List[str]]): Expected document IDs.
+            retrieved_ids (Optional[List[str]]): Retrieved document IDs.
+            expected_texts (Optional[List[str]]): Expected texts (not used in the current implementation).
+            retrieved_texts (Optional[List[str]]): Retrieved texts (not used in the current implementation).
+
+        Raises:
+            ValueError: If the necessary IDs are not provided.
+
+        Returns:
+            RetrievalMetricResult: The result with the computed precision score.
+        """
+        # Checking for the required arguments
+        if (
+            retrieved_ids is None
+            or expected_ids is None
+            or not retrieved_ids
+            or not expected_ids
+        ):
+            raise ValueError("Retrieved ids and expected ids must be provided")
+
+        precision = len(set(retrieved_ids) & set(expected_ids)) / len(retrieved_ids)
+
+        return RetrievalMetricResult(score=precision)
+
+
+class Recall(BaseRetrievalMetric):
+    """Recall metric.
+
+    Attributes:
+        metric_name (str): The name of the metric.
+    """
+
+    metric_name: ClassVar[str] = "recall"
+
+    def compute(
+        self,
+        query: Optional[str] = None,
+        expected_ids: Optional[List[str]] = None,
+        retrieved_ids: Optional[List[str]] = None,
+        expected_texts: Optional[List[str]] = None,
+        retrieved_texts: Optional[List[str]] = None,
+    ) -> RetrievalMetricResult:
+        """Compute recall based on the provided inputs and selected method.
+
+        Parameters:
+            query (Optional[str]): The query string (not used in the current implementation).
+            expected_ids (Optional[List[str]]): Expected document IDs.
+            retrieved_ids (Optional[List[str]]): Retrieved document IDs.
+            expected_texts (Optional[List[str]]): Expected texts (not used in the current implementation).
+            retrieved_texts (Optional[List[str]]): Retrieved texts (not used in the current implementation).
+
+        Raises:
+            ValueError: If the necessary IDs are not provided.
+
+        Returns:
+            RetrievalMetricResult: The result with the computed recall score.
+        """
+        # Checking for the required arguments
+        if (
+            retrieved_ids is None
+            or expected_ids is None
+            or not retrieved_ids
+            or not expected_ids
+        ):
+            raise ValueError("Retrieved ids and expected ids must be provided")
+
+        recall = len(set(retrieved_ids) & set(expected_ids)) / len(expected_ids)
+
+        return RetrievalMetricResult(score=recall)
+
+
 DiscountedGainMode = Literal["linear", "exponential"]
 
 
@@ -199,7 +293,7 @@ class NDCG(BaseRetrievalMetric):
             ValueError: If the necessary IDs are not provided.
 
         Returns:
-            RetrievalMetricResult: The result with the computed MRR score.
+            RetrievalMetricResult: The result with the computed NDCG score.
         """
         # Checking for the required arguments
         if (
@@ -296,6 +390,8 @@ class CohereRerankRelevancyMetric(BaseRetrievalMetric):
 METRIC_REGISTRY: Dict[str, Type[BaseRetrievalMetric]] = {
     "hit_rate": HitRate,
     "mrr": MRR,
+    "precision": Precision,
+    "recall": Recall,
     "ndcg": NDCG,
     "cohere_rerank_relevancy": CohereRerankRelevancyMetric,
 }
