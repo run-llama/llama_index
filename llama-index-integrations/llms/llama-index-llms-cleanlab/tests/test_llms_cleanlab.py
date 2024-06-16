da9bf00c12b18084b429a44cb822ac36c6967202
Ashish Sardana <ashishsardana21@gmail.com>
2024-06-16 16:52:47 -0700

    add simple test

---
+++ llama-index-integrations/llms/llama-index-llms-cleanlab/tests/test_llms_cleanlab.py
@@ -0,0 +1,6 @@
+from llama_index.core.base.llms.base import BaseLLM
+from llama_index.llms.cleanlab import CleanlabTLM
+
+def test_embedding_class():
+    names_of_base_classes = [b.__name__ for b in CleanlabTLM.__mro__]
+    assert BaseLLM.__name__ in names_of_base_classes
