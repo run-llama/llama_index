fb9775cd02e94e097ce8cfd4021bf336913f9d0f
Ashish Sardana <ashishsardana21@gmail.com>
2024-06-17 10:58:02 -0700

    call constructor

--- llama-index-integrations/llms/llama-index-llms-cleanlab/llama_index/llms/cleanlab/base.py
+++ llama-index-integrations/llms/llama-index-llms-cleanlab/llama_index/llms/cleanlab/base.py
@@ -10,9 +10,9 @@ from llama_index.core.base.llms.types import (
 from llama_index.core.base.llms.generic_utils import (
     get_from_param_or_env,
 )
-from llama_index.core.llms.callbacks import llm_completion_callback
+from llama_index.core.llms.callbacks import llm_completion_callback, CallbackManager
 from llama_index.core.llms.custom import CustomLLM
-from llama_index.core.bridge.pydantic import Field
+from llama_index.core.bridge.pydantic import Field, PrivateAttr
 
 from cleanlab_studio import Studio
 
@@ -23,21 +23,28 @@ class CleanlabTLM(CustomLLM):
     max_tokens: int = 512
     model: str = "TLM"
 
+    _client = PrivateAttr()
+
     def __init__(
         self,
         api_key: Optional[str] = None,
         quality_preset: Optional[str] = "medium",
+        model: Optional[str] = "TLM",
+        max_tokens: Optional[int] = 512,
+        callback_manager: Optional[CallbackManager] = None,
         additional_kwargs: Optional[Dict[str, Any]] = None,
     ) -> None:
         """Initialize params."""
-        additional_kwargs = additional_kwargs or {}
-
+        super().__init__(
+            model=model,
+            max_tokens=max_tokens,
+            additional_kwargs=additional_kwargs or {},
+            callback_manager=callback_manager,
+        )
         api_key = get_from_param_or_env("api_key", api_key, "CLEANLAB_API_KEY")
-        #self.quality_preset = quality_preset
 
         studio = Studio(api_key = api_key)
-        client = studio.TLM(quality_preset = quality_preset)
-
+        self._client = studio.TLM(quality_preset = quality_preset)    
 
     @classmethod
     def class_name(cls) -> str:
@@ -47,22 +54,22 @@ class CleanlabTLM(CustomLLM):
     def metadata(self) -> LLMMetadata:
         """Get LLM metadata."""
         return LLMMetadata(
-            context_window=self.context_window,
-            num_output=self.max_tokens,
-            model_name=self.model,
+            context_window=context_window,
+            num_output=max_tokens,
+            model_name=model,
         )
 
     @llm_completion_callback()
     def complete(self, prompt: str, **kwargs: Any) -> CompletionResponse:
         # Prompt TLM for a response and trustworthiness score
-        response: Dict[str, str] = self.client.prompt(prompt)
+        response: Dict[str, str] = self._client.prompt(prompt)
         output = json.dumps(response)
         return CompletionResponse(text=response['text'], additional_kwargs={'trustworthiness_score': response['trustworthiness_score']})
 
     @llm_completion_callback()
     def stream_complete(self, prompt: str, **kwargs: Any) -> CompletionResponseGen:
         # Prompt TLM for a response and trustworthiness score
-        response = self.client.prompt(prompt)
+        response = client.prompt(prompt)
         output = json.dumps(response)
 
         # TODO: figure how to stream additional_kwargs. workaround: dump `trustworthiness_score` as str
