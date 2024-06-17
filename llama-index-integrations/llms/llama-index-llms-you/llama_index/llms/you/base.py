ba858fc9363b8c35997c124748b597548a16d81b
Christopher Tee <christopherfytee@gmail.com>
2024-06-17 21:40:48 +0000

    Update endpoints

--- llama-index-integrations/llms/llama-index-llms-you/llama_index/llms/you/base.py
+++ llama-index-integrations/llms/llama-index-llms-you/llama_index/llms/you/base.py
@@ -12,8 +12,8 @@ from llama_index.core.llms import (
 from llama_index.core.llms.callbacks import llm_completion_callback
 from pydantic import Field
 
-SMART_ENDPOINT = ""  # TODO: Update before submitting PR
-RESEARCH_ENDPOINT = ""  # TODO: Update before submitting PR
+SMART_ENDPOINT = "https://chat-api.you.com/smart"
+RESEARCH_ENDPOINT = "https://chat-api.you.com/research"
 
 
 def _request(base_url: str, api_key: str, **kwargs) -> Dict[str, Any]:
@@ -22,7 +22,7 @@ def _request(base_url: str, api_key: str, **kwargs) -> Dict[str, Any]:
     for better input/output typing support.
     """
     headers = {"x-api-key": api_key}
-    response = requests.get(base_url, headers=headers, params=kwargs)
+    response = requests.post(base_url, headers=headers, json=kwargs)
     response.raise_for_status()
     return response.json()
 
@@ -32,7 +32,7 @@ def _request_stream(
 ) -> Generator[str, None, None]:
     headers = {"x-api-key": api_key}
     params = dict(**kwargs, stream=True)
-    response = requests.get(base_url, headers=headers, stream=True, params=params)
+    response = requests.post(base_url, headers=headers, stream=True, json=params)
     response.raise_for_status()
 
     client = sseclient.SSEClient(response)
