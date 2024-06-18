8e3f0f92eeb23ebd44dc2ef70dc2ee76a91235f1
Ashish Sardana <ashishsardana21@gmail.com>
2024-06-18 11:32:25 -0700

    change function name

--- llama-index-integrations/llms/llama-index-llms-cleanlab/tests/test_llms_cleanlab.py
+++ llama-index-integrations/llms/llama-index-llms-cleanlab/tests/test_llms_cleanlab.py
@@ -2,6 +2,6 @@ from llama_index.core.base.llms.base import BaseLLM
 from llama_index.llms.cleanlab import CleanlabTLM
 
 
-def test_embedding_class():
+def test_llms_cleanlab():
     names_of_base_classes = [b.__name__ for b in CleanlabTLM.__mro__]
     assert BaseLLM.__name__ in names_of_base_classes
