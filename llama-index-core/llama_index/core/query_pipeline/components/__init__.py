555a8d351202bd289a56a439ea0b495e0462d2a6
Jerry Liu <jerryjliu98@gmail.com>
2024-06-19 01:12:40 -0700

    cr

--- llama-index-core/llama_index/core/query_pipeline/components/__init__.py
+++ llama-index-core/llama_index/core/query_pipeline/components/__init__.py
@@ -15,6 +15,7 @@ from llama_index.core.query_pipeline.components.router import (
     SelectorComponent,
 )
 from llama_index.core.query_pipeline.components.tool_runner import ToolRunnerComponent
+from llama_index.core.query_pipeline.components.stateful import StatefulFnComponent
 
 __all__ = [
     "AgentFnComponent",
@@ -28,4 +29,5 @@ __all__ = [
     "RouterComponent",
     "SelectorComponent",
     "ToolRunnerComponent",
+    "StatefulFnComponent"
 ]
