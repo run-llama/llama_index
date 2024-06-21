5c35084cc2b3a1d98ee26fa9532d77a25dee2f4a
Logan Markewich <logan.markewich@live.com>
2024-06-21 09:14:19 -0600

    v0.10.48

--- llama-index-integrations/llms/llama-index-llms-ibm/tests/test_llms_ibm.py
+++
@@ -1,7 +0,0 @@
-from llama_index.core.base.llms.base import BaseLLM
-from llama_index.llms.ibm import WatsonxLLM
-
-
-def test_embedding_class():
-    names_of_base_classes = [b.__name__ for b in WatsonxLLM.__mro__]
-    assert BaseLLM.__name__ in names_of_base_classes
