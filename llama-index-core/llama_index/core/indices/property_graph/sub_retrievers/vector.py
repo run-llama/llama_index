5d80ea092feab1c9046909113892a27941c665c7
theoneamendez <the.one.amendez@gmail.com>
2024-06-24 14:23:29 -0700

    Add filters to neo4j_property_graph since they are currently unsupported. Additionally, fix text_to_cypher when allowed_output_fields is None

--- llama-index-core/llama_index/core/indices/property_graph/sub_retrievers/vector.py
+++ llama-index-core/llama_index/core/indices/property_graph/sub_retrievers/vector.py
@@ -11,7 +11,7 @@ from llama_index.core.graph_stores.types import (
 )
 from llama_index.core.settings import Settings
 from llama_index.core.schema import BaseNode, NodeWithScore, QueryBundle
-from llama_index.core.vector_stores.types import VectorStoreQuery, VectorStore
+from llama_index.core.vector_stores.types import VectorStoreQuery, VectorStore, MetadataFilters
 
 
 class VectorContextRetriever(BasePGRetriever):
@@ -41,6 +41,7 @@ class VectorContextRetriever(BasePGRetriever):
         vector_store: Optional[VectorStore] = None,
         similarity_top_k: int = 4,
         path_depth: int = 1,
+        filters: Optional[MetadataFilters] = None,
         **kwargs: Any
     ) -> None:
         self._retriever_kwargs = kwargs or {}
@@ -48,6 +49,7 @@ class VectorContextRetriever(BasePGRetriever):
         self._similarity_top_k = similarity_top_k
         self._vector_store = vector_store
         self._path_depth = path_depth
+        self._filters = filters
 
         super().__init__(graph_store=graph_store, include_text=include_text, **kwargs)
 
@@ -60,6 +62,7 @@ class VectorContextRetriever(BasePGRetriever):
         return VectorStoreQuery(
             query_embedding=query_bundle.embedding,
             similarity_top_k=self._similarity_top_k,
+            filters=self._filters,
             **self._retriever_kwargs,
         )
 
@@ -80,6 +83,7 @@ class VectorContextRetriever(BasePGRetriever):
         return VectorStoreQuery(
             query_embedding=query_bundle.embedding,
             similarity_top_k=self._similarity_top_k,
+            filters=self._filters,
             **self._retriever_kwargs,
         )
 
