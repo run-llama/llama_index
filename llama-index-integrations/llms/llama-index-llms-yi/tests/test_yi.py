ab2d576e85afc02a11421de3ffd07f9ab7289f24
Yimi81 <1548222878@qq.com>
2024-06-24 21:16:52 +0800

    modify

--- llama-index-integrations/llms/llama-index-llms-yi/tests/test_yi.py
+++ llama-index-integrations/llms/llama-index-llms-yi/tests/test_yi.py
@@ -1,7 +1,7 @@
 from llama_index.llms.yi import Yi
 
 # get api key from: https://platform.01.ai/
-llm = Yi(model="yi-large", api_key="")
+llm = Yi(model="yi-large", api_key="dd36902a1937481d9984227e465c1c88")
 
 response = llm.chat("Hi, who are you?")
 print(response)
