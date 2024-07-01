c2d9c74a2055a8613e8bb741578f5dd5f62e4872
Rui Barbosa <barduinor@gmail.com>
2024-07-01 16:35:24 -0400

    test box client connections

--- llama-index-integrations/readers/llama-index-readers-box/llama_index/readers/box/box_client_ccg.py
+++ llama-index-integrations/readers/llama-index-readers-box/llama_index/readers/box/box_client_ccg.py
@@ -11,14 +11,11 @@ from box_sdk_gen import BoxCCGAuth, CCGConfig
 from box_sdk_gen import FileWithInMemoryCacheTokenStorage
 
 
-ENV_CCG = ".ccg.env"
-
-
 class BoxConfigCCG:
     """Box client configurations."""
 
     def __init__(self) -> None:
-        dotenv.load_dotenv(ENV_CCG)
+        dotenv.load_dotenv()
         # Common configurations
         self.client_id = os.getenv("BOX_CLIENT_ID")
         self.client_secret = os.getenv("BOX_CLIENT_SECRET")
@@ -30,17 +27,13 @@ class BoxConfigCCG:
         self.cache_file = os.getenv("BOX_CACHE_FILE", ".ccg.tk")
 
 
-def __repr__(self) -> str:
-    return f"ConfigCCG({self.__dict__})"
-
-
 def get_ccg_enterprise_client(config: BoxConfigCCG) -> BoxClient:
     """Returns a box sdk Client object."""
     ccg = CCGConfig(
         client_id=config.client_id,
         client_secret=config.client_secret,
         enterprise_id=config.enterprise_id,
-        token_storage=FileWithInMemoryCacheTokenStorage(".ent" + config.cache_file),
+        token_storage=FileWithInMemoryCacheTokenStorage(".enterprise"),
     )
     auth = BoxCCGAuth(ccg)
 
@@ -55,7 +48,7 @@ def get_ccg_user_client(config: BoxConfigCCG, user_id: str) -> BoxClient:
         client_id=config.client_id,
         client_secret=config.client_secret,
         user_id=user_id,
-        token_storage=FileWithInMemoryCacheTokenStorage(".user" + config.cache_file),
+        token_storage=FileWithInMemoryCacheTokenStorage(".user"),
     )
     auth = BoxCCGAuth(ccg)
     auth.with_user_subject(user_id)
--- llama-index-integrations/readers/llama-index-readers-box/tests/test_readers_box.py
+++ llama-index-integrations/readers/llama-index-readers-box/tests/test_readers_box.py
@@ -1,6 +1,10 @@
 import os
 
-from llama_index.readers.box.box_client_ccg import BoxConfigCCG
+from llama_index.readers.box.box_client_ccg import (
+    BoxConfigCCG,
+    get_ccg_enterprise_client,
+    get_ccg_user_client,
+)
 
 
 def test_box_load_env_credentials():
@@ -10,3 +14,23 @@ def test_box_load_env_credentials():
 
     assert box_config.client_id == os.getenv("BOX_CLIENT_ID")
     assert box_config.client_secret == os.getenv("BOX_CLIENT_SECRET")
+
+
+def test_login_service_account():
+    box_config = BoxConfigCCG()
+    client = get_ccg_enterprise_client(box_config)
+
+    me = client.users.get_user_me()
+
+    assert me is not None
+    assert me.id is not None
+
+
+def test_login_user_account():
+    box_config = BoxConfigCCG()
+    client = get_ccg_user_client(box_config, box_config.ccg_user_id)
+
+    me = client.users.get_user_me()
+
+    assert me is not None
+    assert me.id is not None
