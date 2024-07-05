8ed7a6037f755d8ea8abc8807f1fbf1f1f9ef8e9
Logan Markewich <logan.markewich@live.com>
2024-07-05 12:14:41 -0600

    skip tests

--- llama-index-integrations/embeddings/llama-index-embeddings-databricks/tests/test_integration_databricks.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-databricks/tests/test_integration_databricks.py
@@ -1,12 +1,18 @@
 import os
 import pytest
 
-from llama_index.embeddings.databricks import DatabricksEmbedding
+# TODO: Pants, for whatever reason, always breaks on tests that depend on openai embeddings
+# It fails to resolve dependencies properly during testing, so this import will fail in CICD
+try:
+    from llama_index.embeddings.databricks import DatabricksEmbedding
+except ImportError:
+    DatabricksEmbedding = None
 
 
 @pytest.mark.skipif(
     "DATABRICKS_TOKEN" not in os.environ
-    or "DATABRICKS_SERVING_ENDPOINT" not in os.environ,
+    or "DATABRICKS_SERVING_ENDPOINT" not in os.environ
+    or DatabricksEmbedding is None,
     reason="DATABRICKS_TOKEN or DATABRICKS_SERVING_ENDPOINT not set in environment",
 )
 def test_completion():
