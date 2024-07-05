262f91330b1b25c2e527d2dfc38bf03bccbe4f45
Enrico Stauss <enrico.stauss@pace.de>
2024-07-05 10:09:10 +0200

    Adjust the test skipping condition.

--- llama-index-integrations/embeddings/llama-index-embeddings-databricks/tests/test_integration_databricks.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-databricks/tests/test_integration_databricks.py
@@ -5,8 +5,9 @@ from llama_index.embeddings.databricks import DatabricksEmbedding
 
 
 @pytest.mark.skipif(
-    "DATABRICKS_API_KEY" not in os.environ or "DATABRICKS_API_BASE" not in os.environ,
-    reason="DATABRICKS_API_KEY or DATABRICKS_API_BASE not set in environment",
+    "DATABRICKS_TOKEN" not in os.environ
+    or "DATABRICKS_SERVING_ENDPOINT" not in os.environ,
+    reason="DATABRICKS_TOKEN or DATABRICKS_SERVING_ENDPOINT not set in environment",
 )
 def test_completion():
     embed_model = DatabricksEmbedding(model="databricks-bge-large-en")
