05200f4cdd2ee805a84175738b301cf5031a5c8f
Rui Barbosa <barduinor@gmail.com>
2024-07-09 14:46:45 -0400

    usage examples

--- llama-index-integrations/readers/llama-index-readers-box/examples/box_reader.py
+++ llama-index-integrations/readers/llama-index-readers-box/examples/box_reader.py
@@ -49,18 +49,22 @@ def get_testing_data() -> dict:
         "test_csv_id": "1584054196674",
         "test_txt_waiver_id": "1514587167701",
         "test_folder_invoice_po_id": "261452450320",
+        "test_folder_purchase_order_id": "261457585224",
         "test_txt_invoice_id": "1517629086517",
         "test_txt_po_id": "1517628697289",
     }
 
 
-def print_docs(docs: List[Document]):
+def print_docs(label: str, docs: List[Document]):
     print("------------------------------")
-    print(f"Found {len(docs)} document(s)")
-    print("------------------------------")
+    print(f"{label}: {len(docs)} document(s)")
+
     for doc in docs:
+        print("------------------------------")
         file = File.from_dict(doc.extra_info)
-        print(f"{file.id} {file.name} ({file.size})bytes \nText:{doc.text[:100]}...")
+        print(f"File ID: {file.id}\nName: {file.name}\nSize: {file.size} bytes")
+        # print("------------------------------")
+        print(f"Text: {doc.text[:100]} ...")
     print("------------------------------\n\n\n")
 
 
@@ -69,26 +73,32 @@ def main():
     test_data = get_testing_data()
 
     reader = BoxReader(box_client=box_client)
-    docs = reader.load_data(file_ids=[test_data["test_json_id"]])
-    print_docs(docs)
+    docs = reader.load_data(file_ids=[test_data["test_txt_invoice_id"]])
+    print_docs("Box Simple Reader", docs)
 
     reader = BoxReaderTextExtraction(box_client=box_client)
     docs = reader.load_data(file_ids=[test_data["test_txt_waiver_id"]])
-    print_docs(docs)
+    print_docs("BoxReader Text Extraction", docs)
 
     reader = BoxReaderAIPrompt(box_client=box_client)
     docs = reader.load_data(
         file_ids=[test_data["test_txt_waiver_id"]], ai_prompt="summarize this document"
     )
-    print_docs(docs)
+    print_docs("Box Reader AI Prompt", docs)
 
     reader = BoxReaderAIExtract(box_client=box_client)
-    data = get_testing_data()
     docs = reader.load_data(
-        file_ids=[data["test_txt_invoice_id"]],
+        file_ids=[test_data["test_txt_invoice_id"]],
         ai_prompt='{"doc_type","date","total","vendor","invoice_number","purchase_order_number"}',
     )
-    print_docs(docs)
+    print_docs("BoxReader AI Extract", docs)
+
+    docs = reader.load_data(
+        folder_id=test_data["test_folder_purchase_order_id"],
+        ai_prompt='{"doc_type","date","total","vendor","invoice_number","purchase_order_number"}',
+        is_recursive=True,
+    )
+    print_docs("BoxReader AI Extract", docs)
 
 
 if __name__ == "__main__":
