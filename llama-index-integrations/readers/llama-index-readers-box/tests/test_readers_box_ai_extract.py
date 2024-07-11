0c9921efa6ad1e2afae3649be16013fdb52b8a63
Rui Barbosa <barduinor@gmail.com>
2024-07-11 13:52:57 -0400

    Box AI Extraction adding ResourcesReaderMixin

--- llama-index-integrations/readers/llama-index-readers-box/tests/test_readers_box_ai_extract.py
+++ llama-index-integrations/readers/llama-index-readers-box/tests/test_readers_box_ai_extract.py
@@ -67,3 +67,67 @@ def test_box_reader_ai_extract_folder(
         ai_prompt=AI_PROMPT,
     )
     assert len(docs) > 2
+
+
+def test_box_reader_list_resources(box_client_ccg_integration_testing: BoxClient):
+    test_data = get_testing_data()
+    reader = BoxReaderAIExtract(box_client=box_client_ccg_integration_testing)
+    resource_id = test_data["test_csv_id"]
+    resources = reader.list_resources(file_ids=[resource_id])
+    assert len(resources) > 0
+    assert resource_id in resources
+
+
+def test_box_reader_get_resource_info(box_client_ccg_integration_testing: BoxClient):
+    test_data = get_testing_data()
+    reader = BoxReaderAIExtract(box_client=box_client_ccg_integration_testing)
+    resource_id = test_data["test_csv_id"]
+    info = reader.get_resource_info(resource_id)
+    assert info is not None
+    assert info["id"] == resource_id
+
+
+def test_box_reader_load_resource(box_client_ccg_integration_testing: BoxClient):
+    test_data = get_testing_data()
+    reader = BoxReaderAIExtract(box_client=box_client_ccg_integration_testing)
+    resource_id = test_data["test_txt_invoice_id"]
+    docs = reader.load_resource(resource_id, ai_prompt=AI_PROMPT)
+    assert docs is not None
+    assert len(docs) == 1
+    assert docs[0].extra_info["id"] == resource_id
+    doc_0 = json.loads(docs[0].text)
+    assert doc_0["doc_type"] == "Invoice"
+    assert doc_0["total"] == "$1,050"
+    assert doc_0["vendor"] == "Quasar Innovations"
+    assert doc_0["invoice_number"] == "Q2468"
+    assert doc_0["purchase_order_number"] == "003"
+
+
+def test_box_reader_search(box_client_ccg_integration_testing: BoxClient):
+    reader = BoxReaderAIExtract(box_client=box_client_ccg_integration_testing)
+    query = "invoice"
+    resources = reader.search_resources(query=query)
+    assert len(resources) > 0
+
+
+def test_box_reader_search_by_metadata(box_client_ccg_integration_testing: BoxClient):
+    test_data = get_testing_data()
+    reader = BoxReaderAIExtract(box_client=box_client_ccg_integration_testing)
+
+    # Parameters
+    from_ = (
+        test_data["metadata_enterprise_scope"]
+        + "."
+        + test_data["metadata_template_key"]
+    )
+    ancestor_folder_id = test_data["test_folder_invoice_po_id"]
+    query = "documentType = :docType "
+    query_params = {"docType": "Invoice"}
+
+    resources = reader.search_resources_by_metadata(
+        from_=from_,
+        ancestor_folder_id=ancestor_folder_id,
+        query=query,
+        query_params=query_params,
+    )
+    assert len(resources) > 0
