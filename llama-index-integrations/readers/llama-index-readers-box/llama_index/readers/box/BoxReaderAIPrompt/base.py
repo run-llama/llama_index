425f1a60413b8da01bcfc75cf51052eb7cfff682
Rui Barbosa <barduinor@gmail.com>
2024-07-11 11:08:57 -0400

    adding box reader ai prompt doc strings comments

--- llama-index-integrations/readers/llama-index-readers-box/llama_index/readers/box/BoxReaderAIPrompt/base.py
+++ llama-index-integrations/readers/llama-index-readers-box/llama_index/readers/box/BoxReaderAIPrompt/base.py
@@ -22,6 +22,18 @@ logger = logging.getLogger(__name__)
 
 
 class BoxReaderAIPrompt(BaseReader):
+    """
+    A reader class for loading data from Box files using a custom AI prompt.
+
+    This class inherits from the `BaseReader` class and allows specifying a
+    custom AI prompt for data extraction. It utilizes the provided BoxClient object
+    to interact with the Box API and extracts data based on the prompt.
+
+    Attributes:
+        _box_client (BoxClient): An authenticated Box client object used
+            for interacting with the Box API.
+    """
+
     _box_client: BoxClient
 
     @classmethod
@@ -40,6 +52,35 @@ class BoxReaderAIPrompt(BaseReader):
         is_recursive: bool = False,
         individual_document_prompt: bool = True,
     ) -> List[Document]:
+        """
+        Extracts data from Box files using a custom AI prompt and creates Document objects.
+
+        This method utilizes a user-provided AI prompt to potentially extract
+        more specific data from the Box files compared to pre-configured AI
+        services like Box AI Extract. It then creates Document objects containing
+        the extracted data along with file metadata.
+
+        Args:
+            ai_prompt (str): The custom AI prompt that specifies what data to
+                extract from the files.
+            file_ids (Optional[List[str]], optional): A list of Box file IDs
+                to extract data from. If provided, folder_id is ignored.
+                Defaults to None.
+            folder_id (Optional[str], optional): The ID of the Box folder to
+                extract data from. If provided, along with is_recursive set to
+                True, retrieves data from sub-folders as well. Defaults to None.
+            is_recursive (bool, optional): If True and folder_id is provided,
+                extracts data from sub-folders within the specified folder.
+                Defaults to False.
+            individual_document_prompt (bool, optional): If True, applies the
+                provided AI prompt to each document individually. If False,
+                 all documents are used for context to the answer.
+                 Defaults to True.
+
+        Returns:
+            List[Document]: A list of Document objects containing the extracted
+                data and file metadata.
+        """
         # check if the box client is authenticated
         try:
             me = self._box_client.users.get_user_me()
