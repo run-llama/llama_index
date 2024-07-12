703c48abb1af1700a6bcc8087fe8f2495fb3e744
Logan Markewich <logan.markewich@live.com>
2024-07-12 14:45:34 -0600

    Revert "add service decorator"
    This reverts commit b610ac0a0d3917fb3bad6b74726c08429af0ddd3.

--- llama-index-core/llama_index/core/workflow/utils.py
+++ llama-index-core/llama_index/core/workflow/utils.py
@@ -41,9 +41,7 @@ def get_steps_from_class(_class: object) -> dict:
 def get_services_from_class(_class: object) -> dict:
     """Given a class, return the list of its methods that were defined as services."""
     services = {}
-    all_methods = inspect.getmembers(_class, predicate=inspect.ismethod)
-
-    for name, method in all_methods:
+    for method in _class.__dict__.values():
         if hasattr(method, "__is_service__"):
             services[method.__service_name__] = method
     return services
