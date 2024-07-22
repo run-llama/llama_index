2bbaa8ad7132517f81ead02dc823030c8db0cb31
TonyB <zbtsebuaa@outlook.com>
2024-07-22 13:42:41 +0800

    add test

--- llama-index-integrations/llms/llama-index-llms-azure-openai/tests/test_azure_openai.py
+++ llama-index-integrations/llms/llama-index-llms-azure-openai/tests/test_azure_openai.py
@@ -48,3 +48,22 @@ def test_custom_http_client(sync_azure_openai_mock: MagicMock) -> None:
     kwargs = sync_azure_openai_mock.call_args.kwargs
     assert "http_client" in kwargs
     assert kwargs["http_client"] == custom_http_client
+
+
+@patch("llama_index.llms.azure_openai.base.SyncAzureOpenAI")
+def test_custom_azure_ad_token_provider(sync_azure_openai_mock: MagicMock):
+    """
+    Verify that a custom azure ad token provider set for AzureOpenAI.
+    """
+    def custom_azure_ad_token_provider() -> str:
+        return "mock_api_key"
+    mock_instance = sync_azure_openai_mock.return_value
+    # Valid mocked result required to not run into another error
+    mock_instance.chat.completions.create.return_value = mock_chat_completion_v1()
+    azure_openai = AzureOpenAI(
+        engine="foo bar",
+        use_azure_ad=True,
+        azure_ad_token_provider=custom_azure_ad_token_provider,
+    )
+    azure_openai.complete("test prompt")
+    assert azure_openai.api_key == "mock_api_key"
