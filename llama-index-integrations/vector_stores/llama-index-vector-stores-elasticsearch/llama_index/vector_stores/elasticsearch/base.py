34dec27b7df959e05f9aa5a5859fb4c1d10e2b21
Jimmy Longley <jimmylongley@gmail.com>
2024-07-23 20:37:36 -0700

    Bugfix: Don't pass empty list of embeddings to elasticsearch store when using sparse strategy (#14918)
    * Bugfix: Don't pass empty list of embeddings to elasticsearch store when using sparse strategy.
    
    * bump version and add comment
    
    ---------
    
    Co-authored-by: Jimmy Longley <jimmy@booknooklearning.com>

--- llama-index-integrations/vector_stores/llama-index-vector-stores-elasticsearch/llama_index/vector_stores/elasticsearch/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-elasticsearch/llama_index/vector_stores/elasticsearch/base.py
@@ -350,6 +350,10 @@ class ElasticsearchStore(BasePydanticVectorStore):
         if not self._store.num_dimensions:
             self._store.num_dimensions = len(embeddings[0])
 
+        # Omit the vectors argument entirely if embeddings aren't generated.
+        if not any(embeddings):
+            embeddings = None
+
         return await self._store.add_texts(
             texts=texts,
             metadatas=metadatas,
