42a40c01769e15d4d64bc66bd2da35dfb2ce8edf
Logan Markewich <logan.markewich@live.com>
2024-07-25 17:26:19 -0600

    undo incompatible kwarg

--- llama-index-integrations/vector_stores/llama-index-vector-stores-elasticsearch/llama_index/vector_stores/elasticsearch/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-elasticsearch/llama_index/vector_stores/elasticsearch/base.py
@@ -216,7 +216,6 @@ class ElasticsearchStore(BasePydanticVectorStore):
         distance_strategy: Optional[DISTANCE_STRATEGIES] = "COSINE",
         retrieval_strategy: Optional[AsyncRetrievalStrategy] = None,
         metadata_mappings: Optional[Dict[str, Any]] = None,
-        custom_index_settings: Optional[Dict[str, Any]] = None,
         **kwargs,
     ) -> None:
         nest_asyncio.apply()
@@ -252,7 +251,6 @@ class ElasticsearchStore(BasePydanticVectorStore):
             text_field=text_field,
             vector_field=vector_field,
             metadata_mappings=metadata_mappings,
-            custom_index_settings=custom_index_settings,
         )
 
         super().__init__(
