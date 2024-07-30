446f804cd298ddcbeff0ae3166cd1906431c8336
Jimmy Longley <jimmy@booknooklearning.com>
2024-07-29 17:08:41 -0700

    Disable embedding generation when indexing and querying using a strategy other than dense vectors

--- llama-index-integrations/vector_stores/llama-index-vector-stores-elasticsearch/llama_index/vector_stores/elasticsearch/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-elasticsearch/llama_index/vector_stores/elasticsearch/base.py
@@ -268,12 +268,9 @@ class ElasticsearchStore(BasePydanticVectorStore):
             retrieval_strategy=retrieval_strategy,
         )
 
-        # Check if retrieval_strategy is AsyncSparseVectorStrategy
-        print(
-            "DISABLE is_embedding_query?",
-            isinstance(retrieval_strategy, AsyncSparseVectorStrategy),
-        )
-        if isinstance(retrieval_strategy, AsyncSparseVectorStrategy):
+        # Disable query embeddings when using Sparse vectors or BM25.
+        # ELSER generates its own embeddings server-side
+        if not isinstance(retrieval_strategy, AsyncDenseVectorStrategy):
             self.is_embedding_query = False
 
     @property
@@ -343,25 +340,24 @@ class ElasticsearchStore(BasePydanticVectorStore):
         if len(nodes) == 0:
             return []
 
-        embeddings: List[List[float]] = []
+        embeddings: Optional[List[List[float]]] = None
         texts: List[str] = []
         metadatas: List[dict] = []
         ids: List[str] = []
         for node in nodes:
             ids.append(node.node_id)
-            print("\n\nnode.get_embedding()", node.get_embedding())
-            embeddings.append(node.get_embedding())
             texts.append(node.get_content(metadata_mode=MetadataMode.NONE))
             metadatas.append(node_to_metadata_dict(node, remove_text=True))
 
-        if not self._store.num_dimensions:
-            self._store.num_dimensions = len(embeddings[0])
+        # Generate embeddings when using dense vectors. They are not needed
+        # for other strategies.
+        if isinstance(self.retrieval_strategy, AsyncDenseVectorStrategy):
+            embeddings = []
+            for node in nodes:
+                embeddings.append(node.get_embedding())
 
-        # Omit the vectors argument entirely if embeddings aren't generated.
-        # if not any(embeddings):
-        #     embeddings = None
-
-        embeddings = None
+            if not self._store.num_dimensions:
+                self._store.num_dimensions = len(embeddings[0])
 
         return await self._store.add_texts(
             texts=texts,
