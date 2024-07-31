6cba89adc7b36afbaa4c1665b3e22ca3777fa06a
Di Wang <jiankong3@gmail.com>
2024-07-31 15:22:43 -0700

    Update base.py (#15070)

--- llama-index-integrations/readers/llama-index-readers-google/llama_index/readers/google/maps/base.py
+++ llama-index-integrations/readers/llama-index-readers-google/llama_index/readers/google/maps/base.py
@@ -62,7 +62,7 @@ class GoogleMapsTextSearchReader(BaseReader):
                 break
             for place in places:
                 formatted_address = place["formattedAddress"]
-                rating = place["rating"]
+                average_rating = place["rating"]
                 display_name = place["displayName"]
                 if isinstance(display_name, dict):
                     display_name = display_name["text"]
@@ -85,7 +85,7 @@ class GoogleMapsTextSearchReader(BaseReader):
                 place = Place(
                     reviews=reviews,
                     address=formatted_address,
-                    average_rating=rating,
+                    average_rating=average_rating,
                     display_name=display_name,
                     number_of_ratings=number_of_ratings,
                 )
