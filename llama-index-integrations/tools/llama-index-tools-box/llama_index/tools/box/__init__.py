46d4090d6c1da18d5d9723f7248b3b96f90148c4
Rui Barbosa <barduinor@gmail.com>
2024-08-06 17:17:26 -0400

    [Tools:Box:AIPrompt] Prompt Box AI in a context of a single file

--- llama-index-integrations/tools/llama-index-tools-box/llama_index/tools/box/__init__.py
+++ llama-index-integrations/tools/llama-index-tools-box/llama_index/tools/box/__init__.py
@@ -1,6 +1,11 @@
 from llama_index.tools.box.search.base import BoxSearchToolSpec
 from llama_index.tools.box.search_by_metadata.base import BoxSearchByMetadataToolSpec
 from llama_index.tools.box.text_extract.base import BoxTextExtractToolSpec
+from llama_index.tools.box.ai_prompt.base import BoxAIPromptToolSpec
 
-
-__all__ = ["BoxSearchToolSpec", "BoxSearchByMetadataToolSpec", "BoxTextExtractToolSpec"]
+__all__ = [
+    "BoxSearchToolSpec",
+    "BoxSearchByMetadataToolSpec",
+    "BoxTextExtractToolSpec",
+    "BoxAIPromptToolSpec",
+]
---
+++ llama-index-integrations/tools/llama-index-tools-box/tests/test_tools_box_ai_prompt.py
@@ -0,0 +1,17 @@
+from box_sdk_gen import BoxClient
+
+from llama_index.tools.box import BoxAIPromptToolSpec
+
+from tests.conftest import get_testing_data
+
+
+def test_box_tool_extract(box_client_ccg_integration_testing: BoxClient):
+    test_data = get_testing_data()
+
+    box_tool = BoxAIPromptToolSpec(box_client=box_client_ccg_integration_testing)
+
+    doc = box_tool.ai_prompt(
+        file_id=test_data["test_ppt_id"], ai_prompt="Summarize the document"
+    )
+
+    assert doc.text is not None
