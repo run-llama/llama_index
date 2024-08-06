0f6c261c0efc930330b8e69c25965443942620a4
Rui Barbosa <barduinor@gmail.com>
2024-08-06 12:07:47 -0400

    [Tools:Box:Search] fix ci/cd

--- llama-index-integrations/tools/llama-index-tools-box/llama_index/tools/box/search/base.py
+++ llama-index-integrations/tools/llama-index-tools-box/llama_index/tools/box/search/base.py
@@ -1,10 +1,90 @@
+from typing import List, Optional
 from llama_index.core.schema import Document
 from llama_index.core.tools.tool_spec.base import BaseToolSpec
 
+from box_sdk_gen import (
+    BoxClient,
+    SearchForContentScope,
+    SearchForContentContentTypes,
+)
+
+from llama_index.readers.box.BoxAPI.box_api import (
+    box_check_connection,
+    search_files,
+)
+
 
 class BoxSearchToolSpec(BaseToolSpec):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    """Box search tool spec."""
 
-    def search(self, query: str) -> Document:
-        pass
+    _box_client: BoxClient
+
+    def __init__(self, box_client: BoxClient) -> None:
+        self._box_client = box_client
+
+    def search(
+        self,
+        query: Optional[str] = None,
+        scope: Optional[SearchForContentScope] = None,
+        file_extensions: Optional[List[str]] = None,
+        created_at_range: Optional[List[str]] = None,
+        updated_at_range: Optional[List[str]] = None,
+        size_range: Optional[List[int]] = None,
+        owner_user_ids: Optional[List[str]] = None,
+        recent_updater_user_ids: Optional[List[str]] = None,
+        ancestor_folder_ids: Optional[List[str]] = None,
+        content_types: Optional[List[SearchForContentContentTypes]] = None,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Document]:
+        """
+        Searches for Box resources based on specified criteria and returns a list of their IDs.
+
+        This method utilizes the Box API search functionality to find resources
+        matching the provided parameters. It then returns a list containing the IDs
+        of the found resources.
+
+        Args:
+            query (Optional[str], optional): A search query string. Defaults to None.
+            scope (Optional[SearchForContentScope], optional): The scope of the search.
+                Defaults to None.
+            file_extensions (Optional[List[str]], optional): A list of file extensions
+                to filter by. Defaults to None.
+            created_at_range (Optional[List[str]], optional): A list representing a date
+                range for file creation time. Defaults to None.
+            updated_at_range (Optional[List[str]], optional): A list representing a date
+                range for file update time. Defaults to None.
+            size_range (Optional[List[int]], optional): A list representing a size range
+                for files. Defaults to None.
+            owner_user_ids (Optional[List[str]], optional): A list of user IDs to filter
+                by owner. Defaults to None.
+            recent_updater_user_ids (Optional[List[str]], optional): A list of user IDs to
+                filter by recent updater. Defaults to None.
+            ancestor_folder_ids (Optional[List[str]], optional): A list of folder IDs to
+                search within. Defaults to None.
+            content_types (Optional[List[SearchForContentContentTypes]], optional): A list
+                of content types to filter by. Defaults to None.
+            limit (Optional[int], optional): The maximum number of results to return.
+                Defaults to None.
+            offset (Optional[int], optional): The number of results to skip before
+                starting to collect. Defaults to None.
+
+        Returns:
+            List[Document]: A list of documents representing the found resources.
+        """
+        box_check_connection(self._box_client)
+        box_files = search_files(
+            box_client=self._box_client,
+            query=query,
+            scope=scope,
+            file_extensions=file_extensions,
+            created_at_range=created_at_range,
+            updated_at_range=updated_at_range,
+            size_range=size_range,
+            owner_user_ids=owner_user_ids,
+            recent_updater_user_ids=recent_updater_user_ids,
+            ancestor_folder_ids=ancestor_folder_ids,
+            content_types=content_types,
+            limit=limit,
+            offset=offset,
+        )
