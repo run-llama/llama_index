78c3659fb52040ad64cdf77e7bcd61c08867911e
Andrei Fajardo <andrei@nerdai.io>
2024-08-07 11:49:56 -0400

    update to v2

--- llama-index-core/llama_index/core/bridge/pydantic.py
+++ llama-index-core/llama_index/core/bridge/pydantic.py
@@ -1,39 +1,20 @@
-try:
-    import pydantic.v1 as pydantic
-    from pydantic.v1 import (
-        BaseConfig,
-        BaseModel,
-        Field,
-        PrivateAttr,
-        StrictFloat,
-        StrictInt,
-        StrictStr,
-        create_model,
-        root_validator,
-        validator,
-        parse_obj_as,
-    )
-    from pydantic.v1.error_wrappers import ValidationError
-    from pydantic.v1.fields import FieldInfo
-    from pydantic.v1.generics import GenericModel
-except ImportError:
-    import pydantic  # type: ignore
-    from pydantic import (
-        BaseConfig,
-        BaseModel,
-        Field,
-        PrivateAttr,
-        StrictFloat,
-        StrictInt,
-        StrictStr,
-        create_model,
-        root_validator,
-        validator,
-        parse_obj_as,
-    )
-    from pydantic.error_wrappers import ValidationError
-    from pydantic.fields import FieldInfo
-    from pydantic.generics import GenericModel
+import pydantic
+from pydantic import (
+    BaseConfig,
+    BaseModel,
+    Field,
+    PrivateAttr,
+    StrictFloat,
+    StrictInt,
+    StrictStr,
+    create_model,
+    root_validator,
+    validator,
+    parse_obj_as,
+)
+from pydantic.error_wrappers import ValidationError
+from pydantic.fields import FieldInfo
+from pydantic.generics import GenericModel
 
 __all__ = [
     "pydantic",
