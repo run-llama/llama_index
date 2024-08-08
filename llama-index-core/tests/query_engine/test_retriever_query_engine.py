35fd183300b068856bb98dc91c5182e5ae44f345
Massimiliano Pippi <mpippi@gmail.com>
2024-08-08 19:42:53 +0200

    Remove deprecated `ServiceContext` from the `query_engine` package (#15218)
    * remove dep from query_engine
    
    * no need to use openai

--- llama-index-core/tests/query_engine/test_retriever_query_engine.py
+++ llama-index-core/tests/query_engine/test_retriever_query_engine.py
@@ -1,43 +1,20 @@
-import pytest
-from llama_index.core import (
-    Document,
-    ServiceContext,
-    TreeIndex,
-)
-from llama_index.core.indices.tree.select_leaf_retriever import (
-    TreeSelectLeafRetriever,
-)
-from llama_index.core.query_engine.retriever_query_engine import (
-    RetrieverQueryEngine,
-)
-
-try:
-    from llama_index.llms.openai import OpenAI  # pants: no-infer-dep
-except ImportError:
-    OpenAI = None  # type: ignore
+from llama_index.core import Document, TreeIndex
+from llama_index.core.indices.tree.select_leaf_retriever import TreeSelectLeafRetriever
+from llama_index.core.query_engine.retriever_query_engine import RetrieverQueryEngine
+from llama_index.core import Settings
 
 
-@pytest.mark.skipif(OpenAI is None, reason="llama-index-llms-openai not installed")
-def test_query_engine_falls_back_to_inheriting_retrievers_service_context() -> None:
+def test_query_engine_falls_back_to_inheriting_retrievers_service_context(
+    monkeypatch, mock_llm
+) -> None:
     documents = [Document(text="Hi")]
-    gpt35turbo_predictor = OpenAI(
-        temperature=0,
-        model_name="gpt-3.5-turbo-0613",
-        streaming=True,
-        openai_api_key="test-test-test",
-    )
-    gpt35_sc = ServiceContext.from_defaults(
-        llm=gpt35turbo_predictor,
-        chunk_size=512,
-    )
+    monkeypatch.setattr(Settings, "llm", mock_llm)
 
-    gpt35_tree_index = TreeIndex.from_documents(documents, service_context=gpt35_sc)
+    gpt35_tree_index = TreeIndex.from_documents(documents)
     retriever = TreeSelectLeafRetriever(index=gpt35_tree_index, child_branch_factor=2)
     query_engine = RetrieverQueryEngine(retriever=retriever)
 
-    assert (
-        retriever._llm.metadata.model_name == gpt35turbo_predictor.metadata.model_name
-    )
+    assert retriever._llm.class_name() == "MockLLM"
     assert (
         query_engine._response_synthesizer._llm.metadata.model_name
         == retriever._llm.metadata.model_name
