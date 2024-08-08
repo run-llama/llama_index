2d0a4a69144102f2203f0a2aaf9135ca029b1b1b
Massimiliano Pippi <mpippi@gmail.com>
2024-08-08 15:15:02 +0200

    remove dep from tools

--- llama-index-core/tests/tools/test_base.py
+++ llama-index-core/tests/tools/test_base.py
@@ -1,4 +1,5 @@
 """Test tools."""
+
 import json
 from typing import List, Optional
 
@@ -150,11 +151,7 @@ async def test_function_tool_async_defaults_langchain() -> None:
     assert result == "1"
 
 
-from llama_index.core import (
-    ServiceContext,
-    VectorStoreIndex,
-)
-from llama_index.core.embeddings.mock_embed_model import MockEmbedding
+from llama_index.core import VectorStoreIndex
 from llama_index.core.schema import Document
 from llama_index.core.tools import RetrieverTool, ToolMetadata
 
@@ -169,12 +166,7 @@ def test_retreiver_tool() -> None:
         text=("# title2:This is another test.\n" "This is a test v2."),
         metadata={"file_path": "/data/personal/essay.md"},
     )
-    service_context = ServiceContext.from_defaults(
-        llm=None, embed_model=MockEmbedding(embed_dim=1)
-    )
-    vs_index = VectorStoreIndex.from_documents(
-        [doc1, doc2], service_context=service_context
-    )
+    vs_index = VectorStoreIndex.from_documents([doc1, doc2])
     vs_retriever = vs_index.as_retriever()
     vs_ret_tool = RetrieverTool(
         retriever=vs_retriever,
