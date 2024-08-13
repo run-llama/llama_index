627d32aef1834f3bc0c5c99637869934a9c71ee7
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2024-08-13 12:19:24 -0400

    15275 feature request make all events accessible like mappings (#15310)
    * add DictLikeEvent
    
    * remove test event
    
    * cr
    
    * cr

--- llama-index-core/llama_index/core/workflow/events.py
+++ llama-index-core/llama_index/core/workflow/events.py
@@ -4,34 +4,87 @@ from llama_index.core.bridge.pydantic import BaseModel, Field, PrivateAttr
 
 
 class Event(BaseModel):
-    """Base class for event types."""
+    """Base class for event types that mimics dict interface.
+
+    PrivateAttr:
+        _data (Dict[str, Any]): Underlying Python dict.
+
+    Examples:
+        Basic example usage
+        ```python
+        from llama_index.core.workflows.events import Event
+
+        evt = Event(a=1, b=2)
+
+        # can use dot access to get values of `a` and `b`
+        print((evt.a, evt.b))
+
+        # can also set the attrs
+        evt.a = 2
+        ```
+
+        Custom event with additional Fields/PrivateAttr
+        ```python
+        from llama_index.core.workflows.events import Event
+        from llama_index.core.bridge.pydantic import Field, PrivateAttr
+
+        class CustomEvent(Event):
+            field_1: int = Field(description="my custom field")
+            _private_attr_1: int = PrivateAttr()
+
+        evt = CustomEvent(a=1, b=2, field_1=3, _private_attr_1=4)
+
+        # `field_1` and `_private_attr_1` get set as they do with Pydantic BaseModel
+        print(evt.field_1)
+        print(evt._private_attr_1)
+
+        # `a` and `b` get set in the underliying dict, namely `evt._data`
+        print((evt.a, evt.b))
+        ```
+    """
+
+    _data: Dict[str, Any] = PrivateAttr(default_factory=dict)
 
     class Config:
         arbitrary_types_allowed = True
 
+    def __init__(self, **params: Any):
+        """__init__.
 
-class StartEvent(Event):
-    """StartEvent is implicitly sent when a workflow runs. Mimics the interface of a dict."""
-
-    _data: Dict[str, Any] = PrivateAttr(default_factory=dict)
-
-    def __init__(self, **data: Any):
-        super().__init__()
+        NOTE: fields and private_attrs are pulled from params by name.
+        """
+        # extract and set fields, private attrs and remaining shove in _data
+        fields = {}
+        private_attrs = {}
+        data = {}
+        for k, v in params.items():
+            if k in self.__fields__:
+                fields[k] = v
+            elif k in self.__private_attributes__:
+                private_attrs[k] = v
+            else:
+                data[k] = v
+        super().__init__(**fields)
+        for private_attr, value in private_attrs.items():
+            super().__setattr__(private_attr, value)
         self._data = data
 
     def __getattr__(self, __name: str) -> Any:
-        if __name in self._data:
-            return self._data[__name]
+        if __name in self.__private_attributes__ or __name in self.__fields__:
+            return super().__getattr__(__name)
         else:
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{__name}'"
-            )
+            try:
+                return self._data[__name]
+            except KeyError:
+                raise AttributeError(
+                    f"'{self.__class__.__name__}' object has no attribute '{__name}'"
+                )
 
-    def __setattr__(self, __name: str, value: Any) -> None:
-        if __name in self._data:
-            self._data[__name] = value
+    def __setattr__(self, name, value) -> None:
+        if name in self.__private_attributes__ or name in self.__fields__:
+            super().__setattr__(name, value)
         else:
-            super().__setattr__(__name, value)
+            self._data.__setitem__(name, value)
 
     def __getitem__(self, key: str) -> Any:
         return self._data[key]
@@ -62,6 +115,10 @@ class StartEvent(Event):
 
     def dict(self, *args: Any, **kwargs: Any) -> Dict[str, Any]:
         return self._data
+
+
+class StartEvent(Event):
+    """StartEvent is implicitly sent when a workflow runs."""
 
 
 class StopEvent(Event):
