874fb570a1e25afd79c030cb7620f2667218700d
Logan Markewich <logan.markewich@live.com>
2024-08-19 12:35:59 -0600

    check remote reader urls

--- llama-index-integrations/readers/llama-index-readers-remote/llama_index/readers/remote/base.py
+++ llama-index-integrations/readers/llama-index-readers-remote/llama_index/readers/remote/base.py
@@ -53,6 +53,23 @@ class RemoteReader(BaseReader):
         from urllib.parse import urlparse
         from urllib.request import Request, urlopen
 
+        # check the URL
+        parsed_url = urlparse(url)
+
+        # Check if the scheme is http or https
+        if parsed_url.scheme not in (
+            "http",
+            "https",
+            "ftp",
+            "ws",
+            "wss",
+            "sftp",
+            "ftps",
+        ):
+            raise ValueError(
+                "Invalid URL scheme. Only http, https, ftp, ftps, sftp, ws, and wss are allowed."
+            )
+
         extra_info = {"Source": url}
 
         req = Request(url, headers={"User-Agent": "Magic Browser"})
