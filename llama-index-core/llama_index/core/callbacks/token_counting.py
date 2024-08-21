e0b2942d0a675231fe90764b99d4226474b7aeff
Logan Markewich <logan.markewich@live.com>
2024-08-21 10:54:04 -0600

    dont assume raw response is a dict

--- llama-index-core/llama_index/core/callbacks/token_counting.py
+++ llama-index-core/llama_index/core/callbacks/token_counting.py
@@ -42,7 +42,7 @@ def get_tokens_from_response(
     """Get the token counts from a raw response."""
     raw_response = response.raw
     if not isinstance(raw_response, dict):
-        response.model_dump()
+        raw_response = dict(raw_response)
 
     usage = raw_response.get("usage", {})
     if usage is None:
