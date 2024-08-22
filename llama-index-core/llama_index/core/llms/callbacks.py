455fc5582fbd2b323bbb431f950e117280e189ab
guodong <songoodong@163.com>
2024-08-22 10:24:18 +0800

    Update callbacks.py
    fix with async and sync api

--- llama-index-core/llama_index/core/llms/callbacks.py
+++ llama-index-core/llama_index/core/llms/callbacks.py
@@ -52,7 +52,9 @@ def llm_chat_callback() -> Callable:
         async def wrapped_async_llm_chat(
             _self: Any, messages: Sequence[ChatMessage], **kwargs: Any
         ) -> Any:
-            with wrapper_logic(_self) as callback_manager:
+            with wrapper_logic(_self) as callback_manager, callback_manager.as_trace(
+                "chat"
+            ):
                 span_id = active_span_id.get()
                 model_dict = _self.to_dict()
                 model_dict.pop("api_key", None)
@@ -409,7 +411,9 @@ def llm_completion_callback() -> Callable:
 
         def wrapped_llm_predict(_self: Any, *args: Any, **kwargs: Any) -> Any:
             prompt = extract_prompt(*args, **kwargs)
-            with wrapper_logic(_self) as callback_manager:
+            with wrapper_logic(_self) as callback_manager, callback_manager.as_trace(
+                "completion"
+            ):
                 span_id = active_span_id.get()
                 model_dict = _self.to_dict()
                 model_dict.pop("api_key", None)
