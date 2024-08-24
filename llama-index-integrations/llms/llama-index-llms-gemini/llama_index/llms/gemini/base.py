4a2cbece83246ad47bd390aa09ee4eaa4d611712
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2024-08-24 10:33:03 -0600

    [fix] Gemini LLM pydantic v2 issue (#15620)
    fix pydantic v2 issue

--- llama-index-integrations/llms/llama-index-llms-gemini/llama_index/llms/gemini/base.py
+++ llama-index-integrations/llms/llama-index-llms-gemini/llama_index/llms/gemini/base.py
@@ -140,7 +140,7 @@ class Gemini(CustomLLM):
 
         model_meta = genai.get_model(model)
 
-        model = genai.GenerativeModel(
+        genai_model = genai.GenerativeModel(
             model_name=model,
             generation_config=final_gen_config,
             safety_settings=safety_settings,
@@ -167,7 +167,7 @@ class Gemini(CustomLLM):
         )
 
         self._model_meta = model_meta
-        self._model = model
+        self._model = genai_model
 
     @classmethod
     def class_name(cls) -> str:
