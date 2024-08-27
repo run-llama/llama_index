b12d1c35f4c0580746f9b1522e3156067224345b
Francisco Oliveira <francisco@scottytechnologies.com>
2024-08-27 15:09:10 +0100

    now test is skipped if no project id is set.

--- llama-index-integrations/llms/llama-index-llms-anthropic/tests/test_llms_anthropic.py
+++ llama-index-integrations/llms/llama-index-llms-anthropic/tests/test_llms_anthropic.py
@@ -1,6 +1,7 @@
 from llama_index.core.base.llms.base import BaseLLM
 from llama_index.llms.anthropic import Anthropic
 import os
+import pytest
 
 
 def test_text_inference_embedding_class():
@@ -8,16 +9,17 @@ def test_text_inference_embedding_class():
     assert BaseLLM.__name__ in names_of_base_classes
 
 
+@pytest.mark.skipif(
+    os.getenv("ANTHROPIC_PROJECT_ID") is None,
+    reason="Project ID not available to test Vertex AI integration",
+)
 def test_anthropic_through_vertex_ai():
-    if os.getenv("ANTHROPIC_PROJECT_ID"):
-        anthropic_llm = Anthropic(
-            model=os.getenv("ANTHROPIC_MODEL", "claude-3-5-sonnet@20240620"),
-            region=os.getenv("ANTHROPIC_REGION", "europe-west1"),
-            project_id=os.getenv("ANTHROPIC_PROJECT_ID"),
-        )
+    anthropic_llm = Anthropic(
+        model=os.getenv("ANTHROPIC_MODEL", "claude-3-5-sonnet@20240620"),
+        region=os.getenv("ANTHROPIC_REGION", "europe-west1"),
+        project_id=os.getenv("ANTHROPIC_PROJECT_ID"),
+    )
 
-        completion_response = anthropic_llm.complete(
-            "Give me a recipe for banana bread"
-        )
+    completion_response = anthropic_llm.complete("Give me a recipe for banana bread")
 
-        assert isinstance(completion_response.text, str)
+    assert isinstance(completion_response.text, str)
