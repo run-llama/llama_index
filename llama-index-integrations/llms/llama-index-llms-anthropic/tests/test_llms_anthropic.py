981338a8cde396e5d19d2413f826034d52260d48
Francisco Oliveira <francisco@scottytechnologies.com>
2024-08-27 12:14:33 +0100

    adding tests, notebook changes, and dependency changes

--- llama-index-integrations/llms/llama-index-llms-anthropic/tests/test_llms_anthropic.py
+++ llama-index-integrations/llms/llama-index-llms-anthropic/tests/test_llms_anthropic.py
@@ -1,7 +1,20 @@
 from llama_index.core.base.llms.base import BaseLLM
 from llama_index.llms.anthropic import Anthropic
+import os
 
 
 def test_text_inference_embedding_class():
     names_of_base_classes = [b.__name__ for b in Anthropic.__mro__]
     assert BaseLLM.__name__ in names_of_base_classes
+
+
+def test_anthropic_through_vertex_ai():
+    anthropic_llm = Anthropic(
+        model=os.getenv("ANTHROPIC_MODEL", "claude-3-5-sonnet@20240620"),
+        region=os.getenv("ANTHROPIC_REGION", "europe-west1"),
+        project_id=os.getenv("ANTHROPIC_PROJECT_ID"),
+    )
+
+    completion_response = anthropic_llm.complete("Give me a recipe for banana bread")
+
+    assert isinstance(completion_response.text, str)
