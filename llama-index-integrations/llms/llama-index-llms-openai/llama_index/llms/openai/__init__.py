501454a19b58b000fd8a9bc72bba087b32cec506
Tomisin Jenrola <13075885+ToJen@users.noreply.github.com>
2024-09-19 19:11:04 -0230

    fix: `resolve_tool_choice` import error

--- llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/__init__.py
+++ llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/__init__.py
@@ -1,3 +1,4 @@
 from llama_index.llms.openai.base import AsyncOpenAI, OpenAI, SyncOpenAI, Tokenizer
+from llama_index.llms.openai.utils import resolve_tool_choice
 
-__all__ = ["OpenAI", "Tokenizer", "SyncOpenAI", "AsyncOpenAI"]
+__all__ = ["OpenAI", "Tokenizer", "SyncOpenAI", "AsyncOpenAI", "resolve_tool_choice"]
