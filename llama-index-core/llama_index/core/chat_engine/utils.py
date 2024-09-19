5c485033d299318f5d5e79dbe6b2bab40cbcad2a
Logan <logan.markewich@live.com>
2024-09-19 13:02:20 -0600

    Use response synthesizer in context chat engines (#16017)

--- llama-index-core/llama_index/core/chat_engine/utils.py
+++ llama-index-core/llama_index/core/chat_engine/utils.py
@@ -1,3 +1,4 @@
+from typing import List
 from llama_index.core.base.llms.types import (
     ChatMessage,
     ChatResponse,
@@ -5,7 +6,43 @@ from llama_index.core.base.llms.types import (
     ChatResponseAsyncGen,
     MessageRole,
 )
+from llama_index.core.llms.llm import LLM
+from llama_index.core.prompts import ChatPromptTemplate
+from llama_index.core.response_synthesizers import CompactAndRefine
+from llama_index.core.callbacks import CallbackManager
 from llama_index.core.types import TokenGen, TokenAsyncGen
+
+
+def get_prefix_messages_with_context(
+    context_template: str,
+    system_prompt: str,
+    prefix_messages: List[ChatMessage],
+    chat_history: List[ChatMessage],
+    llm_metadata_system_role: MessageRole,
+) -> List[ChatMessage]:
+    context_str_w_sys_prompt = context_template + system_prompt.strip()
+    return [
+        ChatMessage(content=context_str_w_sys_prompt, role=llm_metadata_system_role),
+        *prefix_messages,
+        *chat_history,
+        ChatMessage(content="{query_str}", role=MessageRole.USER),
+    ]
+
+
+def get_response_synthesizer(
+    llm: LLM,
+    callback_manager: CallbackManager,
+    qa_messages: List[ChatMessage],
+    refine_messages: List[ChatMessage],
+    streaming: bool = False,
+) -> CompactAndRefine:
+    return CompactAndRefine(
+        llm=llm,
+        callback_manager=callback_manager,
+        text_qa_template=ChatPromptTemplate.from_messages(qa_messages),
+        refine_template=ChatPromptTemplate.from_messages(refine_messages),
+        streaming=streaming,
+    )
 
 
 def response_gen_from_query_engine(response_gen: TokenGen) -> ChatResponseGen:
