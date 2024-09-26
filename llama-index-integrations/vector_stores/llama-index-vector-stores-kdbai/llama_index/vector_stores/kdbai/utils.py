2693db3361a1ebd8177143975cc0168aedfbe1b6
Alexandros Giannakoulopoulos <agiannakoulopoulos@kx.com>
2024-09-26 13:02:13 +0100

    KDBAI v1.4 Updates

--- llama-index-integrations/vector_stores/llama-index-vector-stores-kdbai/llama_index/vector_stores/kdbai/utils.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-kdbai/llama_index/vector_stores/kdbai/utils.py
@@ -23,34 +23,17 @@ def default_sparse_encoder(text: str) -> Dict[int, int]:
 
 
 # MATCH THE METADATA COLUMN DATA TYPE TO ITS PYTYPE
-def convert_metadata_col_v2(column_name, column_type, column_value):
+def convert_metadata_col(column, value):
     try:
-        if column_type == "s":
-            return str(column_value)
-        elif column_type == "C":
-            return column_value.encode("utf-8")
-        elif column_type == "p":
-            return pd.to_datetime(column_value)
-        elif column_type == "n":
-            return pd.to_timedelta(column_value)
-        return column_value.astype(column_type)
-    except Exception as e:
-        logger.error(
-            f"Failed to convert column {column_name} to qtype {column_type}: {e}"
-        )
-
-
-def convert_metadata_col_v1(column, value):
-    try:
-        if column["pytype"] == "str":
+        if column["type"] == "str":
             return str(value)
-        elif column["pytype"] == "bytes":
+        elif column["type"] == "bytes":
             return value.encode("utf-8")
-        elif column["pytype"] == "datetime64[ns]":
+        elif column["type"] == "datetime64[ns]":
             return pd.to_datetime(value)
-        elif column["pytype"] == "timedelta64[ns]":
+        elif column["type"] == "timedelta64[ns]":
             return pd.to_timedelta(value)
-        return value.astype(column["pytype"])
+        return value.astype(column["type"])
     except Exception as e:
         logger.error(
             f"Failed to convert column {column['name']} to type {column['pytype']}: {e}"
