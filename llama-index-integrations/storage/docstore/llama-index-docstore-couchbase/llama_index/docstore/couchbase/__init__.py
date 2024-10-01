377a01d4a51378687f9029138699b179b4470955
Nithish Raghunandanan <12782505+nithishr@users.noreply.github.com>
2024-10-01 19:10:41 +0200

    Add the doc store

---
+++ llama-index-integrations/storage/docstore/llama-index-docstore-couchbase/llama_index/docstore/couchbase/__init__.py
@@ -0,0 +1,4 @@
+from llama_index.docstore.couchbase.base import CouchbaseDocumentStore
+
+
+__all__ = ["CouchbaseDocumentStore"]
---
+++ llama-index-integrations/storage/docstore/llama-index-docstore-couchbase/llama_index/docstore/couchbase/base.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from llama_index.core.storage.docstore.keyval_docstore import KVDocStore
+from llama_index.storage.kvstore.couchbase import CouchbaseKVStore
+from llama_index.core.storage.docstore.types import DEFAULT_BATCH_SIZE
+
+
+class CouchbaseDocumentStore(KVDocStore):
+    """Couchbase Document (Node) store.
+    A documents store for Document and Node objects using Couchbase.
+    """
+
+    def __init__(
+        self,
+        couchbase_kvstore: CouchbaseKVStore,
+        namespace: Optional[str] = None,
+        batch_size: int = DEFAULT_BATCH_SIZE,
+        node_collection_suffix: Optional[str] = None,
+        ref_doc_collection_suffix: Optional[str] = None,
+        metadata_collection_suffix: Optional[str] = None,
+    ) -> None:
+        """Init a CouchbaseDocumentStore."""
+        super().__init__(
+            couchbase_kvstore,
+            namespace=namespace,
+            batch_size=batch_size,
+            node_collection_suffix=node_collection_suffix,
+            ref_doc_collection_suffix=ref_doc_collection_suffix,
+            metadata_collection_suffix=metadata_collection_suffix,
+        )
