c7b67e73591a7baa396d7811ef623bd24fd29521
George He <georgewho96@gmail.com>
2024-10-03 15:40:02 -0700

    Fix OpenAIMultiModal: Refactor OpenAI update_tool_calls (#16309)

--- llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
+++ llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
@@ -440,3 +440,50 @@ def resolve_tool_choice(tool_choice: Union[str, dict] = "auto") -> Union[str, di
         return {"type": "function", "function": {"name": tool_choice}}
 
     return tool_choice
+
+
+def update_tool_calls(
+    tool_calls: List[ChoiceDeltaToolCall],
+    tool_calls_delta: Optional[List[ChoiceDeltaToolCall]],
+) -> List[ChoiceDeltaToolCall]:
+    """
+    Use the tool_calls_delta objects received from openai stream chunks
+    to update the running tool_calls object.
+
+    Args:
+        tool_calls (List[ChoiceDeltaToolCall]): the list of tool calls
+        tool_calls_delta (ChoiceDeltaToolCall): the delta to update tool_calls
+
+    Returns:
+        List[ChoiceDeltaToolCall]: the updated tool calls
+    """
+    # openai provides chunks consisting of tool_call deltas one tool at a time
+    if tool_calls_delta is None:
+        return tool_calls
+
+    tc_delta = tool_calls_delta[0]
+
+    if len(tool_calls) == 0:
+        tool_calls.append(tc_delta)
+    else:
+        # we need to either update latest tool_call or start a
+        # new tool_call (i.e., multiple tools in this turn) and
+        # accumulate that new tool_call with future delta chunks
+        t = tool_calls[-1]
+        if t.index != tc_delta.index:
+            # the start of a new tool call, so append to our running tool_calls list
+            tool_calls.append(tc_delta)
+        else:
+            # not the start of a new tool call, so update last item of tool_calls
+
+            # validations to get passed by mypy
+            assert t.function is not None
+            assert tc_delta.function is not None
+            assert t.function.arguments is not None
+            assert t.function.name is not None
+            assert t.id is not None
+
+            t.function.arguments += tc_delta.function.arguments or ""
+            t.function.name += tc_delta.function.name or ""
+            t.id += tc_delta.id or ""
+    return tool_calls
