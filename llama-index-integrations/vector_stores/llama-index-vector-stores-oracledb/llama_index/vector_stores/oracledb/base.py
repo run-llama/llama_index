f5095a5707c9c0cdfe4f2a7d52a61d23a76a0b3e
Alexander Falaleev <107076685+mckinseyalexanderfalaleev@users.noreply.github.com>
2024-10-04 21:37:52 +0100

    Fix delete method for Oracledb vector store

--- llama-index-integrations/vector_stores/llama-index-vector-stores-oracledb/llama_index/vector_stores/oracledb/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-oracledb/llama_index/vector_stores/oracledb/base.py
@@ -536,7 +536,7 @@ class OraLlamaVS(BasePydanticVectorStore):
     @_handle_exceptions
     def delete(self, doc_id: str, **kwargs: Any) -> None:
         with self._client.cursor() as cursor:
-            ddl = f"DELETE FROM {self.table_name} WHERE id = :doc_id"
+            ddl = f"DELETE FROM {self.table_name} WHERE doc_id = :doc_id"
             cursor.execute(ddl, [doc_id])
             self._client.commit()
 
