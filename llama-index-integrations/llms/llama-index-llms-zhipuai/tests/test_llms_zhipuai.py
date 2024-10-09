2fd72f5f206cf9d66692bf2ee136078c73ca046d
Guodong <songoodong@163.com>
2024-10-09 22:53:25 +0800

    Update test_llms_zhipuai.py

--- llama-index-integrations/llms/llama-index-llms-zhipuai/tests/test_llms_zhipuai.py
+++ llama-index-integrations/llms/llama-index-llms-zhipuai/tests/test_llms_zhipuai.py
@@ -1,3 +1,5 @@
+import os
+import pytest
 from llama_index.core.base.llms.base import BaseLLM
 from llama_index.llms.zhipuai import ZhipuAI
 
@@ -5,3 +7,24 @@ from llama_index.llms.zhipuai import ZhipuAI
 def test_text_inference_embedding_class():
     names_of_base_classes = [b.__name__ for b in ZhipuAI.__mro__]
     assert BaseLLM.__name__ in names_of_base_classes
+
+
+def test_zhipuai_llm_model_alias():
+    model = "glm-test"
+    api_key = "api_key_test"
+    llm = ZhipuAI(model=model, api_key=api_key)
+    assert llm.model == model
+    assert llm._model_kwargs is not None
+
+
+@pytest.mark.skipif(
+    os.environ.get("ZHIPUAI_API_KEY") is None, reason="ZHIPUAI_API_KEY not set"
+)
+def test_completion():
+    model = "glm-4"
+    api_key = os.environ.get("ZHIPUAI_API_KEY")
+    llm = ZhipuAI(model=model, api_key=api_key)
+    assert llm.complete("who are you")
+    
+    
+    
