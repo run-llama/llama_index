c46b92e1d544cc1a53cba4e00351302c07b8fc07
Gal Shubeli <gal@falkordb.com>
2024-10-10 13:46:36 +0300

    fix-unittest

--- llama-index-integrations/graph_stores/llama-index-graph-stores-falkordb/tests/test_graph_stores_falkordb.py
+++ llama-index-integrations/graph_stores/llama-index-graph-stores-falkordb/tests/test_graph_stores_falkordb.py
@@ -1,9 +1,10 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from llama_index.core.graph_stores.types import GraphStore
 from llama_index.graph_stores.falkordb.base import FalkorDBGraphStore
 
 
 @patch("llama_index.graph_stores.falkordb.base.FalkorDBGraphStore")
-def test_falkordb_class(mock_db: FalkorDBGraphStore):
-    assert isinstance(mock_db, GraphStore)
+def test_falkordb_class(MockFalkorDBGraphStore: MagicMock):
+    instance: FalkorDBGraphStore = MockFalkorDBGraphStore.return_value()
+    assert isinstance(instance, GraphStore)
