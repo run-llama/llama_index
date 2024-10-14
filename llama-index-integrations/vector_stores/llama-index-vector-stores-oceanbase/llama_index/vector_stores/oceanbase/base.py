2c0663dc1c2779db77ca595ceffcc9c51925f360
shanhaikang.shk <shanhaikang.shk@oceanbase.com>
2024-10-14 20:30:29 +0800

    make lint
    Signed-off-by: shanhaikang.shk <shanhaikang.shk@oceanbase.com>

--- llama-index-integrations/vector_stores/llama-index-vector-stores-oceanbase/llama_index/vector_stores/oceanbase/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-oceanbase/llama_index/vector_stores/oceanbase/base.py
@@ -1,9 +1,11 @@
 """OceanBase Vector Store."""
 
+import math
 import logging
 import json
 from typing import Any, Optional, List
 
+from llama_index.core.bridge.pydantic import PrivateAttr
 from llama_index.core.utils import iter_batch
 from llama_index.core.schema import BaseNode, MetadataMode
 from llama_index.core.vector_stores.types import (
@@ -20,6 +22,7 @@ from llama_index.core.vector_stores.utils import (
 from sqlalchemy import Column, Table, String, JSON, text, func
 from sqlalchemy.dialects.mysql import LONGTEXT
 from pyobvector import ObVecClient, VECTOR
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -95,6 +98,21 @@ def _to_oceanbase_filter(metadata_filters: Optional[MetadataFilters] = None) -> 
     return f" {metadata_filters.condition.value} ".join(filters)
 
 
+def _euclidean_similarity(distance: float) -> float:
+    return 1.0 - distance / math.sqrt(2)
+
+
+def _neg_inner_product_similarity(distance: float) -> float:
+    return -distance
+
+
+def _normalize(vector: List[float]) -> List[float]:
+    arr = np.array(vector)
+    norm = np.linalg.norm(arr)
+    arr = arr / norm
+    return arr.tolist()
+
+
 class OceanBaseVectorStore(BasePydanticVectorStore):
     """OceanBase Vector Store.
 
@@ -103,15 +121,15 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
     See the following documentation for how to deploy OceanBase:
     https://github.com/oceanbase/oceanbase-doc/blob/V4.3.1/en-US/400.deploy/500.deploy-oceanbase-database-community-edition/100.deployment-overview.md
 
-    IF USING L2/IP metric, IT IS HIGHLY SUGGESTED TO NORMALIZE YOUR DATA.
+    IF USING L2/INNER_PRODUCT metric, IT IS HIGHLY SUGGESTED TO set `normalize = True`.
 
     Args:
-        client (ObVecClient): OceanBase vector store client.
+        _client (ObVecClient): OceanBase vector store client.
             Refer to `pyobvector` for more information.
         dim (int): Dimension of embedding vector.
         table_name (str): Which table name to use. Defaults to "llama_vector".
         vidx_metric_type (str): Metric method of distance between vectors.
-            This parameter takes values in `l2` and `ip`. Defaults to `l2`.
+            This parameter takes values in `l2` and `inner_product`. Defaults to `l2`.
         vidx_algo_params (Optional[dict]): Which index params to use. Now OceanBase
             supports HNSW only. Refer to `DEFAULT_OCEANBASE_HNSW_BUILD_PARAM`
             for example.
@@ -129,6 +147,7 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
             documentation for more examples.
         extra_columns (Optional[List[Column]]): Extra sqlalchemy columns
             to add to the table.
+        normalize (bool): normalize vector or not.
 
     Examples:
         `pip install llama-index-vector-stores-oceanbase`
@@ -156,6 +175,22 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
 
     stores_text: bool = True
 
+    _client: ObVecClient = PrivateAttr()
+    _dim: int = PrivateAttr()
+    _table_name: str = PrivateAttr()
+    _vidx_metric_type: str = PrivateAttr()
+    _vidx_algo_params: dict = PrivateAttr()
+    _primary_field: str = PrivateAttr()
+    _doc_id_field: str = PrivateAttr()
+    _vector_field: str = PrivateAttr()
+    _text_field: str = PrivateAttr()
+    _metadata_field: str = PrivateAttr()
+    _vidx_name: str = PrivateAttr()
+    _partitions: Optional[Any] = PrivateAttr()
+    _extra_columns: Optional[List[Column]] = PrivateAttr()
+    _hnsw_ef_search: int = PrivateAttr()
+    _normalize: bool = PrivateAttr()
+
     def __init__(
         self,
         client: ObVecClient,
@@ -173,6 +208,7 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
         vidx_name: str = DEFAULT_OCEANBASE_VEC_INDEX_NAME,
         partitions: Optional[Any] = None,
         extra_columns: Optional[List[Column]] = None,
+        normalize: bool = False,
         **kwargs,
     ):
         super().__init__()
@@ -182,7 +218,7 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
         except ImportError:
             raise ImportError(
                 "Could not import pyobvector package. "
-                "Please install it with `pip install pyobvector=0.1.2`."
+                "Please install it with `pip install pyobvector`."
             )
 
         if client is not None:
@@ -191,87 +227,90 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
         else:
             raise ValueError("client not specified")
 
-        self.dim = dim
+        self._dim = dim
         self._client: ObVecClient = client
-        self.table_name = table_name
-        self.extra_columns = extra_columns
-        self.vidx_metric_type = vidx_metric_type.lower()
-        if self.vidx_metric_type not in ("l2", "ip"):
-            raise ValueError("`vidx_metric_type` should be set in `l2`/`ip`.")
-        self.vidx_algo_params = vidx_algo_params or DEFAULT_OCEANBASE_HNSW_BUILD_PARAM
+        self._table_name = table_name
+        self._extra_columns = extra_columns
+        self._vidx_metric_type = vidx_metric_type.lower()
+        if self._vidx_metric_type not in ("l2", "inner_product"):
+            raise ValueError(
+                "`vidx_metric_type` should be set in `l2`/`inner_product`."
+            )
+        self._vidx_algo_params = vidx_algo_params or DEFAULT_OCEANBASE_HNSW_BUILD_PARAM
 
-        self.primary_field = primary_field
-        self.doc_id_field = doc_id_field
-        self.vector_field = vector_field
-        self.text_field = text_field
-        self.metadata_field = metadata_field
-        self.vidx_name = vidx_name
-        self.partition = partitions
-        self.hnsw_ef_search = -1
+        self._primary_field = primary_field
+        self._doc_id_field = doc_id_field
+        self._vector_field = vector_field
+        self._text_field = text_field
+        self._metadata_field = metadata_field
+        self._vidx_name = vidx_name
+        self._partition = partitions
+        self._hnsw_ef_search = -1
+        self._normalize = normalize
 
         if drop_old:
-            self._client.drop_table_if_exist(table_name=self.table_name)
+            self._client.drop_table_if_exist(table_name=self._table_name)
 
         self._create_table_with_index()
 
     def _parse_metric_type_str_to_dist_func(self) -> Any:
-        if self.vidx_metric_type == "l2":
+        if self._vidx_metric_type == "l2":
             return func.l2_distance
-        if self.vidx_metric_type == "cosine":
+        if self._vidx_metric_type == "cosine":
             return func.cosine_distance
-        if self.vidx_metric_type == "ip":
-            return func.inner_product
-        raise ValueError(f"Invalid vector index metric type: {self.vidx_metric_type}")
+        if self._vidx_metric_type == "inner_product":
+            return func.negative_inner_product
+        raise ValueError(f"Invalid vector index metric type: {self._vidx_metric_type}")
 
     def _load_table(self) -> None:
         table = Table(
-            self.table_name,
+            self._table_name,
             self._client.metadata_obj,
             autoload_with=self._client.engine,
         )
         column_names = [column.name for column in table.columns]
-        optional_len = len(self.extra_columns or [])
+        optional_len = len(self._extra_columns or [])
         assert len(column_names) == (5 + optional_len)
 
         logging.info(f"load exist table with {column_names} columns")
-        self.primary_field = column_names[0]
-        self.doc_id_field = column_names[1]
-        self.vector_field = column_names[2]
-        self.text_field = column_names[3]
-        self.metadata_field = column_names[4]
+        self._primary_field = column_names[0]
+        self._doc_id_field = column_names[1]
+        self._vector_field = column_names[2]
+        self._text_field = column_names[3]
+        self._metadata_field = column_names[4]
 
     def _create_table_with_index(self):
-        if self._client.check_table_exists(self.table_name):
+        if self._client.check_table_exists(self._table_name):
             self._load_table()
             return
 
         cols = [
             Column(
-                self.primary_field, String(4096), primary_key=True, autoincrement=False
+                self._primary_field, String(4096), primary_key=True, autoincrement=False
             ),
-            Column(self.doc_id_field, String(4096)),
-            Column(self.vector_field, VECTOR(self.dim)),
-            Column(self.text_field, LONGTEXT),
-            Column(self.metadata_field, JSON),
+            Column(self._doc_id_field, String(4096)),
+            Column(self._vector_field, VECTOR(self._dim)),
+            Column(self._text_field, LONGTEXT),
+            Column(self._metadata_field, JSON),
         ]
-        if self.extra_columns is not None:
-            cols.extend(self.extra_columns)
+        if self._extra_columns is not None:
+            cols.extend(self._extra_columns)
 
         vidx_params = self._client.prepare_index_params()
         vidx_params.add_index(
-            field_name=self.vector_field,
+            field_name=self._vector_field,
             index_type=OCEANBASE_SUPPORTED_VECTOR_INDEX_TYPE,
-            index_name=self.vidx_name,
-            metric_type=self.vidx_metric_type,
-            params=self.vidx_algo_params,
+            index_name=self._vidx_name,
+            metric_type=self._vidx_metric_type,
+            params=self._vidx_algo_params,
         )
 
         self._client.create_table_with_index_params(
-            table_name=self.table_name,
+            table_name=self._table_name,
             columns=cols,
             indexes=None,
             vidxs=vidx_params,
-            partitions=self.partition,
+            partitions=self._partition,
         )
 
     @classmethod
@@ -282,6 +321,45 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
     def client(self) -> Any:
         """Get client."""
         return self._client
+
+    def get_nodes(
+        self,
+        node_ids: Optional[List[str]] = None,
+        filters: Optional[MetadataFilters] = None,
+    ) -> List[BaseNode]:
+        """Get nodes from OceanBase.
+
+        Args:
+            node_ids (Optional[List[str]], optional): IDs of nodes to delete.
+                Defaults to None.
+            filters (Optional[MetadataFilters], optional): Metadata filters.
+                Defaults to None.
+
+        Returns:
+            List[BaseNode]: List of text nodes.
+        """
+        if filters is not None:
+            filter = _to_oceanbase_filter(filters)
+        else:
+            filter = None
+
+        res = self._client.get(
+            table_name=self._table_name,
+            ids=node_ids,
+            where_clause=[text(filter)] if filter is not None else None,
+            output_column_name=[
+                self._text_field,
+                self._metadata_field,
+            ],
+        )
+
+        return [
+            metadata_dict_to_node(
+                metadata=(json.loads(r[1]) if not isinstance(r[1], dict) else r[1]),
+                text=r[0],
+            )
+            for r in res.fetchall()
+        ]
 
     def add(
         self,
@@ -310,17 +388,21 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
 
         data = [
             {
-                self.primary_field: node.id_,
-                self.doc_id_field: node.ref_doc_id or None,
-                self.vector_field: node.get_embedding(),
-                self.text_field: node.get_content(metadata_mode=MetadataMode.NONE),
-                self.metadata_field: node_to_metadata_dict(node, remove_text=True),
+                self._primary_field: node.id_,
+                self._doc_id_field: node.ref_doc_id or None,
+                self._vector_field: (
+                    node.get_embedding()
+                    if not self._normalize
+                    else _normalize(node.get_embedding())
+                ),
+                self._text_field: node.get_content(metadata_mode=MetadataMode.NONE),
+                self._metadata_field: node_to_metadata_dict(node, remove_text=True),
                 **extra,
             }
             for node, extra in zip(nodes, extra_data)
         ]
         for data_batch in iter_batch(data, batch_size):
-            self._client.insert(self.table_name, data_batch)
+            self._client.insert(self._table_name, data_batch)
         return [node.id_ for node in nodes]
 
     def delete(self, ref_doc_id: str, **delete_kwargs: Any) -> None:
@@ -330,8 +412,8 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
             ref_doc_id (str): The doc_id of the document to delete.
         """
         self._client.delete(
-            table_name=self.table_name,
-            where_clause=[text(f"{self.doc_id_field}={ref_doc_id}")],
+            table_name=self._table_name,
+            where_clause=[text(f"{self._doc_id_field}='{ref_doc_id}'")],
         )
 
     def delete_nodes(
@@ -354,14 +436,21 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
             filter = None
 
         self._client.delete(
-            table_name=self.table_name,
-            ids=node_ids if node_ids is not None else None,
+            table_name=self._table_name,
+            ids=node_ids,
             where_clause=[text(filter)] if filter is not None else None,
         )
 
     def clear(self) -> None:
         """Clears table."""
-        self._client.perform_raw_text_sql(text(f"TRUNCATE TABLE {self.table_name}"))
+        self._client.perform_raw_text_sql(f"TRUNCATE TABLE {self._table_name}")
+
+    def _parse_distance_to_similarities(self, distance: float) -> float:
+        if self._vidx_metric_type == "l2":
+            return _euclidean_similarity(distance)
+        elif self._vidx_metric_type == "inner_product":
+            return _neg_inner_product_similarity(distance)
+        raise ValueError(f"Metric Type {self._vidx_metric_type} is not supported")
 
     def query(
         self, query: VectorStoreQuery, param: Optional[dict] = None, **kwargs: Any
@@ -380,23 +469,32 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
         ef_search = search_param.get(
             "efSearch", DEFAULT_OCEANBASE_HNSW_SEARCH_PARAM["efSearch"]
         )
-        if ef_search != self.hnsw_ef_search:
+        if ef_search != self._hnsw_ef_search:
             self._client.set_ob_hnsw_ef_search(ef_search)
-            self.hnsw_ef_search = ef_search
+            self._hnsw_ef_search = ef_search
+
+        if query.filters:
+            qfilters = _to_oceanbase_filter(query.filters)
+        else:
+            qfilters = None
 
         res = self._client.ann_search(
-            table_name=self.table_name,
-            vec_data=query.query_embedding,
-            vec_column_name=self.vector_field,
+            table_name=self._table_name,
+            vec_data=(
+                query.query_embedding
+                if not self._normalize
+                else _normalize(query.query_embedding)
+            ),
+            vec_column_name=self._vector_field,
             distance_func=self._parse_metric_type_str_to_dist_func(),
             with_dist=True,
             output_column_names=[
-                self.primary_field,
-                self.text_field,
-                self.metadata_field,
+                self._primary_field,
+                self._text_field,
+                self._metadata_field,
             ],
             topk=query.similarity_top_k,
-            where_clause=text(_to_oceanbase_filter(query.filters)),
+            where_clause=([text(qfilters)] if qfilters else None),
         )
 
         records = []
@@ -410,6 +508,6 @@ class OceanBaseVectorStore(BasePydanticVectorStore):
                 )
                 for r in records
             ],
-            similarities=[r[3] for r in records],
+            similarities=[self._parse_distance_to_similarities(r[3]) for r in records],
             ids=[r[0] for r in records],
         )
