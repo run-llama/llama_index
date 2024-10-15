3f9343f558522454f73bcb23a03adb1a33f7858b
Guodong <songoodong@163.com>
2024-10-15 20:29:09 +0800

    update zhipuai llm and fix issues (#16513)
    * fix bug
    
    * fix decorate
    
    * update class name
    
    * update import
    
    * restore class name

--- llama-index-integrations/llms/llama-index-llms-zhipuai/tests/test_llms_zhipuai.py
+++ llama-index-integrations/llms/llama-index-llms-zhipuai/tests/test_llms_zhipuai.py
@@ -4,7 +4,7 @@ from llama_index.core.base.llms.base import BaseLLM
 from llama_index.llms.zhipuai import ZhipuAI
 
 
-def test_text_inference_embedding_class():
+def test_llm_class():
     names_of_base_classes = [b.__name__ for b in ZhipuAI.__mro__]
     assert BaseLLM.__name__ in names_of_base_classes
 
@@ -26,10 +26,21 @@ def test_zhipuai_llm_metadata():
 
 
 @pytest.mark.skipif(
-    os.environ.get("ZHIPUAI_API_KEY") is None, reason="ZHIPUAI_API_KEY not set"
+    os.getenv("ZHIPUAI_API_KEY") is None, reason="ZHIPUAI_API_KEY not set"
 )
 def test_completion():
     model = "glm-4"
-    api_key = os.environ.get("ZHIPUAI_API_KEY")
+    api_key = os.getenv("ZHIPUAI_API_KEY")
     llm = ZhipuAI(model=model, api_key=api_key)
     assert llm.complete("who are you")
+
+
+@pytest.mark.asyncio()
+@pytest.mark.skipif(
+    os.getenv("ZHIPUAI_API_KEY") is None, reason="ZHIPUAI_API_KEY not set"
+)
+async def test_async_completion():
+    model = "glm-4"
+    api_key = os.getenv("ZHIPUAI_API_KEY")
+    llm = ZhipuAI(model=model, api_key=api_key)
+    assert await llm.acomplete("who are you")
