ec50cbefa5bf7cde369564940d2b1852f3a9d38d
Enrico Stauss <enrico.stauss@pace.de>
2024-10-18 15:28:37 +0200

    test: Add unit tests for dense/sparse/hybrid retrieval for {query, aquery}
    # Conflicts:
    #	llama-index-integrations/vector_stores/llama-index-vector-stores-qdrant/tests/test_vector_stores_qdrant.py

--- llama-index-integrations/vector_stores/llama-index-vector-stores-qdrant/tests/conftest.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-qdrant/tests/conftest.py
@@ -36,3 +36,42 @@ async def vector_store() -> QdrantVectorStore:
     await vector_store.async_add(nodes)
 
     return vector_store
+
+
+@pytest_asyncio.fixture
+async def hybrid_vector_store() -> QdrantVectorStore:
+    client = qdrant_client.QdrantClient(":memory:")
+    aclient = qdrant_client.AsyncQdrantClient(":memory:")
+    vector_store = QdrantVectorStore(
+        "test", client=client, aclient=aclient, enable_hybrid=True
+    )
+
+    nodes = [
+        TextNode(
+            text="test1",
+            id_="11111111-1111-1111-1111-111111111111",
+            embedding=[1.0, 0.0],
+            sparse_embedding=([3, 41], [0.3, 0.41]),
+            relationships={NodeRelationship.SOURCE: RelatedNodeInfo(node_id="test-0")},
+        ),
+        TextNode(
+            text="test2",
+            id_="22222222-2222-2222-2222-222222222222",
+            embedding=[0.0, 1.0],
+            sparse_embedding=([2, 16], [0.2, 0.16]),
+            relationships={NodeRelationship.SOURCE: RelatedNodeInfo(node_id="test-0")},
+        ),
+        TextNode(
+            text="test3",
+            id_="33333333-3333-3333-3333-333333333333",
+            embedding=[1.0, 1.0],
+            sparse_embedding=([7], [0.7]),
+        ),
+    ]
+
+    vector_store.add(nodes)
+
+    # in-memory client does not share data between instances
+    await vector_store.async_add(nodes)
+
+    return vector_store
