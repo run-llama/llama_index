35b57e5eac6c802e5ea944326b4d81699932323b
Gal Shubeli <gal@falkordb.com>
2024-10-27 10:02:32 +0200

    update-pg

--- llama-index-integrations/graph_stores/llama-index-graph-stores-falkordb/tests/test_pg_stores_falkordb.py
+++ llama-index-integrations/graph_stores/llama-index-graph-stores-falkordb/tests/test_pg_stores_falkordb.py
@@ -1,14 +1,17 @@
 import os
+import time
 import unittest
+import docker
 
 from llama_index.graph_stores.falkordb import FalkorDBPropertyGraphStore
 from llama_index.core.graph_stores.types import Relation, EntityNode
 from llama_index.core.schema import TextNode
 
-falkordb_url = os.environ.get("FALKORDB_TEST_URL")
+# Set up FalkorDB URL
+falkordb_url = os.getenv("FALKORDB_TEST_URL", "redis://localhost:6379")
 
-# Check availability of FalkorDB
-falkordb_available = falkordb_url is not None
+# Set up Docker client
+docker_client = docker.from_env()
 
 
 @unittest.skipIf(falkordb_url is None, "No FalkorDB URL provided")
@@ -16,8 +19,24 @@ class TestFalkorDBPropertyGraphStore(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Setup method called once for the entire test class."""
+        # Start FalkorDB container
+        cls.container = docker_client.containers.run(
+            "falkordb/falkordb:latest",
+            detach=True,
+            name="falkordb_test_instance",
+            ports={"6379/tcp": 6379},
+        )
+        time.sleep(2)  # Allow time for the container to initialize
+
+        # Set up the FalkorDB store and clear database
         cls.pg_store = FalkorDBPropertyGraphStore(url=falkordb_url)
         cls.pg_store.structured_query("MATCH (n) DETACH DELETE n")  # Clear the database
+
+    @classmethod
+    def tearDownClass(cls):
+        """Teardown method called once after all tests are done."""
+        cls.container.stop()
+        cls.container.remove()
 
     def test_upsert_triplet(self):
         # Create two entity nodes
@@ -35,6 +54,7 @@ class TestFalkorDBPropertyGraphStore(unittest.TestCase):
         self.pg_store.upsert_nodes([entity1, entity2])
         self.pg_store.upsert_relations([relation])
 
+        # Test nodes
         source_node = TextNode(text="Logan (age 28), works for LlamaIndex since 2023.")
         relations = [
             Relation(
@@ -52,6 +72,7 @@ class TestFalkorDBPropertyGraphStore(unittest.TestCase):
         self.pg_store.upsert_llama_nodes([source_node])
         self.pg_store.upsert_relations(relations)
 
+        # Retrieve nodes and assert
         kg_nodes = self.pg_store.get(ids=[entity1.id])
         self.assertEqual(len(kg_nodes), 1)
         self.assertEqual(kg_nodes[0].label, "PERSON")
@@ -73,7 +94,7 @@ class TestFalkorDBPropertyGraphStore(unittest.TestCase):
         result = self.pg_store.structured_query(query)
         self.assertEqual(len(result), 2)
 
-        # Get the original text node back
+        # Verify original text node
         llama_nodes = self.pg_store.get_llama_nodes([source_node.node_id])
         self.assertEqual(len(llama_nodes), 1)
         self.assertEqual(llama_nodes[0].text, source_node.text)
@@ -89,11 +110,8 @@ class TestFalkorDBPropertyGraphStore(unittest.TestCase):
         self.assertEqual(kg_nodes[0].name, "Logan")
         self.assertEqual(kg_nodes[0].properties["location"], "Canada")
 
-        # Deleting
-        # Delete our entities
+        # Deleting nodes
         self.pg_store.delete(ids=[entity1.id, entity2.id])
-
-        # Delete our text nodes
         self.pg_store.delete(ids=[source_node.node_id])
 
         nodes = self.pg_store.get(ids=[entity1.id, entity2.id])
@@ -102,6 +120,7 @@ class TestFalkorDBPropertyGraphStore(unittest.TestCase):
         text_nodes = self.pg_store.get_llama_nodes([source_node.node_id])
         self.assertEqual(len(text_nodes), 0)
 
+        # Switching graph and refreshing schema
         self.pg_store.switch_graph("new_graph")
         self.pg_store.refresh_schema()
 
