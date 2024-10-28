387ceca637f928545db86655e24b0802279389c8
yimao <yimao@anyid.me>
2024-10-28 19:12:50 +0800

    add async test method

--- llama-index-integrations/llms/llama-index-llms-dashscope/tests/test_dashscope.py
+++ llama-index-integrations/llms/llama-index-llms-dashscope/tests/test_dashscope.py
@@ -46,3 +46,25 @@ def test_dashscope_chat(dashscope_llm, mocker):
     response = dashscope_llm.chat(messages=[ChatMessage.from_str("hi, there!?")])
     assert isinstance(response, ChatResponse)
     assert response.message.content == "hi, there!"
+
+
+@pytest.mark.asyncio()
+async def test_dashscope_acomplete(dashscope_llm, mocker):
+    mocker.patch(
+        "llama_index.llms.dashscope.base.acall_with_messages",
+        return_value=mock_response,
+    )
+    response = await dashscope_llm.acomplete("hi, there!?")
+    assert isinstance(response, CompletionResponse)
+    assert response.text == "hi, there!"
+
+
+@pytest.mark.asyncio()
+async def test_dashscope_achat(dashscope_llm, mocker):
+    mocker.patch(
+        "llama_index.llms.dashscope.base.acall_with_messages",
+        return_value=mock_response,
+    )
+    response = await dashscope_llm.achat(messages=[ChatMessage.from_str("hi, there!?")])
+    assert isinstance(response, ChatResponse)
+    assert response.message.content == "hi, there!"
