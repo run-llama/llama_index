18034167f8df7364958e4f0a732a6ca59b56a5e6
yimao <yimao@anyid.me>
2024-10-29 10:20:34 +0800

    update test method with the fixture definition

--- llama-index-integrations/llms/llama-index-llms-dashscope/tests/test_dashscope.py
+++ llama-index-integrations/llms/llama-index-llms-dashscope/tests/test_dashscope.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from llama_index.core.base.llms.types import (
@@ -13,36 +15,45 @@ def dashscope_llm():
     return DashScope(api_key="test")
 
 
-mock_response = {
-    "status_code": 200,
-    "request_id": "4438deec-2d21-9b9c-b405-a47459fd8f75",
-    "code": "",
-    "message": "",
-    "output": {
-        "choices": [
-            {
-                "finish_reason": "stop",
-                "message": {"role": "assistant", "content": "hi, there!"},
-            }
-        ]
-    },
-    "usage": {"total_tokens": 161, "output_tokens": 91, "input_tokens": 70},
-}
+@pytest.fixture()
+def dashscope_api_response():
+    return {
+        "status_code": 200,
+        "request_id": "4438deec-2d21-9b9c-b405-a47459fd8f75",
+        "code": "",
+        "message": "",
+        "output": {
+            "choices": [
+                {
+                    "finish_reason": "stop",
+                    "message": {"role": "assistant", "content": "hi, there!"},
+                }
+            ]
+        },
+        "usage": {"total_tokens": 161, "output_tokens": 91, "input_tokens": 70},
+    }
 
 
-def test_dashscope_complete(dashscope_llm, mocker):
-    mocker.patch(
-        "llama_index.llms.dashscope.base.call_with_messages", return_value=mock_response
-    )
-    response = dashscope_llm.complete("hi, there!?")
+@pytest.fixture()
+def prompt() -> str:
+    return "hi, there!"
+
+
+@patch("llama_index.llms.dashscope.base.call_with_messages")
+def test_dashscope_complete(
+    mock_call_with_messages, dashscope_llm, dashscope_api_response, prompt
+):
+    mock_call_with_messages.return_value = dashscope_api_response
+    response = dashscope_llm.complete(prompt)
     assert isinstance(response, CompletionResponse)
     assert response.text == "hi, there!"
 
 
-def test_dashscope_chat(dashscope_llm, mocker):
-    mocker.patch(
-        "llama_index.llms.dashscope.base.call_with_messages", return_value=mock_response
-    )
-    response = dashscope_llm.chat(messages=[ChatMessage.from_str("hi, there!?")])
+@patch("llama_index.llms.dashscope.base.call_with_messages")
+def test_dashscope_chat(
+    mock_call_with_messages, dashscope_llm, dashscope_api_response, prompt
+):
+    mock_call_with_messages.return_value = dashscope_api_response
+    response = dashscope_llm.chat(messages=[ChatMessage.from_str(prompt)])
     assert isinstance(response, ChatResponse)
     assert response.message.content == "hi, there!"
