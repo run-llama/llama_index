c0de1b8ba6d83262a772decf6b47115d15e1f3c2
George He <georgewho96@gmail.com>
2024-11-01 17:00:26 -0700

    Update chroma dep

--- llama-index-integrations/vector_stores/llama-index-vector-stores-chroma/llama_index/vector_stores/chroma/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-chroma/llama_index/vector_stores/chroma/base.py
@@ -332,7 +332,7 @@ class ChromaVectorStore(BasePydanticVectorStore):
         if filters:
             where = _to_chroma_filter(filters)
         else:
-            where = {}
+            where = None
 
         self._collection.delete(ids=node_ids, where=where)
 
