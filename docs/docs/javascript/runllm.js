b57d8ce744716872ee4bd3b818ed45e8e4906ca8
Laurie Voss <github@seldo.com>
2024-11-13 13:39:48 -0800

    Cmd-J because cmd-K conflicted with algolia

--- docs/docs/javascript/runllm.js
+++ docs/docs/javascript/runllm.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("runllm-server-address", "https://api.runllm.com");
     script.setAttribute("runllm-assistant-id", "209");
     script.setAttribute("runllm-position", "BOTTOM_RIGHT");
-    script.setAttribute("runllm-keyboard-shortcut", "Mod+k");
+    script.setAttribute("runllm-keyboard-shortcut", "Mod+j");
     script.setAttribute(
       "runllm-slack-community-url",
       "https://discord.com/invite/eN6D2HQ4aX"
