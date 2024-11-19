e0dea0f736db63916118271523f75e7bf8b2b73c
Andrei Fajardo <andrei@nerdai.io>
2024-11-19 13:07:34 -0500

    initial design of WorkflowProfile and auxiliary classes

---
+++ llama-index-core/llama_index/core/workflow/profiler.py
@@ -0,0 +1,35 @@
+import uuid
+from llama_index.core.bridge.pydantic import BaseModel, Field
+from .workflow import Workflow
+from .handler import WorkflowHandler
+from .context import Context
+from typing import Any, List, Optional
+
+
+class Checkpoint(BaseModel):
+    id_: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    prev_id: Optional[str] = Field(
+        description="Id of previous checkpoint. None if there is no prev checkpoint."
+    )
+    run_id: str = Field(description="The id of the run this checkpoint is a part of.")
+
+
+class WorkflowProfiler:
+    def __init__(self, workflow: Workflow):
+        self.workflow = workflow
+        self.checkpoints = List[Checkpoint] = []
+
+    def _load_checkpoint(checkpoint: Checkpoint) -> Context:
+        ...
+
+    def run(self) -> WorkflowHandler:
+        ...
+
+    def run_from(self, checkpoint: Checkpoint) -> WorkflowHandler:
+        ...
+
+    def get_run_result(self, run_id: str) -> Any:
+        ...
+
+    def get_run(self, run_id: str) -> List[Checkpoint]:
+        ...
