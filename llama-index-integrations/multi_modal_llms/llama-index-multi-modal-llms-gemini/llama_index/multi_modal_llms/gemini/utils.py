4f83fa2c28e94e28b92487e1532374d380276419
michaeleisele <michael.eisele@codify.ch>
2024-11-19 09:05:53 +0100

    feat[react-multimodal]: fix format and linting

--- llama-index-integrations/multi_modal_llms/llama-index-multi-modal-llms-gemini/llama_index/multi_modal_llms/gemini/utils.py
+++ llama-index-integrations/multi_modal_llms/llama-index-multi-modal-llms-gemini/llama_index/multi_modal_llms/gemini/utils.py
@@ -3,11 +3,12 @@ from typing import Optional, Sequence, Any
 from llama_index.core.multi_modal_llms.base import ChatMessage
 from llama_index.core.schema import ImageDocument
 
+
 def generate_gemini_multi_modal_chat_message(
-        prompt: str,
-        role: str,
-        image_documents: Optional[Sequence[ImageDocument]] = None,
-        **kwargs: Any,
+    prompt: str,
+    role: str,
+    image_documents: Optional[Sequence[ImageDocument]] = None,
+    **kwargs: Any,
 ) -> ChatMessage:
     # if image_documents is empty, return text only chat message
     if image_documents is None or len(image_documents) == 0:
