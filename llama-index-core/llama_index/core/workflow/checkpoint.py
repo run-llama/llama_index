4c2518b0159462862ccd3dbb83fb8c29fd292d62
Andrei Fajardo <andrei@nerdai.io>
2024-11-21 13:15:09 -0500

    fix broken tests part 1

--- llama-index-core/llama_index/core/workflow/checkpoint.py
+++ llama-index-core/llama_index/core/workflow/checkpoint.py
@@ -1,12 +1,17 @@
 import uuid
-from llama_index.core.bridge.pydantic import BaseModel, Field
-from typing import Optional, Dict
+from llama_index.core.bridge.pydantic import (
+    BaseModel,
+    Field,
+    ConfigDict,
+)
+from typing import Optional, Dict, Any
 from .events import Event
 
 
 class Checkpoint(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
     id_: str = Field(default_factory=lambda: str(uuid.uuid4()))
     last_completed_step: Optional[str]
     input_event: Optional[Event]
-    output_event: Event
-    ctx_state: Dict
+    output_event: Any  # since we allow for bogus step return types
+    ctx_state: Dict[str, Any]
