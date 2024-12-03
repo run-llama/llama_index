3ee6954968ba84372c782c3a216d7c2b7a108e27
SuperSecureHuman <supersecurehuman@gmail.com>
2024-12-03 19:37:02 +0530

    lint

--- llama-index-integrations/postprocessor/llama-index-postprocessor-bedrock-rerank/llama_index/postprocessor/bedrock_rerank/base.py
+++ llama-index-integrations/postprocessor/llama-index-postprocessor-bedrock-rerank/llama_index/postprocessor/bedrock_rerank/base.py
@@ -16,8 +16,7 @@ dispatcher = get_dispatcher(__name__)
 class AWSBedrockRerank(BaseNodePostprocessor):
     top_n: int = Field(description="Top N nodes to return.")
     model_id: str = Field(description="AWS Bedrock model ID.")
-    region_name: str = Field(
-        description="AWS region name for the Bedrock service.")
+    region_name: str = Field(description="AWS region name for the Bedrock service.")
 
     _client: Any = PrivateAttr()
     _model_package_arn: str = PrivateAttr()
@@ -29,14 +28,14 @@ class AWSBedrockRerank(BaseNodePostprocessor):
         region_name: str = "us-west-2",
         **kwargs: Any,
     ):
-        super().__init__(top_n=top_n,
-                         model_id=model_id,
-                         region_name=region_name)
+        super().__init__(top_n=top_n, model_id=model_id, region_name=region_name)
         try:
             import boto3
+
             self.region_name = region_name or boto3.Session().region_name
-            self._client = boto3.client("bedrock-agent-runtime",
-                                        region_name=self.region_name)
+            self._client = boto3.client(
+                "bedrock-agent-runtime", region_name=self.region_name
+            )
             self._model_package_arn = (
                 f"arn:aws:bedrock:{self.region_name}::foundation-model/{self.model_id}"
             )
@@ -58,7 +57,8 @@ class AWSBedrockRerank(BaseNodePostprocessor):
                 nodes=nodes,
                 top_n=self.top_n,
                 model_name=self.model_id,
-            ))
+            )
+        )
 
         if query_bundle is None:
             raise ValueError("Missing query bundle in extra info.")
@@ -67,13 +67,13 @@ class AWSBedrockRerank(BaseNodePostprocessor):
             return []
 
         with self.callback_manager.event(
-                CBEventType.RERANKING,
-                payload={
-                    EventPayload.NODES: nodes,
-                    EventPayload.MODEL_NAME: self.model_id,
-                    EventPayload.QUERY_STR: query_bundle.query_str,
-                    EventPayload.TOP_K: self.top_n,
-                },
+            CBEventType.RERANKING,
+            payload={
+                EventPayload.NODES: nodes,
+                EventPayload.MODEL_NAME: self.model_id,
+                EventPayload.QUERY_STR: query_bundle.query_str,
+                EventPayload.TOP_K: self.top_n,
+            },
         ) as event:
             texts = [
                 node.node.get_content(metadata_mode=MetadataMode.EMBED)
@@ -83,22 +83,22 @@ class AWSBedrockRerank(BaseNodePostprocessor):
             # Prepare the text sources for AWS Bedrock
             text_sources = []
             for text in texts:
-                text_sources.append({
-                    "type": "INLINE",
-                    "inlineDocumentSource": {
-                        "type": "TEXT",
-                        "textDocument": {
-                            "text": text
+                text_sources.append(
+                    {
+                        "type": "INLINE",
+                        "inlineDocumentSource": {
+                            "type": "TEXT",
+                            "textDocument": {"text": text},
                         },
-                    },
-                })
+                    }
+                )
 
-            queries = [{
-                "type": "TEXT",
-                "textQuery": {
-                    "text": query_bundle.query_str
-                },
-            }]
+            queries = [
+                {
+                    "type": "TEXT",
+                    "textQuery": {"text": query_bundle.query_str},
+                }
+            ]
 
             rerankingConfiguration = {
                 "type": "BEDROCK_RERANKING_MODEL",
@@ -106,15 +106,16 @@ class AWSBedrockRerank(BaseNodePostprocessor):
                     "numberOfResults": self.top_n,
                     "modelConfiguration": {
                         "modelArn": self._model_package_arn,
-                    }
-                }
+                    },
+                },
             }
 
             try:
                 response = self._client.rerank(
                     queries=queries,
                     sources=text_sources,
-                    rerankingConfiguration=rerankingConfiguration)
+                    rerankingConfiguration=rerankingConfiguration,
+                )
 
                 results = response["results"]
             except Exception as e:
