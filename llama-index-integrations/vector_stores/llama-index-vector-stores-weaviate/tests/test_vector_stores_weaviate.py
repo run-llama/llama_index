f6c121e4c166e4c9910bbd0b2469ea4a87b2d44a
Andre Wisplinghoff <andre.wisplinghoff@gmail.com>
2024-12-06 16:52:23 +0100

    Add test_async_basic_flow()

--- llama-index-integrations/vector_stores/llama-index-vector-stores-weaviate/tests/test_vector_stores_weaviate.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-weaviate/tests/test_vector_stores_weaviate.py
@@ -1,3 +1,4 @@
+import asyncio
 import weaviate.classes as wvc
 import weaviate
 import pytest
@@ -259,3 +260,45 @@ def test_to_weaviate_filter_with_nested_filters():
     assert or_filters[1].target == "c"
     assert or_filters[1].operator == "GreaterThan"
     assert or_filters[1].value == 3
+
+
+@pytest.fixture(scope="module")
+async def async_client():
+    client = weaviate.use_async_with_embedded()
+    asyncio.run(async_client.connect())
+    yield client
+    asyncio.run(client.close())
+
+
+# TODO do we need a fixture like this?
+# @pytest.fixture(scope="module")
+# def async_vector_store(async_client):
+#     return vector_store
+
+
+@pytest.mark.asyncio()
+async def test_async_basic_flow(async_client):
+    async_vector_store = WeaviateVectorStore(weaviate_client=async_client)
+
+    nodes = [
+        TextNode(text="Hello world.", embedding=[0.0, 0.0, 0.3]),
+        TextNode(text="This is a test.", embedding=[0.3, 0.0, 0.0]),
+    ]
+
+    await async_vector_store.async_add(nodes)
+
+    query = VectorStoreQuery(
+        query_embedding=[0.3, 0.0, 0.0],
+        similarity_top_k=2,
+        query_str="world",
+        mode=VectorStoreQueryMode.DEFAULT,
+    )
+
+    results = await async_vector_store.aquery(query)
+
+    assert len(results.nodes) == 2
+    assert results.nodes[0].text == "This is a test."
+    assert results.similarities[0] == 1.0
+
+    assert results.similarities[0] > results.similarities[1]
+
