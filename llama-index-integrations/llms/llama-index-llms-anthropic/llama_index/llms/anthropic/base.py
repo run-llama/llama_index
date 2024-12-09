0f4c5ee4c997c3a283157ea89ef26b9e84ddf705
Jeffrey Ling <jeffrey.r.ling@gmail.com>
2024-12-09 10:25:01 -0700

    address comment

--- llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/base.py
+++ llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/base.py
@@ -63,6 +63,19 @@ DEFAULT_ANTHROPIC_MODEL = "claude-2.1"
 DEFAULT_ANTHROPIC_MAX_TOKENS = 512
 
 
+class AnthropicTokenizer:
+    def __init__(self, client, model) -> None:
+        self._client = client
+        self.model = model
+
+    def encode(self, text: str, *args: Any, **kwargs: Any) -> List[int]:
+        count = self._client.beta.messages.count_tokens(
+            messages=[{"role": "user", "content": text}],
+            model=self.model,
+        ).input_tokens
+        return [1] * count
+
+
 class Anthropic(FunctionCallingLLM):
     """Anthropic LLM.
 
@@ -210,18 +223,6 @@ class Anthropic(FunctionCallingLLM):
 
     @property
     def tokenizer(self) -> Tokenizer:
-        class AnthropicTokenizer:
-            def __init__(self, client, model) -> None:
-                self._client = client
-                self.model = model
-
-            def encode(self, text: str, *args: Any, **kwargs: Any) -> List[int]:
-                count = self._client.beta.messages.count_tokens(
-                    messages=[{"role": "user", "content": text}],
-                    model=self.model,
-                ).input_tokens
-                return [1] * count
-
         return AnthropicTokenizer(self._client, self.model)
 
     @property
