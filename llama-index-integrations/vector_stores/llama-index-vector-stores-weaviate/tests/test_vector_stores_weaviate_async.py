f04ee25708cafb40cfccf6fac75c538bcdd5bc17
Andre Wisplinghoff <andre.wisplinghoff@commerzbank.com>
2024-12-09 16:56:45 +0000

    Move client connect & close to fixture also in async tests

--- llama-index-integrations/vector_stores/llama-index-vector-stores-weaviate/tests/test_vector_stores_weaviate_async.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-weaviate/tests/test_vector_stores_weaviate_async.py
@@ -5,51 +5,43 @@ from llama_index.core.vector_stores.types import (
     VectorStoreQueryMode,
 )
 import pytest
+import pytest_asyncio
 import weaviate
 
 
-# @pytest.fixture(scope="module") TODO
-# async def async_client():
-#    client = weaviate.use_async_with_embedded()
-#    asyncio.run(async_client.connect())
-#    yield client
-#    asyncio.run(client.close())
+@pytest_asyncio.fixture(scope="module")
+async def async_client():
+    client = weaviate.use_async_with_embedded()
+    await client.connect()
+    yield client
+    await client.close()
 
 
-# TODO do we need a fixture like this?
-# @pytest.fixture(scope="module")
-# def async_vector_store(async_client):
-#     return vector_store
+@pytest_asyncio.fixture(scope="module")
+def async_vector_store(async_client):
+    return WeaviateVectorStore(weaviate_aclient=async_client)
 
 
-@pytest.mark.asyncio()
-async def test_async_basic_flow():
-    async_client = weaviate.use_async_with_embedded()
-    await async_client.connect()
-    try:
-        async_vector_store = WeaviateVectorStore(weaviate_aclient=async_client)
+@pytest.mark.asyncio(loop_scope="module")
+async def test_async_basic_flow(async_vector_store):
+    nodes = [
+        TextNode(text="Hello world.", embedding=[0.0, 0.0, 0.3]),
+        TextNode(text="This is a test.", embedding=[0.3, 0.0, 0.0]),
+    ]
 
-        nodes = [
-            TextNode(text="Hello world.", embedding=[0.0, 0.0, 0.3]),
-            TextNode(text="This is a test.", embedding=[0.3, 0.0, 0.0]),
-        ]
+    await async_vector_store.async_add(nodes)
 
-        await async_vector_store.async_add(nodes)
+    query = VectorStoreQuery(
+        query_embedding=[0.3, 0.0, 0.0],
+        similarity_top_k=2,
+        query_str="world",
+        mode=VectorStoreQueryMode.DEFAULT,
+    )
 
-        query = VectorStoreQuery(
-            query_embedding=[0.3, 0.0, 0.0],
-            similarity_top_k=2,
-            query_str="world",
-            mode=VectorStoreQueryMode.DEFAULT,
-        )
+    results = await async_vector_store.aquery(query)
 
-        results = await async_vector_store.aquery(query)
+    assert len(results.nodes) == 2
+    assert results.nodes[0].text == "This is a test."
+    assert results.similarities[0] == 1.0
 
-        assert len(results.nodes) == 2
-        assert results.nodes[0].text == "This is a test."
-        assert results.similarities[0] == 1.0
-
-        assert results.similarities[0] > results.similarities[1]
-
-    finally:
-        await async_client.close()
+    assert results.similarities[0] > results.similarities[1]
