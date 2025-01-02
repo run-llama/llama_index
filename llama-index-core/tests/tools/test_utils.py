7754d09b224e3255b5c83e05e6110495fa0c0b8a
wdhorton <william.horton@includedhealth.com>
2025-01-02 15:42:35 -0500

    feat: support typing.Annotation for adding parameter descriptions
    Using pydantic.Field works fine, but typing.Annotation is a more Pythonic and less library-dependent way to
    add description strings to function parameters. So now you can write a function like this:
    
        def tmp_function(x: Annotated[int, 'An integer'] = 3) -> str:
            return str(x)
    
    And it will pass the description of x in the tool description to the LLM. It doesn't remove the existing behavior,
    but it gives a new option that makes use of Python's built-in way to do this, rather than relying on Pydantic.

--- llama-index-core/tests/tools/test_utils.py
+++ llama-index-core/tests/tools/test_utils.py
@@ -1,6 +1,6 @@
 """Test utils."""
 
-from typing import List
+from typing import List, Annotated
 
 from llama_index.core.bridge.pydantic import Field
 from llama_index.core.tools.utils import create_schema_from_function
@@ -35,6 +35,28 @@ def test_create_schema_from_function_with_field() -> None:
     """Test create_schema_from_function with pydantic.Field."""
 
     def tmp_function(x: int = Field(3, description="An integer")) -> str:
+        return str(x)
+
+    schema = create_schema_from_function("TestSchema", tmp_function)
+    actual_schema = schema.model_json_schema()
+
+    assert "x" in actual_schema["properties"]
+    assert actual_schema["properties"]["x"]["type"] == "integer"
+    assert actual_schema["properties"]["x"]["default"] == 3
+    assert actual_schema["properties"]["x"]["description"] == "An integer"
+
+    # Test the created schema
+    instance = schema()
+    assert instance.x == 3  # type: ignore
+
+    instance = schema(x=5)
+    assert instance.x == 5  # type: ignore
+
+
+def test_create_schema_from_function_with_typing_annotated() -> None:
+    """Test create_schema_from_function with pydantic.Field."""
+
+    def tmp_function(x: Annotated[int, "An integer"] = 3) -> str:
         return str(x)
 
     schema = create_schema_from_function("TestSchema", tmp_function)
