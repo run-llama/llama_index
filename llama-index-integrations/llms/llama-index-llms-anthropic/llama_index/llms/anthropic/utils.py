a728407882cdb1349a9f3ad95f20ef842d887aae
Ashish Bhatia <ashishb@ashishb.net>
2025-01-13 21:09:50 -0800

    Lint fix

--- llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/utils.py
+++ llama-index-integrations/llms/llama-index-llms-anthropic/llama_index/llms/anthropic/utils.py
@@ -174,8 +174,9 @@ def messages_to_anthropic_messages(
                     img_bytes = block.resolve_image(as_base64=True).read()
                     img_str = img_bytes.decode("utf-8")
 
+                    block_type = "document" if block.image_mimetype == "application/pdf" else "image"
                     block = ImageBlockParam(
-                        type="document" if block.image_mimetype == "application/pdf" else "image",
+                        type=block_type,
                         source={
                             "type": "base64",
                             "media_type": block.image_mimetype,
