1f1d059bf78c2c8c36b27e4e14945df49374062b
白宦成 <bestony@linux.com>
2025-01-15 11:15:38 +0000

    feat: add model comment

--- llama-index-integrations/llms/llama-index-llms-stepfun/llama_index/llms/stepfun/base.py
+++ llama-index-integrations/llms/llama-index-llms-stepfun/llama_index/llms/stepfun/base.py
@@ -15,6 +15,19 @@ DEFAULT_MODEL = "step-1v-8k"
 
 
 class StepFun(OpenAILike):
+    """
+    The StepFun class is a subclass of OpenAILike and is used to interact with the StepFun model.
+
+    Parameters:
+        model (str): The name of the Stepfun model to use. See https://platform.stepfun.com/docs/llm/modeloverview for options.
+        context_window (int): The maximum size of the context window for the model. See https://platform.stepfun.com/docs/llm/modeloverview for options.
+        is_chat_model (bool): Indicates whether the model is a chat model.
+
+    Attributes:
+        model (str): The name of the Stepfun model to use.
+        context_window (int): The maximum size of the context window for the model.
+        is_chat_model (bool): Indicates whether the model is a chat model.
+    """
 
     model: str = Field(
         description="The Stepfun model to use. See https://platform.stepfun.com/docs/llm/modeloverview for options."
@@ -40,6 +53,22 @@ class StepFun(OpenAILike):
         api_key: Optional[str] = None,
         **kwargs: Any,
     ) -> None:
+        """
+        Initialize the OpenAI API client.
+
+        Args:
+            model (str): The name of the model to use. Defaults to DEFAULT_MODEL.
+            temperature (float): The temperature to use for the model. Defaults to DEFAULT_TEMPERATURE.
+            max_tokens (int): The maximum number of tokens to generate. Defaults to DEFAULT_NUM_OUTPUTS.
+            additional_kwargs (Optional[Dict[str, Any]]): Additional keyword arguments to pass to the model. Defaults to None.
+            max_retries (int): The maximum number of retries to make when calling the API. Defaults to 5.
+            api_base (Optional[str]): The base URL for the API. Defaults to DEFAULT_API_BASE.
+            api_key (Optional[str]): The API key to use. Defaults to None.
+            **kwargs (Any): Additional keyword arguments to pass to the model.
+
+        Returns:
+            None
+        """
         additional_kwargs = additional_kwargs or {}
 
         api_base = get_from_param_or_env("api_base", api_base, "STEPFUN_API_BASE")
