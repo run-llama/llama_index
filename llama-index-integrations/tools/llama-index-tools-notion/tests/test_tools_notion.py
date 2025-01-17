892ffcde0f02a72398278e2494e51acc0865c20c
minglu7 <1347866672@qq.com>
2025-01-17 10:08:40 +0800

    fix: add pytest

--- llama-index-integrations/tools/llama-index-tools-notion/tests/test_tools_notion.py
+++ llama-index-integrations/tools/llama-index-tools-notion/tests/test_tools_notion.py
@@ -1,3 +1,6 @@
+import os
+
+import pytest
 from llama_index.core.tools.tool_spec.base import BaseToolSpec
 from llama_index.tools.notion import NotionToolSpec
 
@@ -13,24 +16,28 @@ def test_class():
     assert BaseToolSpec.__name__ in names_of_base_classes
 
 
+@pytest.mark.skipif('NOTION_INTEGRATION_TOKEN' not in os.environ, reason="NOTION_INTEGRATION_TOKEN is not set")
 def test_load_data_with_page_ids():
     tool = NotionToolSpec()
     content = tool.load_data(page_ids=page_ids)
     assert content
 
 
+@pytest.mark.skipif('NOTION_INTEGRATION_TOKEN' not in os.environ, reason="NOTION_INTEGRATION_TOKEN is not set")
 def test_load_data_with_database_ids():
     tool = NotionToolSpec()
     content = tool.load_data(database_ids=database_ids)
     assert content
 
 
+@pytest.mark.skipif('NOTION_INTEGRATION_TOKEN' not in os.environ, reason="NOTION_INTEGRATION_TOKEN is not set")
 def test_load_data_with_page_ids_and_database_ids():
     tool = NotionToolSpec()
     content = tool.load_data(page_ids=page_ids, database_ids=database_ids)
     assert content
 
 
+@pytest.mark.skipif('NOTION_INTEGRATION_TOKEN' not in os.environ, reason="NOTION_INTEGRATION_TOKEN is not set")
 def test_search_data():
     tool = NotionToolSpec()
     result = tool.search_data(query="Website")  # replace with your search query
