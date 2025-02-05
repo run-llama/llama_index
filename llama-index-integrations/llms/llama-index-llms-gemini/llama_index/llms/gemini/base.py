f938a1bd882abf742ad21d67a9836656372c2c46
ex0ns <ex0ns@users.noreply.github.com>
2025-02-05 21:36:38 +0100

    feat: support basic function call for gemini (google-generativeai) (#17696)

--- llama-index-integrations/llms/llama-index-llms-gemini/llama_index/llms/gemini/base.py
+++ llama-index-integrations/llms/llama-index-llms-gemini/llama_index/llms/gemini/base.py
@@ -2,7 +2,8 @@
 
 import os
 import warnings
-from typing import Any, Dict, Optional, Sequence, cast
+import uuid
+from typing import TYPE_CHECKING, Union, List, Any, Dict, Optional, Sequence, cast
 
 import google.generativeai as genai
 from google.generativeai.types import generation_types
@@ -13,15 +14,17 @@ from llama_index.core.base.llms.types import (
     ChatResponseGen,
     CompletionResponse,
     CompletionResponseGen,
+    CompletionResponseAsyncGen,
     LLMMetadata,
+    MessageRole,
 )
 from llama_index.core.bridge.pydantic import Field, PrivateAttr
 from llama_index.core.callbacks import CallbackManager
 from llama_index.core.constants import DEFAULT_NUM_OUTPUTS, DEFAULT_TEMPERATURE
+from llama_index.core.llms.llm import ToolSelection
 from llama_index.core.llms.callbacks import llm_chat_callback, llm_completion_callback
-from llama_index.core.llms.custom import CustomLLM
+from llama_index.core.llms.function_calling import FunctionCallingLLM
 from llama_index.core.utilities.gemini_utils import (
-    ROLES_FROM_GEMINI,
     merge_neighboring_same_role_messages,
 )
 
@@ -49,8 +52,11 @@ GEMINI_MODELS = (
     "gemini-1.0-pro",
 )
 
+if TYPE_CHECKING:
+    from llama_index.core.tools.types import BaseTool
 
-class Gemini(CustomLLM):
+
+class Gemini(FunctionCallingLLM):
     """
     Gemini LLM.
 
@@ -181,6 +187,8 @@ class Gemini(CustomLLM):
             num_output=self.max_tokens,
             model_name=self.model,
             is_chat_model=True,
+            # All gemini models support function calling
+            is_function_calling_model=True,
         )
 
     @llm_completion_callback()
@@ -208,10 +216,30 @@ class Gemini(CustomLLM):
         self, prompt: str, formatted: bool = False, **kwargs: Any
     ) -> CompletionResponseGen:
         request_options = self._request_options or kwargs.pop("request_options", None)
-        it = self._model.generate_content(
-            prompt, stream=True, request_options=request_options, **kwargs
-        )
-        yield from map(completion_from_gemini_response, it)
+
+        def gen():
+            it = self._model.generate_content(
+                prompt, stream=True, request_options=request_options, **kwargs
+            )
+            for r in it:
+                yield completion_from_gemini_response(r)
+
+        return gen()
+
+    @llm_completion_callback()
+    def astream_complete(
+        self, prompt: str, formatted: bool = False, **kwargs: Any
+    ) -> CompletionResponseAsyncGen:
+        request_options = self._request_options or kwargs.pop("request_options", None)
+
+        async def gen():
+            it = await self._model.generate_content_async(
+                prompt, stream=True, request_options=request_options, **kwargs
+            )
+            async for r in it:
+                yield completion_from_gemini_response(r)
+
+        return gen()
 
     @llm_chat_callback()
     def chat(self, messages: Sequence[ChatMessage], **kwargs: Any) -> ChatResponse:
@@ -256,19 +284,11 @@ class Gemini(CustomLLM):
             for r in response:
                 top_candidate = r.candidates[0]
                 content_delta = top_candidate.content.parts[0].text
-                role = ROLES_FROM_GEMINI[top_candidate.content.role]
-                raw = {
-                    **(type(top_candidate).to_dict(top_candidate)),  # type: ignore
-                    **(
-                        type(response.prompt_feedback).to_dict(response.prompt_feedback)  # type: ignore
-                    ),
-                }
                 content += content_delta
-                yield ChatResponse(
-                    message=ChatMessage(role=role, content=content),
-                    delta=content_delta,
-                    raw=raw,
-                )
+                llama_resp = chat_from_gemini_response(r)
+                llama_resp.delta = content_delta
+                llama_resp.message.content = content
+                yield llama_resp
 
         return gen()
 
@@ -278,7 +298,7 @@ class Gemini(CustomLLM):
     ) -> ChatResponseAsyncGen:
         request_options = self._request_options or kwargs.pop("request_options", None)
         merged_messages = merge_neighboring_same_role_messages(messages)
-        *history, next_msg = map(chat_message_to_gemini, messages)
+        *history, next_msg = map(chat_message_to_gemini, merged_messages)
         chat = self._model.start_chat(history=history)
         response = await chat.send_message_async(
             next_msg, stream=True, request_options=request_options, **kwargs
@@ -289,18 +309,86 @@ class Gemini(CustomLLM):
             async for r in response:
                 top_candidate = r.candidates[0]
                 content_delta = top_candidate.content.parts[0].text
-                role = ROLES_FROM_GEMINI[top_candidate.content.role]
-                raw = {
-                    **(type(top_candidate).to_dict(top_candidate)),  # type: ignore
-                    **(
-                        type(response.prompt_feedback).to_dict(response.prompt_feedback)  # type: ignore
-                    ),
-                }
                 content += content_delta
-                yield ChatResponse(
-                    message=ChatMessage(role=role, content=content),
-                    delta=content_delta,
-                    raw=raw,
-                )
+                llama_resp = chat_from_gemini_response(r)
+                llama_resp.delta = content_delta
+                llama_resp.message.content = content
+                yield llama_resp
 
         return gen()
+
+    def _prepare_chat_with_tools(
+        self,
+        tools: Sequence["BaseTool"],
+        user_msg: Optional[Union[str, ChatMessage]] = None,
+        chat_history: Optional[List[ChatMessage]] = None,
+        verbose: bool = False,
+        allow_parallel_tool_calls: bool = False,
+        tool_choice: Union[str, dict] = "auto",
+        strict: Optional[bool] = None,
+        **kwargs: Any,
+    ) -> Dict[str, Any]:
+        """Predict and call the tool."""
+        from google.generativeai.types import FunctionDeclaration, ToolDict
+
+        tool_declarations = []
+        for tool in tools:
+            descriptions = {}
+            for param_name, param_schema in tool.metadata.get_parameters_dict()[
+                "properties"
+            ].items():
+                param_description = param_schema.get("description", None)
+                if param_description:
+                    descriptions[param_name] = param_description
+
+            tool.metadata.fn_schema.__doc__ = tool.metadata.description
+            tool_declarations.append(
+                FunctionDeclaration.from_function(tool.metadata.fn_schema, descriptions)
+            )
+
+        if isinstance(user_msg, str):
+            user_msg = ChatMessage(role=MessageRole.USER, content=user_msg)
+
+        messages = chat_history or []
+        if user_msg:
+            messages.append(user_msg)
+
+        return {
+            "messages": messages,
+            "tools": ToolDict(function_declarations=tool_declarations)
+            if tool_declarations
+            else None,
+            **kwargs,
+        }
+
+    def get_tool_calls_from_response(
+        self,
+        response: ChatResponse,
+        error_on_no_tool_call: bool = True,
+        **kwargs: Any,
+    ) -> List[ToolSelection]:
+        """Predict and call the tool."""
+        tool_calls = response.message.additional_kwargs.get("tool_calls", [])
+
+        if len(tool_calls) < 1:
+            if error_on_no_tool_call:
+                raise ValueError(
+                    f"Expected at least one tool call, but got {len(tool_calls)} tool calls."
+                )
+            else:
+                return []
+
+        tool_selections = []
+        for tool_call in tool_calls:
+            if not isinstance(tool_call, genai.protos.FunctionCall):
+                raise ValueError("Invalid tool_call object")
+
+            tool_selections.append(
+                ToolSelection(
+                    tool_id=str(uuid.uuid4()),
+                    tool_name=tool_call.name,
+                    tool_kwargs=dict(tool_call.args),
+                )
+            )
+
+        return tool_selections
--- llama-index-integrations/llms/llama-index-llms-gemini/tests/test_llms_gemini.py
+++ llama-index-integrations/llms/llama-index-llms-gemini/tests/test_llms_gemini.py
@@ -1,18 +1,24 @@
 import os
 
+from llama_index.core.tools.function_tool import FunctionTool
 import pytest
 from llama_index.core.base.llms.base import BaseLLM
 from llama_index.core.base.llms.types import ChatMessage, ImageBlock, MessageRole
 from llama_index.llms.gemini import Gemini
 from llama_index.llms.gemini.utils import chat_message_to_gemini
 
+from google.ai.generativelanguage_v1beta.types import (
+    FunctionCallingConfig,
+    ToolConfig,
+)
 
-def test_embedding_class():
+
+def test_embedding_class() -> None:
     names_of_base_classes = [b.__name__ for b in Gemini.__mro__]
     assert BaseLLM.__name__ in names_of_base_classes
 
 
-def test_chat_message_to_gemini():
+def test_chat_message_to_gemini() -> None:
     msg = ChatMessage("Some content")
     assert chat_message_to_gemini(msg) == {
         "role": MessageRole.USER,
@@ -30,21 +36,50 @@ def test_chat_message_to_gemini():
 @pytest.mark.skipif(
     os.environ.get("GOOGLE_API_KEY") is None, reason="GOOGLE_API_KEY not set"
 )
-def test_generate_image_prompt():
+def test_generate_image_prompt() -> None:
     msg = ChatMessage("Tell me the brand of the car in this image:")
     msg.blocks.append(
         ImageBlock(
             url="https://upload.wikimedia.org/wikipedia/commons/5/52/Ferrari_SP_FFX.jpg"
         )
     )
-    response = Gemini().chat(messages=[msg])
+    response = Gemini(model="models/gemini-1.5-flash").chat(messages=[msg])
     assert "ferrari" in str(response).lower()
 
 
 @pytest.mark.skipif(
     os.environ.get("GOOGLE_API_KEY") is None, reason="GOOGLE_API_KEY not set"
 )
-def test_chat_stream():
+def test_chat_stream() -> None:
     msg = ChatMessage("List three types of software testing strategies")
-    response = list(Gemini().stream_chat(messages=[msg]))
+    response = list(Gemini(model="models/gemini-1.5-flash").stream_chat(messages=[msg]))
     assert response
+
+
+@pytest.mark.skipif(
+    os.environ.get("GOOGLE_API_KEY") is None, reason="GOOGLE_API_KEY not set"
+)
+def test_chat_with_tools() -> None:
+    def add(a: int, b: int) -> int:
+        """Add two integers and returns the result integer."""
+        return a + b
+
+    add_tool = FunctionTool.from_defaults(fn=add)
+    msg = ChatMessage("What is the result of adding 2 and 3?")
+    model = Gemini(model="models/gemini-1.5-flash")
+    response = model.chat_with_tools(
+        user_msg=msg,
+        tools=[add_tool],
+        tool_config=ToolConfig(
+            function_calling_config=FunctionCallingConfig(
+                mode=FunctionCallingConfig.Mode.ANY
+            )
+        ),
+    )
+
+    tool_calls = model.get_tool_calls_from_response(response)
+    assert len(tool_calls) == 1
+    assert tool_calls[0].tool_name == "add"
+    assert tool_calls[0].tool_kwargs == {"a": 2, "b": 3}
+
+    assert len(response.additional_kwargs["tool_calls"]) >= 1
