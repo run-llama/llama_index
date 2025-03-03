8f7b3ac505ef1b461d417308d87a1ff8db239d88
Logan Markewich <logan.markewich@live.com>
2025-03-03 10:53:16 -0600

    mistral multimodal wraps normal mistral

--- llama-index-integrations/multi_modal_llms/llama-index-multi-modal-llms-mistralai/tests/test_multi-modal-llms_mistral.py
+++ llama-index-integrations/multi_modal_llms/llama-index-multi-modal-llms-mistralai/tests/test_multi-modal-llms_mistral.py
@@ -1,10 +1,10 @@
-from llama_index.core.multi_modal_llms.base import MultiModalLLM
+from llama_index.llms.mistralai import MistralAI
 from llama_index.multi_modal_llms.mistralai import MistralAIMultiModal
 
 
 def test_embedding_class():
     names_of_base_classes = [b.__name__ for b in MistralAIMultiModal.__mro__]
-    assert MultiModalLLM.__name__ in names_of_base_classes
+    assert MistralAI.__name__ in names_of_base_classes
 
 
 def test_init():
