535c790ec03252626e003436428e684beb72d60d
Alex Sherstinsky <alexsherstinsky@users.noreply.github.com>
2025-03-03 11:47:51 -0800

    clean up

--- llama-index-integrations/readers/llama-index-readers-file/tests/test_readers_file.py
+++ llama-index-integrations/readers/llama-index-readers-file/tests/test_readers_file.py
@@ -42,30 +42,6 @@ def test_classes():
     assert BaseReader.__name__ in names_of_base_classes
 
     names_of_base_classes = [b.__name__ for b in ImageVisionLLMReader.__mro__]
-    ###ALEX
-    # assert BaseReader.__name__ in names_of_base_classes
-    # assert hasattr(ImageVisionLLMReader, "__init__")
-    # assert hasattr(ImageVisionLLMReader, "load_data")
-    # assert hasattr(ImageVisionLLMReader, "_import_torch")
-    # image_vision_llm_reader = ImageVisionLLMReader(
-    #     parser_config={
-    #         "processor": None,
-    #         "model": None,
-    #         "device": "cpu",
-    #         "dtype": float,
-    #     }
-    # )
-    # assert hasattr(image_vision_llm_reader, "_torch")
-    # assert hasattr(image_vision_llm_reader, "_torch_imported")
-    # image_vision_llm_reader._import_torch()
-    # print(f'\nZZZZZZZZZZ\n{image_vision_llm_reader._torch}')
-    # print(f'\nYYYYYYYYYY\n{image_vision_llm_reader._torch_imported}')
-    # # b = image_vision_llm_reader.load_data(file=None)
-    # # image_vision_llm_reader = ImageVisionLLMReader()
-    # # result = image_vision_llm_reader.load_data(file="/Users/alexsherstinsky/Desktop/BookImages/BuildALargeLanguageModelFromScratch.jpg")
-    # # print(result)
-    # # print(str(type(result)))
-    ###ALEX
 
     names_of_base_classes = [b.__name__ for b in IPYNBReader.__mro__]
     assert BaseReader.__name__ in names_of_base_classes
