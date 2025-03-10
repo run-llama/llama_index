dce0125689953043f15385da74614a553ed4b27c
jayfish0 <jason@tinyfish.io>
2025-03-10 15:00:31 -0700

    Change messages order

--- llama-index-integrations/tools/llama-index-tools-agentql/llama_index/tools/agentql/messages.py
+++ llama-index-integrations/tools/llama-index-tools-agentql/llama_index/tools/agentql/messages.py
@@ -1,4 +1,3 @@
-UNAUTHORIZED_ERROR_MESSAGE = "Invalid AgentQL API key provided. Please provide a valid API Key. You can create one at https://dev.agentql.com."
 QUERY_PROMPT_REQUIRED_ERROR_MESSAGE = (
     "Invalid arguments provided. Either 'query' or 'prompt' must be provided."
 )
@@ -6,6 +5,7 @@ QUERY_PROMPT_EXCLUSIVE_ERROR_MESSAGE = (
     "Invalid arguments provided. Only one of 'query' or 'prompt' should be provided."
 )
 UNSET_API_KEY_ERROR_MESSAGE = "No AgentQL API key provided. You can set your API key in code by specifying the `api_key` argument or by setting the `AGENTQL_API_KEY` environment variable. You can create an API key at https://dev.agentql.com."
+UNAUTHORIZED_ERROR_MESSAGE = "Invalid AgentQL API key provided. Please provide a valid API Key. You can create one at https://dev.agentql.com."
 MISSING_BROWSER_ERROR_MESSAGE = (
     "Browser Instance not found. A browser instance is required to use this tool."
 )
