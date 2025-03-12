50f0bb21a06b539822b470e3167444fb1b15ff8e
Logan Markewich <logan.markewich@live.com>
2025-03-12 17:41:21 +0000

    remove assert for multimodal llms

--- llama-index-core/llama_index/core/indices/multi_modal/base.py
+++ llama-index-core/llama_index/core/indices/multi_modal/base.py
@@ -139,6 +139,10 @@ class MultiModalVectorStoreIndex(VectorStoreIndex):
         retriever = cast(MultiModalVectorIndexRetriever, self.as_retriever(**kwargs))
 
         llm = llm or Settings.llm
+        if "multi" not in llm.class_name():
+            logger.warning(
+                f"Warning: {llm.class_name()} does not appear to be a multi-modal LLM. This may not work as expected."
+            )
 
         return SimpleMultiModalQueryEngine(
             retriever,
