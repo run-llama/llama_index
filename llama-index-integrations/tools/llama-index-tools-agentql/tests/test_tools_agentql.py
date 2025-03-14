b2360c3a6354dcf0b516bfe92e8e30894329e4ff
jayfish0 <jason@tinyfish.io>
2025-03-13 22:48:56 -0700

    Delete test

--- llama-index-integrations/tools/llama-index-tools-agentql/tests/test_tools_agentql.py
+++
@@ -1,7 +0,0 @@
-from llama_index.core.tools.tool_spec.base import BaseToolSpec
-from llama_index.tools.agentql.base import AgentQLToolSpec
-
-
-def test_class():
-    names_of_base_classes = [b.__name__ for b in AgentQLToolSpec.__mro__]
-    assert BaseToolSpec.__name__ in names_of_base_classes
