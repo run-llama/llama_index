b97153b98867291813a2319870f4a2fda704e082
Amaan <amaan@ai20labs.com>
2025-03-24 15:25:37 +0530

    fix imports for dappier real time search and ai recommendations

--- llama-index-integrations/tools/llama-index-tools-dappier/llama_index/tools/dappier/__init__.py
+++ llama-index-integrations/tools/llama-index-tools-dappier/llama_index/tools/dappier/__init__.py
@@ -1,5 +1,9 @@
-from llama_index.tools.dappier.base import DappierAIRecommendationsToolSpec
-from llama_index.tools.dappier.base import DappierRealTimeSearchToolSpec
+from llama_index.tools.dappier.ai_recommendations.base import (
+    DappierAIRecommendationsToolSpec,
+)
+from llama_index.tools.dappier.real_time_search.base import (
+    DappierRealTimeSearchToolSpec,
+)
 
 __all__ = [
     "DappierAIRecommendationsToolSpec",
