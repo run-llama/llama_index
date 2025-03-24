9ac168d0d08e99da594f0d669351a73fdd4dacaf
Amaan <amaan@ai20labs.com>
2025-03-24 17:01:15 +0530

    move dappier tools tests into it's own file

---
+++ llama-index-integrations/tools/llama-index-tools-dappier/tests/test_tools_dappier_ai_recommendations.py
@@ -0,0 +1,9 @@
+from llama_index.core.tools.tool_spec.base import BaseToolSpec
+from llama_index.tools.dappier import DappierAIRecommendationsToolSpec
+
+
+def test_class():
+    names_of_base_classes = [
+        b.__name__ for b in DappierAIRecommendationsToolSpec.__mro__
+    ]
+    assert BaseToolSpec.__name__ in names_of_base_classes
