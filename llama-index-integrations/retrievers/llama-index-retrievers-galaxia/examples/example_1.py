6a2e31f8b48375c2274a9267cc16fc0dcc1835b6
Robert Różański <rrozanski@smabbler.com>
2025-03-31 12:52:58 +0300

    update example

--- llama-index-integrations/retrievers/llama-index-retrievers-galaxia/examples/example_1.py
+++ llama-index-integrations/retrievers/llama-index-retrievers-galaxia/examples/example_1.py
@@ -1,16 +1,12 @@
 #! /usr/bin/env python3
 
-from llama_index.core.schema import NodeWithScore, QueryBundle, TextNode
+from llama_index.core.schema import QueryBundle
 from llama_index.retrievers.galaxia import GalaxiaRetriever
 
-api_key = "<your API key>"
-api_url = "https://dev.api.smabbler.com"
-knowledge_base_id = "<your KB ID>"
-
-gr = GalaxiaRetriever(
-    api_url,
-    api_key,
-    knowledge_base_id,
+retriever = GalaxiaRetriever(
+    api_url="https://beta.api.smabbler.com",
+    api_key="<key>",
+    knowledge_base_id="<knowledge_base_id>",
 )
 
 query = "What is Marie Curie's nationality?"
