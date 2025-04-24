e9d75f8e7a6ad72a6b95f14473294f48b944ec40
rostyslav.borovyk <rostyslav.borovyk@oxylabs.io>
2025-04-24 15:13:44 +0300

    Removed extra tests, updated docs

--- llama-index-integrations/readers/llama-index-readers-web/tests/test_readers_rss.py
+++ llama-index-integrations/readers/llama-index-readers-web/tests/test_readers_rss.py
@@ -1,5 +1,3 @@
-import os
-
 from llama_index.readers.web import RssReader
 
 
@@ -19,23 +17,3 @@ def test_rss_reader_user_agent():
     reader = RssReader(user_agent="MyApp/1.0 +http://example.com/")
     documents = reader.load_data(urls=["https://rsshub.app/hackernews/newest"])
     assert len(documents) > 0
-
-
-def test_oxylabs():
-    from llama_index.readers.web.oxylabs_web.base import OxylabsWebReader
-
-    reader = OxylabsWebReader(
-        username=os.environ["OXYLABS_USERNAME"], password=os.environ["OXYLABS_PASSWORD"]
-    )
-
-    docs = reader.load_data(
-        [
-            "https://sandbox.oxylabs.io/products/1",
-            "https://sandbox.oxylabs.io/products/2",
-        ],
-        {
-            "parse": False,
-        },
-    )
-
-    print(docs[0].text)
