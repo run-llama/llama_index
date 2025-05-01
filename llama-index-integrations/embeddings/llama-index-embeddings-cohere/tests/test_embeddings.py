10e22e0e63301daf232eb832436f30362e89d1b3
enrico-stauss <73635664+enrico-stauss@users.noreply.github.com>
2025-05-01 02:55:54 +0200

    Fix Cohere Embedding Multiprocessing with Custom Endpoint (#18551)

--- llama-index-integrations/embeddings/llama-index-embeddings-cohere/tests/test_embeddings.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-cohere/tests/test_embeddings.py
@@ -41,3 +41,21 @@ async def test_async_embedding():
     )
 
     await emb.aget_query_embedding("I love Cohere!")
+
+
+def test_cohere_embeddings_custom_endpoint_multiprocessing():
+    """When used in multiprocessing, the CohereEmbedding instance will be serialized and deserialized. This test
+    verifies, that custom base_url's are retained in the spawned processes.
+    """
+    # Arrange: Create a CohereEmbeddings instance with a custom base_url
+    custom_base_url = "test_endpoint"
+    api_key = "test_api_key"
+    embeddings = CohereEmbedding(api_key=api_key, base_url=custom_base_url)
+
+    # Act: Simulate serialization and deserialization
+    serialized_data = embeddings.__getstate__()
+    deserialized_embeddings = CohereEmbedding.__new__(CohereEmbedding)
+    deserialized_embeddings.__setstate__(serialized_data)
+
+    # Assert: Verify that the deserialized instance retains the correct base_url
+    assert deserialized_embeddings.base_url == custom_base_url
