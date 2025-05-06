9925bdf0cb5adab75e76d05c71d67a504fca2b14
Logan Markewich <logan.markewich@live.com>
2025-05-05 19:00:18 -0600

    actually skip?

--- llama-index-integrations/readers/llama-index-readers-web/tests/test_readers_oxylabs.py
+++ llama-index-integrations/readers/llama-index-readers-web/tests/test_readers_oxylabs.py
@@ -22,7 +22,7 @@ READER_TEST_PARAM = pytest.param(
     id="response_success",
 )
 
-skip_if_py39_or_lower = sys.version_info <= (3, 9)
+skip_if_py39_or_lower = sys.version_info < (3, 10)
 
 @pytest.mark.skipif(skip_if_py39_or_lower, reason="Pytest does not support Python 3.9")
 @pytest.mark.parametrize(
