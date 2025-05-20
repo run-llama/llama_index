0b7f289713f4d3fac984392011f1d102c443cd82
Logan Markewich <logan.markewich@live.com>
2025-05-20 10:06:57 -0600

    add quick code comment

--- llama-index-integrations/llms/llama-index-llms-bedrock-converse/llama_index/llms/bedrock_converse/utils.py
+++ llama-index-integrations/llms/llama-index-llms-bedrock-converse/llama_index/llms/bedrock_converse/utils.py
@@ -188,6 +188,8 @@ def _content_block_to_bedrock_format(
         else:
             data = base64.b64decode(block.data)
         title = block.title
+        # NOTE: At the time of writing, "txt" format works for all file types
+        # The API then infers the format from the file type based on the bytes
         return {
             "document": {"format": "txt", "name": title, "source": {"bytes": data}}
         }
