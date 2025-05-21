239a9eba79a31bc1ad95a431dbbd58baf8f01f82
Logan Markewich <logan.markewich@live.com>
2025-05-21 15:19:21 -0600

    fix raptor

--- llama-index-packs/llama-index-packs-raptor/llama_index/packs/raptor/clustering.py
+++ llama-index-packs/llama-index-packs-raptor/llama_index/packs/raptor/clustering.py
@@ -11,8 +11,9 @@ import umap
 from sklearn.mixture import GaussianMixture
 from typing import Dict, List, Optional
 
+from llama_index.core import get_tokenizer
 from llama_index.core.schema import BaseNode
-import llama_index.core as core
+
 
 # Set a random seed for reproducibility
 RANDOM_SEED = 224
@@ -120,11 +121,13 @@ def get_clusters(
     nodes: List[BaseNode],
     embedding_map: Dict[str, List[List[float]]],
     max_length_in_cluster: int = 10000,  # 10k tokens max per cluster
-    tokenizer: tiktoken.Encoding = core.global_tokenizer, # use tokenizer from llama_index
+    tokenizer: Optional[tiktoken.Encoding] = None, # use tokenizer from llama_index
     reduction_dimension: int = 10,
     threshold: float = 0.1,
     prev_total_length=None,  # to keep track of the total length of the previous clusters
 ) -> List[List[BaseNode]]:
+    tokenizer = tokenizer or get_tokenizer()
+
     # get embeddings
     embeddings = np.array([np.array(embedding_map[node.id_]) for node in nodes])
 
