77ffbe72e7edc6c923c31aa81efe76bc54480bee
Logan Markewich <logan.markewich@live.com>
2025-05-26 09:23:51 -0600

    remove stray file

--- llama-index-integrations/postprocessor/llama-index-postprocessor-rankllm-rerank/llama_index/postprocessor/rankllm_rerank/test.py
+++
@@ -1,5 +0,0 @@
-from rank_llm.rerank.reranker import Reranker
-
-Reranker.create_model_coordinator(
-    model_path="rank_zephyr", default_model_coordinator=None, interactive=False, vllm_batched=True
-)
