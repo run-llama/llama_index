7e2f0800e4cb3049342c0495536420fa7651d65c
Logan Markewich <logan.markewich@live.com>
2025-05-28 11:01:52 -0600

    ocd nit

--- llama-index-integrations/tools/llama-index-tools-mcp/llama_index/tools/mcp/base.py
+++ llama-index-integrations/tools/llama-index-tools-mcp/llama_index/tools/mcp/base.py
@@ -2,13 +2,13 @@ import asyncio
 import logging
 from typing import Any, Callable, Dict, List, Optional, Type
 
+from mcp.client.session import ClientSession
+from mcp.types import Resource
+from pydantic import BaseModel, Field, create_model
+
 from llama_index.core.tools.function_tool import FunctionTool
 from llama_index.core.tools.tool_spec.base import BaseToolSpec
 from llama_index.core.tools.types import ToolMetadata
-from pydantic import BaseModel, Field, create_model
-
-from mcp.client.session import ClientSession
-from mcp.types import Resource
 
 # Map JSON Schema types to Python types
 json_type_mapping: Dict[str, Type] = {
@@ -64,6 +64,8 @@ class McpToolSpec(BaseToolSpec):
         client: An MCP client instance implementing ClientSession, and it should support the following methods in ClientSession:
             - list_tools: List all tools.
             - call_tool: Call a tool.
+            - list_resources: List all resources.
+            - read_resource: Read a resource.
         allowed_tools: If set, only return tools with the specified names.
         include_resources: Whether to include resources in the tool list.
 
@@ -89,12 +91,14 @@ class McpToolSpec(BaseToolSpec):
         """
         response = await self.client.list_tools()
         tools = response.tools if hasattr(response, "tools") else []
+
         if self.allowed_tools is None:
             # get all tools by default
             return tools
 
         if any(self.allowed_tools):
             return [tool for tool in tools if tool.name in self.allowed_tools]
+
         logging.warning(
             "Returning an empty tool list due to the empty `allowed_tools` list. Please ensure `allowed_tools` is set appropriately."
         )
@@ -105,7 +109,21 @@ class McpToolSpec(BaseToolSpec):
         An asynchronous method to get the resources list from MCP Client.
         """
         response = await self.client.list_resources()
-        return response.resources if hasattr(response, "resources") else []
+        resources = response.resources if hasattr(response, "resources") else []
+        if self.allowed_tools is None:
+            return resources
+
+        if any(self.allowed_tools):
+            return [
+                resource
+                for resource in resources
+                if resource.name in self.allowed_tools
+            ]
+
+        logging.warning(
+            "Returning an empty resource list due to the empty `allowed_tools` list. Please ensure `allowed_tools` is set appropriately."
+        )
+        return []
 
     def _create_tool_fn(self, tool_name: str) -> Callable:
         """
