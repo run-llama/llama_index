4f844734967fc4433abc666352d3460025887d61
piotrekkr <piotrekkr@users.noreply.github.com>
2025-05-31 23:59:18 +0200

    Fix linter issues

--- llama-index-integrations/tools/llama-index-tools-mcp/llama_index/tools/mcp/client.py
+++ llama-index-integrations/tools/llama-index-tools-mcp/llama_index/tools/mcp/client.py
@@ -1,7 +1,17 @@
 import warnings
 from contextlib import asynccontextmanager
 from datetime import timedelta
-from typing import Optional, List, Dict, Tuple, Callable, AsyncIterator, Awaitable, Dict
+from typing import (
+    Optional,
+    List,
+    Dict,
+    Tuple,
+    Callable,
+    AsyncIterator,
+    Awaitable,
+    Dict,
+    Any,
+)
 from urllib.parse import urlparse, parse_qs
 from mcp.client.session import ClientSession, ProgressFnT
 from mcp.client.sse import sse_client
@@ -74,6 +84,7 @@ class BasicMCPClient(ClientSession):
         auth: Optional OAuth client provider for authentication.
         sampling_callback: Optional callback for handling sampling messages.
         headers: Optional headers to pass by sse client or streamable http client
+
     """
 
     def __init__(
@@ -163,7 +174,9 @@ class BasicMCPClient(ClientSession):
             # Check if this is a streamable HTTP endpoint (default) or SSE
             if enable_sse(self.command_or_url):
                 # SSE transport
-                async with sse_client(self.command_or_url, auth=self.auth, headers=self.headers) as streams:
+                async with sse_client(
+                    self.command_or_url, auth=self.auth, headers=self.headers
+                ) as streams:
                     async with ClientSession(
                         *streams,
                         read_timeout_seconds=timedelta(seconds=self.timeout),
