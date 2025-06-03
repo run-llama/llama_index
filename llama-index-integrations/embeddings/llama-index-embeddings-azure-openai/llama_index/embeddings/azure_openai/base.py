1359a5300b15216766aa0ac0db0173d2016e03bf
Martim Santos <72747170+martimfasantos@users.noreply.github.com>
2025-06-03 16:19:51 +0100

    Re-fix the `api_base` parameter in AzureOpenAIEmbeddings (#18948)

--- llama-index-integrations/embeddings/llama-index-embeddings-azure-openai/llama_index/embeddings/azure_openai/base.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-azure-openai/llama_index/embeddings/azure_openai/base.py
@@ -87,18 +87,18 @@ class AzureOpenAIEmbedding(OpenAIEmbedding):
         async_http_client: Optional[httpx.AsyncClient] = None,
         **kwargs: Any,
     ):
-        azure_endpoint = get_from_param_or_env(
-            "azure_endpoint", azure_endpoint, "AZURE_OPENAI_ENDPOINT", None
-        )
+        # OpenAI base_url (api_base) and azure_endpoint are mutually exclusive
+        if api_base:
+            azure_endpoint = None
+        else:
+            azure_endpoint = get_from_param_or_env(
+                "azure_endpoint", azure_endpoint, "AZURE_OPENAI_ENDPOINT", None
+            )
 
         if not use_azure_ad:
             api_key = get_from_param_or_env(
                 "api_key", api_key, "AZURE_OPENAI_API_KEY", None
             )
-
-        # OpenAI base_url and azure_endpoint are mutually exclusive
-        if api_base:
-            azure_endpoint = None
 
         azure_deployment = resolve_from_aliases(
             azure_deployment,
