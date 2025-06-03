5a1249ed70d8d7b11a0ed35610990e35499d62f5
Yarikama <125861728+yarikama@users.noreply.github.com>
2025-06-03 22:35:24 +0800

    fix(bedrock-converse): handle empty message content to prevent Valida… (#18914)

--- llama-index-integrations/llms/llama-index-llms-bedrock-converse/llama_index/llms/bedrock_converse/utils.py
+++ llama-index-integrations/llms/llama-index-llms-bedrock-converse/llama_index/llms/bedrock_converse/utils.py
@@ -183,6 +183,8 @@ def _content_block_to_bedrock_format(
 ) -> Optional[Dict[str, Any]]:
     """Convert content block to AWS Bedrock Converse API required format."""
     if isinstance(block, TextBlock):
+        if not block.text:
+            return None
         return {
             "text": block.text,
         }
@@ -252,18 +254,14 @@ def messages_to_converse_messages(
     converse_messages = []
     system_prompt = ""
     for message in messages:
-        if message.role == MessageRole.SYSTEM:
-            system_prompt += message.content + "\n"
+        if message.role == MessageRole.SYSTEM and message.content:
+            system_prompt += (message.content) + "\n"
         elif message.role in [MessageRole.FUNCTION, MessageRole.TOOL]:
             # convert tool output to the AWS Bedrock Converse format
             content = {
                 "toolResult": {
                     "toolUseId": message.additional_kwargs["tool_call_id"],
-                    "content": [
-                        {
-                            "text": message.content or "",
-                        },
-                    ],
+                    "content": [{"text": message.content}] if message.content else [],
                 }
             }
             if status := message.additional_kwargs.get("status"):
