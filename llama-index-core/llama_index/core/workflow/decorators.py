125bcebc2e1f4dc909a780a92f079730cc19544c
Massimiliano Pippi <mpippi@gmail.com>
2025-06-11 16:57:42 +0200

    backward compatible step

--- llama-index-core/llama_index/core/workflow/decorators.py
+++ llama-index-core/llama_index/core/workflow/decorators.py
@@ -1,1 +1,9 @@
-from workflows.decorators import step, StepConfig  # noqa
+from workflows.decorators import StepConfig  # noqa
+from workflows.decorators import step as upstream_step  # noqa
+
+from typing import Callable
+
+
+def step(*args, **kwargs) -> Callable:
+    kwargs.pop("pass_context")
+    return upstream_step(*args, **kwargs)
