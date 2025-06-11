99531bc9161f5a1849007119ad6dcc6ad5eb0467
Massimiliano Pippi <mpippi@gmail.com>
2025-06-11 17:02:51 +0200

    fix typing

--- llama-index-core/llama_index/core/workflow/decorators.py
+++ llama-index-core/llama_index/core/workflow/decorators.py
@@ -1,9 +1,10 @@
 from workflows.decorators import StepConfig  # noqa
 from workflows.decorators import step as upstream_step  # noqa
 
-from typing import Callable
+from typing import Callable, Any
 
 
-def step(*args, **kwargs) -> Callable:
+def step(*args: Any, **kwargs: Any) -> Callable:
+    # Remove old, unused parameter
     kwargs.pop("pass_context")
     return upstream_step(*args, **kwargs)
