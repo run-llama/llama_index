9a05ed155569836a7640f888f1a4ce338a32f10a
Clelia (Astra) Bertelli <133636879+AstraBert@users.noreply.github.com>
2025-06-16 15:23:17 +0200

    Fixing HiveReader (#18990)
    * Preventing malicious use of HiveReader.load_data() with query parametrization + several bug fixes
    
    * Adding validation tests
    
    * Adding query parametrization
    
    * Adjusting tests
    
    * Moving hive reader to stale packages (see PR for that)
    
    * Publish last hive reader version with deprecation warning
    
    * change in deprecated decorator

--- llama-index-integrations/readers/llama-index-readers-hive/llama_index/readers/hive/base.py
+++ llama-index-integrations/readers/llama-index-readers-hive/llama_index/readers/hive/base.py
@@ -1,11 +1,37 @@
 """Hive data reader."""
 
-from typing import List, Optional
+try:
+    from pyhive import hive
+except ImportError:
+    raise ImportError("`hive` package not found, please run `pip install pyhive`")
+try:
+    import sqlglot
+except ImportError:
+    raise ImportError("`sqlglot` package not found, please run `pip install sqlglot`")
 
+from typing import List, Optional, Tuple
+from deprecated import deprecated
 from llama_index.core.readers.base import BaseReader
 from llama_index.core.schema import Document
 
 
+class InvalidSqlError(Exception):
+    """Raise when invalid SQL is passed."""
+
+
+def _validate_sql_query(statements: List[str]) -> None:
+    if len(statements) > 1:
+        raise InvalidSqlError("You cannot pass multiple statements into the query")
+    if not statements[0].lower().startswith("select"):
+        raise InvalidSqlError("You must provide a SELECT query")
+    if "or" in statements[0].lower():
+        raise InvalidSqlError("The use of OR is not allowed to prevent SQL Injections")
+
+
+@deprecated(
+    reason="llama-index-readers-hive has been deprecated since v0.3.1 on the grounds of security concerns for SQL query handling, and will thus no longer be maintained. Use this package with caution.",
+    version="0.3.1",
+)
 class HiveReader(BaseReader):
     """
     Read documents from a Hive.
@@ -32,13 +58,6 @@ class HiveReader(BaseReader):
         auth: Optional[str] = None,
     ):
         """Initialize with parameters."""
-        try:
-            from pyhive import hive
-        except ImportError:
-            raise ImportError(
-                "`hive` package not found, please run `pip install pyhive`"
-            )
-
         self.con = hive.Connection(
             host=host,
             port=port,
@@ -48,26 +67,37 @@ class HiveReader(BaseReader):
             password=password,
         )
 
-    def load_data(self, query: str) -> List[Document]:
+    def load_data(
+        self, query: str, params: Optional[Tuple[str, ...]] = None
+    ) -> List[Document]:
         """
         Read data from the Hive.
 
         Args:
-            query (str): The query used to query data from Hive
+            query (str): Query with which to extract data from Hive. Parametrized values must be represented as '%s'.
+            params (Optional[Tuple[str, ...]): Parametrized values.
+
         Returns:
             List[Document]: A list of documents.
 
         """
         try:
-            cursor = self.con.cursor().execute(query)
-            cursor.execute(query)
+            if params:
+                filled_query = query % tuple(repr(p) for p in params)
+            else:
+                filled_query = query
+            parsed_query = sqlglot.parse(filled_query)
+            statements = [statement.sql() for statement in parsed_query]
+            _validate_sql_query(statements=statements)
+            cursor = self.con.cursor()
+            cursor.execute(operation=query, parameters=params)
             rows = cursor.fetchall()
         except Exception:
             raise Exception(
-                "Throws Exception in execution, please check your connection params and query "
+                "Throws Exception in execution, please check your connection params and query."
             )
 
-        documents = []
+        documents: List[Document] = []
         for row in rows:
-            documents = Document(text=row)
+            documents.append(Document(text=str(row)))
         return documents
