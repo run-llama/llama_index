834e967ce7eb6ea025624ae3b9ac49be7eecd353
William Easton <bill.easton@elastic.co>
2025-06-19 18:51:22 -0500

    add batch support

--- llama-index-integrations/embeddings/llama-index-embeddings-fastembed/llama_index/embeddings/fastembed/base.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-fastembed/llama_index/embeddings/fastembed/base.py
@@ -24,33 +24,32 @@ class FastEmbedEmbedding(BaseEmbedding):
     """
 
     model_name: str = Field(
-        "BAAI/bge-small-en-v1.5",
+        default="BAAI/bge-small-en-v1.5",
         description="Name of the FastEmbedding model to use.\n"
-        "Defaults to 'BAAI/bge-small-en-v1.5'.\n"
         "Find the list of supported models at "
         "https://qdrant.github.io/fastembed/examples/Supported_Models/",
     )
 
     max_length: int = Field(
-        512,
+        default=512,
         description="The maximum number of tokens. Defaults to 512.\n"
         "Unknown behavior for values > 512.",
     )
 
     cache_dir: Optional[str] = Field(
-        None,
+        default=None,
         description="The path to the cache directory.\n"
         "Defaults to `local_cache` in the parent directory",
     )
 
     threads: Optional[int] = Field(
-        None,
+        default=None,
         description="The number of threads single onnxruntime session can use.\n"
         "Defaults to None",
     )
 
     doc_embed_type: Literal["default", "passage"] = Field(
-        "default",
+        default="default",
         description="Type of embedding method to use for documents.\n"
         "Available options are 'default' and 'passage'.",
     )
@@ -60,15 +59,15 @@ class FastEmbedEmbedding(BaseEmbedding):
         description="The ONNX providers to use for the embedding model.",
     )
 
-    _model: Any = PrivateAttr()
+    _model: TextEmbedding = PrivateAttr()
 
     @classmethod
-    def class_name(self) -> str:
+    def class_name(cls) -> str:
         return "FastEmbedEmbedding"
 
     def __init__(
         self,
-        model_name: Optional[str] = "BAAI/bge-small-en-v1.5",
+        model_name: str = "BAAI/bge-small-en-v1.5",
         max_length: Optional[int] = 512,
         cache_dir: Optional[str] = None,
         threads: Optional[int] = None,
@@ -78,10 +77,6 @@ class FastEmbedEmbedding(BaseEmbedding):
     ):
         super().__init__(
             model_name=model_name,
-            max_length=max_length,
-            threads=threads,
-            doc_embed_type=doc_embed_type,
-            providers=providers,
             **kwargs,
         )
 
@@ -95,16 +90,25 @@ class FastEmbedEmbedding(BaseEmbedding):
         )
 
     def _get_text_embedding(self, text: str) -> List[float]:
+        return self._get_text_embeddings([text])[0]
+
+    async def _aget_text_embedding(self, text: str) -> List[float]:
+        return self._get_text_embedding(text)
+
+    def _get_text_embeddings(self, texts: List[str]) -> List[List[float]]:
         embeddings: List[np.ndarray]
         if self.doc_embed_type == "passage":
-            embeddings = list(self._model.passage_embed(text))
+            embeddings = list(self._model.passage_embed(texts))
         else:
-            embeddings = list(self._model.embed(text))
-        return embeddings[0].tolist()
+            embeddings = list(self._model.embed(texts))
+        return [embedding.tolist() for embedding in embeddings]
+
+    async def _aget_text_embeddings(self, texts: List[str]) -> List[List[float]]:
+        return self._get_text_embeddings(texts)
 
     def _get_query_embedding(self, query: str) -> List[float]:
-        query_embeddings: np.ndarray = next(self._model.query_embed(query))
-        return query_embeddings.tolist()
+        query_embeddings: list[np.ndarray] = list(self._model.query_embed(query))
+        return query_embeddings[0].tolist()
 
     async def _aget_query_embedding(self, query: str) -> List[float]:
         return self._get_query_embedding(query)
