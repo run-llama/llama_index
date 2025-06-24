a3769583d478c30ade0d7ee1792bddabcb650aa3
YIWEI-HE <YIWEI.HE@ibm.com>
2025-06-23 18:54:32 -0700

    Add testcases. Fix some problems.

--- llama-index-integrations/vector_stores/llama-index-vector-stores-db2/llama_index/vector_stores/db2/__init__.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-db2/llama_index/vector_stores/db2/__init__.py
@@ -1,3 +1,16 @@
-from llama_index.vector_stores.db2.base import DB2LlamaVS, DistanceStrategy
+from llama_index.vector_stores.db2.base import (
+    DB2LlamaVS,
+    _create_table,
+    _table_exists,
+    drop_table,
+    DB2LlamaVS,
+    DistanceStrategy,
+)
 
-__all__ = ["DB2LlamaVS", "DistanceStrategy"]
+__all__ = [
+    "DB2LlamaVS",
+    "_create_table",
+    "_table_exists",
+    "drop_table",
+    "DistanceStrategy",
+]
