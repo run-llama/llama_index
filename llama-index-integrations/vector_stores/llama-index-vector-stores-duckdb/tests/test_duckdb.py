75db7f416346b04026c08df79fddcfc2bf101ae8
William Easton <bill.easton@elastic.co>
2025-07-10 07:47:45 -0500

    DuckDB Async and Faster Cosine Similarity (#19383)
    * Small clean-up and add embeddings
    
    * bump version and lock dependencies
    
    * Add Async and switch to array_cosine_similarity
    
    * bump version
    
    * chore: make tests pass
    
    ---------
    
    Co-authored-by: Clelia (Astra) Bertelli <clelia@runllama.ai>

--- llama-index-integrations/vector_stores/llama-index-vector-stores-duckdb/tests/test_duckdb.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-duckdb/tests/test_duckdb.py
@@ -1,7 +1,8 @@
+import asyncio
 import pytest
 from typing import List
 import importlib.util
-
+import duckdb
 from llama_index.core.schema import NodeRelationship, RelatedNodeInfo, TextNode
 from llama_index.core.vector_stores.types import VectorStoreQuery, FilterOperator
 from llama_index.vector_stores.duckdb import DuckDBVectorStore
@@ -112,11 +113,27 @@ def vector_store() -> DuckDBVectorStore:
     return DuckDBVectorStore(embed_dim=3)
 
 
+@pytest.fixture(scope="module")
+def vector_store_async() -> DuckDBVectorStore:
+    return DuckDBVectorStore(embed_dim=3)
+
+
 def test_instance_creation_from_memory(
     vector_store: DuckDBVectorStore,
 ) -> None:
     assert isinstance(vector_store, DuckDBVectorStore)
     assert vector_store.database_name == ":memory:"
+
+
+def test_instance_creation_from_client(
+    text_node_list: List[TextNode],
+) -> None:
+    client = duckdb.connect(":memory:")
+    vector_store = DuckDBVectorStore(client=client)
+
+    vector_store.add(text_node_list)
+    nodes = vector_store.get_nodes(node_ids=["c330d77f-90bd-4c51-9ed2-57d8d693b3b0"])
+    assert len(nodes) == 1
 
 
 def test_duckdb_add_and_query(
@@ -506,3 +523,38 @@ def test_filter_missing_key(
     )
     nodes = vector_store.get_nodes(filters=filters)
     assert len(nodes) == 0
+
+
+async def test_async(
+    vector_store_async: DuckDBVectorStore, text_node_list: List[TextNode]
+):
+    await vector_store_async.async_add(text_node_list)
+    nodes = await vector_store_async.aget_nodes()
+    assert len(nodes) == len(text_node_list)
+
+    await vector_store_async.adelete_nodes(node_ids=[nodes[0].node_id])
+    nodes = await vector_store_async.aget_nodes()
+    assert len(nodes) == len(text_node_list) - 1
+
+    await vector_store_async.aclear()
+    assert len(vector_store_async.get_nodes()) == 0
+
+    def generate_large_node_list(num_nodes: int) -> List[TextNode]:
+        return [
+            TextNode(
+                text=f"Node {'i' * 10000}", id_=f"node_{i}", embedding=[0.5, 0.5, 0.5]
+            )
+            for i in range(num_nodes)
+        ]
+
+    large_node_list = generate_large_node_list(100)
+    tasks = [
+        vector_store_async.async_add(large_node_list),
+        vector_store_async.async_add(large_node_list),
+        vector_store_async.aget_nodes(),
+        vector_store_async.aget_nodes(),
+        vector_store_async.aclear(),
+        vector_store_async.aclear(),
+    ]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    assert not any(isinstance(result, Exception) for result in results)
