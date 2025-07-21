444344e17106bda07de972b4f5c547eacf7eb49f
Renyi Qu <mikustokes@gmail.com>
2025-07-21 16:35:08 +0000

    fixed unit test for jira_issue

--- llama-index-integrations/tools/llama-index-tools-jira-issue/tests/test_tools_jira_issue.py
+++ llama-index-integrations/tools/llama-index-tools-jira-issue/tests/test_tools_jira_issue.py
@@ -1,7 +1,7 @@
 from llama_index.core.tools.tool_spec.base import BaseToolSpec
-from llama_index.tools.jira import JiraToolSpec
+from llama_index.tools.jira_issue.base import JiraIssueToolSpec
 
 
 def test_class():
-    names_of_base_classes = [b.__name__ for b in JiraToolSpec.__mro__]
+    names_of_base_classes = [b.__name__ for b in JiraIssueToolSpec.__mro__]
     assert BaseToolSpec.__name__ in names_of_base_classes
