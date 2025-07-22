8223abf0247cecb27f25f91aa6c26328a2e5cd38
Florian Bacho <florian.bacho.ext@mediatransports.com>
2025-07-22 20:31:06 +0200

    Fix simple web reader test

--- llama-index-integrations/readers/llama-index-readers-web/tests/test_simple_webreader.py
+++ llama-index-integrations/readers/llama-index-readers-web/tests/test_simple_webreader.py
@@ -5,11 +5,11 @@ from llama_index.readers.web import SimpleWebPageReader
 
 @pytest.fixture()
 def url() -> str:
-    return "https://www.pringles.com"
+    return "https://docs.llamaindex.ai/en/stable/module_guides/workflow/"
 
 
 def test_simple_web_reader(url: str) -> None:
-    documents = SimpleWebPageReader(timeout=1).load_data(urls=[url])
+    documents = SimpleWebPageReader().load_data(urls=[url])
     assert len(documents) > 0
     assert isinstance(documents[0].id_, str)
     assert documents[0].id_ != url
