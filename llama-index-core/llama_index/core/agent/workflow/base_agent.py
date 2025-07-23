run-llama/llama_index

commit 30a8b4607ce914338242de6d72d63e15aac9c51d

fix: add system prompt to memory and use it also for structured generation (#19490)

llama-index-core/llama_index/core/agent/workflow/base_agent.py
@@ -323,12 +323,17 @@ class BaseWorkflowAgent(
                 ]
             )
             await ctx.store.set("user_msg_str", content_str)
-        elif chat_history:
+        elif chat_history and not all(
+            message.role == "system" for message in chat_history
+        ):
             # If no user message, use the last message from chat history as user_msg_str
+            user_hist: List[ChatMessage] = [
+                msg for msg in chat_history if msg.role == "user"
+            ]
             content_str = "\n".join(
                 [
                     block.text
-                    for block in chat_history[-1].blocks
+                    for block in user_hist[-1].blocks
                     if isinstance(block, TextBlock)
                 ]
             )
@@ -605,6 +610,7 @@ class BaseWorkflowAgent(
                 chat_history=chat_history,
                 memory=memory,
                 max_iterations=max_iterations,
+                system_prompt=self.system_prompt,
                 **kwargs,
             )
             return super().run(

llama-index-core/llama_index/core/agent/utils.py
@@ -3,7 +3,7 @@
 import json
 
 from llama_index.core.llms import ChatMessage, TextBlock
-from typing import List, Type, Dict, Any, cast
+from typing import List, Type, Dict, Any, Optional, cast
 from llama_index.core.bridge.pydantic import BaseModel
 from llama_index.core.agent.types import TaskStep
 from llama_index.core.base.llms.types import ChatMessage, MessageRole
@@ -21,9 +21,12 @@ def add_user_step_to_memory(
         print(f"Added user message to memory: {step.input}")
 
 
-def messages_to_xml_format(messages: List[ChatMessage]) -> ChatMessage:
+def messages_to_xml_format(messages: List[ChatMessage]) -> List[ChatMessage]:
     blocks = [TextBlock(text="<current_conversation>\n")]
+    system_msg: Optional[ChatMessage] = None
     for message in messages:
+        if message.role.value == "system":
+            system_msg = message
         blocks.append(TextBlock(text=f"\t<{message.role.value}>\n"))
         for block in message.blocks:
             if isinstance(block, TextBlock):
@@ -35,7 +38,10 @@ def messages_to_xml_format(messages: List[ChatMessage]) -> ChatMessage:
             text="Given the conversation, format the output according to the provided schema."
         )
     )
-    return ChatMessage(role="user", blocks=blocks)
+    messages = [ChatMessage(role="user", blocks=blocks)]
+    if system_msg:
+        messages.insert(0, system_msg)
+    return messages
 
 
 async def generate_structured_response(
@@ -44,5 +50,5 @@ async def generate_structured_response(
     xml_message = messages_to_xml_format(messages)
     structured_response = await llm.as_structured_llm(
         output_cls,
-    ).achat(messages=[xml_message])
+    ).achat(messages=xml_message)
     return cast(Dict[str, Any], json.loads(structured_response.message.content))

llama-index-core/llama_index/core/agent/workflow/multi_agent_workflow.py
@@ -345,12 +345,17 @@ class AgentWorkflow(Workflow, PromptMixin, metaclass=AgentWorkflowMeta):
                 ]
             )
             await ctx.store.set("user_msg_str", content_str)
-        elif chat_history:
+        elif chat_history and not all(
+            message.role == "system" for message in chat_history
+        ):
             # If no user message, use the last message from chat history as user_msg_str
+            user_hist: List[ChatMessage] = [
+                msg for msg in chat_history if msg.role == "user"
+            ]
             content_str = "\n".join(
                 [
                     block.text
-                    for block in chat_history[-1].blocks
+                    for block in user_hist[-1].blocks
                     if isinstance(block, TextBlock)
                 ]
             )

llama-index-core/llama_index/core/agent/workflow/workflow_events.py
@@ -10,7 +10,7 @@ from llama_index.core.bridge.pydantic import (
     BaseModel,
 )
 from llama_index.core.tools import ToolSelection, ToolOutput
-from llama_index.core.llms import ChatMessage
+from llama_index.core.llms import ChatMessage, MessageRole
 from llama_index.core.workflow import Event, StartEvent
 
 
@@ -113,6 +113,15 @@ class ToolCallResult(Event):
 class AgentWorkflowStartEvent(StartEvent):
     def __init__(self, **data: Any) -> None:
         """Convert chat_history items to ChatMessage objects if they aren't already"""
+        if "system_prompt" in data and data["system_prompt"]:
+            if "chat_history" in data and data["chat_history"]:
+                data["chat_history"].append(
+                    ChatMessage(role=MessageRole.SYSTEM, content=data["system_prompt"])
+                )
+            else:
+                data["chat_history"] = [
+                    ChatMessage(role=MessageRole.SYSTEM, content=data["system_prompt"])
+                ]
         if "chat_history" in data and data["chat_history"]:
             converted_history = []
             for i, msg in enumerate(data["chat_history"]):
@@ -128,6 +137,19 @@ class AgentWorkflowStartEvent(StartEvent):
                             f"Invalid message: {msg}"
                         )
                         raise
+            if (
+                not all(message.role == "system" for message in converted_history)
+                and converted_history[-1].role != "user"
+            ):
+                # Put the last user message in the last position
+                user_hist_enum = [
+                    index
+                    for index, message in enumerate(converted_history)
+                    if message.role == "user"
+                ]
+                last_user_index = user_hist_enum[-1]
+                last_user_message = converted_history.pop(last_user_index)
+                converted_history.append(last_user_message)
             data["chat_history"] = converted_history
 
         super().__init__(**data)

llama-index-core/tests/agent/utils/test_agent_utils.py
@@ -132,9 +132,22 @@ def chat_messages() -> List[ChatMessage]:
     ]
 
 
+@pytest.fixture()
+def chat_messages_sys(chat_messages: List[ChatMessage]) -> List[ChatMessage]:
+    return [
+        ChatMessage(role="system", content="You are a helpful assistant."),
+        *chat_messages,
+    ]
+
+
 @pytest.fixture
 def xml_string() -> str:
     return "<current_conversation>\n\t<user>\n\t\t<message>hello</message>\n\t</user>\n\t<assistant>\n\t\t<message>hello back</message>\n\t</assistant>\n\t<user>\n\t\t<message>how are you?</message>\n\t</user>\n\t<assistant>\n\t\t<message>I am good, thank you.</message>\n\t</assistant>\n</current_conversation>\n\nGiven the conversation, format the output according to the provided schema."
+
+
+@pytest.fixture
+def xml_string_sys() -> str:
+    return "<current_conversation>\n\t<system>\n\t\t<message>You are a helpful assistant.</message>\n\t</system>\n\t<user>\n\t\t<message>hello</message>\n\t</user>\n\t<assistant>\n\t\t<message>hello back</message>\n\t</assistant>\n\t<user>\n\t\t<message>how are you?</message>\n\t</user>\n\t<assistant>\n\t\t<message>I am good, thank you.</message>\n\t</assistant>\n</current_conversation>\n\nGiven the conversation, format the output according to the provided schema."
 
 
 @pytest.fixture
@@ -144,11 +157,26 @@ def structured_response() -> str:
 
 def test_messages_to_xml(chat_messages: List[ChatMessage], xml_string: str) -> None:
     msg = messages_to_xml_format(chat_messages)
-    assert isinstance(msg, ChatMessage)
+    assert len(msg) == 1
+    assert isinstance(msg[0], ChatMessage)
     s = ""
-    for block in msg.blocks:
+    for block in msg[0].blocks:
         s += block.text
     assert s == xml_string
+
+
+def test_messages_to_xml_sys(
+    chat_messages_sys: List[ChatMessage], xml_string_sys: str
+) -> None:
+    msg = messages_to_xml_format(chat_messages_sys)
+    assert len(msg) == 2
+    assert isinstance(msg[0], ChatMessage)
+    assert msg[0].role == "system"
+    assert msg[0].content == "You are a helpful assistant."
+    s = ""
+    for block in msg[1].blocks:
+        s += block.text
+    assert s == xml_string_sys
 
 
 @pytest.mark.asyncio

commit 99169825933b8765d6c7baed3a118f200256b51f

fix(setup_agent): Don't duplicate `system_prompt` message.
This fixes a bug introduced in https://github.com/run-llama/llama_index/pull/19490 .

In that PR, `AgentWorkflowStartEvent` was already including a ChatMessage for the system_prompt before reaching `setup_agent`, which was then inserting a duplicate.

llama-index-core/llama_index/core/agent/workflow/base_agent.py
@@ -352,12 +352,6 @@ class BaseWorkflowAgent(
         """Main agent handling logic."""
         llm_input = [*ev.input]
 
-        if self.system_prompt:
-            llm_input = [
-                ChatMessage(role="system", content=self.system_prompt),
-                *llm_input,
-            ]
-
         state = await ctx.store.get("state", default=None)
         formatted_input_with_state = await ctx.store.get(
             "formatted_input_with_state", default=False

llama-index-core/tests/agent/workflow/test_single_agent_workflow.py
@@ -153,8 +153,11 @@ def retry_calculator_agent():
 async def test_single_function_agent(function_agent):
     """Test single agent with state management."""
     handler = function_agent.run(user_msg="test")
-    async for _ in handler.stream_events():
-        pass
+    async for event in handler.stream_events():
+        if isinstance(event, AgentInput):
+            assert (
+                sum(message.role == MessageRole.SYSTEM for message in event.input) == 1
+            )
 
     response = await handler
     assert "Success with the FunctionAgent" in str(response.response)