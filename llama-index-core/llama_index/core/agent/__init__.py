90d167e920a41226efaec45de3d50a32efa6bc43
Logan Markewich <logan.markewich@live.com>
2025-07-23 12:40:44 -0600

    removing function calling agent

--- llama-index-core/llama_index/core/agent/__init__.py
+++ llama-index-core/llama_index/core/agent/__init__.py
@@ -1,5 +1,4 @@
 # agent runner + agent worker
-from llama_index.core.agent.custom.pipeline_worker import QueryPipelineAgentWorker
 from llama_index.core.agent.custom.simple import CustomSimpleAgentWorker
 from llama_index.core.agent.custom.simple_function import FnAgentWorker
 from llama_index.core.agent.react.base import ReActAgent
@@ -12,8 +11,6 @@ from llama_index.core.agent.runner.planner import StructuredPlannerAgent
 from llama_index.core.agent.runner.parallel import ParallelAgentRunner
 from llama_index.core.agent.types import Task
 from llama_index.core.chat_engine.types import AgentChatResponse
-from llama_index.core.agent.function_calling.base import FunctionCallingAgent
-from llama_index.core.agent.function_calling.step import FunctionCallingAgentWorker
 
 __all__ = [
     "AgentRunner",
@@ -23,11 +20,8 @@ __all__ = [
     "ReActAgent",
     "ReActOutputParser",
     "CustomSimpleAgentWorker",
-    "QueryPipelineAgentWorker",
     "ReActChatFormatter",
-    "FunctionCallingAgentWorker",
     "FnAgentWorker",
-    "FunctionCallingAgent",
     # beta
     "MultimodalReActAgentWorker",
     # schema-related
