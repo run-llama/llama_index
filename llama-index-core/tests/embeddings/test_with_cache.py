a4cdaa267388ac83bb68f224a44bd1d4c5ac8ce7
Eric Shtivelberg <eric.shtiv@gmail.com>
2025-07-26 01:04:14 +0300

    improve cache tests

--- llama-index-core/tests/embeddings/test_with_cache.py
+++ llama-index-core/tests/embeddings/test_with_cache.py
@@ -1,52 +1,97 @@
 import pytest
 from llama_index.core import MockEmbedding
 from llama_index.core.storage.kvstore import SimpleKVStore
-from typing import List, Any
+from unittest.mock import patch
+
+expected_embedding = [0.5, 0.5, 0.5, 0.5]
+expected_embeddings = [expected_embedding]
 
 
-@pytest.fixture()
-def expected_embeddings() -> List[Any]:
-    return [[0.5, 0.5, 0.5, 0.5], [[0.5, 0.5, 0.5, 0.5]]]
-
-
-def test_sync_methods_with_cache(expected_embeddings: List[Any]):
-    kwargs_dict = {"embeddings_cache": SimpleKVStore()}
-    embed_model = MockEmbedding(embed_dim=4, **kwargs_dict)
+def test_sync_get_with_cache():
+    embeddings_cache = SimpleKVStore()
+    embed_model = MockEmbedding(embed_dim=4, embeddings_cache=embeddings_cache)
     text = "Hello"
-    texts = ["Hello"]
+
     text_embedding = embed_model.get_text_embedding(text)
-    assert text_embedding == expected_embeddings[0]
-    assert (
-        embed_model.embeddings_cache.get(key="Hello", collection="embeddings")
-        is not None
-    )
-    embd_dict = embed_model.embeddings_cache.get(key="Hello", collection="embeddings")
+    assert text_embedding == expected_embedding
+    assert embeddings_cache.get(key="Hello", collection="embeddings") is not None
+
+    embd_dict = embeddings_cache.get(key="Hello", collection="embeddings")
     first_key = next(iter(embd_dict.keys()))
-    assert embd_dict[first_key] == expected_embeddings[0]
-    text_embeddings = embed_model.get_text_embedding_batch(texts)
-    assert text_embeddings == expected_embeddings[1]
-    embd_dict = embed_model.embeddings_cache.get(key="Hello", collection="embeddings")
-    first_key = next(iter(embd_dict.keys()))
-    assert embd_dict[first_key] == expected_embeddings[0]
+    assert embd_dict[first_key] == expected_embedding
+
+
+def test_sync_get_batch_with_cache():
+    """Test mixed scenario with some cached and some new inputs."""
+    embeddings_cache = SimpleKVStore()
+    embed_model = MockEmbedding(embed_dim=4, embeddings_cache=embeddings_cache)
+    texts = ["Cached1", "Miss1", "Cached2", "Miss2"]
+
+    # Pre-cache
+    embed_model.get_text_embedding("Cached1")
+    embed_model.get_text_embedding("Cached2")
+
+    with patch.object(
+        embed_model,
+        "_get_text_embeddings",
+        wraps=embed_model._get_text_embeddings,
+    ) as mock_get_embeddings:
+        text_embeddings = embed_model.get_text_embedding_batch(texts)
+        assert text_embeddings == expected_embeddings * 4, (
+            f"{text_embeddings} != {expected_embeddings * 4}"
+        )
+
+        assert mock_get_embeddings.call_count == 1
+
+        # Check cache
+        for text in texts:
+            embd_dict = embeddings_cache.get(key=text, collection="embeddings")
+
+            first_key = next(iter(embd_dict.keys()))
+            assert embd_dict[first_key] == expected_embedding
 
 
 @pytest.mark.asyncio
-async def test_async_methods_with_cache(expected_embeddings: List[Any]):
-    kwargs_dict = {"embeddings_cache": SimpleKVStore()}
-    embed_model = MockEmbedding(embed_dim=4, **kwargs_dict)
+async def test_async_get_with_cache():
+    embeddings_cache = SimpleKVStore()
+    embed_model = MockEmbedding(embed_dim=4, embeddings_cache=embeddings_cache)
     text = "Hello"
-    texts = ["Hello"]
+
     text_embedding = await embed_model.aget_text_embedding(text)
-    assert text_embedding == expected_embeddings[0]
-    assert (
-        embed_model.embeddings_cache.get(key="Hello", collection="embeddings")
-        is not None
-    )
-    embd_dict = embed_model.embeddings_cache.get(key="Hello", collection="embeddings")
+    assert text_embedding == expected_embedding
+    assert embeddings_cache.get(key="Hello", collection="embeddings") is not None
+
+    embd_dict = embeddings_cache.get(key="Hello", collection="embeddings")
     first_key = next(iter(embd_dict.keys()))
-    assert embd_dict[first_key] == expected_embeddings[0]
-    text_embeddings = await embed_model.aget_text_embedding_batch(texts)
-    assert text_embeddings == expected_embeddings[1]
-    embd_dict = embed_model.embeddings_cache.get(key="Hello", collection="embeddings")
-    first_key = next(iter(embd_dict.keys()))
-    assert embd_dict[first_key] == expected_embeddings[0]
+    assert embd_dict[first_key] == expected_embedding
+
+
+@pytest.mark.asyncio
+async def test_async_get_batch_with_cache():
+    """Test mixed scenario with some cached and some new inputs."""
+    embeddings_cache = SimpleKVStore()
+    embed_model = MockEmbedding(embed_dim=4, embeddings_cache=embeddings_cache)
+    texts = ["Cached1", "Miss1", "Cached2", "Miss2"]
+
+    # Pre-cache
+    await embed_model.aget_text_embedding("Cached1")
+    await embed_model.aget_text_embedding("Cached2")
+
+    with patch.object(
+        embed_model,
+        "_aget_text_embeddings",
+        wraps=embed_model._get_text_embeddings,
+    ) as mock_get_embeddings:
+        text_embeddings = await embed_model.aget_text_embedding_batch(texts)
+        assert text_embeddings == expected_embeddings * 4, (
+            f"{text_embeddings} != {expected_embeddings * 4}"
+        )
+
+        assert mock_get_embeddings.call_count == 2
+
+        # Check cache
+        for text in texts:
+            embd_dict = embeddings_cache.get(key=text, collection="embeddings")
+
+            first_key = next(iter(embd_dict.keys()))
+            assert embd_dict[first_key] == expected_embedding
