6b5c856c5c227d6bc6be82bb97a4199f421f05fc
shibolin <shibolin@trip.com>
2025-08-18 14:53:50 +0800

    change comment to make it more clear

--- llama-index-core/llama_index/core/chat_engine/types.py
+++ llama-index-core/llama_index/core/chat_engine/types.py
@@ -325,7 +325,7 @@ class StreamingAgentChatResponse:
                                 self.aqueue.get(), timeout=0.1
                             )
                         except asyncio.TimeoutError:
-                            # In case the stream is done but the queue is not empty
+                            #Break only when the stream is done and the queue is empty
                             if self.is_done and self.aqueue.empty():
                                 break
                             continue
