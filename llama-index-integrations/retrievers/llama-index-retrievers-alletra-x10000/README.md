f5c682d3b6bf5960f107b68a28a4296cc237c838
singhmad <madhur.singh@hpe.com>
2025-09-05 05:48:42 +0000

    llama-index-retriever integration for hpe-di solution

---
+++ llama-index-integrations/retrievers/llama-index-retrievers-alletra-x10000/README.md
@@ -0,0 +1,72 @@
+# DataIntelligence-llamaindex-sdk
+
+### Data Intelligence SDK will provide interfaces for the user applications to do semantic similarity search on the embeddings. 
+
+## How to use SDK
+
+### Install llama-index
+
+```python
+pip install llama-index
+```
+
+### Install SDK
+
+```shell
+cd ./llama-index-retriever-data-intelligence
+```
+
+```python
+pip install .
+
+```
+
+### Data Intelligence solution supports inline embedding generation
+``` python
+# Add steps to enable inline vector embedding generation
+# Create pipeline
+# Create collection
+# Add bucket to collection
+```
+
+
+### Driver code
+- Feel free to change embedding and llm models.
+
+```python
+from llama_index.retrievers.data_intelligence import DataIntelligenceRetriever
+from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core.chat_engine import CondensePlusContextChatEngine
+from llama_index.core import Settings
+# TODO import python data intelligence client library
+
+
+# Settings.llm = None
+Settings.llm = HuggingFaceLLM(
+    model_name="HuggingFaceH4/zephyr-7b-beta",
+    tokenizer_name="HuggingFaceH4/zephyr-7b-beta",
+    context_window=3900,
+    max_new_tokens=25,
+    generate_kwargs={"temperature": 0.7, "top_k": 50, "top_p": 0.95},
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    device_map="auto",
+)
+
+retriever = DataIntelligenceRetriever(uri="https://example.com",
+                                      s3_access_key="",
+                                      s3_secret_key="",
+                                      collection_name="testcollection", 
+                                      top_k=50,
+                                      search_config={
+                                            "metric": "cosine",
+                                            "ef_search": 100
+                                        }
+                                      )
+memory = ChatMemoryBuffer.from_defaults(token_limit=1000)
+chatbot = CondensePlusContextChatEngine.from_defaults(retriever=retriever, memory=memory)
+
+res = chatbot.chat("Who was the eldest daughter of the steward of the old Lord de Versely?")
+print(res)
+```
---
+++ llama-index-integrations/retrievers/llama-index-retrievers-alletra-x10000/tests/test_base.py
@@ -0,0 +1,119 @@
+import pytest
+from unittest.mock import MagicMock
+from llama_index.core.schema import QueryBundle, NodeWithScore, TextNode
+from llama_index.retrievers.data_intelligence.base import DataIntelligenceRetriever
+
+
+def test_data_intelligence_retriever_initialization():
+    retriever = DataIntelligenceRetriever(
+        uri="http://example.com",
+        s3_access_key="test_access_key",
+        s3_secret_key="test_secret_key",
+        collection_name="test_collection",
+        search_config={"param1": "value1"},
+        top_k=5,
+    )
+    assert retriever.uri == "http://example.com"
+    assert retriever.access_key == "test_access_key"
+    assert retriever.secret_key == "test_secret_key"
+    assert retriever.collection_name == "test_collection"
+    assert retriever.search_config == {"param1": "value1"}
+    assert retriever.top_k == 5
+
+
+def test_data_intelligence_retriever_retrieve(mocker):
+    mock_client = mocker.patch(
+        "llama_index.retrievers.data_intelligence.base.DIClient")
+
+    mock_response = MagicMock()
+    mock_response = [
+        {
+            "dataChunk": "chunk1",
+            "score": 0.9,
+            "chunkMetadata": {
+                "objectKey": "value",
+                "startCharIndex": 1,
+                "endCharIndex": 2,
+                "bucketName": "string",
+                "pageLabel": "string",
+                "versionId": "string",
+            }
+        },
+        {
+            "dataChunk": "chunk2",
+            "score": 0.8,
+            "chunkMetadata": {
+                "objectKey": "value",
+                "startCharIndex": 1,
+                "endCharIndex": 2,
+                "bucketName": "string",
+                "pageLabel": "string",
+                "versionId": "string",
+            }
+        },
+    ]
+    mock_client.return_value.similarity_search.return_value = mock_response
+
+    retriever = DataIntelligenceRetriever(
+        uri="http://example.com",
+        s3_access_key="test_access_key",
+        s3_secret_key="test_secret_key",
+        collection_name="test_collection",
+        search_config={"param1": "value1"},
+        top_k=2,
+    )
+    query_bundle = QueryBundle(query_str="test query")
+    result = retriever._retrieve(query_bundle)
+
+    assert len(result) == 2
+    assert isinstance(result[0], NodeWithScore)
+    assert result[0].node.text == "chunk1"
+    assert result[0].score == 0.9
+    assert result[0].node.metadata["objectKey"] == "value"
+    assert result[1].node.text == "chunk2"
+    assert result[1].score == 0.8
+    assert result[1].node.metadata["objectKey"] == "value"
+
+
+def test_data_intelligence_retriever_retrieve_empty_response(mocker):
+
+    retriever = DataIntelligenceRetriever(
+        uri="http://example.com",
+        s3_access_key="test_access_key",
+        s3_secret_key="test_secret_key",
+        collection_name="test_collection",
+        search_config={"param1": "value1"},
+        top_k=2,
+    )
+
+    # Mock the DIClient to return an empty response
+    mock_client = mocker.patch(
+        "llama_index.retrievers.data_intelligence.base.DIClient")
+    mock_client.return_value.similarity_search.return_value.json.return_value = []
+
+    # Call the method
+    query_bundle = QueryBundle(query_str="test query")
+    result = retriever._retrieve(query_bundle)
+
+    # Assertions
+    assert result == []
+
+
+def test_data_intelligence_retriever_retrieve_error_handling(mocker):
+    mock_client = mocker.patch(
+        "llama_index.retrievers.data_intelligence.base.DIClient")
+    mock_client.return_value.similarity_search.side_effect = Exception(
+        "Test exception")
+
+    retriever = DataIntelligenceRetriever(
+        uri="http://example.com",
+        s3_access_key="test_access_key",
+        s3_secret_key="test_secret_key",
+        collection_name="test_collection",
+        search_config={"param1": "value1"},
+        top_k=2,
+    )
+    query_bundle = QueryBundle(query_str="test query")
+
+    with pytest.raises(Exception, match="Test exception"):
+        retriever._retrieve(query_bundle)
