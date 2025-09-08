7006e93aabe5fca2841e99ad0cfb2db2e496c434
fcaccialanza <federico.caccialanza@it.abb.com>
2025-09-08 09:44:19 +0200

    fix: missing None in pop

--- llama-index-integrations/vector_stores/llama-index-vector-stores-azureaisearch/llama_index/vector_stores/azureaisearch/base.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-azureaisearch/llama_index/vector_stores/azureaisearch/base.py
@@ -1211,7 +1211,9 @@ class AzureAISearchVectorStore(BasePydanticVectorStore):
         semantic_configuration_name = None
 
         # NOTE: users can provide odata_filters directly to the query and any other search parameters like scoring_profile etc .
-        odata_filters = kwargs.pop("odata_filters") or kwargs.pop("odata_filter")
+        odata_filters = kwargs.pop("odata_filters", None) or kwargs.pop(
+            "odata_filter", None
+        )
         if odata_filters is not None:
             odata_filter = odata_filters
         elif query.filters is not None:
@@ -1267,7 +1269,9 @@ class AzureAISearchVectorStore(BasePydanticVectorStore):
         odata_filter = None
 
         # NOTE: users can provide odata_filters directly to the query
-        odata_filters = kwargs.pop("odata_filters") or kwargs.pop("odata_filter")
+        odata_filters = kwargs.pop("odata_filters", None) or kwargs.pop(
+            "odata_filter", None
+        )
         if odata_filters is not None:
             odata_filter = odata_filters
         else:
