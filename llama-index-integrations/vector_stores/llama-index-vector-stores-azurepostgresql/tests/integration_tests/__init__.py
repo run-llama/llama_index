d1ff9ffb853f58a7fca5acf5af31a68eb02b36ff
Logan Markewich <logan.markewich@live.com>
2025-09-22 14:24:20 -0600

    remove unused tests

--- llama-index-integrations/vector_stores/llama-index-vector-stores-azurepostgresql/tests/integration_tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# TODO(arda): Remove this group once we have a stable/improved GitHub CI pipeline.
-"""Placeholder for integration tests (necessary to pass the CI for now)."""
--- llama-index-integrations/vector_stores/llama-index-vector-stores-azurepostgresql/tests/integration_tests/test_placeholder.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.mark.compile
-def test_placeholder():
-    """Placeholder test to ensure the integration tests module is recognized."""
-    assert True
