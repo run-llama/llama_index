218ffc8825f1e33cd77d56a84dc6fb1ccce413d7
Clelia (Astra) Bertelli <clelia@runllama.ai>
2025-09-29 16:55:59 +0200

    chore: make type checker happy again

--- llama-index-core/llama_index/core/base/llms/types.py
+++ llama-index-core/llama_index/core/base/llms/types.py
@@ -450,7 +450,8 @@ class ToolCallBlock(BaseModel):
     )
     tool_name: str = Field(description="Name of the called tool")
     tool_kwargs: dict[str, Any] | object | str = Field(
-        default_factory=dict, description="Arguments provided to the tool, if available"
+        default_factory=dict,
+        description="Arguments provided to the tool, if available",  # type: ignore
     )
 
 
