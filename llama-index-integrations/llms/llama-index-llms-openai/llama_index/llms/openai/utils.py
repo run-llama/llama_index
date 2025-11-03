f7121a2c4ddfeb756dbb2534b78947f66b06409e
Sushrut Barmate <thelastlife0001@gmail.com>
2025-11-03 20:49:23 +0530

    FixV2: Correct DocumentBlock type for OpenAI from 'input_file' to 'file' (#20203)

--- llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
+++ llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
@@ -361,7 +361,7 @@ def to_openai_message_dict(
                 mimetype = block._guess_mimetype()
             content.append(
                 {
-                    "type": "input_file",
+                    "type": "file",
                     "filename": block.title,
                     "file_data": f"data:{mimetype};base64,{b64_string}",
                 }
