532885c241cd72428f15732128bba24f7e056324
Meo <miaobuao@outlook.com>
2025-12-13 23:14:04 +0800

    fix(vector_stores/lancedb): add missing '<' filter operator

--- llama-index-integrations/vector_stores/llama-index-vector-stores-lancedb/llama_index/vector_stores/lancedb/util.py
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-lancedb/llama_index/vector_stores/lancedb/util.py
@@ -6,6 +6,7 @@ sql_operator_mapper = {
     FilterOperator.EQ: " = ",
     FilterOperator.GT: " > ",
     FilterOperator.GTE: " >= ",
+    FilterOperator.LT: " < ",
     FilterOperator.LTE: " <= ",
     FilterOperator.TEXT_MATCH: " LIKE ",
     FilterOperator.NE: " NOT LIKE ",
