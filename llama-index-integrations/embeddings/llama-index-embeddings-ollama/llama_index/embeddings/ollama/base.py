f063f41bc9184056d2ec0a3126fd4a3b05333e87
Lucie Vomelova <lucka.vomelova@seznam.cz>
2026-01-07 15:00:51 +0100

    bump package version, change back param name

--- llama-index-integrations/embeddings/llama-index-embeddings-ollama/llama_index/embeddings/ollama/base.py
+++ llama-index-integrations/embeddings/llama-index-embeddings-ollama/llama_index/embeddings/ollama/base.py
@@ -129,11 +129,11 @@ class OllamaEmbedding(BaseEmbedding):
         )
         return result.embeddings[0]
 
-    async def aget_general_text_embedding(self, texts: str) -> List[float]:
+    async def aget_general_text_embedding(self, prompt: str) -> List[float]:
         """Asynchronously get Ollama embedding."""
         result = await self._async_client.embed(
             model=self.model_name,
-            input=texts,
+            input=prompt,
             options=self.ollama_additional_kwargs,
             keep_alive=self.keep_alive,
         )
