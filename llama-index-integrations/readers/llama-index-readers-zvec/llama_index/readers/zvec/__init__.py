3465082eee3224c4b06b547f8af9ed8588fcb1fc
lc285652 <lc285652@alibaba-inc.com>
2026-01-29 16:01:24 +0800

    init code

---
+++ llama-index-integrations/readers/llama-index-readers-zvec/llama_index/readers/zvec/__init__.py
@@ -0,0 +1,3 @@
+from llama_index.readers.zvec.base import ZvecReader
+
+__all__ = ["ZvecReader"]
---
+++ llama-index-integrations/vector_stores/llama-index-vector-stores-zvec/llama_index/vector_stores/zvec/__init__.py
@@ -0,0 +1,3 @@
+from llama_index.vector_stores.zvec.base import ZvecVectorStore
+
+__all__ = ["ZvecVectorStore"]
