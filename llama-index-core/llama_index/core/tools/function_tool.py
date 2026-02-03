6f91c75e347bbf4455793f1bb9bed3b7a20f9f29
Logan Markewich <logan.markewich@live.com>
2026-02-03 17:59:19 -0600

    fix 3.14+ tool indent

--- llama-index-core/llama_index/core/tools/function_tool.py
+++ llama-index-core/llama_index/core/tools/function_tool.py
@@ -14,6 +14,8 @@ from typing import (
     get_origin,
 )
 import re
+import os
+from textwrap import indent
 
 if TYPE_CHECKING:
     from llama_index.core.bridge.langchain import StructuredTool, Tool
@@ -176,6 +178,11 @@ class FunctionTool(AsyncBaseTool):
             assert fn_to_parse is not None, "fn must be provided"
             name = name or fn_to_parse.__name__
             docstring = fn_to_parse.__doc__ or ""
+
+            # in python 3.14+, __doc__ returns a cleaned docstring
+            # we want the indent though
+            if os.version_info >= (3, 14):
+                docstring = indent(docstring, " " * 4).strip()
 
             # Get function signature
             fn_sig = inspect.signature(fn_to_parse)
