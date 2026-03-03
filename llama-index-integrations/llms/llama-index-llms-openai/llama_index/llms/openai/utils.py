af57d860315eab4790445e3cf6bab7eb49fbc3c7
Martina Lupini <97310491+martinalupini@users.noreply.github.com>
2026-03-03 11:27:08 +0100

    fix-openai-toolcall-after-thinking #20333 (#20725)
    * fix #20333
    
    * fix #20333
    
    * fix #20333 when store=False
    
    * fix #20333 when store=False
    
    * fix #20333 when store=False
    
    * fix #20333, updated version to 0.6.24

--- llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
+++ llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
@@ -352,6 +352,7 @@ def to_openai_message_dict(
     message: ChatMessage,
     drop_none: bool = False,
     model: Optional[str] = None,
+    store: bool = False,
 ) -> ChatCompletionMessageParam:
     """Convert a ChatMessage to an OpenAI message dict."""
     content = []
@@ -533,6 +534,7 @@ def to_openai_responses_message_dict(
     message: ChatMessage,
     drop_none: bool = False,
     model: Optional[str] = None,
+    store: bool = False,
 ) -> Union[str, Dict[str, Any], List[Dict[str, Any]]]:
     """Convert a ChatMessage to an OpenAI message dict."""
     content = []
@@ -581,17 +583,20 @@ def to_openai_responses_message_dict(
                         "detail": block.detail or "auto",
                     }
                 )
+
+        # Omit reasoning items when store is set to False
         elif isinstance(block, ThinkingBlock):
-            if block.content and "id" in block.additional_information:
-                reasoning.append(
-                    {
-                        "type": "reasoning",
-                        "id": block.additional_information["id"],
-                        "summary": [
-                            {"type": "summary_text", "text": block.content or ""}
-                        ],
-                    }
-                )
+            if store:
+                if block.content and "id" in block.additional_information:
+                    reasoning.append(
+                        {
+                            "type": "reasoning",
+                            "id": block.additional_information["id"],
+                            "summary": [
+                                {"type": "summary_text", "text": block.content or ""}
+                            ],
+                        }
+                    )
         elif isinstance(block, ToolCallBlock):
             tool_calls.extend(
                 [
@@ -698,6 +703,7 @@ def to_openai_message_dicts(
     drop_none: bool = False,
     model: Optional[str] = None,
     is_responses_api: bool = False,
+    store: bool = False,
 ) -> Union[List[ChatCompletionMessageParam], str]:
     """Convert generic messages to OpenAI message dicts."""
     if is_responses_api:
@@ -707,6 +713,7 @@ def to_openai_message_dicts(
                 message,
                 drop_none=drop_none,
                 model="o3-mini",  # hardcode to ensure developer messages are used
+                store=store,
             )
             if isinstance(message_dicts, list):
                 final_message_dicts.extend(message_dicts)
@@ -730,6 +737,7 @@ def to_openai_message_dicts(
                 message,
                 drop_none=drop_none,
                 model=model,
+                store=store,
             )
             for message in messages
         ]
