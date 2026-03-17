3f4636d81e71e87df09300a66f79a50553398d9a
Clelia (Astra) Bertelli <133636879+AstraBert@users.noreply.github.com>
2026-03-17 15:37:41 +0100

    fix: llama-cloud managed index and remove llamaparse reader (#21043)

--- llama-index-integrations/indices/llama-index-indices-managed-llama-cloud/llama_index/indices/managed/llama_cloud/__init__.py
+++ llama-index-integrations/indices/llama-index-indices-managed-llama-cloud/llama_index/indices/managed/llama_cloud/__init__.py
@@ -1,8 +1,8 @@
-from llama_index.indices.managed.llama_cloud.base import LlamaCloudIndex
-from llama_index.indices.managed.llama_cloud.retriever import LlamaCloudRetriever
-from llama_index.indices.managed.llama_cloud.composite_retriever import (
+from llama_cloud.lib.index.base import LlamaCloudIndex
+from llama_cloud.lib.index.composite_retriever import (
     LlamaCloudCompositeRetriever,
 )
+from llama_cloud.lib.index.retriever import LlamaCloudRetriever
 
 __all__ = [
     "LlamaCloudIndex",
