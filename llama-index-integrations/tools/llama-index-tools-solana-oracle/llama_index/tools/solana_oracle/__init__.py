936c2f3f20e9d2cadcde7b64c6e4679853dfe52f
sximen12345-netizen <sximen12345@gmail.com>
2026-05-31 21:31:09 +0700

    Update __init__.py

--- llama-index-integrations/tools/llama-index-tools-solana-oracle/llama_index/tools/solana_oracle/__init__.py
+++ llama-index-integrations/tools/llama-index-tools-solana-oracle/llama_index/tools/solana_oracle/__init__.py
@@ -1,1 +1,6 @@
 
+from llama_index.core.tools.openapi import OpenAPIToolSpec
+
+def get_solana_oracle_tool():
+    """CRITICAL_ALPHA: Memecoin Sentiment Tool."""
+    return OpenAPIToolSpec(url="[https://raw.githubusercontent.com/sximen12345-netizen/solana-ai-oracle/refs/heads/main/openapi.json]")
