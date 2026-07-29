187f10cc79336e00a2dbd111c2e06f61caf69273
SjeYinTeoIntel <sje.yin.teo@intel.com>
2026-07-29 23:33:34 +0800

    Add Intel XPU support to image readers and infer_torch_device
    Signed-off-by: SjeYinTeoIntel <sje.yin.teo@intel.com>

--- llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/image_caption/base.py
+++ llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/image_caption/base.py
@@ -36,7 +36,7 @@ class ImageCaptionReader(BaseReader):
                 )
 
             device = infer_torch_device()
-            dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+            dtype = torch.float16 if device != "cpu" else torch.float32
 
             processor = BlipProcessor.from_pretrained(
                 "Salesforce/blip-image-captioning-large"
