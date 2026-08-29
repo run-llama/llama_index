98c52d8ac8f8a89bc2d79cf1d71054b0424a9d27
Logan <logan.markewich@live.com>
2026-08-29 09:32:32 -0600

    fix: resolve a ton of security alerts (#22855)

--- docs/api_reference/api_reference/llama_deploy/README.txt
+++
@@ -1,2 +0,0 @@
-Documentation content will be pulled from https://github.com/run-llama/llama_deploy,
-make sure to run `python docs/merge_llama_deploy_docs.py` before building docs.
--- llama-index-integrations/llms/llama-index-llms-ai21/llama_index/llms/ai21/utils.py
+++ llama-index-integrations/llms/llama-index-llms-ai21/llama_index/llms/ai21/utils.py
@@ -15,6 +15,10 @@ JAMBA_MODELS = {
     "jamba-1.5-mini": 256_000,
     "jamba-1.5-large": 256_000,
     "jamba-1.5": 256_000,
+    # Legacy Jurassic-2 models
+    "j2-light": 8_192,
+    "j2-mid": 8_192,
+    "j2-ultra": 8_192,
 }
 
 _SYSTEM_ERR_MESSAGE = "System message must be at beginning of message list."
